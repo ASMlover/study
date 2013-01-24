42c2d8548704c205c16558f603e582ffe3cd3346
ASMlover <asmlover@126.com>
2013-01-24 17:24:21 +0800

    finished implementation of binary search tree.

--- data-structure/btree/btree-search/btree.c
+++ data-structure/btree/btree-search/btree.c
@@ -1,3 +1,31 @@
+/*
+ * Copyright (c) 2013 ASMlover. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list ofconditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materialsprovided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include "btree.h"
@@ -117,6 +145,26 @@ preorder(btree_node_t node, void (*visit)(int, element_t))
     visit(node->key, node->val);
     preorder(node->lchild, visit);
     preorder(node->rchild, visit);
+  }
+}
+
+static void 
+inorder(btree_node_t node, void (*visit)(int, element_t))
+{
+  if (NULL != node) {
+    inorder(node->lchild, visit);
+    visit(node->key, node->val);
+    inorder(node->rchild, visit);
+  }
+}
+
+static void 
+postorder(btree_node_t node, void (*visit)(int, element_t)) 
+{
+  if (NULL != node) {
+    postorder(node->lchild, visit);
+    postorder(node->rchild, visit);
+    visit(node->key, node->val);
   }
 }
 
@@ -259,4 +307,20 @@ btree_preorder(btree_t bt, void (*visit)(int, element_t))
   ASSERT_BT_ARGUMENTS(bt, visit);
 
   preorder(bt->root, visit);
+}
+
+void 
+btree_inorder(btree_t bt, void (*visit)(int, element_t)) 
+{
+  ASSERT_BT_ARGUMENTS(bt, visit);
+
+  inorder(bt->root, visit);
+}
+
+void 
+btree_postorder(btree_t bt, void (*visit)(int, element_t)) 
+{
+  ASSERT_BT_ARGUMENTS(bt, visit);
+
+  postorder(bt->root, visit);
 }
--- data-structure/btree/btree-search/main.c
+++ data-structure/btree/btree-search/main.c
@@ -1,10 +1,44 @@
+/*
+ * Copyright (c) 2013 ASMlover. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list ofconditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materialsprovided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include "btree.h"
 
 
-#define ARRAY_N   (10)
+#define ARRAY_N   (20)
+
+static void 
+visit_double(int key, element_t val) 
+{
+  fprintf(stdout, "The element {key=>%d, val=>%lf}\n", key, *(double*)val);
+}
 
 static void 
 search_btree_test(btree_t bt, int rand_n, double rand_d) 
@@ -22,6 +56,16 @@ search_btree_test(btree_t bt, int rand_n, double rand_d)
     fprintf(stdout, "insert => {key=>%d, val=>%lf}\n", key_array[i], *d);
     btree_insert(bt, key_array[i], d);
   }
+  
+  fprintf(stdout, "\nsearch binary tree size : %d\n", btree_size(bt));
+  btree_preorder(bt, visit_double);
+
+  fprintf(stdout, "\nsearch binary tree size : %d\n", btree_size(bt));
+  btree_inorder(bt, visit_double);
+
+  fprintf(stdout, "\nsearch binary tree size : %d\n", btree_size(bt));
+  btree_postorder(bt, visit_double);
+
   fprintf(stdout, "\nsearch binary tree size : %d\n", btree_size(bt));
   for (i = 0; i < ARRAY_N; ++i) {
     d = btree_remove(bt, key_array[i]);
