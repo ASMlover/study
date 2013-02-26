ba2892d1fcc3e606505fc5678c6004cd64135e75
ASMlover <asmlover@126.com>
2013-02-26 12:38:44 +0800

    added common utils lib for 0MQ demo

---
+++ zeroMQ/c/utils/utils.c
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2013 ASMlover. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list ofconditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materialsprovided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#if (defined(_WIN32))
+  #include <windows.h>
+#else
+  #include <sys/time.h>
+#endif 
+#include <string.h>
+#include <zmq.h>
+
+#include "utils.h"
+
+
+
+uint64_t 
+get_clock(void)
+{
+#if (defined(_WIN32))
+  SYSTEMTIME s;
+  GetLocalTime(&s);
+  return (uint64_t)(s.wSecond * 1000 + s.wMilliseconds);
+#else
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return (uint64_t)(tv.tv_sec * 1000 + tv.tv_usec / 1000);
+#endif
+}
+
+void 
+set_identity(void* socket, const char* identity)
+{
+  zmq_setsockopt(socket, ZMQ_IDENTITY, identity, strlen(identity));
+}
