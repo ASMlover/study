1918e5b1147bb2cfdc49031a76649cf9dcb37b76
ASMlover <asmlover@126.com>
2013-03-13 17:39:41 +0800

    fixed bug of clua.c in linux

--- lua/lua-game/lua-wrapper/clua.c
+++ lua/lua-game/lua-wrapper/clua.c
@@ -26,6 +26,7 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+#include <string.h>
 #include <lua.h>
 #include <lualib.h>
 #include <lauxlib.h>
