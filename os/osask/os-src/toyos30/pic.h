96de44cc3b5f1c633221f8b0f88674750fcb1ba4
ASMlover <asmlover@126.com>
2013-05-06 20:53:31 +0800

    added demo about a easy FIFO buffer

---
+++ os/osask/os-src/toyos30/pic.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2013 ASMlover. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list ofconditions and the following disclaimer.
+ *
+ *    notice, this list of conditions and the following disclaimer in
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    the documentation and/or other materialsprovided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef __INTERRUPT_PIC_HEADER_H__
+#define __INTERRUPT_PIC_HEADER_H__
+
+#define PIC0_ICW1   (0x0020)
+#define PIC0_OCW2   (0x0020)
+#define PIC0_IMR    (0x0021)
+#define PIC0_ICW2   (0x0021)
+#define PIC0_ICW3   (0x0021)
+#define PIC0_ICW4   (0x0021)
+#define PIC1_ICW1   (0x00a0)
+#define PIC1_OCW2   (0x00a0)
+#define PIC1_IMR    (0x00a1)
+#define PIC1_ICW2   (0x00a1)
+#define PIC1_ICW3   (0x00a1)
+#define PIC1_ICW4   (0x00a1)
+
+
+typedef struct key_buff_s {
+  unsigned char data[32];
+  int next;
+} key_buff_t;
+
+extern void init_pic(void);
+extern void interrupt_handler21(int* esp);
+extern void interrupt_handler2c(int* esp);
+extern void interrupt_handler27(int* esp);
+
+#endif  /* __INTERRUPT_PIC_HEADER_H__ */
