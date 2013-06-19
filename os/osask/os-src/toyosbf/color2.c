a0f10bce6f3e830583b7b4e080b0b9e87f0ac6f1
ASMlover <asmlover@126.com>
2013-06-19 21:46:31 +0800

    setting initialize position of user window

---
+++ os/osask/os-src/toyosbf/color2.c
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2013 ASMlover. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list ofconditions and the following disclaimer.
+ *
+ *    notice, this list of conditions and the following disclaimer in
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    the documentation and/or other materialsprovided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#include "user_api.h"
+
+
+
+static unsigned char 
+rgb2pal(int r, int g, int b, int x, int y)
+{
+  static int table[4] = { 3, 1, 0, 2 };
+  int dat;
+
+  x &= 1;
+  y &= 1;
+  dat = table[x + y * 2];
+  r = (r * 21) / 256;     /* r => 0 ~ 20 */
+  g = (g * 21) / 256;
+  b = (b * 21) / 256;
+  r = (r + dat) / 4;      /* r => 0 ~ 5 */
+  g = (g + dat) / 4;
+  b = (b + dat) / 4;
+
+  return (16 + r + g * 6 + b * 36);
+}
+
+void 
+HariMain(void)
+{
+  char* buf;
+  int win, x, y;
+
+  init_mem();
+
+  buf = (char*)malloc(144 * 164);
+  win = win_open(buf, 144, 164, -1, "WIN-COLOR2");
+  for (y = 0; y < 128; ++y) {
+    for (x = 0; x < 128; ++x) 
+      buf[(x + 8) + (y + 28) * 144] = rgb2pal(x * 2, y * 2, 0, x, y);
+  }
+  win_refresh(win, 8, 28, 136, 156);
+  getkey(1);
+
+  api_end();
+}
