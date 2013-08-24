fb7eeeb9d2bdba5712817447ae406a282d61708f
ASMlover <asmlover@126.com>
2013-08-24 10:32:37 +0800

    add test for single memory allocator

--- c++/mempool/pool/single-allocator/allocator_test.cc
+++ c++/mempool/pool/single-allocator/allocator_test.cc
@@ -25,11 +25,50 @@
 //! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 //! POSSIBILITY OF SUCH DAMAGE.
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "allocator.h"
+
+struct test_data_t {
+  int id_;
+  char name_[32];
+};
 
 
 int 
 main(int argc, char* argv[])
 {
+  const int ALLOC_TIMES = 10000, LOOP_TIMES = 500;
+  clock_t beg, end;
+  int counter;
+  size_t size = sizeof(test_data_t);
+  test_data_t* arr[ALLOC_TIMES];
+
+
+  counter = 0;
+  beg = clock();
+  while (counter++ < LOOP_TIMES) {
+    for (int i = 0; i < ALLOC_TIMES; ++i) {
+      arr[i] = (test_data_t*)malloc(size);
+
+      free(arr[i]);
+    }
+  }
+  end = clock();
+  fprintf(stdout, "default use %lu\n", end - beg);
+  
+  
+  counter = 0;
+  beg = clock();
+  while (counter++ < LOOP_TIMES) {
+    for (int i = 0; i < ALLOC_TIMES; ++i) {
+      arr[i] = (test_data_t*)allocator_t::singleton().alloc(size);
+
+      allocator_t::singleton().dealloc(arr[i], size);
+    }
+  }
+  end = clock();
+  fprintf(stdout, "default use %lu\n", end - beg);
+
   return 0;
 }
