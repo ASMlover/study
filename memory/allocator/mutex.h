6d4877ec38488e7dcb021139744f521a5148450f
ASMlover <asmlover@126.com>
2013-08-24 19:48:39 +0800

    add mutex for windows

---
+++ memory/allocator/mutex.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2013 ASMlover. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list ofconditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materialsprovided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef __MUTEX_HEADER_H__
+#define __MUTEX_HEADER_H__
+
+#if defined(_MSC_VER) && defined(_WINDOWS_)
+  #include <windows.h>
+
+  typedef CRITICAL_SECTION  mutex_t;
+#elif defined(__linux__)
+  #include <pthread.h>
+  
+  typedef pthread_mutex_t   mutex_t;
+#endif
+
+extern int mutex_init(mutex_t* mutex);
+extern void mutex_destroy(mutex_t* mutex);
+extern void mutex_lock(mutex_t* mutex);
+extern int mutex_trylock(mutex_t* mutex);
+extern void mutex_unlock(mutex_t* mutex);
+
+#endif  /* __MUTEX_HEADER_H__ */
