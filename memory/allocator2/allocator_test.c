cd6af7491dbd9b4171a68cd694a9334ff0493627
ASMlover <asmlover@126.com>
2013-08-26 20:44:26 +0800

    add test demo for allocator

--- memory/allocator2/allocator_test.c
+++ memory/allocator2/allocator_test.c
@@ -26,12 +26,46 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "allocator.h"
+
+
+#define ALLOC_TIMES   (10000)
+#define LOOP_TIMES    (500)
 
 
 int 
 main(int argc, char* argv[])
 {
+  int* arr[ALLOC_TIMES];
+  int i, counter;
+  clock_t beg, end;
+
+  counter = 0;
+  beg = clock();
+  while (counter++ < LOOP_TIMES) {
+    for (i = 0; i < ALLOC_TIMES; ++i) 
+      arr[i] = (int*)malloc(sizeof(int));
+    
+    for (i = 0; i < ALLOC_TIMES; ++i) 
+      free(arr[i]);
+  }
+  end = clock();
+  fprintf(stdout, "default use : %lu\n", end - beg);
+  
+  counter = 0;
+  beg = clock();
+  while (counter++ < LOOP_TIMES) {
+    for (i = 0; i < ALLOC_TIMES; ++i) 
+      arr[i] = (int*)al_malloc(sizeof(int));
+    
+    for (i = 0; i < ALLOC_TIMES; ++i) 
+      al_free(arr[i]);
+  }
+  end = clock();
+  fprintf(stdout, "allocator use : %lu\n", end - beg);
+
   return 0;
 }
