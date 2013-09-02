13a3a58097008d8d136c3686b346fd0f5dcd8ca9
ASMlover <asmlover@126.com>
2013-09-02 23:53:10 +0800

    add locker tools for thread pool of windows platform

---
+++ c++/thread/thread-pool-win2/locker.h
@@ -0,0 +1,79 @@
+//! Copyright (c) 2013 ASMlover. All rights reserved.
+//!
+//! Redistribution and use in source and binary forms, with or without
+//! modification, are permitted provided that the following conditions
+//! are met:
+//!
+//!  * Redistributions of source code must retain the above copyright
+//!    notice, this list ofconditions and the following disclaimer.
+//!
+//!  * Redistributions in binary form must reproduce the above copyright
+//!    notice, this list of conditions and the following disclaimer in
+//!    the documentation and/or other materialsprovided with the
+//!    distribution.
+//!
+//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+//! COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//! POSSIBILITY OF SUCH DAMAGE.
+#ifndef __LOCKER_HEADER_H__
+#define __LOCKER_HEADER_H__
+
+#if defined(_WINDOWS_) || defined(_MSC_VER)
+  #include <windows.h>
+#endif
+
+#include "noncopyable.h"
+
+
+class mutex_t : noncopyable {
+  CRITICAL_SECTION mutex_;
+public:
+  mutex_t(void)
+  {
+    InitializeCriticalSection(&mutex_);
+  }
+
+  ~mutex_t(void)
+  {
+    DeleteCriticalSection(&mutex_);
+  }
+
+  inline void 
+  lock(void)
+  {
+    EnterCriticalSection(&mutex_);
+  }
+
+  inline void 
+  unlock(void)
+  {
+    LeaveCriticalSection(&mutex_);
+  }
+};
+
+
+class guard_t : noncopyable {
+  mutex_t& mutex_;
+public:
+  guard_t(mutex_t& mutex)
+    : mutex_(mutex)
+  {
+    mutex_.lock();
+  }
+
+  ~guard_t(void)
+  {
+    mutex_.unlock();
+  }
+};
+
+#endif  //! __LOCKER_HEADER_H__
