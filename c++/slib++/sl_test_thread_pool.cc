b405cd0f2ebb1dcbd090711fe88cd86b7655301e
ASMlover <asmlover@126.com>
2013-09-07 12:11:31 +0800

    add unit test for thread pool of slib++

---
+++ c++/slib++/sl_test_thread_pool.cc
@@ -0,0 +1,71 @@
+//! Copyright (c) 2013 ASMlover. All rights reserved.
+//!
+//! Redistribution and use in source and binary forms, with or without
+//! modification, are permitted provided that the following conditions
+//! are met:
+//!
+//!  * Redistributions of source code must retain the above copyright
+//!    notice, this list ofconditions and the following disclaimer.
+//!
+//!  * Redistributions in binary form must reproduce the above copyright
+//!    notice, this list of conditions and the following disclaimer in
+//!    the documentation and/or other materialsprovided with the
+//!    distribution.
+//!
+//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+//! COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//! POSSIBILITY OF SUCH DAMAGE.
+#if defined(_WINDOWS_) || defined(_MSC_VER)
+  #include <windows.h>
+#elif defined(__linux__)
+  #include <unistd.h>
+#endif
+#include <stdio.h>
+#include "sl_test_header.h"
+#include "sl_thread_pool.h"
+
+
+
+
+static inline void 
+worker(void* arg)
+{
+  char* name = static_cast<char*>(arg);
+#if defined(_WINDOWS_) || defined(_MSC_VER)
+  DWORD tid = GetCurrentThreadId();
+#elif defined(__linux__)
+  pthread_t tid = pthread_self();
+#endif 
+
+  for (int i = 0; i < 5; ++i) {
+    fprintf(stdout, "\t\t[%s:%lu] - count is > %d\n", name, tid, i + 1);
+#if defined(_WINDOWS_) || defined(_MSC_VER)
+    Sleep(1000);
+#elif defined(__linux__)
+    sleep(1);
+#endif
+  }
+}
+
+
+TEST_IMPL(thread_pool)
+{
+  sl::thread_pool_t tp;
+  tp.start();
+
+
+  tp.run(worker, (void*)"worker_#1");
+  tp.run(worker, (void*)"worker_#2");
+  tp.run(worker, (void*)"worker_#3");
+  tp.run(worker, (void*)"worker_#4");
+  tp.run(worker, (void*)"worker_#5");
+}
