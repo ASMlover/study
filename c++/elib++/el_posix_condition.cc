cccc8ae2ef55c6ae051ec739e3039aa3f65fc40c
ASMlover <asmlover@126.com>
2013-09-17 21:38:22 +0800

    add condition for linux platform

---
+++ c++/elib++/el_posix_condition.cc
@@ -0,0 +1,64 @@
+//! Copyright (c) 2013 ASMlover. All rights reserved.
+//!
+//! Redistribution and use in source and binary forms, with or without
+//! modification, are permitted provided that the following conditions
+//! are met:
+//!
+//!  * Redistributions of source code must retain the above copyright
+//!    notice, this list ofconditions and the following disclaimer.
+//!
+//!  * Redistributions in binary form must reproduce the above copyright
+//!    notice, this list of conditions and the following disclaimer in
+//!    the documentation and/or other materialsprovided with the
+//!    distribution.
+//!
+//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+//! COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//! POSSIBILITY OF SUCH DAMAGE.
+#include "el_tools.h"
+#include "el_mutex.h"
+#include "el_condition.h"
+
+
+
+namespace el {
+
+Condition::Condition(Mutex& mutex)
+  : mutex_(mutex)
+{
+  PthreadCall("cv init", pthread_cond_init(&cond_, 0));
+}
+
+Condition::~Condition(void)
+{
+  PthreadCall("cv destroy", pthread_cond_destroy(&cond_));
+}
+
+void 
+Condition::Signal(void)
+{
+  PthreadCall("cv signal", pthread_cond_signal(&cond_));
+}
+
+void 
+Condition::SignalAll(void)
+{
+  PthreadCall("cv broadcast", pthread_cond_broadcast(&cond_));
+}
+
+void 
+Condition::Wait(void)
+{
+  PthreadCall("cv wait", pthread_cond_wait(&cond_, mutex_.mutex()));
+}
+
+}
