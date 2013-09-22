f5535e35f207552cdc6dd7ddfb6dc21017d37448
ASMlover <asmlover@126.com>
2013-09-22 21:33:49 +0800

    add io tools for windows platform

---
+++ c++/elib++/el_win_io.cc
@@ -0,0 +1,89 @@
+//! Copyright (c) 2013 ASMlover. All rights reserved.
+//!
+//! Redistribution and use in source and binary forms, with or without
+//! modification, are permitted provided that the following conditions
+//! are met:
+//!
+//!  * Redistributions of source code must retain the above copyright
+//!    notice, this list ofconditions and the following disclaimer.
+//!
+//!  * Redistributions in binary form must reproduce the above copyright
+//!    notice, this list of conditions and the following disclaimer in
+//!    the documentation and/or other materialsprovided with the
+//!    distribution.
+//!
+//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+//! COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//! POSSIBILITY OF SUCH DAMAGE.
+#include <windows.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include "el_io.h"
+
+
+
+namespace el {
+
+static inline int 
+ColorVfprintf(FILE* stream, int color, const char* format, va_list ap)
+{
+  HANDLE out_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+  CONSOLE_SCREEN_BUFFER_INFO info;
+  GetConsoleScreenBufferInfo(out_handle, &info);
+  WORD old_color = info.wAttributes;
+
+  switch (color) {
+  case COLOR_RED:
+    color = FOREGROUND_INTENSITY | FOREGROUND_RED;
+    break;
+  case COLOR_GREEN:
+    color = FOREGROUND_INTENSITY | FOREGROUND_GREEN;
+    break;
+  default:
+    color = old_color;
+  }
+
+  SetConsoleTextAttribute(out_handle, (WORD)color);
+  int ret = vfprintf(stream, format, ap);
+  SetConsoleTextAttribute(out_handle, old_color);
+
+  return ret;
+}
+
+
+
+
+int 
+ColorPrintf(int color, const char* format, ...)
+{
+  va_list ap;
+
+  va_start(ap, format);
+  int ret = ColorVfprintf(stdout, color, format, ap);
+  va_end(ap);
+
+  return ret;
+}
+
+int 
+ColorFprintf(FILE* stream, int color, const char* format, ...)
+{
+  va_list ap;
+
+  va_start(ap, format);
+  int ret = ColorVfprintf(stream, color, format, ap);
+  va_end(ap);
+
+  return ret;
+}
+
+}
--- c++/elib++/makefile.mk
+++ c++/elib++/makefile.mk
@@ -35,7 +35,7 @@ LDFLAGS	= -INCREMENTAL -DEBUG -PDB:$(OUT).pdb -manifest\
 	-manifestfile:$(OUT).manifest -manifestuac:no 
 OBJS	= el_test_main.obj el_test_mutex.obj el_test_spinlock.obj\
 	\
-	el_win_condition.obj el_allocator.obj
+	el_win_condition.obj el_allocator.obj el_win_io.obj
 
 
 
