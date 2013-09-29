f2937f5e32464277a4b5d6d12061bcf1ea84d655
ASMlover <asmlover@126.com>
2013-09-29 20:24:33 +0800

    updated makefile for elibpp

--- c++/elibpp/makefile.mk
+++ c++/elibpp/makefile.mk
@@ -25,7 +25,7 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-OUT	= elib++.exe 
+OUT	= elibpp.exe 
 RM	= rm 
 CC	= cl -c -nologo 
 MT	= mt -nologo
