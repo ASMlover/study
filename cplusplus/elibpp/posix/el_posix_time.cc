0ef44a487d9be7b4bdd1344481bc5f627ddafa10
ASMlover <asmlover@126.com>
2013-10-02 21:29:50 +0800

    finished implementation of el::Time module for linux platform

---
+++ cplusplus/elibpp/posix/el_posix_time.cc
@@ -0,0 +1,53 @@
+//! Copyright (c) 2013 ASMlover. All rights reserved.
+//!
+//! Redistribution and use in source and binary forms, with or without
+//! modification, are permitted provided that the following conditions
+//! are met:
+//!
+//!  * Redistributions of source code must retain the above copyright
+//!    notice, this list ofconditions and the following disclaimer.
+//!
+//!  * Redistributions in binary form must reproduce the above copyright
+//!    notice, this list of conditions and the following disclaimer in
+//!    the documentation and/or other materialsprovided with the
+//!    distribution.
+//!
+//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+//! COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//! POSSIBILITY OF SUCH DAMAGE.
+#include <sys/time.h>
+#include <time.h>
+#include "../el_time.h"
+
+
+namespace el {
+
+uint32_t 
+Clock(void)
+{
+  struct timeval tv;
+
+  gettimeofday(&tv, (void*)0);
+  return (((tv.tv_sec - 1000000000) * 1000) + (tv.tv_usec / 1000));
+}
+
+void 
+Sleep(uint32_t millitm)
+{
+  struct timespec t;
+
+  t.tv_sec = millitm / 1000;
+  t.tv_nsec = (millitm % 1000) * 1000000;
+  nanosleep(&t, NULL);
+}
+
+}
