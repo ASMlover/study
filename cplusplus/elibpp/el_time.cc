b40a06ffa66f080a13b7ae95ff9614313beb65ea
ASMlover <asmlover@126.com>
2013-10-02 20:58:01 +0800

    add time module for elibpp

---
+++ cplusplus/elibpp/el_time.cc
@@ -0,0 +1,58 @@
+//! Copyright (c) 2013 ASMlover. All rights reserved.
+//!
+//! Redistribution and use in source and binary forms, with or without
+//! modification, are permitted provided that the following conditions
+//! are met:
+//!
+//!  * Redistributions of source code must retain the above copyright
+//!    notice, this list ofconditions and the following disclaimer.
+//!
+//!  * Redistributions in binary form must reproduce the above copyright
+//!    notice, this list of conditions and the following disclaimer in
+//!    the documentation and/or other materialsprovided with the
+//!    distribution.
+//!
+//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+//! COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//! POSSIBILITY OF SUCH DAMAGE.
+#include <sys/timeb.h>
+#include <time.h>
+#include "el_time.h"
+
+
+namespace el {
+
+bool 
+Localtime(Time* time)
+{
+  if (NULL == time) {
+    return false;
+  }
+  else {
+    struct timeb tb;
+    struct tm*   now;
+
+    ftime(&tb);
+    now = localtime(&tb.time);
+    time->year = (uint16_t)(now->tm_year + 1900);
+    time->mon  = (uint8_t)(now->tm_mon + 1);
+    time->day  = (uint8_t)(now->tm_mday);
+    time->hour = (uint8_t)(now->tm_hour);
+    time->min  = (uint8_t)(now->tm_min);
+    time->sec  = (uint8_t)(now->tm_sec);
+    time->millitm = (uint16_t)(tb.millitm);
+
+    return true;
+  }
+}
+
+}
---
+++ cplusplus/elibpp/el_time.h
@@ -0,0 +1,51 @@
+//! Copyright (c) 2013 ASMlover. All rights reserved.
+//!
+//! Redistribution and use in source and binary forms, with or without
+//! modification, are permitted provided that the following conditions
+//! are met:
+//!
+//!  * Redistributions of source code must retain the above copyright
+//!    notice, this list ofconditions and the following disclaimer.
+//!
+//!  * Redistributions in binary form must reproduce the above copyright
+//!    notice, this list of conditions and the following disclaimer in
+//!    the documentation and/or other materialsprovided with the
+//!    distribution.
+//!
+//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+//! COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//! POSSIBILITY OF SUCH DAMAGE.
+#ifndef __EL_TIME_HEADER_H__
+#define __EL_TIME_HEADER_H__
+
+#include "el_config.h"
+
+namespace el {
+
+struct Time {
+  uint16_t  year;
+  uint8_t   mon;
+  uint8_t   day;
+  uint8_t   hour;
+  uint8_t   min;
+  uint8_t   sec;
+  uint16_t  millitm;
+};
+
+extern bool Localtime(Time* time);
+extern uint32_t Clock(void);
+extern void Sleep(uint32_t millitm);
+
+};
+
+
+#endif  //! __EL_TIME_HEADER_H__
