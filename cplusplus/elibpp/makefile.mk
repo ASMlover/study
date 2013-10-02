f4bba8f1f80a6e12e8de049868bfbc1168225500
ASMlover <asmlover@126.com>
2013-10-02 21:25:04 +0800

    implementation el:Time module for windows platform

--- cplusplus/elibpp/makefile.mk
+++ cplusplus/elibpp/makefile.mk
@@ -33,12 +33,12 @@ LINK	= link -nologo
 CFLAGS	= -O2 -W3 -MD -GS -Zi -Fd"vc.pdb" -EHsc -DNDEBUG\
 	-D_CRT_SECURE_NO_WARNINGS
 LDFLAGS	= -INCREMENTAL -DEBUG -PDB:$(OUT).pdb -manifest\
-	-manifestfile:$(OUT).manifest -manifestuac:no 
+	-manifestfile:$(OUT).manifest -manifestuac:no winmm.lib
 OBJS	= el_test_main.obj el_test_mutex.obj el_test_spinlock.obj\
 	el_test_condition.obj el_test_thread.obj el_test_thread_pool.obj\
 	el_test_allocator.obj el_test_buffer.obj el_test_file.obj\
 	\
-	el_win_condition.obj el_win_io.obj el_win_file.obj\
+	el_win_condition.obj el_win_io.obj el_win_file.obj el_win_time.obj\
 	\
 	el_allocator.obj el_thread_pool.obj el_buffer.obj el_time.obj
 
