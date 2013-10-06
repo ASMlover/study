a8622808f19ecdcb7b30ac5ba882a32a488d43fd
ASMlover <asmlover@126.com>
2013-10-06 21:10:39 +0800

    fixed bug of unit test of condition

--- slib/test/sl_test_cond.c
+++ slib/test/sl_test_cond.c
@@ -26,6 +26,7 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+#include <string.h>
 #include "sl_test_header.h"
 #include "../src/sl_thread.h"
 #include "../src/sl_mutex.h"
