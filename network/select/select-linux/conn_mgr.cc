5974f80e8631216b52862b668375603237b4c58e
ASMlover <asmlover@126.com>
2013-10-23 22:53:59 +0800

    add connector manager module

---
+++ network/select/select-linux/conn_mgr.cc
@@ -0,0 +1,139 @@
+//! Copyright (c) 2013 ASMlover. All rights reserved.
+//!
+//! Redistribution and use in source and binary forms, with or without
+//! modification, are permitted provided that the following conditions
+//! are met:
+//!
+//!  * Redistributions of source code must retain the above copyright
+//!    notice, this list ofconditions and the following disclaimer.
+//!
+//!  * Redistributions in binary form must reproduce the above copyright
+//!    notice, this list of conditions and the following disclaimer in
+//!    the documentation and/or other materialsprovided with the
+//!    distribution.
+//!
+//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+//! COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//! POSSIBILITY OF SUCH DAMAGE.
+#include "common.h"
+#include "socket.h"
+#include "conn_mgr.h"
+
+
+
+ConnectorMgr::ConnectorMgr(void)
+{
+}
+
+ConnectorMgr::~ConnectorMgr(void)
+{
+}
+
+void 
+ConnectorMgr::CloseAll(void)
+{
+  LockerGuard<SpinLock> guard(spinlock_);
+  std::map<int, std::pair<int Socket*> >::iterator it;
+  for (it = connectors_.begin(); it != connectors_.end(); ++it) {
+    it->second.second->Close();
+    delete it->second.second;
+  }
+  connectors_.clear();
+}
+
+bool 
+ConnectorMgr::Insert(int fd, int ev)
+{
+  Socket* s = new Socket();
+  if (NULL == s) {
+    LOG_ERR("new Socket error ...\n");
+    return false;
+  }
+  s->Attach(fd);
+
+  LockerGuard<SpinLock> guard(spinlock_);
+  std::map<int, std::pair<int, Socket*> >::iterator it;
+  it = connectors_.find(fd);
+  if (it == connectors_.end())
+    connectors_[fd] = std::make_pair<int, Socket*>(ev, s);
+  return true;
+}
+
+void 
+ConnectorMgr::Remove(int fd)
+{
+  LockerGuard<SpinLock> guard(spinlock_);
+  std::map<int, std::pair<int, Socket*> >::iterator it;
+  it = connectors_.find(fd);
+  if (it != connectors_.end()) {
+    it->second.second->Close();
+    delete it->second.second;
+    connectors_.erase(it);
+  }
+}
+
+void 
+ConnectorMgr::AddEvent(int fd, int ev)
+{
+  LockerGuard<SpinLock> guard(spinlock_);
+  std::map<int, std::pair<int, Socket*> >::iterator it;
+  it = connectors_.find(fd);
+  if (it != connectors_.end())
+    it->second.first |= ev;
+}
+
+void 
+ConnectorMgr::DelEvent(int fd, int ev)
+{
+  LockerGuard<SpinLock> guard(spinlock_);
+  std::map<int, std::pair<int, Socket*> >::iterator it;
+  it = connectors_.find(fd);
+  if (it != connectors_.end())
+    it->second.first &= ~ev;
+}
+
+Socket* 
+ConnectorMgr::GetConnector(int fd)
+{
+  Socket* s = NULL;
+  {
+    LockerGuard<SpinLock> guard(spinlock_);
+    std::map<int, std::pair<int, Socket*> >::iterator it;
+    it = connectors_.find(fd);
+    if (it != connectors_.end())
+      s = it->second.second;
+  }
+
+  return s;
+}
+
+void 
+ConnectorMgr::InitSelectSets(fd_set* rset, fd_set* wset)
+{
+  if (NULL == rset || NULL == wset)
+    return;
+
+  LockerGuard<SpinLock> guard(spinlock_);
+  std::map<int, std::pair<int, Socket*> >::iterator it;
+  for (it = connectors_.begin(); it != connectors_.end();) {
+    if (-1 == it->second.second->fd()) {
+      delete it->second.second;
+      it = connectors_.erase(it);
+    }
+    else {
+      FD_SET(it->first, rset);
+      FD_SET(it->first, wset);
+
+      ++it;
+    }
+  }
+}
