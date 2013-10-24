3e32f97a382ef075cb6bc0c7771577367a32b289
ASMlover <asmlover@126.com>
2013-10-24 21:01:42 +0800

    add net library about loaded windows socket library

---
+++ network/select/select-server3/net.h
@@ -0,0 +1,49 @@
+//! Copyright (c) 2013 ASMlover. All rights reserved.
+//!
+//! Redistribution and use in source and binary forms, with or without
+//! modification, are permitted provided that the following conditions
+//! are met:
+//!
+//!  * Redistributions of source code must retain the above copyright
+//!    notice, this list ofconditions and the following disclaimer.
+//!
+//!  * Redistributions in binary form must reproduce the above copyright
+//!    notice, this list of conditions and the following disclaimer in
+//!    the documentation and/or other materialsprovided with the
+//!    distribution.
+//!
+//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+//! COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//! POSSIBILITY OF SUCH DAMAGE.
+#ifndef __NET_HEADER_H__
+#define __NET_HEADER_H__
+
+enum NetType {
+  kNetTypeInvalid = -1, 
+  kNetTypeError   = -1, 
+};
+
+class NetLibrary {
+  static bool loaded_;
+
+  NetLibrary(const NetLibrary&);
+  NetLibrary& operator =(const NetLibrary&);
+public:
+  explicit NetLibrary(void);
+  ~NetLibrary(void);
+
+  static NetLibrary& Singleton(void);
+  bool Init(void);
+  void Destroy(void);
+};
+
+#endif  //! __NET_HEADER_H__
