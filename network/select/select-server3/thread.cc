c41edfc0f48219c17400819006f0213079ac302c
ASMlover <asmlover@126.com>
2013-10-24 21:41:51 +0800

    add thread module

---
+++ network/select/select-server3/thread.cc
@@ -0,0 +1,87 @@
+//! Copyright (c) 2013 ASMlover. All rights reserved.
+//!
+//! Redistribution and use in source and binary forms, with or without
+//! modification, are permitted provided that the following conditions
+//! are met:
+//!
+//!  * Redistributions of source code must retain the above copyright
+//!    notice, this list ofconditions and the following disclaimer.
+//!
+//!  * Redistributions in binary form must reproduce the above copyright
+//!    notice, this list of conditions and the following disclaimer in
+//!    the documentation and/or other materialsprovided with the
+//!    distribution.
+//!
+//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+//! COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//! POSSIBILITY OF SUCH DAMAGE.
+#include "logging.h"
+#include "thread.h"
+
+
+
+Thread::Thread(void (*routine)(void*), void* argument)
+  : thread_(NULL)
+  , start_event_(NULL)
+  , routine_(routine)
+  , argument_(argument)
+{
+}
+
+Thread::~Thread(void)
+{
+  Join();
+}
+
+bool 
+Thread::Start(void)
+{
+  start_event_ = CreateEvent(NULL, TRUE, FALSE, NULL);
+  if (NULL == start_event_) {
+    LOG_FAIL("CreateEvent failed err-code(%d)\n", Errno());
+    return false;
+  }
+  thread_ = CreateThread(NULL, 0, &Thread::Routine, this, 0, NULL);
+  if (NULL == thread_) {
+    LOG_FAIL("CreateThread failed err-code(%d)\n", Errno());
+    return false;
+  }
+
+  WaitForSingleObject(start_event_, INFINITE);
+  CloseHandle(start_event_);
+
+  return true;
+}
+
+void 
+Thread::Join(void)
+{
+  if (NULL != thread_) {
+    WaitForSingleObject(thread_, INFINITE);
+    CloseHandle(thread_);
+    thread_ = NULL;
+  }
+}
+
+DWORD WINAPI 
+Thread::Routine(void* argument)
+{
+  Thread* self = static_cast<Thread*>(argument);
+  if (NULL == self)
+    return 0;
+  SetEvent(self->start_event_);
+
+  if (NULL != self->routine_)
+    self->routine_(self->argument_);
+
+  return 0;
+}
