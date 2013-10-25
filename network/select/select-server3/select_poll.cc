fbbd8d6615f85d542a371d76a432194c9e1dd2e9
ASMlover <asmlover@126.com>
2013-10-25 22:13:21 +0800

    add select poller module

---
+++ network/select/select-server3/select_poll.cc
@@ -0,0 +1,216 @@
+//! Copyright (c) 2013 ASMlover. All rights reserved.
+//!
+//! Redistribution and use in source and binary forms, with or without
+//! modification, are permitted provided that the following conditions
+//! are met:
+//!
+//!  * Redistributions of source code must retain the above copyright
+//!    notice, this list ofconditions and the following disclaimer.
+//!
+//!  * Redistributions in binary form must reproduce the above copyright
+//!    notice, this list of conditions and the following disclaimer in
+//!    the documentation and/or other materialsprovided with the
+//!    distribution.
+//!
+//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+//! COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//! POSSIBILITY OF SUCH DAMAGE.
+#ifndef _WINDOWS_
+# include <winsock2.h>
+#endif
+#include "net.h"
+#include "logging.h"
+#include "socket.h"
+#include "select_poll.h"
+
+
+
+
+SelectPoll::SelectPoll(void)
+  : rbytes_(kSocketBuffer)
+  , wbytes_(kSocketBuffer)
+  , handler_(NULL)
+  , spinlock_()
+{
+  connectors_.clear();
+  FD_ZERO(&rset_);
+  FD_ZERO(&wset_);
+}
+
+SelectPoll::~SelectPoll(void)
+{
+  CloseAll();
+}
+
+void 
+SelectPoll::CloseAll(void)
+{
+  if (NULL == handler_)
+    return;
+
+  LockerGuard<SpinLock> guard(spinlock_);
+  std::map<int, std::pair<int, Socket*> >::iterator it;
+  for (it = connectors_.begin(); it != connectors_.end(); ++it) {
+    it->second.second->Close();
+    handler_->CloseEvent(it->first);
+    delete it->second.second;
+  }
+  connectors_.clear();
+}
+
+bool 
+SelectPoll::Insert(int fd, int ev)
+{
+  Socket* s = new Socket();
+  if (NULL == s) {
+    LOG_ERR("new Socket error\n");
+    return false;
+  }
+  s->Attach(fd);
+  s->SetReadBuffer(rbytes_);
+  s->SetWriteBuffer(wbytes_);
+
+  LockerGuard<SpinLock> guard(spinlock_);
+  std::map<int, std::pair<int, Socket*> >::iterator it;
+  it = connectors_.find(fd);
+  if (it == connectors_.end())
+    connectors_[fd] = std::make_pair<int, Socket*>(ev, s);
+
+  return true;
+}
+
+void 
+SelectPoll::Remove(int fd)
+{
+  if (NULL == handler_)
+    return;
+
+  LockerGuard<SpinLock> guard(spinlock_);
+  std::map<int, std::pair<int, Socket*> >::iterator it;
+  it = connectors_.find(fd);
+  if (it != connectors_.end()) {
+    it->second.second->Close();
+    handler_->CloseEvent(it->first);
+    delete it->second.second;
+    connectors_.erase(it);
+  }
+}
+
+void 
+SelectPoll::AddEvent(int fd, int ev)
+{
+  LockerGuard<SpinLock> guard(spinlock_);
+  std::map<int, std::pair<int, Socket*> >::iterator it;
+  it = connectors_.find(fd);
+  if (it != connectors_.end()) 
+    it->second.first |= ev;
+}
+
+void 
+SelectPoll::DelEvent(int fd, int ev)
+{
+  LockerGuard<SpinLock> guard(spinlock_);
+  std::map<int, std::pair<int, Socket*> >::iterator it;
+  it = connectors_.find(fd);
+  if (it != connectors_.end()) 
+    it->second.first &= ~ev;
+}
+
+bool 
+SelectPoll::Polling(int millitm)
+{
+  if (NULL == handler_)
+    return false;
+
+  struct timeval timeout;
+  if (-1 == millitm) {
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 500;
+  }
+  else {
+    timeout.tv_sec = millitm / 1000;
+    timeout.tv_usec = millitm % 1000 * 1000;
+  }
+
+  int max_fd;
+  InitSets(&max_fd);
+
+  int ret = select(max_fd + 1, &rset_, &wset_, NULL, &timeout);
+  if (kNetTypeError == ret || 0 == ret)
+    return false;
+
+  DispatchEvent(&rset_, EventHandler::kEventTypeRead);
+  DispatchEvent(&wset_, EventHandler::kEventTypeWrite);
+
+  return true;
+}
+
+void 
+SelectPoll::InitSets(int* max_fd)
+{
+  FD_ZERO(&rset_);
+  FD_ZERO(&wset_);
+  *max_fd = 0;
+
+  int fd;
+  LockerGuard<SpinLock> guard(spinlock_);
+  std::map<int, std::pair<int, Socket*> >::iterator it;
+  for (it = connectors_.begin(); it != connectors_.end();) {
+    fd = it->first;
+
+    if (kNetTypeInvalid == it->second.second->fd()) {
+      handler_->CloseEvent(fd);
+      delete it->second.second;
+      it = connectors_.erase(it);
+    }
+    else {
+      if (it->second.first & EventHandler::kEventTypeRead)
+        FD_SET(fd, &rset_);
+
+      if (it->second.first & EventHandler::kEventTypeWrite)
+        FD_SET(fd, &wset_);
+
+      if (fd > *max_fd)
+        *max_fd = fd;
+
+      ++it;
+    }
+  }
+}
+
+void 
+SelectPoll::DispatchEvent(fd_set* set, int ev)
+{
+  for (unsigned int i = 0; i < set->fd_count; ++i) {
+    Socket* s = NULL;
+
+    {
+      LockerGuard<SpinLock> guard(spinlock_);
+      std::map<int, std::pair<int, Socket*> >::iterator it;
+      it = connectors_.find(set->fd_array[i]);
+      if (it != connectors_.end())
+        s = it->second.second;
+    }
+
+    if (NULL == s)
+      continue;
+
+    switch (ev) {
+    case EventHandler::kEventTypeRead:
+      handler_->ReadEvent(s);
+      break;
+    case EventHandler::kEventTypeWrite:
+      handler_->WriteEvent(s);
+      break;
+    }
+  }
+}
---
+++ network/select/select-server3/select_poll.h
@@ -0,0 +1,77 @@
+//! Copyright (c) 2013 ASMlover. All rights reserved.
+//!
+//! Redistribution and use in source and binary forms, with or without
+//! modification, are permitted provided that the following conditions
+//! are met:
+//!
+//!  * Redistributions of source code must retain the above copyright
+//!    notice, this list ofconditions and the following disclaimer.
+//!
+//!  * Redistributions in binary form must reproduce the above copyright
+//!    notice, this list of conditions and the following disclaimer in
+//!    the documentation and/or other materialsprovided with the
+//!    distribution.
+//!
+//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+//! COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//! POSSIBILITY OF SUCH DAMAGE.
+#ifndef __SELECT_POLL_HEADER_H__
+#define __SELECT_POLL_HEADER_H__
+
+#ifndef _WINDOWS_
+# include <winsock2.h>
+#endif
+#include <map>
+#include "locker.h"
+
+class Socket;
+struct EventHandler;
+class SelectPoll {
+  enum {kSocketBuffer = 16 * 1024};
+  int rbytes_;
+  int wbytes_;
+  EventHandler* handler_;
+  SpinLock spinlock_;
+  std::map<int, std::pair<int, Socket*> > connectors_;
+  fd_set rset_;
+  fd_set wset_;
+
+  SelectPoll(const SelectPoll&);
+  SelectPoll& operator =(const SelectPoll&);
+public:
+  explicit SelectPoll(void);
+  ~SelectPoll(void);
+
+  inline void Attach(EventHandler* handler) 
+  {
+    handler_ = handler;
+  }
+
+  inline void SetBuffer(int rbytes, int wbytes)
+  {
+    rbytes_ = rbytes;
+    wbytes_ = wbytes;
+  }
+public:
+  void CloseAll(void);
+  bool Insert(int fd, int ev);
+  void Remove(int fd);
+  void AddEvent(int fd, int ev);
+  void DelEvent(int fd, int ev);
+
+  bool Polling(int millitm = -1);
+private:
+  void InitSets(int* max_fd);
+  void DispatchEvent(fd_set* set, int ev);
+};
+
+#endif  //! __SELECT_POLL_HEADER_H__
