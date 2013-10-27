bd885a06e4f117d3e6c9f34ca59b6ae7c01b6eac
ASMlover <asmlover@126.com>
2013-10-27 22:26:11 +0800

    add buffer module

---
+++ network/select/select-server3/buffer.cc
@@ -0,0 +1,114 @@
+//! Copyright (c) 2013 ASMlover. All rights reserved.
+//!
+//! Redistribution and use in source and binary forms, with or without
+//! modification, are permitted provided that the following conditions
+//! are met:
+//!
+//!  * Redistributions of source code must retain the above copyright
+//!    notice, this list ofconditions and the following disclaimer.
+//!
+//!  * Redistributions in binary form must reproduce the above copyright
+//!    notice, this list of conditions and the following disclaimer in
+//!    the documentation and/or other materialsprovided with the
+//!    distribution.
+//!
+//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+//! COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//! POSSIBILITY OF SUCH DAMAGE.
+#include <stdlib.h>
+#include <string.h>
+#include "logging.h"
+#include "buffer.h"
+
+
+
+Buffer::Buffer(void)
+  : buffer_(NULL)
+  , storage_(0)
+  , pos_(0)
+{
+}
+
+Buffer::~Buffer(void)
+{
+  Destroy();
+}
+
+bool 
+Buffer::Init(int storage)
+{
+  if (storage <= 0) {
+    LOG_FAIL("init storage failed\n");
+    return false;
+  }
+
+  pos_ = 0;
+  storage_ = storage;
+  buffer_ = (char*)malloc(storage_);
+
+  return (NULL != buffer_);
+}
+
+void 
+Buffer::Destroy(void)
+{
+  if (NULL != buffer_) {
+    free(buffer_);
+    buffer_ = NULL;
+  }
+  storage_ = 0;
+  pos_ = 0;
+}
+
+int 
+Buffer::Put(const char* buffer, int length)
+{
+  if (pos_ + length > storage_)
+    Regrow(storage_ + kDefaultStorage);
+
+  memcpy(buffer_ + pos_, buffer, length);
+  pos_ += length;
+
+  return length;
+}
+
+int 
+Buffer::Get(int length, char* buffer)
+{
+  int copy_bytes = pos_ <= length ? pos_ : length;
+  if (0 == copy_bytes)
+    return 0;
+
+  memcpy(buffer, buffer_, copy_bytes);
+  if (copy_bytes != pos_) 
+    memmove(buffer_, buffer_ + copy_bytes, pos_ - copy_bytes);
+
+  pos_ -= copy_bytes;
+  return copy_bytes;
+}
+
+
+bool 
+Buffer::Regrow(int new_storage)
+{
+  if (new_storage <= storage_)
+    return true;
+
+  buffer_ = (char*)realloc(buffer_, new_storage);
+  if (NULL == buffer_) {
+    LOG_FAIL("realloc failed\n");
+    return false;
+  }
+
+  storage_ = new_storage;
+  return true;
+}
---
+++ network/select/select-server3/buffer.h
@@ -0,0 +1,61 @@
+//! Copyright (c) 2013 ASMlover. All rights reserved.
+//!
+//! Redistribution and use in source and binary forms, with or without
+//! modification, are permitted provided that the following conditions
+//! are met:
+//!
+//!  * Redistributions of source code must retain the above copyright
+//!    notice, this list ofconditions and the following disclaimer.
+//!
+//!  * Redistributions in binary form must reproduce the above copyright
+//!    notice, this list of conditions and the following disclaimer in
+//!    the documentation and/or other materialsprovided with the
+//!    distribution.
+//!
+//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+//! COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//! POSSIBILITY OF SUCH DAMAGE.
+#ifndef __BUFFER_HEADER_H__
+#define __BUFFER_HEADER_H__
+
+class Buffer {
+  enum {kDefaultStorage = 16 * 1024};
+  char* buffer_;
+  int   storage_;
+  int   pos_;
+
+  Buffer(const Buffer&);
+  Buffer& operator =(const Buffer&);
+public:
+  explicit Buffer(void);
+  ~Buffer(void);
+
+  inline int length(void) const 
+  {
+    return pos_;
+  }
+
+  inline int free_length(void) const 
+  {
+    return storage_ - pos_;
+  }
+public:
+  bool Init(int storage = kDefaultStorage);
+  void Destroy(void);
+
+  int Put(const char* buffer, int length);
+  int Get(int length, char* buffer);
+private:
+  bool Regrow(int new_storage);
+};
+
+#endif  //! __BUFFER_HEADER_H__
