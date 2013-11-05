13784c11a394dd369f90e20c7b1b9d2cea83a431
ASMlover <asmlover@126.com>
2013-11-05 22:39:58 +0800

    updated test demo for libnet

--- network/select/libnet/main.cc
+++ network/select/libnet/main.cc
@@ -49,11 +49,13 @@ public:
 
   virtual bool ReadEvent(Socket* s)
   {
-    char buf[128] = {0};
-    if (s->Read(sizeof(buf), buf)) {
-      fprintf(stdout, "recv from client : %s\n", buf);
+    Packet packet;
+    if (s->ReadPacket(&packet)) {
+      fprintf(stdout, "recv from client : %s\n", packet.GetData());
 
-      s->Write(buf, strlen(buf));
+      s->WritePacket(&packet);
+
+      packet.ReleaseData();
     }
 
     return true;
@@ -86,41 +88,63 @@ ServerMain(const char* ip = "127.0.0.1", unsigned short port = 5555)
 
 
 
+
+
+static bool _s_connected = false;
+class ClientHandler : public EventHandler {
+public:
+  virtual bool ReadEvent(Socket* s)
+  {
+    Packet packet;
+    if (s->ReadPacket(&packet)) {
+      fprintf(stdout, "recv from server : %s\n", packet.GetData());
+
+      packet.ReleaseData();
+    }
+
+    return true;
+  }
+
+  virtual void CloseEvent(Socket* s)
+  {
+    fprintf(stdout, "client [%d] closed\n", s->fd());
+    _s_connected = false;
+  }
+};
+
 void 
 ClientMain(const char* ip = "127.0.0.1", unsigned short port = 5555)
 {
-  Socket s;
-  s.Open();
+  ClientHandler ch;
+  SelectNetwork network;
 
-  if (s.Connect(ip, port)) {
-    fprintf(stdout, "connect to server success ...\n");
-  }
-  else {
+  network.Attach(&ch);
+  network.Init();
+
+  Socket* s = network.Connect(ip, port);
+  if (NULL == s) {
     fprintf(stderr, "connect to server failed ...\n");
     return;
   }
+  else {
+    fprintf(stdout, "connect to server success ...\n");
+  }
+  _s_connected = true;
 
   char buf[128];
   struct timeb tb;
   struct tm* now;
-  while (true) {
+  while (_s_connected) {
     ftime(&tb);
     now = localtime(&tb.time);
     sprintf(buf, "[%04d-%02d-%02d %02d:%02d:%02d:%03d]", 
         now->tm_year + 1900, now->tm_mon + 1, now->tm_mday, 
         now->tm_hour, now->tm_min, now->tm_sec, tb.millitm);
-    if (kNetTypeError == s.WriteBlock(buf, strlen(buf)))
+    if (!s->WritePacket(buf, strlen(buf)))
       break;
-
-    memset(buf, 0, sizeof(buf));
-    if (kNetTypeError == s.ReadBlock(sizeof(buf), buf))
-      break;
-    fprintf(stdout, "recv from server : %s\n", buf);
 
     ToolsLib::Sleep(10);
   }
-
-  s.Close();
 }
 
 
