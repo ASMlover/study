b1b2b0cdd14620493de8b09149a11753d25097f4
ASMlover <asmlover@126.com>
2013-11-16 21:32:07 +0800

    fixed bug of net

--- network/epoll/libnet/net.cc
+++ network/epoll/libnet/net.cc
@@ -24,9 +24,6 @@
 //! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 //! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 //! POSSIBILITY OF SUCH DAMAGE.
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
 #include "net.h"
 
 
