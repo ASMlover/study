316aac51e91ce2f0a70bcf4fb1e68d244a1cfc50
ASMlover <asmlover@126.com>
2013-11-27 22:18:47 +0800

    add demo for libnet

--- network/epoll/libnet/main.cc
+++ network/epoll/libnet/main.cc
@@ -24,13 +24,117 @@
 //! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 //! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 //! POSSIBILITY OF SUCH DAMAGE.
+#include <sys/timeb.h>
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include "net.h"
+#include "os_tool.h"
+#include "address.h"
+#include "Socket.h"
+#include "network.h"
+
+
+class ConnectorHandler : public EventHandler {
+public:
+  virtual bool AcceptEvent(Socket* s, Address* addr)
+  {
+    fprintf(stdout, "accept connector [%d]\n", s->fd());
+    return true;
+  }
+
+  virtual void CloseEvent(Socket* s) 
+  {
+    fprintf(stdout, "connector [%d] closed\n", s->fd());
+  }
+
+  virtual bool ReadEvent(Socket* s)
+  {
+    char buf[128] = {0};
+    if (s->Read(sizeof(buf), buf)) {
+      fprintf(stdout, "recv from connector [%d] : %s\n", s->fd(), buf);
+
+      s->Write(buf, strlen(buf));
+    }
+
+    return true;
+  }
+};
+
+static void 
+ServerMain(const char* ip = "127.0.0.1", unsigned short port = 5555)
+{
+  ConnectorHandler ch;
+  Network network;
+
+  network.Attach(&ch);
+  network.Init();
+
+  if (!network.Listen(ip, port)) {
+    fprintf(stdout, "server <%s, %d> starting failed ...\n", ip, port);
+    return;
+  }
+  else {
+    fprintf(stdout, "server <%s, %d> starting success ...\n", ip, port);
+  }
+
+  while (true) 
+    Tools::Sleep(100);
+
+  network.Destroy();
+}
+
+static void 
+ClientMain(const char* ip = "127.0.0.1", unsigned short port = 5555)
+{
+  Socket s;
+
+  s.Open();
+
+  if (!s.Connect(ip, port)) {
+    fprintf(stdout, "connect to server<%s, %d> failed\n", ip, port);
+    return;
+  }
+  else {
+    fprintf(stdout, "connect to server<%s, %d> success\n", ip, port);
+  }
+
+  char buf[128];
+  struct timeb tb;
+  struct tm* now;
+  while (true) {
+    ftime(&tb);
+    now = localtime(&tb.time);
+
+    sprintf(buf, "[%04d-%02d-%02d %02d:%02d:%02d:%03d]", 
+        now->tm_year + 1900, now->tm_mon + 1, now->tm_mday, 
+        now->tm_hour, now->tm_min, now->tm_sec, tb.millitm);
+    s.WriteBlock(buf, strlen(buf));
+
+    memset(buf, 0, sizeof(buf));
+    if (kNetTypeError == s.ReadBlock(sizeof(buf), buf)) 
+      break;
+    fprintf(stdout, "recv form server : %s\n", buf);
+
+    Tools::Sleep(10);
+  }
+}
 
 
 int 
 main(int argc, char* argv[])
 {
-  fprintf(stdout, "Hello, world!\n");
+  if (argc < 2)
+    return 0;
+
+  NetLibrary::Singleton().Init();
+
+  if (0 == strcmp("srv", argv[1]))
+    ServerMain();
+  else if (0 == strcmp("clt", argv[1]))
+    ClientMain();
+
+  NetLibrary::Singleton().Destroy();
 
   return 0;
 }
