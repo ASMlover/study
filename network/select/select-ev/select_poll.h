1f6ea9eac8196271a51b4598934bdfd12cea3109
ASMlover <asmlover@126.com>
2013-11-30 22:19:32 +0800

    add select poll module

---
+++ network/select/select-ev/select_poll.h
@@ -0,0 +1,55 @@
+//! Copyright (c) 2013 ASMlover. All rights reserved.
+//!
+//! Redistribution and use in source and binary forms, with or without
+//! modification, are permitted provided that the following conditions
+//! are met:
+//!
+//!  * Redistributions of source code must retain the above copyright
+//!    notice, this list ofconditions and the following disclaimer.
+//!
+//!  * Redistributions in binary form must reproduce the above copyright
+//!    notice, this list of conditions and the following disclaimer in
+//!    the documentation and/or other materialsprovided with the
+//!    distribution.
+//!
+//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+//! COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//! POSSIBILITY OF SUCH DAMAGE.
+#ifndef __SELECT_POLL_HEADER_H__
+#define __SELECT_POLL_HEADER_H__
+
+#include <vector>
+
+class Socket;
+struct SelectEntry;
+class SelectPoll : public EventPoll {
+  bool has_removed_;
+  fd_set rset_in_;
+  fd_set wset_in_;
+  fd_set rset_out_;
+  fd_set wset_out_;
+  std::vector<SelectEntry> entry_list_;
+
+  SelectPoll(const SelectPoll&);
+  SelectPoll& operator =(const SelectPoll&);
+public:
+  explicit SelectPoll(void);
+  ~SelectPoll(void);
+
+  virtual bool Insert(int fd, Socket* s);
+  virtual void Remove(int fd);
+  virtual bool AddEvent(int fd, int ev);
+  virtual bool DelEvent(int fd, int ev);
+  virtual bool Dispatch(EventDispatcher* dispatcher, int millitm);
+};
+
+#endif  //! __SELECT_POLL_HEADER_H__
