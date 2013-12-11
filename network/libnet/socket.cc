28503b32332c97a70a3384d89b210d004a65d88a
ASMlover <asmlover@126.com>
2013-12-11 21:33:09 +0800

    finished the implementation of Socket

--- network/libnet/socket.cc
+++ network/libnet/socket.cc
@@ -42,3 +42,188 @@
 #include "logging.h"
 #include "address.h"
 #include "socket.h"
+
+
+
+
+Socket::Socket(void)
+  : fd_(kNetTypeInval)
+{
+}
+
+Socket::~Socket(void)
+{
+  Close();
+}
+
+bool 
+Socket::SetOption(int level, int optname, int optval)
+{
+  if (kNetTypeInval == fd_)
+    return false;
+
+  if (kNetTypeError == setsockopt(fd_, 
+        level, optname, 
+#if defined(PLATFORM_WIN)
+        (const char*)&optval, 
+#elif defined(PLATFORM_POSIX)
+        (const void*)&optval, 
+#endif
+        sizeof(optval)))
+    return false;
+
+  return true;
+}
+
+bool 
+Socket::SetNonBlock(void)
+{
+  if (kNetTypeInval == fd_)
+    return false;
+
+#if defined(PLATFORM_WIN)
+  u_long val = 1;
+  if (kNetTypeError == ioctlsocket(fd_, FIONBIO, &val))
+    return false;
+#elif defined(PLATFORM_POSIX)
+  int opt = fcntl(fd_, F_GETFL);
+  if (kNetTypeError == opt)
+    return false;
+
+  if (kNetTypeError == fcntl(fd_, F_SETFL, opt | O_NONBLOCK))
+    return false;
+#endif
+
+  return true;
+}
+
+bool 
+Socket::SetTcpNoDelay(bool nodelay)
+{
+  return SetOption(IPPROTO_TCP, TCP_NODELAY, (nodelay ? 1 : 0));
+}
+
+bool 
+Socket::SetReuseAddr(bool reuse)
+{
+  return SetOption(SOL_SOCKET, SO_REUSEADDR, (reuse ? 1 : 0));
+}
+
+bool 
+Socket::SetKeepAlive(bool keep)
+{
+  return SetOption(SOL_SOCKET, SO_KEEPALIVE, (keep ? 1 : 0));
+}
+
+
+bool 
+Socket::Open(void)
+{
+  fd_ = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (kNetTypeInval == fd_)
+    return false;
+
+  return true;
+}
+
+void 
+Socket::Close(void)
+{
+  if (kNetTypeInval != fd_) {
+#if defined(PLATFORM_WIN)
+    shutdown(fd_, SD_BOTH);
+    closesocket(fd_);
+#elif defined(PLATFORM_POSIX)
+    shutdown(fd_, SHUT_RDWR);
+    close(fd_);
+#endif
+    fd_ = kNetTypeInval;
+  }
+}
+
+bool 
+Socket::Bind(const char* ip, uint16_t port)
+{
+  if (kNetTypeInval == fd_)
+    return false;
+
+  struct sockaddr_in host_addr;
+  host_addr.sin_addr.s_addr = 
+    (NULL == ip ? htonl(INADDR_ANY) : inet_addr(ip));
+  host_addr.sin_family      = AF_INET;
+  host_addr.sin_port        = htons(port);
+  if (kNetTypeError == bind(fd_, 
+        (struct sockaddr*)&host_addr, sizeof(host_addr)))
+    return false;
+
+  return true;
+}
+
+bool 
+Socket::Listen(void)
+{
+  if (kNetTypeInval == fd_)
+    return false;
+
+  if (kNetTypeError == listen(fd_, SOMAXCONN))
+    return false;
+
+  return true;
+}
+
+bool 
+Socket::Accept(Socket* s, Address* addr)
+{
+  if (kNetTypeInval == fd_ || NULL == s)
+    return false;
+
+  struct sockaddr_in remote_addr;
+  socklen_t addrlen = sizeof(remote_addr);
+  int fd = accept(fd_, (struct sockaddr*)&remote_addr, &addrlen);
+  if (kNetTypeInval == fd)
+    return false;
+
+  s->Attach(fd);
+  if (NULL != addr)
+    addr->Attach(&remote_addr);
+
+  return true;
+}
+
+bool 
+Socket::Connect(const char* ip, uint16_t port)
+{
+  if (kNetTypeInval == fd_)
+    return false;
+
+  if (NULL == ip)
+    ip = "127.0.0.1";
+
+  struct sockaddr_in remote_addr;
+  remote_addr.sin_addr.s_addr = inet_addr(ip);
+  remote_addr.sin_family      = AF_INET;
+  remote_addr.sin_port        = htons(port);
+  if (kNetTypeError == connect(fd_, 
+        (struct sockaddr*)&remote_addr, sizeof(remote_addr)))
+    return false;
+
+  return true;
+}
+
+int 
+Socket::Read(uint32_t bytes, char* buffer)
+{
+  if (kNetTypeInval == fd_ || 0 == bytes || NULL == buffer)
+    return kNetTypeError;
+
+  return recv(fd_, buffer, bytes, 0);
+}
+
+int 
+Socket::Write(const char* buffer, uint32_t bytes)
+{
+  if (kNetTypeInval == fd_ || NULL == buffer || 0 == bytes)
+    return kNetTypeError;
+
+  return send(fd_, buffer, bytes, 0);
+}
