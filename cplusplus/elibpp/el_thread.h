4cfc33b08fdcca1a86a80aabceca1b28ea49aa5c
ASMlover <asmlover@126.com>
2013-12-22 11:22:03 +0800

    improved the implementation of Thread

--- cplusplus/elibpp/el_thread.h
+++ cplusplus/elibpp/el_thread.h
@@ -27,9 +27,9 @@
 #ifndef __EL_THREAD_HEADER_H__
 #define __EL_THREAD_HEADER_H__
 
-#if defined(_WINDOWS_) || defined(_MSC_VER)
+#if defined(PLATFORM_WIN)
 # include "./win/el_win_thread.h"
-#elif defined(__linux__)
+#elif defined(PLATFORM_LINUX)
 # include "./posix/el_posix_thread.h"
 #endif
 
