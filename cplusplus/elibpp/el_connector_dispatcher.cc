5dc3a334d2affa4664161b489f69e5f2e4777b39
ASMlover <asmlover@126.com>
2013-12-25 21:31:35 +0800

    finished the implementation of Connector Dispatcher module

--- cplusplus/elibpp/el_connector_dispatcher.cc
+++ cplusplus/elibpp/el_connector_dispatcher.cc
@@ -49,28 +49,107 @@ ConnectorDispatcher::~ConnectorDispatcher(void)
 void 
 ConnectorDispatcher::CloseAll(void)
 {
+  std::map<int, Connector*>::iterator it;
+  for (it = connectors_.begin(); it != connectors_.end(); ++it) {
+    if (NULL != it->second) {
+      it->second->Close();
+      delete it->second;
+    }
+  }
+  connectors_.clear();
 }
 
 Connector* 
 ConnectorDispatcher::Insert(int fd)
 {
+  LockerGuard<SpinLock> guard(spinlock_);
+  std::map<int, Connector*>::iterator it = connectors_.find(fd);
+  if (it == connectors_.end()) {
+    Connector* conn = new Connector();
+    if (NULL == conn)
+      return NULL;
+
+    conn->Attach(fd);
+    conn->SetNonBlock();
+    conn->SetTcpNoDelay();
+    conn->SetReuseAddr();
+    conn->SetKeepAlive();
+    conn->SetReadBuffer(rbytes_);
+    conn->SetWriteBuffer(wbytes_);
+
+    connectors_[fd] = conn;
+
+    return conn;
+  }
+
   return NULL;
 }
 
 void 
 ConnectorDispatcher::Remove(int fd)
 {
+  LockerGuard<SpinLock> guard(spinlock_);
+  std::map<int, Connector*>::iterator it = connectors_.find(fd);
+  if (it != connectors_.end()) {
+    if (NULL != it->second) {
+      it->second->Close();
+      delete it->second;
+    }
+
+    connectors_.erase(it);
+  }
 }
 
 bool 
 ConnectorDispatcher::DispatchReader(Poller* poller, Connector* conn)
 {
+  if (NULL == handler_ || NULL == poller || NULL == conn)
+    return false;
+
+  int read_bytes = conn->DealWithAsyncRead();
+  if (read_bytes > 0) {
+    handler_->ReadEvent(conn);
+  }
+  else {
+    if (0 == read_bytes || EAGAIN != NERROR()) {
+      poller->Remove(conn);
+
+      handler_->CloseEvent(conn);
+
+      Remove(conn->fd());
+    }
+  }
+
   return true;
 }
 
 bool 
 ConnectorDispatcher::DispatchWriter(Poller* poller, Connector* conn)
 {
+  if (NULL == handler_ || NULL == poller || NULL == conn)
+    return false;
+
+  int write_bytes = conn->DealWithAsyncWrite();
+  if (write_bytes > 0) {
+    if (conn->WriteBufferEmpty()) {
+      poller->DelEvent(conn, kEventTypeWrite);
+      conn->SetWritable();
+    }
+  }
+  else {
+    if (0 == write_bytes || EAGAIN != NERROR()) {
+      poller->Remove(conn);
+
+      handler_->CloseEvent(conn);
+
+      Remove(conn->fd());
+    }
+    else {
+      poller->AddEvent(conn, kEventTypeWrite);
+      conn->SetWritable(false);
+    }
+  }
+
   return true;
 }
 
