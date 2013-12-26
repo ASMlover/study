e445bf32a695abe4c6daec9bc6562e0c764c1ad4
ASMlover <asmlover@126.com>
2013-12-26 21:53:12 +0800

    add network handler module for elibpp

---
+++ cplusplus/elibpp/el_network_handler.cc
@@ -0,0 +1,75 @@
+//! Copyright (c) 2013 ASMlover. All rights reserved.
+//!
+//! Redistribution and use in source and binary forms, with or without
+//! modification, are permitted provided that the following conditions
+//! are met:
+//!
+//!  * Redistributions of source code must retain the above copyright
+//!    notice, this list ofconditions and the following disclaimer.
+//!
+//!  * Redistributions in binary form must reproduce the above copyright
+//!    notice, this list of conditions and the following disclaimer in
+//!    the documentation and/or other materialsprovided with the
+//!    distribution.
+//!
+//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+//! COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//! POSSIBILITY OF SUCH DAMAGE.
+#include "el_net_internal.h"
+#include "el_socket.h"
+#include "el_net_worker.h"
+#include "el_net_listener.h"
+#include "el_network_handler.h"
+
+
+namespace el {
+
+
+NetworkHandler::NetworkHandler(void)
+{
+}
+
+NetworkHandler::~NetworkHandler(void)
+{
+}
+
+bool 
+NetworkHandler::Init(int worker_count, uint32_t rbuf, uint32_t wbuf)
+{
+  return true;
+}
+
+void 
+NetworkHandler::Destroy(void)
+{
+}
+
+NetWorker* 
+NetworkHandler::SuitableWorker(void)
+{
+  return NULL;
+}
+
+void 
+NetworkHandler::MarkNextSuitableWorker(void)
+{
+}
+
+
+bool 
+NetworkHandler::Listen(const char* ip, uint16_t port)
+{
+  return true;
+}
+
+
+}
---
+++ cplusplus/elibpp/el_network_handler.h
@@ -0,0 +1,68 @@
+//! Copyright (c) 2013 ASMlover. All rights reserved.
+//!
+//! Redistribution and use in source and binary forms, with or without
+//! modification, are permitted provided that the following conditions
+//! are met:
+//!
+//!  * Redistributions of source code must retain the above copyright
+//!    notice, this list ofconditions and the following disclaimer.
+//!
+//!  * Redistributions in binary form must reproduce the above copyright
+//!    notice, this list of conditions and the following disclaimer in
+//!    the documentation and/or other materialsprovided with the
+//!    distribution.
+//!
+//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+//! COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//! POSSIBILITY OF SUCH DAMAGE.
+#ifndef __EL_NETWORK_HANDLER_H__
+#define __EL_NETWORK_HANDLER_H__
+
+namespace el {
+
+class NetWorker;
+class NetListener;
+class NetworkHandler : private NonCopyable {
+  enum {
+    kDefaultWorkerCount = 1, 
+    kDefaultBufferSize  = 16 * 1024, 
+  };
+  ConnectorHolder* holder_;
+  int worker_count_;
+  NetWorker* workers_;
+  NetListener* listener_;
+  int suitable_worker_;
+  EventHandler* handler_;
+public:
+  explicit NetworkHandler(void);
+  ~NetworkHandler(void);
+
+  inline void Attach(EventHandler* handler)
+  {
+    handler_ = handler;
+  }
+public:
+  bool Init(
+      int worker_count = kDefaultWorkerCount, 
+      uint32_t rbuf = kDefaultBufferSize, 
+      uint32_t wbuf = kDefaultBufferSize);
+  void Destroy(void);
+
+  NetWorker* SuitableWorker(void);
+  void MarkNextSuitableWorker(void);
+
+  bool Listen(const char* ip = "0.0.0.0", uint16_t port = 5555);
+};
+
+}
+
+#endif  //! __EL_NETWORK_HANDLER_H__
