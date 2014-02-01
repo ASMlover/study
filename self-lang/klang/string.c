388d78a546120ddb0781661e99070aa92ee747a6
ASMlover <asmlover@126.com>
2014-02-01 21:34:31 +0800

    finished the implementation of string module

--- self-lang/klang/string.c
+++ self-lang/klang/string.c
@@ -28,6 +28,12 @@
  */
 #include "global.h"
 
+#define KL_STRING_ALLOC_SZ  (256)
+
+
+static char* s_str_literal_buffer = NULL;
+static int   s_str_literal_buffer_size = 0;
+static int   s_str_literal_buffer_alloc_sz = 0;
 
 
 char* 
@@ -37,4 +43,45 @@ KL_create_identifier(const char* str)
   strcpy(new_str, str);
 
   return new_str;
+}
+
+
+
+void 
+KL_open_string_literal(void)
+{
+  s_str_literal_buffer_size = 0;
+}
+
+char* 
+KL_close_string_literal(void)
+{
+  char* new_str = KL_util_malloc(s_str_literal_buffer_size + 1);
+  memcpy(new_str, s_str_literal_buffer, s_str_literal_buffer_size);
+  new_str[s_str_literal_buffer_size] = 0;
+
+  return new_str;
+}
+
+void 
+KL_add_string_literal(int letter)
+{
+  if (s_str_literal_buffer_size ==s_str_literal_buffer_alloc_sz) {
+    s_str_literal_buffer_alloc_sz += KL_STRING_ALLOC_SZ;
+    s_str_literal_buffer
+      = (char*)KL_realloc(s_str_literal_buffer, 
+          s_str_literal_buffer_alloc_sz);
+  }
+
+  s_str_literal_buffer[s_str_literal_buffer_size] = (char)letter;
+  ++s_str_literal_buffer_size;
+}
+
+void 
+KL_reset_string_literal_buffer(void)
+{
+  KL_free(s_str_literal_buffer);
+  s_str_literal_buffer = NULL;
+  s_str_literal_buffer_size = 0;
+  s_str_literal_buffer_alloc_sz = 0;
 }
