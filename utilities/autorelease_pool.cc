6184ce384a82b5bfb617dba61de23107d2f77703
ASMlover <asmlover@126.com>
2014-04-10 20:52:07 +0800

    add AutoreleasePool module

---
+++ utilities/autorelease_pool.cc
@@ -0,0 +1,130 @@
+//! Copyright (c) 2014 ASMlover. All rights reserved.
+//!
+//! Redistribution and use in source and binary forms, with or without
+//! modification, are permitted provided that the following conditions
+//! are met:
+//!
+//!  * Redistributions of source code must retain the above copyright
+//!    notice, this list ofconditions and the following disclaimer.
+//!
+//!  * Redistributions in binary form must reproduce the above copyright
+//!    notice, this list of conditions and the following disclaimer in
+//!    the documentation and/or other materialsprovided with the
+//!    distribution.
+//!
+//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+//! COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//! POSSIBILITY OF SUCH DAMAGE.
+#include <utility.h>
+#include "ref.h"
+#include "autorelease_pool.h"
+
+
+AutoreleasePool::AutoreleasePool(void) 
+  : name_("") {
+  objects_array_.reserve(150);
+  PoolManager::GetInstance()->Push(this);
+}
+
+AutoreleasePool::AutoreleasePool(const std::string& name)
+  : name_(name) {
+  objects_array_.reserve(150);
+  PoolManager::GetInstance()->Push(this);
+}
+
+AutoreleasePool::~AutoreleasePool(void) {
+  Clear();
+  PoolManager::GetInstance()->Pop();
+}
+
+void AutoreleasePool::AddObject(Ref* object) {
+  objects_array_.push_back(object);
+}
+
+void AutoreleasePool::Clear(void) {
+  for (const auto& obj : objects_array_) 
+    obj->Release();
+
+  objects_array_.Clear();
+}
+
+bool AutoreleasePool::Contains(Ref* object) const {
+  for (const auto& obj : objects_array_) {
+    if (object == obj)
+      return true;
+  }
+
+  return false;
+}
+
+
+
+
+static PoolManager* s_sharedPoolManager = nullptr;
+PoolManager::GetInstance(void) {
+  if (nullptr == s_sharedPoolManager) {
+    s_sharedPoolManager = new PoolManager();
+    s_sharedPoolManager->current_pool_ 
+        = new AutoreleasePool("utility auto release pool");
+    s_sharedPoolManager->pool_stack_.push_back(
+        s_sharedPoolManager->current_pool_);
+  }
+
+  return s_sharedPoolManager;
+}
+
+void PoolManager::DestroyInstance(void) {
+  if (nullptr != s_sharedPoolManager) {
+    delete s_sharedPoolManager;
+    s_sharedPoolManager = nullptr;
+  }
+}
+
+
+PoolManager::PoolManager(void) 
+  : current_pool_(nullptr) {
+}
+
+PoolManager::~PoolManager(void) {
+  while (!pool_stack_.empty()) {
+    AutoreleasePool* pool = pool_stack_.back();
+    pool_stack_.pop_back();
+
+    delete pool;
+  }
+}
+
+AutoreleasePool* PoolManager::GetCurrentPool(void) const {
+  return current_pool_;
+}
+
+bool PoolManager::IsObjectInPools(Ref* object) const {
+  for (const auto& pool : pool_stack_) {
+    if (pool->Contains(object))
+      return true;
+  }
+
+  return false;
+}
+
+void PoolManager::Push(AutoreleasePool* pool) {
+  pool_stack_.push_back(pool);
+  current_pool_ = pool;
+}
+
+void PoolManager::Pop(void) {
+  if (!pop_back.empty())
+    pool_stack_.pop_back();
+
+  if (pool_stack_.size() > 1)
+    current_pool_ = pool_stack_.back();
+}
---
+++ utilities/autorelease_pool.h
@@ -0,0 +1,90 @@
+//! Copyright (c) 2014 ASMlover. All rights reserved.
+//!
+//! Redistribution and use in source and binary forms, with or without
+//! modification, are permitted provided that the following conditions
+//! are met:
+//!
+//!  * Redistributions of source code must retain the above copyright
+//!    notice, this list ofconditions and the following disclaimer.
+//!
+//!  * Redistributions in binary form must reproduce the above copyright
+//!    notice, this list of conditions and the following disclaimer in
+//!    the documentation and/or other materialsprovided with the
+//!    distribution.
+//!
+//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+//! COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//! POSSIBILITY OF SUCH DAMAGE.
+#ifndef __AUTORELEASE_POOL_HEADER_H__
+#define __AUTORELEASE_POOL_HEADER_H__
+
+#include <vector>
+#include <deque>
+
+class AutoreleasePool : private util::UnCopyable {
+  std::string       name_;
+  std::vector<Ref*> objects_array_;
+public:
+  // Don't create an auto release pool in heap, 
+  // create it in stack.
+  AutoreleasePool(void);
+
+  // Create an auto release pool with special name.
+  explicit AutoreleasePool(const std::string& name);
+  ~AutoreleasePool(void);
+
+  // Add a given object into this pool.
+  //
+  // The same object may be added several times to the 
+  // same pool, when the pool is destructed, the object's 
+  // Ref::Release() method will be called for each time 
+  // it was added.
+  void AddObject(Ref* object);
+
+  // Clear the auto release pool.
+  //
+  // Ref::Release() will be called for each time the managed 
+  // objects is added to the pool.
+  void Clear(void);
+
+  // Checks whether the pool contains the specified object.
+  bool Contains(Ref* object) const;
+};
+
+
+class PoolManager {
+  AutoreleasePool*              current_pool_;
+  std::deque<AutoreleasePool*>  pool_stack_;
+
+  PoolManager(const PoolManager&);
+  PoolManager& operator=(const PoolManager&);
+public:
+  static PoolManager* GetInstance(void);
+  static void DestroyInstance(void);
+
+  // Get current auto release pool, there is at least one 
+  // auto release pool that created by engine. 
+  // You can create your own auto release pool at demand, 
+  // which will be put into auto release pool stack.
+  AutoreleasePool* GetCurrentPool(void) const;
+
+  bool IsObjectInPools(Ref* object) const;
+
+  void Push(AutoreleasePool* pool);
+  void Pop(void);
+private:
+  PoolManager(void);
+  ~PoolManager(void);
+};
+
+
+#endif  // __AUTORELEASE_POOL_HEADER_H__
