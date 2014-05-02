de6dbea9b207d03663288fce68b629f7e8311982
ASMlover <asmlover@126.com>
2014-05-02 19:32:21 +0800

    add test case for Allocator

---
+++ cplusplus/eutil/test/el_allocator_test.cc
@@ -0,0 +1,61 @@
+// Copyright (c) 2014 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#include "el_unit.h"
+#include "../el_allocator.h"
+#include "../el_time.h"
+
+
+
+class Integer 
+  : public el::SmallAllocator, private el::UnCopyable {
+  int val_;
+public:
+  explicit Integer(int val = 0) 
+    : val_(val) {
+  }
+
+  ~Integer(void) {
+  }
+
+  inline operator int(void) const {
+    return val_;
+  }
+};
+
+
+
+UNIT_IMPL(Allocator) {
+  const int count = 100000;
+
+  uint64_t beg = el::GetTick();
+  for (int i = 0; i < count; ++i) {
+    Integer* v = new Integer(i * 2);
+    delete v;
+  }
+  uint64_t end = el::GetTick();
+  fprintf(stdout, "\t\tuse millitm => %llu\n", end - beg);
+}
