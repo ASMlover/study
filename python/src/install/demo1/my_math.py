7e568280e42893a4dcbc5323a3aaa2b83eabe1f3
asmlover <asmlover@126.com>
2014-06-26 20:33:52 +0800

    add demo about creating a setup by using distutils module

---
+++ python/src/install/demo1/my_math.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+#
+# Copyright (c) 2014 ASMlover. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list ofconditions and the following disclaimer.
+#
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in
+#    the documentation and/or other materialsprovided with the
+#    distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+def square(x):
+  ''' Squares a number and returns the result.
+
+  >>> square(2)
+  4
+
+  >>> square(3)
+  9
+
+  >>> square(1.2)
+  1.44
+  '''
+  return x * x
+
+
+def product(x, y):
+  ''' Multiplication two numbers and returns the result.
+
+  >>> product(2, 3)
+  6
+
+  >>> product(3, 4)
+  12
+
+  >>> product(1.0, 3.0)
+  3.0
+
+  >>> product(0, 2.3)
+  0.0
+  '''
+  return x * y
+
+
+
+
+if __name__ == '__main__':
+  import doctest
+  import my_math
+  doctest.testmod(my_math)
