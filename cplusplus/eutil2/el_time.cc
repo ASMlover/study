0a95a0a9390bb21cf6c6ee31fdcf9ab6584c0ece
ASMlover <asmlover@126.com>
2014-09-09 21:03:35 +0800

    fixed bug of time module

--- cplusplus/eutil2/el_time.cc
+++ cplusplus/eutil2/el_time.cc
@@ -36,7 +36,7 @@ bool GetTime(Time& time) {
   struct tm* now = localtime(&tb.time);
   time.year = static_cast<uint16_t>(now->tm_year + 1900);
   time.mon  = static_cast<uint8_t>(now->tm_mon + 1);
-  time.sec  = static_cast<uint8_t>(now->tm_mday);
+  time.day  = static_cast<uint8_t>(now->tm_mday);
   time.hour = static_cast<uint8_t>(now->tm_hour);
   time.min  = static_cast<uint8_t>(now->tm_min);
   time.sec  = static_cast<uint8_t>(now->tm_sec);
