099ba0d023c13e3d06ffb2caf968df49ab3047c8
ASMlover <asmlover@126.com>
2014-12-10 22:02:48 +0800

    ad demo about tcp server of python

---
+++ python/network/tcp/server.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+#
+# Copyright (c) 2014 ASMlover. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list ofconditions and the following disclaimer.
+#
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in
+#    the documentation and/or other materialsprovided with the
+#    distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import socket
+import time
+import threading
+
+ADDRESS = ('', 5555)
+RECVBUF = 1024
+
+running = None
+def MessageHandler():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(ADDRESS)
+    server.listen(5)
+    running = True
+    
+    if server is None:
+        print 'start server failed ...'
+        running = False
+        return
+    print 'start server suceess ...'
+    while running:
+        client, addr = server.accept()
+        if client is not None:
+            data = client.recv(RECVBUF)
+            if data is not None:
+                print 'recevice from client', addr, data
+                client.send(data)
+            client.close()
+    server.close()
+
+def ServerMain():
+    trd = threading.Thread(target=MessageHandler)
+    trd.start()
+
+    try:
+        while True:
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        running = False
+
+if __name__ == '__main__':
+    ServerMain()
