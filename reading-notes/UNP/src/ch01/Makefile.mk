5215faaf5bb3f651bc9bfff9c1a9b7597022b0d6
ASMlover <asmlover@126.com>
2015-01-07 22:00:48 +0800

    updated the build Makefile for these demos

--- reading-notes/UNP/src/ch01/Makefile.mk
+++ reading-notes/UNP/src/ch01/Makefile.mk
@@ -27,22 +27,28 @@
 
 CLT_OUT	= echo_client.exe
 SRV_OUT	= echo_server.exe
+OUT	= $(CLT_OUT) $(SRV_OUT)
 RM	= del
 CC	= cl -c -nologo
 MT	= mt -nologo
 LINK	= link -nologo
-CFLAGS	= -O2 -W3 -MT -GS -Zi -Fd"vc.pdb" -D_DEBUG\
+CFLAGS	= -O2 -W3 -MD -GS -Zi -Fd"vc.pdb" -D_DEBUG\
 	-D_CRT_SECURE_NO_WARNINGS -D_CRT_NONSTDC_NO_WARNINGS
 CLT_LDFLAGS	= -INCREMENTAL -DEBUG -PDB:$(CLT_OUT).pdb -manifest\
 	-manifestfile:$(CLT_OUT).manifest -manifestuac:no\
-	-LIBPATH:..\common ws2_32.lib libcommon.lib 
+	-LIBPATH:"..\common" ws2_32.lib libcommon.lib 
 SRV_LDFLAGS	= -INCREMENTAL -DEBUG -PDB:$(SRV_OUT).pdb -manifest\
 	-manifestfile:$(SRV_OUT).manifest -manifestuac:no\
-	-LIBPATH:..\common ws2_32.lib libcommon.lib 
+	-LIBPATH:"..\common" ws2_32.lib libcommon.lib 
 CLT_OBJS	= echo_client.obj
 SRV_OBJS	= echo_server.obj
+OBJS	= $(CLT_OBJS) $(SRV_OBJS)
 
-all: $(CLT_OUT) $(SRV_OUT)
+all: $(OUT)
+rebuild: clean all
+clean:
+	$(RM) $(OUT) $(OBJS) *.pdb *.manifest *.ilk
+
 $(CLT_OUT): $(CLT_OBJS)
 	$(LINK) -out:$(CLT_OUT) $(CLT_OBJS) $(CLT_LDFLAGS)
 	$(MT) -manifest $(CLT_OUT).manifest -outputresource:$(CLT_OUT);1
@@ -51,5 +57,3 @@ $(SRV_OUT): $(SRV_OBJS)
 	$(MT) -manifest $(SRV_OUT).manifest -outputresource:$(SRV_OUT);1
 .c.obj:
 	$(CC) $(CFLAGS) -I"..\common" $<
-clean:
-	$(RM) $(OUT) $(OBJS) *.pdb *.manifest *.ilk
