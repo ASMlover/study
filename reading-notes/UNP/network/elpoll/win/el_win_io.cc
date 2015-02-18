d113b841471055f8f471ff55c9225d5d2dbd9ced
ASMloer <asmlover@126.com>
2015-02-18 10:07:31 +0800

    add color IO with windows platform

---
+++ reading-notes/UNP/network/elpoll/win/el_win_io.cc
@@ -0,0 +1,56 @@
+// Copyright (c) 2015 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#include "../el_poll.h"
+#include "../el_io.h"
+
+namespace el {
+
+int ColorVfprintf(FILE* stream,
+    ColorType color, const char* format, va_list ap) {
+  HANDLE out_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+  CONSOLE_SCREEN_BUFFER_INFO info;
+  GetConsoleScreenBufferInfo(out_handle, &info);
+  WORD old_color = info.wAttributes;
+  WORD new_color = old_color;
+
+  switch (color) {
+  case ColorType::COLORTYPE_RED:
+    new_color = FOREGROUND_INTENSITY | FOREGROUND_RED;
+    break;
+  case ColorType::COLORTYPE_GREEN:
+    new_color = FOREGROUND_INTENSITY | FOREGROUND_GREEN;
+    break;
+  }
+
+  SetConsoleTextAttribute(out_handle, new_color);
+  int ret = vfprintf(stream, format, ap);
+  SetConsoleTextAttribute(out_handle, old_color);
+
+  return ret;
+}
+
+}
