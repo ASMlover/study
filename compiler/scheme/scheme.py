869734bdfc62cdf44b62b9b867099a6bcd8071d0
ASMlover <asmlover@126.com>
2015-07-19 21:10:17 +0800

    an easy implementation of a little scheme

---
+++ compiler/scheme/scheme.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+#
+# Copyright (c) 2015 ASMlover. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list ofconditions and the following disclaimer.
+#
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in
+#    the documentation and/or other materialsprovided with the
+#    distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+Symbol = str
+List = list
+Number = (int, float)
+
+try:
+    apply = apply
+except NameError:
+    def apply(f, *args, **kwargs):
+        return f(*args, **kwargs)
+
+# parsing
+def atom(token):
+    try:
+        return int(token)
+    except ValueError:
+        try:
+            return float(token)
+        except ValueError:
+            return Symbol(token)
+
+def read_from_tokens(tokens):
+    if len(tokens) == 0:
+        raise SyntaxError('unexcepted EOF while reading')
+    token = tokens.pop(0)
+    if '(' == token:
+        L = []
+        while tokens[0] != ')':
+            L.append(read_from_tokens(tokens))
+        tokens.pop(0)
+        return L
+    elif ')' == token:
+        raise SyntaxError('unexcepted )')
+    else:
+        return atom(token)
+
+def tokenize(s):
+    return s.replace('(', ' ( ').replace(')', ' ) ').split()
+
+def parse(s):
+    return read_from_tokens(tokenize(s))
+
+# environment
+class Environment(dict):
+    def __init__(self, params=(), args=(), outer=None):
+        self.update(zip(params, args))
+        self.outer = outer
+
+    def find(self, var):
+        return self if (var in self) else self.outer.find(var)
+
+def standard_environment():
+    import math
+    import operator as op
+    env = Environment()
+    env.update(vars(math))
+    env.update({
+        '+': op.add, 
+        '-': op.sub,
+        '*': op.mul, 
+        '/': op.truediv,
+        '>': op.gt, 
+        '<': op.lt,
+        '>=': op.ge,
+        '<=': op.le, 
+        '=': op.eq,
+        'abs': abs,
+        'append': op.add, 
+        'apply': apply, 
+        'begin': lambda *x: x[-1],
+        'car': lambda x: x[0],
+        'cdr': lambda x: x[1:],
+        'cons': lambda x, y: [x] + y,
+        'eq?': op.is_, 
+        'equal?': op.eq,
+        'length': len, 
+        'list': lambda *x: list(x),
+        'list?': lambda x: isinstance(x, list),
+        'map': map,
+        'max': max,
+        'min': min, 
+        'not': op.not_, 
+        'null?': lambda x: x == [],
+        'number?': lambda x: isinstance(x, Number),
+        'procedure?': callable, 
+        'round': round, 
+        'symbol?': lambda x: isinstance(x, Symbol),
+    })
+    return env
+
+global_env = standard_environment()
+
+# eval
+class Procedure(object):
+    def __init__(self, params, body, env):
+        self.params, self.body, self.env = params, body, env
+
+    def __call__(self, *args):
+        return eval(self.body, Environment(self.params, args, self.env))
+
+def eval(x, env=global_env):
+    if isinstance(x, Symbol):
+        return env.find(x)[x]
+    elif not isinstance(x, List):
+        return x
+    elif x[0] == 'quote':
+        (_, exp) = x
+        return exp
+    elif x[0] == 'if':
+        (_, test, conseq, alt) = x
+        exp = (conseq if eval(text, env) else alt)
+        return eval(exp, env)
+    elif x[0] == 'define':
+        (_, var, exp) = x
+        env[var] = eval(exp, env)
+    elif x[0] == 'set!':
+        (_, var, exp) = x
+        env.find(var)[var] = eval(exp, env)
+    elif x[0] == 'lambda':
+        (_, params, body) = x
+        return Procedure(params, body, env)
+    else:
+        proc = eval(x[0], env)
+        args = [eval(exp, env) for exp in x[1:]]
+        return proc(*args)
+
+# repl
+def lispstr(exp):
+    if isinstance(exp, list):
+        return '(' + ' '.join(map(lispstr, exp)) + ')'
+    else:
+        return str(exp)
+
+def repl(prompt='scheme>>>'):
+    while True:
+        v = eval(parse(input(prompt)))
+        if v is not None:
+            print(lispstr(v))
+
+
+if __name__ == '__main__':
+    repl()
