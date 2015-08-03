7a9d2c2692b635782b45aff72e7c7a42300c6706
ASMlover <asmlover@126.com>
2015-08-04 00:33:09 +0800

    add interpreter for eLisp

---
+++ compiler/eLisp/eLisp/interpreter.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+#
+# Copyright (c) 2015 ASMlover. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list ofconditions and the following disclaimer.
+#
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in
+#    the documentation and/or other materialsprovided with the
+#    distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import readline
+
+from eLisp.lexer import Lexer
+from eLisp.parser import Parser
+from eLisp.model import Symbol, Boolean, EmptyList
+from eLisp.scope import (
+        setup_environment, define_variable, 
+        lookup_variable_value, extend_environment)
+from eLisp.expr import (
+        selfeval, quote, definition, variable, assignment, 
+        conditional, lambdaexpr, procedure, sequence, binding)
+from eLisp.expr.util import cons, is_load, load
+
+class Interpreter(object):
+    def __init__(self):
+        self.env = setup_environment()
+
+    def interpret(self, expr):
+        return self._eval(expr, self.env)
+
+    def _eval(self, expr, env):
+        pass
+
+    def _eval_definition(self, expr, env):
+        define_variable(
+                definition.definition_variable(expr), 
+                self._eval(definition.definition_value(expr), env), 
+                env)
+        return Symbol('ok')
+
+    def _eval_assignment(self, expr, env):
+        env.set_variable_value(
+                assignment.assignment_variable(expr), 
+                self._eval(assignment.assignment_value(expr), env))
+        return Symbol('ok')
+
+    def _eval_binding(self, expr, env):
+        return self._eval(
+                cons(lambdaexpr.make_lambda(binding.binding_variables(expr), 
+                        binding.binding_body(expr)),
+                    binding.binding_values(expr)), 
+                env)
+
+    def _eval_if(self, expr, env):
+        if self._eval(conditional.if_predicate(expr), env):
+            return self._eval(conditional.if_consequent(expr), env)
+        return self._eval(conditional.if_alternative(expr), env)
+
+    def _eval_and(self, expr, env):
+        if procedure.no_operands(procedure.operands(expr)):
+            return Boolean(True)
+
+        def inner(expr, env):
+            if sequence.is_last_expr(expr):
+                return self._eval(sequence.first_expr(expr), env)
+            if not self._eval(sequence.first_expr(expr), env):
+                return Boolean(False)
+            return inner(sequence.rest_exprs(expr), env)
+
+        return inner(sequence.rest_exprs(expr), env)
+
+    def _eval_or(self, expr, env):
+        if procedure.no_operands(procedure.operands(expr)):
+            return Boolean(False)
+
+        def inner(expr, env):
+            if sequence.is_last_expr(expr):
+                return self._eval(sequence.first_expr(expr), env)
+            first_value = self._eval(sequence.first_expr(expr), env)
+            if first_value:
+                return first_value
+            return inner(sequence.rest_exprs(expr), env)
+
+        return inner(sequence.rest_exprs(expr), env)
+
+    def _eval_sequence(self, expressions, env):
+        if sequence.is_last_expr(expressions):
+            return self._eval(sequence.first_expr(expressions), env)
+        else:
+            self._eval(sequence.first_expr(expressions), env)
+            return self._eval_sequence(sequence.rest_exprs(expressions), env)
+
+    def _list_of_values(self, expressions, env):
+        if procedure.no_operands(expressions):
+            return EmptyList
+        else:
+            return cons(
+                    self._eval(procedure.first_operand(expressions), env), 
+                    self._list_of_values(procedure.rest_operands(expressions), env))
+
+    def _apply(self, proc, args):
+        if procedure.is_primitive_procedure(proc):
+            return procedure.apply_primitive_procedure(proc, args)
+        elif procedure.is_compound_procedure(proc):
+            return self._eval_sequence(
+                    procedure.procedure_body(proc), 
+                    extend_environment(
+                        procedure.procedure_parameters(proc), 
+                        args, 
+                        procedure.procedure_environment(proc)))
