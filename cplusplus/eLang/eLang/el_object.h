a36ab761ab44f10af1824af74cdc85d1e7a74d02
ASMlover <asmlover@126.com>
2015-10-07 17:31:02 +0800

    # define the base interface of Object

--- cplusplus/eLang/eLang/el_object.h
+++ cplusplus/eLang/eLang/el_object.h
@@ -35,6 +35,15 @@
 namespace el {
 
 class Expr;
+class ArrayObject;
+class Block;
+class BlockObject;
+class DynamicObject;
+class Environment;
+class Fiber;
+class FiberObject;
+class Interpreter;
+class Object;
 
 typedef Value (*PrimitiveFun)(Fiber& fiber,
     const Value& value, const ArgReader& args);
@@ -51,7 +60,84 @@ public:
   }
 
   Value(const Value& other);
-  ~Value(void);
+  ~Value(void) {
+    Clear();
+  }
+
+  Value GetField(int name) const;
+  void SetField(int name, const Value& value) const;
+  Value SendMessage(
+      Fiber& fiber, StringId message_id, const ArgReader& args) const;
+
+  bool operator==(const Value& other) const {
+    return obj_ == other.obj_;
+  }
+
+  bool operator!=(const Value& other) const {
+    return obj_ != other.obj_;
+  }
+
+  Value& operator=(const Value& other);
+  
+  inline bool IsNil(void) const {
+    return nullptr == obj_;
+  }
+
+  void Clear(void);
+  const Value& Parent(void) const;
+  void Trace(std::ostream& cout) const;
+
+  double AsNumber(void) const;
+  String AsString(void) const;
+  ArrayObject* AsArray(void) const;
+  BlockObject* AsBlock(void) const;
+  DynamicObject* AsDynamic(void) const;
+  FiberObject* AsFiber(void) const;
+};
+
+std::ostream& operator<<(std::ostream& cout, const Value& value);
+
+class Object {
+  friend class Value;
+  Value parent_;
+  int   ref_count_;
+protected:
+  Object(const Value& parent)
+    : parent_(parent)
+    , ref_count_(1) {
+  }
+public:
+  virtual ~Object(void) {}
+
+  virtual double AsNumber(void) const {
+    return 0;
+  }
+
+  virtual String AsString(void) const {
+    return "";
+  }
+
+  virtual ArrayObject* AsArray(void) {
+    return nullptr;
+  }
+
+  virtual BlockObject* AsBlock(void) {
+    return nullptr;
+  }
+
+  virtual DynamicObject* AsDynamic(void) {
+    return nullptr;
+  }
+
+  virtual FiberObject* AsFiber(void) {
+    return nullptr;
+  }
+
+  inline const Value& Parent(void) const {
+    return parent_;
+  }
+
+  virtual void Trace(std::ostream& stream) const = 0;
 };
 
 }
