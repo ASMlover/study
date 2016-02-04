a62da1634a646573562c6a45040e8f7ebb7db466
ASMlover <asmlover@126.com>
2016-02-04 19:42:58 +0800

    chore(runner): the repl and running of pylike

--- scc/pylike/tyr_main.py
+++ scc/pylike/tyr_main.py
@@ -28,9 +28,45 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+import argparse
 import tyr_interpreter as interpreter
 
 tyr:
     input = raw_input
 except NameError:
     pass
+
+def parse_args():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('-v', '--verbose', action='store_true')
+    argparser.add_argument('file', nargs='?')
+    return argparser.parse_args()
+
+def interpret_file(path, verbose=False):
+    with open(path) as f:
+        print (interpreter.evaluate(f.read(), verbose=verbose))
+
+def repl():
+    print ('TYR 0.1. Press Ctrl+C to exit.')
+    env = interpreter.create_global_env()
+    buf = ''
+    try:
+        while True:
+            inp = input('>>> ' if not buf else '')
+            if inp == '':
+                print (interpreter.evaluate_env(buf, env))
+                buf = ''
+            else:
+                buf += '\n' + inp
+    except KeyboardInterrupt:
+        pass
+
+def main():
+    args = parse_args()
+    if args.file:
+        interpret_file(args.file, args.verbose)
+    else:
+        repl()
+
+if __name__ == '__main__':
+    main()
