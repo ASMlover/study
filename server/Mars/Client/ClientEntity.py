0db9d8c8fdfe5ad41f77a517ea60403c91ab10f0
ASMlover <asmlover@126.com>
2016-03-31 21:33:25 +0800

    chore(Client): updated the client entity

--- server/Mars/Client/ClientEntity.py
+++ server/Mars/Client/ClientEntity.py
@@ -39,3 +39,37 @@ from MarsLog.LogManager import LogManager
 class ClientEntity(object):
     def __init__(self, entityId=None):
         super(ClientEntity, self).__init__()
+        self.logger = LogManager.getLogger('ClientEntity.%s' % self.__class__.__name__)
+        self.logger.info('ClientEntity.__init__')
+        self.eid = (entityId is None) and IdCreator.genId() or entityId
+        EntityManager.addEntity(self.eid, self, False)
+        self.server = None
+
+    def setServer(self, server):
+        self.server = server
+
+    def onEntityCreation(self):
+        """通知Client这就是player"""
+        self.logger.info('ClientEntity.onEntityCreation: eid=%s', self.eid)
+
+    def onLoseServer(self):
+        """与Server断开连接"""
+        self.logger.info('ClientEntity.onLoseServer: eid=%s', self.eid)
+
+    def onDestroy(self):
+        pass
+
+    def initFromDict(self, dictData):
+        pass
+
+    def destroy(self):
+        self.onDestroy()
+        if self.server:
+            self.server.destroy()
+            self.server = None
+        self.logger = None
+        EntityManager.delEntity(self.eid)
+
+    def onReconnected(self, extraData):
+        """重连成功"""
+        self.logger.info('ClientEntity.onReconnected: eid=%s, extraData=%s', self.eid, extraData)
