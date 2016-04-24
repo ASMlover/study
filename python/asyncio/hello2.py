42a72e65f334fca2a7611b46695684ca528f406c
ASMlover <asmlover@126.com>
2016-04-24 18:34:01 +0800

    chore(asyncio): updated the demo of hello of asyncio

--- python/asyncio/hello2.py
+++ python/asyncio/hello2.py
@@ -37,7 +37,8 @@ def hello():
     yield from asyncio.sleep(1)
     print('Hello, again! (%s)' % threading.currentThread())
 
-loop = asyncio.get_event_loop()
-tasks = [hello(), hello()]
-loop.run_until_complete(asyncio.wait(tasks))
-loop.close()
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    tasks = [hello(), hello()]
+    loop.run_until_complete(asyncio.wait(tasks))
+    loop.close()
