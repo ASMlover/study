d87ab23b92b569606abee106801e9f80919833a9
ASMlover <asmlover@126.com>
2016-05-24 21:18:19 +0800

    chore(btool): updated the static library template

---
+++ python/btool/templates/Linux/static_lib.mk
@@ -0,0 +1,22 @@
+# static library makefile
+
+OUT = {out}
+RM = rm
+CC = g++
+AR = ar
+CFLAGS = -g{cflags}{inc_dir}
+OBJS = {objs}
+SRCS = {srcs}
+
+all: $(OUT)
+
+rebuild: clean all
+
+clean:
+	$(RM) $(OUT) $(OBJS)
+
+$(OUT): $(OBJS)
+	$(AR) -cru $@ $^
+
+$(OBJS): $(SRCS)
+	$(CC) -o $*.o -c $(CFLAGS) $^
