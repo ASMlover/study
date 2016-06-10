cfc19b30293eab6c23d4b679605376bcc900c61a
ASMlover <asmlover@126.com>
2016-06-10 20:37:08 +0800

    chore(eMake): add the Makefile template for linux

--- python/eMake/templates/executable.linux.mk
+++ python/eMake/templates/executable.linux.mk
@@ -1,1 +1,22 @@
 # executable makefile
+
+OUTOBJ = obj
+OUTBIN = bin
+TARGET = $(OUTBIN)/{target}.exe
+RM = rm -rf
+CC = g++
+CFLAGS = -g{cflags} {inc_dir}
+LDFLAGS = {ldflags}{link_dir}{link_libs}
+OBJS = {objs}
+
+all: $(TARGET)
+
+rebuild: clean all
+
+clean:
+	$(RM) $(OUTBIN)/*.* $(OUTOBJ)/*.*
+
+$(TARGET): $(OBJS)
+	$(CC) -o $@ $^ $(LDFLAGS)
+
+{emake_objs}
