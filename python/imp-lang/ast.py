1ac10c7879389795d524d2eb75246adc4e5928a8
ASMlover <asmlover@126.com>
2016-07-04 20:36:17 +0800

    chore(imp-lang): updated AST for imp-lang

--- python/imp-lang/ast.py
+++ python/imp-lang/ast.py
@@ -28,7 +28,7 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-class BaseAst(object):
+class BaseAST(object):
     def __eq__(self, other):
         return (isinstance(other, self.__class__)
                 and self.__dict__ == other.__dict__)
@@ -36,164 +36,167 @@ class BaseAst(object):
     def __ne__(self, other):
         return not self.__eq__(other)
 
-class StmtAst(BaseAst):
+class StmtAST(BaseAST):
     pass
 
-def ExprAst(BaseAst):
+def ExprAST(BaseAST):
     pass
 
-class AssignStmtAst(BaseAst):
+class AssignStmtAST(StmtAST):
     def __init__(self, name, expr):
         self.name = name
         self.expr = expr
 
     def __repr__(self):
-        return 'AssignStmtAst(%s, %s)' % (self.name, self.expr)
+        return 'AssignStmtAST(%s, %s)' % (self.name, self.expr)
 
     def eval(self, env):
         env[self.name] = self.expr.eval(env)
 
-class CompoundStmtAst(StmtAst):
+class CompoundStmtAST(StmtAST):
     def __init__(self, first, second):
         self.first = first
         self.second = second
 
     def __repr__(self):
-        return 'CompoundStmtAst(%s, %s)' % (self.first, self.second)
+        return 'CompoundStmtAST(%s, %s)' % (self.first, self.second)
 
     def eval(self, env):
         self.first.eval(env)
         self.second.eval(env)
 
-class IfStmtAst(StmtAst):
-    def __init__(self, cond, true_stmt, false_stmt):
+class IfStmtAST(StmtAST):
+    def __init__(self, cond, true_stmt, else_stmt):
         self.cond = cond
         self.true_stmt = true_stmt
-        self.false_stmt = false_stmt
+        self.else_stmt = else_stmt
 
     def __repr__(self):
-        return 'IfStmtAst(%s, %s, %s)' % (
-                self.cond, self.true_stmt, self.false_stmt)
+        return 'IfStmtAST(%s, %s, %s)' % (
+                self.cond, self.true_stmt, self.else_stmt)
 
     def eval(self, env):
         if self.cond.eval(env):
             self.true_stmt.eval(env)
         else:
-            self.false_stmt and self.false_stmt.eval(env)
+            self.else_stmt and self.else_stmt.eval(env)
 
-class WhileStmtAst(StmtAst):
+class WhileStmtAST(StmtAST):
     def __init__(self, cond, body):
         self.cond = cond
         self.body = body
 
     def __repr__(self):
-        return 'WhileStmtAst(%s, %s)' % (self.cond, self.body)
+        return 'WhileStmtAST(%s, %s)' % (self.cond, self.body)
 
     def eval(self, env):
         while self.cond.eval(env):
             self.body.eval(env)
 
-class IntExprAst(ExprAst):
-    def __init__(self, v):
-        self.v = v
+class IntExprAST(ExprAST):
+    def __init__(self, value):
+        self.value = value
 
     def __repr__(self):
-        return 'IntExprAst(%d)' % self.v
+        return 'IntExprAST(%d)' % self.value
 
     def eval(self, env):
-        return self.v
+        return self.value
 
-class VariableExprAst(ExprAst):
+class VariableExprAST(ExprAST):
     def __init__(self, name):
         self.name = name
 
     def __repr__(self):
-        return 'VariableExprAst(%s)' % self.name
+        return 'VariableExprAST(%s)' % self.name
 
     def eval(self, env):
         return env.get(self.name, 0)
 
-class BinOperExprAst(ExprAst):
-    def __init__(self, op, left, right):
+class BinaryOperExprAST(ExprAST):
+    def __init__(self, op, lexpr, rexpr):
         self.op = op
-        self.left = left
-        self.right = right
+        self.lexpr = lexpr
+        self.rexpr = rexpr
 
     def __repr__(self):
-        return 'BinOperExprAst(%s, %s, %s)' % (
-                self.op, self.left, self.right)
+        return 'BinaryOperExprAST(%s, %s, %s)' % (
+                self.op, self.lexpr, self.rexpr)
 
     def eval(self, env):
-        lval = self.left.eval(env)
-        rval = self.right.eval(env)
+        lvalue = self.lexpr.eval(env)
+        rvalue = self.rexpr.eval(env)
         if self.op == '+':
-            val = lval + rval
+            value = lvalue + rvalue
         elif self.op == '-':
-            val = lval - rval
+            value = lvalue - rvalue
         elif self.op == '*':
-            val = lval * rval
+            value = lvalue * rvalue
         elif self.op == '/':
-            val = lval / rval
+            if rvalue == 0:
+                raise ZeroDivisionError('%s is zero' % self.rexpr)
+            value = lvalue / rvalue
         else:
             raise RuntimeError('Unknown operator: %s' % self.op)
-        return val
+        return value
 
-class LogicExprAst(ExprAst):
-    def __init__(self, op, left, right):
+class LogicOperExprAST(ExprAST):
+    def __init__(self, op, lexpr, rexpr):
         self.op = op
-        self.left = left
-        self.right = right
+        self.lexpr = lexpr
+        self.rexpr = rexpr
 
     def __repr__(self):
-        return 'LogicExprAst(%s, %s, %s)' % (self.op, self.left, self.right)
+        return 'LogicOperExprAST(%s, %s, %s)' % (
+                self.op, self.lexpr, self.rexpr)
 
     def eval(self, env):
-        lval = self.left.eval(env)
-        rval = self.right.eval(env)
+        lvalue = self.lexpr.eval(env)
+        rvalue = self.rexpr.eval(env)
         if self.op == '<':
-            val = lval < rval
+            value = lvalue < rvalue
         elif self.op == '<=':
-            val = lval <= rval
+            value = lvalue <= rvalue
         elif self.op == '>':
-            val = lval > rval
+            value = lvalue > rvalue
         elif self.op == '>=':
-            val = lval >= rval
+            value = lvalue >= rvalue
         elif self.op == '==':
-            val = lval == rval
+            value = lvalue == rvalue
         elif self.op == '!=':
-            val = lval != rval
+            value = lvalue != rvalue
         else:
             raise RuntimeError('Unknown operator: %s' % self.op)
-        return val
+        return value
 
-class AndExprAst(ExprAst):
-    def __init__(self, left, right):
-        self.left = left
-        self.right = right
-
-    def __repr__(self):
-        return 'AndExprAst(%s, %s)' % (self.left, self.right)
-
-    def eval(self, env):
-        return self.left.eval(env) and self.right.eval(env)
-
-class OrExprAst(ExprAst):
-    def __init__(self, left, right):
-        self.left = left
-        self.right = right
+class AndExprAST(ExprAST):
+    def __init__(self, lexpr, rexpr):
+        self.lexpr = lexpr
+        self.rexpr = rexpr
 
     def __repr__(self):
-        return 'OrExprAst(%s, %s)' % (self.left, self.right)
+        return 'AndExprAST(%s, %s)' % (self.lexpr, self.rexpr)
 
     def eval(self, env):
-        return self.left.eval(env) or self.right.eval(env)
+        return self.lexpr.eval(env) and self.rexpr.eval(env)
 
-class NotExprAst(ExprAst):
+class OrExprAST(ExprAST):
+    def __init__(self, lexpr, rexpr):
+        self.lexpr = lexpr
+        self.rexpr = rexpr
+
+    def __repr__(self):
+        return 'OrExprAST(%s, %s)' % (self.lexpr, self.rexpr)
+
+    def eval(self, env):
+        return self.lexpr.eval(env) or self.rexpr.eval(env)
+
+class NotExprAST(ExprAST):
     def __init__(self, expr):
         self.expr = expr
 
     def __repr__(self):
-        return 'NotExprAst(%s)' % self.expr
+        return 'NotExprAST(%s)' % self.expr
 
     def eval(self, env):
         return not self.expr.eval(env)
