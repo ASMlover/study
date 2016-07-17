927a5ca2212555ea136ba65b095c8e34c7268443
ASMlover <asmlover@126.com>
2016-07-17 18:22:40 +0800

    chore(vm): updated the vm implementation

--- python/py-interpreter/tiny/tiny3.py
+++ python/py-interpreter/tiny/tiny3.py
@@ -28,7 +28,10 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+import collections
+import dis
 import inspect
+import sys
 import types
 
 class Stack(list):
@@ -45,6 +48,9 @@ class Stack(list):
 
     def top(self):
         return self[-1]
+
+    def size(self):
+        return len(self)
 
 class VMError(Exception):
     pass
@@ -80,11 +86,20 @@ class Frame(object):
     def _code_top(self):
         return self.code_stack.top()
 
+    def _code_size(self):
+        return self.code_stack.size()
+
     def _block_push(self, block):
         self.block_stack.push(block)
 
     def _block_pop(self):
         return self.block_stack.pop()
+
+    def _block_top(self):
+        return self.block_stack.top()
+
+    def _block_size(self):
+        return self.block_stack.size()
 
 class Function(object):
     __slots__ = [
@@ -125,6 +140,8 @@ class Function(object):
 def make_cell(value):
     fun = (lambda x: lambda: x)(value)
     return fun.__closure__[0]
+
+Block = collections.namedtuple('Block', 'type, handler, stack_height')
 
 class VM(object):
     def __init__(self):
@@ -185,8 +202,95 @@ class VM(object):
         else:
             self.frame = None
 
-    def run_frame(self):
-        pass
+    def run_frame(self, frame):
+        self.push_frame(frame)
+        while True:
+            byte_name, arguments = self.parse_byte_and_args()
+            why = self.dispatch(byte_name, arguments)
+            while why and frame.block_stack:
+                why = self.manage_block_stack(why)
+
+            if why:
+                break
+        self.pop_frame()
+
+        if why == 'exception':
+            exc, val, tb = self.last_exception
+            e = exc(val)
+            e.__traceback__ = tb
+            raise e
+        return self.return_value
+
+    def parse_byte_and_args(self):
+        f = self.frame
+        op_offset = f.last_instruction
+        byte_code = f.code_obj.co_code[op_offset]
+        f.last_instruction += 1
+        byte_name = dis.opname[byte_code]
+        if byte_code >= dis.HAVE_ARGUMENT:
+            arg = f.code_obj.co_code[f.last_instruction:f.last_instruction+2]
+            f.last_instruction += 2
+            arg_val = arg[0] + (arg[1] * 256)
+            if byte_code in dis.hasconst:
+                arg = f.code_obj.co_consts[arg_val]
+            elif byte_code in dis.hasname:
+                arg = f.code_obj.co_names[arg_val]
+            elif byte_code in dis.haslocal:
+                arg = f.code_obj.co_varnames[arg_val]
+            elif byte_code in dis.hasjrel:
+                arg = f.last_instruction + arg_val
+            else:
+                arg = arg_val
+            argument = [arg]
+        else:
+            argument = []
+
+        return byte_name, argument
+
+    def dispatch(self, byte_name, argument):
+        why = None
+        try:
+            bytecode_fn = getattr(self, '_ir_%s' % byte_name, None)
+            if bytecode_fn is None:
+                if byte_name.startswith('unary_'):
+                    self.unary_operation(byte_name[6:])
+                elif byte_name.startswith('binary_'):
+                    self.binary_operation(byte_name[7:])
+                else:
+                    raise VMError("unsupported bytecode type: %s" % byte_name)
+            else:
+                why = bytecode_fn(*argument)
+        except:
+            self.last_exception = sys.exc_info()[:2] + (None,)
+            why = 'exception'
+        return why
+
+    def push_block(self, b_type, handler=None):
+        stack_height = self.frame._block_size()
+        self.frame._block_push(Block(b_type, handler, stack_height))
+
+    def pop_block(self):
+        return self.frame._block_pop()
+
+    def unwind_block(self, block):
+        if block.type == 'except-handler':
+            offset = 3
+        else:
+            offset = 0
+
+        while self.frame._code_size() > block.level + offset:
+            traceback, value, exctype = self._frame_code_popn(3)
+            self.last_exception = exctype, value, traceback
+
+    def manage_block_stack(self, why):
+        frame = self.frame
+        block = frame._block_top()
+        if block.type == 'loop' and why == 'continue':
+            self.jump(self.return_value)
+            why = None
+            return why
+        self.pop_block()
+        self.unwind_block(block)
 
 def main():
     pass
