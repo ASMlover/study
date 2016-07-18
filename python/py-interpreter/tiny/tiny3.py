fe51135ac14d20ac00e3b4bfa13f73300baa497e
ASMlover <asmlover@126.com>
2016-07-18 22:35:24 +0800

    chore(vm): updated the bytecode implementation of vm

--- python/py-interpreter/tiny/tiny3.py
+++ python/py-interpreter/tiny/tiny3.py
@@ -31,6 +31,7 @@
 import collections
 import dis
 import inspect
+import operator as op
 import sys
 import types
 
@@ -162,11 +163,11 @@ class VM(object):
     def _frame_code_top(self):
         return self.frame._code_top()
 
-    def _frame_code_pop(self):
-        return self.frame._code_pop()
-
     def _frame_code_pushn(self, *vals):
         self.frame._code_pushn(*vals)
+
+    def _frame_code_pop(self):
+        return self.frame._code_pop()
 
     def _frame_code_popn(self, n):
         if n:
@@ -291,6 +292,189 @@ class VM(object):
             return why
         self.pop_block()
         self.unwind_block(block)
+
+        if block.type == 'loop' and why == 'break':
+            why = None
+            self.jump(block.handler)
+            return why
+
+        if block.type in ['setup-except', 'finally'] and why == 'exception':
+            self.push_block('except-handler')
+            exctype, value, tb = self.last_exception
+            self._frame_code_pushn(tb, value, exctype)
+            self._frame_code_pushn(tb, value, exctype)
+            why = None
+            self.jump(block.handler)
+            return why
+        elif block.type == 'finally':
+            if why in ('return', 'continue'):
+                self._frame_code_pushn(self.return_value)
+            self._frame_code_pushn(why)
+            why = None
+            self.jump(block.handler)
+            return why
+        return why
+
+    def _ir_LOAD_CONST(self, const):
+        self._frame_code_pushn(const)
+
+    def _ir_POP_TOP(self):
+        self._frame_code_pop()
+
+    def _ir_LOAD_NAME(self, name):
+        frame = self.frame
+        if name in frame.f_locals:
+            val = frame.f_locals[name]
+        elif name in frame.f_globals:
+            val = frame.f_globals[name]
+        elif name in frame.f_builtins:
+            val = frame.f_builtins[name]
+        else:
+            raise NameError('name "%s" is not defined' % name)
+        self._frame_code_pushn(val)
+
+    def _ir_STORE_NAME(self, name):
+        self.frame.f_locals[name] = self._frame_code_pop()
+
+    def _ir_LOAD_FAST(self, name):
+        if name in self.frame.f_locals:
+            val = self.frame.f_locals[name]
+        else:
+            raise UnboundLocalError(
+                    'local var "%s" referenced before assignment' % name)
+            self._frame_code_pushn(val)
+
+    def _ir_STORE_FAST(self, name):
+        self.frame.f_locals[name] = self._frame_code_pop()
+
+    def _ir_LOAD_GLOBAL(self, name):
+        f = self.frame
+        if name in f.f_globals:
+            val = f.f_globals[name]
+        elif name in f.f_builtins:
+            val = f.f_builtins[name]
+        else:
+            raise NameError('global name "%s" if not defined' % name)
+        self._frame_code_pushn(val)
+
+    BINARY_OPERATORS = {
+        'POWER': pow,
+        'MULTIPLY': op.mul,
+        'FLOOR_DIVIDE': op.floordiv,
+        'TRUE_DIVIDE': op.truediv,
+        'MODULO': op.mod,
+        'ADD': op.add,
+        'SUBTRACT': op.sub,
+        'SUBSCR': op.getitem,
+        'LSHIFT': op.lshift,
+        'RSHIFT': op.rshift,
+        'AND': op.and_,
+        'XOR': op.xor,
+        'OR': op.or_,
+    }
+    def binary_operation(self, op_type):
+        x, y = self._frame_code_popn(2)
+        self._frame_code_pushn(self.BINARY_OPERATORS[op_type](x, y))
+
+    COMPARE_OPERATORS = [
+        op.lt,
+        op.le,
+        op.eq,
+        op.ne,
+        op.gt,
+        op.ge,
+        lambda x, y: x in y,
+        lambda x, y: x not in y,
+        lambda x, y: x is y,
+        lambda x, y: x is not y,
+        lambda x, y: issubclass(x, Exception) and issubclass(x, y),
+    ]
+    def _ir_COMPARE_OP(self, opnum):
+        x, y = self._frame_code_popn(2)
+        self._frame_code_pushn(self.COMPARE_OPERATORS[opnum](x, y))
+
+    def _ir_LOAD_ATTR(self, attr):
+        obj = self._frame_code_pop()
+        val = getattr(obj, attr)
+        self._frame_code_pushn(val)
+
+    def _ir_STORE_ATTR(self, name):
+        val, obj = self._frame_code_popn(2)
+        setattr(obj, name, val)
+
+    def _ir_BUILD_LIST(self, count):
+        elems = self._frame_code_popn(count)
+        self._frame_code_pushn(elems)
+
+    def _ir_BUILD_MAP(self, size):
+        self._frame_code_pushn({})
+
+    def _ir_STORE_MAP(self):
+        the_map, val, key = self._frame_code_popn(3)
+        the_map[key] = val
+        self._frame_code_pushn(the_map)
+
+    def _ir_LIST_APPEND(self, count):
+        val = self._frame_code_pop()
+        the_list = self.frame.code_stack[-count]
+        the_list.append(val)
+
+    def _ir_JUMP_FORWARD(self, jump):
+        self.jump(jump)
+
+    def _ir_JUMP_ABSOLUTE(self, jump):
+        self.jump(jump)
+
+    def _ir_POP_JUMP_IF_TRUE(self, jump):
+        val = self._frame_code_pop()
+        if val:
+            self.jump(jump)
+
+    def _ir_POP_JUMP_IF_FALSE(self, jump):
+        val = self._frame_code_pop()
+        if not val:
+            self.jump(jump)
+
+    def _ir_SETUP_LOOP(self, dest):
+        self.push_block('loop', dest)
+
+    def _ir_GET_ITER(self):
+        self._frame_code_pushn(iter(self._frame_code_pop()))
+
+    def _ir_FOR_ITER(self, jump):
+        iterobj = self._frame_code_top()
+        try:
+            val = next(iterobj)
+            self._frame_code_pushn(val)
+        except StopIteration:
+            self._frame_code_pop()
+            self.jump(jump)
+
+    def _ir_BREAK_LOOP(self):
+        return 'break'
+
+    def _ir_POP_BLOCK(self):
+        self.pop_block()
+
+    def _ir_MAKE_FUNCTION(self, argc):
+        name = self._frame_code_pop()
+        code = self._frame_code_pop()
+        defaults = self._frame_code_popn(argc)
+        globs = self.frame.f_globals
+        fn = Function(name, code, globs, defaults, None, self)
+        self._frame_code_pushn(fn)
+
+    def _ir_CALL_FUNCTION(self, arg):
+        len_lw, len_pos = divmod(arg, 256)
+        pos_args = self._frame_code_popn(len_pos)
+
+        fn = self._frame_code_pop()
+        val = fn(*pos_args)
+        self._frame_code_pushn(val)
+
+    def _ir_RETURN_VALUE(self):
+        self.return_value = self._frame_code_pop()
+        return 'return'
 
 def main():
     pass
