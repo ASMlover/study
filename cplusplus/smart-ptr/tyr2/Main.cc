cf95a29463effcafe13a073bc49ee9e722cf15d6
ASMloer <asmlover@126.com>
2016-09-07 19:43:56 +0800

    chore(tyr): add shared count and smart pointer implementation

--- cplusplus/smart-ptr/tyr2/Main.cc
+++ cplusplus/smart-ptr/tyr2/Main.cc
@@ -24,7 +24,7 @@
 // LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
-#include "Tyr.h"
+#include "TSmartPtr.h"
 
 int main(int argc, char* argv[]) {
   UNUSED(argc)
