6dda1443c1c0e3e0416e4df889fa393edbadad20
ASMlover <asmlover@126.com>
2016-10-08 22:18:30 +0800

    feat(platform): add platform utils functions for windows platform

---
+++ cplusplus/tyr2/basic/win/TPlatformUtils.cc
@@ -0,0 +1,52 @@
+// Copyright (c) 2016 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#include <Windows.h>
+#include <stdint.h>
+#include "TPlatformUtils.h"
+
+namespace tyr { namespace basic {
+
+static const uint64_t kEpoch = 116444736000000000ULL;
+
+int gettimeofday(struct timeval* tv, struct timezone* /*tz*/) {
+  if (tv) {
+    FILETIME ft;
+    SYSTEMTIME st;
+    ULARGE_INTEGER uli;
+
+    GetSystemTime(&st);
+    SystemTimeToFileTime(&st, &ft);
+    uli.LowPart = ft.dwLowDateTime;
+    uli.HighPart = ft.dwHighDateTime;
+
+    tv->tv_sec = (long)((uli.QuadPart - kEpoch) / 10000000L);
+    tv->tv_usec = (long)(st.wMilliseconds * 1000);
+  }
+  return 0;
+}
+
+}}
