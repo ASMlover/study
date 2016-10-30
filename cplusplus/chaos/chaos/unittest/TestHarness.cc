62c126d9adc6efd3c72ac7fa2dc7910d04293957
ASMlover <asmlover@126.com>
2016-10-30 21:50:46 +0800

    feat(chaos.unittest): add unittest module for chaos

---
+++ cplusplus/chaos/chaos/unittest/TestHarness.cc
@@ -0,0 +1,73 @@
+// Copyright (c) 2016 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#include <memory>
+#include <vector>
+#include "TestHarness.h"
+
+namespace chaos {
+
+struct HarnessContext {
+  const char* base;
+  const char* name;
+  void (*closure)(void);
+
+  HarnessContext(const char* b, const char* n, void (*fn)(void))
+    : base(b)
+    , name(n)
+    , closure(fn) {
+  }
+};
+typedef std::vector<HarnessContext> HarnessContextVector;
+std::unique_ptr<HarnessContextVector> g_tests;
+
+bool register_testharness(const char* base, const char* name, void (*closure)(void)) {
+  if (!g_tests)
+    g_tests.reset(new HarnessContextVector);
+
+  HarnessContext hc(base, name, closure);
+  g_tests->push_back(hc);
+
+  return true;
+}
+
+int run_all_testharness(void) {
+  int total_tests = 0;
+  int passed_tests = 0;
+
+  if (g_tests && !g_tests->empty()) {
+    total_tests = static_cast<int>(g_tests->size());
+
+    for (auto& hc : *g_tests) {
+      hc.closure();
+      ++passed_tests;
+    }
+  }
+  fprintf(stdout, "========== PASSED (%d/%d) test harness\n", passed_tests, total_tests);
+  return 0;
+}
+
+}
---
+++ cplusplus/chaos/chaos/unittest/TestHarness.h
@@ -0,0 +1,117 @@
+// Copyright (c) 2016 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#ifndef CHAOS_UNITTEST_TESTHARNESS_H
+#define CHAOS_UNITTEST_TESTHARNESS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sstream>
+
+namespace chaos {
+
+class FakeTester {};
+
+class Tester {
+  bool ok_;
+  const char* fname_;
+  int lineno_;
+  std::stringstream ss_;
+public:
+  Tester(const char* fname, int lineno)
+    : ok_(true)
+    , fname_(fname)
+    , lineno_(lineno) {
+  }
+
+  ~Tester(void) {
+    if (!ok_) {
+      fprintf(stderr, "%s:%d - %s\n", fname_, lineno_, ss_.str().c_str());
+      exit(1);
+    }
+  }
+
+  Tester& is_true(bool b, const char* msg) {
+    if (!b) {
+      ss_ << " Assection failure " << msg;
+      ok_ = false;
+    }
+    return *this;
+  }
+
+#define BINARY_OP(name, op)\
+  template <typename X, typename Y>\
+  Tester& name(const X& x, const Y& y) {\
+    if (!(x op y)) {\
+      ss_ << " failed: " << x << (" " #op " " ) << y;\
+      ok_ = false;\
+    }\
+    return *this;\
+  }
+
+  BINARY_OP(is_eq, ==)
+  BINARY_OP(is_ne, !=)
+  BINARY_OP(is_ge, >=)
+  BINARY_OP(is_gt, >)
+  BINARY_OP(is_le, <=)
+  BINARY_OP(is_lt, <)
+#undef BINARY_OP
+
+  template <typename V>
+  Tester& operator<<(const V& value) {
+    if (!ok_)
+      ss_ << " " << value;
+    return *this;
+  }
+};
+
+#define CHAOS_CHECK_TRUE(c)   chaos::Tester(__FILE__, __LINE__).is_true((c), #c)
+#define CHAOS_CHECK_EQ(a, b)  chaos::Tester(__FILE__, __LINE__).is_eq((a), (b))
+#define CHAOS_CHECK_NE(a, b)  chaos::Tester(__FILE__, __LINE__).is_ne((a), (b))
+#define CHAOS_CHECK_GE(a, b)  chaos::Tester(__FILE__, __LINE__).is_ge((a), (b))
+#define CHAOS_CHECK_GT(a, b)  chaos::Tester(__FILE__, __LINE__).is_gt((a), (b))
+#define CHAOS_CHECK_LE(a, b)  chaos::Tester(__FILE__, __LINE__).is_le((a), (b))
+#define CHAOS_CHECK_LT(a, b)  chaos::Tester(__FILE__, __LINE__).is_lt((a), (b))
+
+bool register_testharness(const char* base, const char* name, void (*closure)(void));
+int run_all_testharness(void);
+
+}
+
+#define CHAOS_TEST(Name, Base)\
+class _TestHarness_##Name : public Base {\
+public:\
+  void _run(void);\
+  static void _run_harness(void) {\
+    _TestHarness_##Name t;\
+    t._run();\
+  }\
+};\
+static bool _TestHarness_##Name_ignored =\
+  chaos::register_testharness(#Base, #Name, &_TestHarness_##Name::_run_harness);\
+void _TestHarness_##Name::_run(void)
+
+#endif // CHAOS_UNITTEST_TESTHARNESS_H
