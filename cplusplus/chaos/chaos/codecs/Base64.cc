dc1800f6114dfd1702de49c298c08bc721ca9c5f
ASMlover <asmlover@126.com>
2016-11-12 22:13:15 +0800

    chore(chaos.codecs): updated the implementation of base64 module

--- cplusplus/chaos/chaos/codecs/Base64.cc
+++ cplusplus/chaos/chaos/codecs/Base64.cc
@@ -43,11 +43,48 @@ namespace Base64 {
   }
 
   std::string encode(const std::string& s) {
-    return "";
+    std::string r;
+
+    const byte_t* b = reinterpret_cast<const byte_t*>(s.c_str());
+    int n = static_cast<int>(s.size());
+    for (int i = 0; i < n; i += 3) {
+      r.push_back(kEncode64[b[i] >> 2]);
+
+      if (i + 1 < n) {
+        r.push_back(kEncode64[((b[i] & 0x03) << 4) | (b[i + 1] >> 4)]);
+      }
+      else {
+        r.push_back(kEncode64[(b[i] & 0x03) << 4]);
+        r.push_back('=');
+        r.push_back('=');
+      }
+
+      if (i + 2 < n) {
+        r.push_back(kEncode64[((b[i + 1] & 0x0f) << 2) | (b[i + 2] >> 6)]);
+      }
+      else {
+        r.push_back(kEncode64[(b[i + 1] & 0x0f) << 2]);
+        r.push_back('=');
+      }
+    }
+
+    return r;
   }
 
   std::string decode(const std::string& s) {
-    return "";
+    std::string r;
+
+    const byte_t* b = reinterpret_cast<const byte_t*>(s.c_str());
+    int n = static_cast<int>(s.size());
+    for (int i = 0; i < n; i += 4) {
+      r.push_back((kDecode64[b[i]] << 2) | ((kDecode64[b[i + 1]] >> 4) & 0x03));
+      if (b[i + 2] != '=')
+        r.push_back((kDecode64[b[i + 1]] << 4) | ((kDecode64[b[i + 2]] >> 2) & 0x0f));
+      if (b[i + 3] != '=')
+        r.push_back((kDecode64[b[i + 2]] << 6) | (kDecode64[b[i + 3]] & 0x3f));
+    }
+
+    return r;
   }
 }
 
