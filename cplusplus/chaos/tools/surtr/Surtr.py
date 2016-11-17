50b192382532ccf1efe3721c49f0f7493135ca8d
ASMlover <asmlover@126.com>
2016-11-17 22:36:57 +0800

    chore(chaos.surtr): updated the implementation of surtr tool for building C/C++

--- cplusplus/chaos/tools/surtr/Surtr.py
+++ cplusplus/chaos/tools/surtr/Surtr.py
@@ -37,6 +37,8 @@ import shutil
 import subprocess
 import sys
 
+OUTDIR = 'build'
+
 if sys.version_info.major < 3:
     import codecs
     def do_open(fname, mode='rb', encoding=None):
@@ -65,23 +67,24 @@ def safe_rm(path):
 def get_options():
     parser = argparse.ArgumentParser(description='Surtr C/C++ building tool')
     parser.add_argument('option', nargs='?', help='config|build|rebuild|clean|remove the project')
+    parser.add_argument('surtr_path', nargs='?', help='relative path of `Surtr`')
     args = parser.parse_args()
-    return args.option
+    return args.option, args.surtr_path
 
-def get_conf(tool_dir='./', platform='linux'):
+def get_conf(tool_path='./', platform='linux'):
     ptname = 'posix'
     if platform == 'windows':
         ptname = 'windows'
 
-    surtr_dir = '{tool_dir}/surtr/templates'.format(tool_dir=tool_dir)
+    surtr_path = '{tool_path}/surtr/templates'.format(tool_path=tool_path)
 
     # get building Makefile template
-    fname = '{surtr_dir}/build.{pt}.mk'.format(surtr_dir=surtr_dir, pt=ptname)
+    fname = '{surtr_path}/build.{pt}.mk'.format(surtr_path=surtr_path, pt=ptname)
     with do_open(fname, 'r', encoding='utf-8') as fp:
         build_mk = fp.read()
 
     # get object generation template
-    fname = '{surtr_dir}/obj.{pt}.mk'.format(surtr_dir=surtr_dir, pt=ptname)
+    fname = '{surtr_path}/obj.{pt}.mk'.format(surtr_path=surtr_path, pt=ptname)
     with do_open(fname, 'r', encoding='utf-8') as fp:
         obj_mk = fp.read()
 
@@ -90,56 +93,139 @@ def get_conf(tool_dir='./', platform='linux'):
         with do_open('surtr.conf', 'r', encoding='utf-8') as fp:
             build_conf = json.load(fp)
     except Exception:
-        fname = '{surtr_dir}/default.{pt}.conf'.format(surtr_dir=surtr_dir, pt=ptname)
+        fname = '{surtr_path}/default.{pt}.conf'.format(surtr_path=surtr_path, pt=ptname)
         with do_open(fname, 'r', encoding='utf-8') as fp:
             build_conf = json.load(fp)
     return build_mk, obj_mk, build_conf
 
-def get_sources_for_dir(dirpath, recursive=True):
+def get_sources_for_dir(dirpath, valid_exts, recursive=True, exclusive=[]):
     cur_sources = os.listdir(dirpath)
     all_sources = []
     for source_name in cur_sources:
         source_fullpath = os.path.join(dirpath, source_name).replace('\\', '/')
-        if os.path.isdir(source_fullpath) and recursive:
-            all_sources.extend(get_sources_for_dir(source_fullpath))
+        if os.path.isdir(source_fullpath) and recursive and source_name not in exclusive:
+            all_sources.extend(get_sources_for_dir(source_fullpath, valid_exts, recursive, exclusive))
         else:
-            if os.path.splitext(source_name)[1][1:] in ('cpp', 'cc'):
+            if os.path.splitext(source_name)[1][1:] in valid_exts:
                 all_sources.append(source_fullpath)
     return all_sources
 
-def get_all_sources(dirs=(('./', True),)):
+def get_all_sources(sources_paths, valid_exts=('cpp', 'cc', 'cxx')):
     all_sources = []
-    for path, recursive in dirs:
-        all_sources.extend(get_sources_for_dir(path, recursive))
+    includes_dirs = sources_paths['includes']
+    exclusive = sources_paths['exclusive']
+    for sources_dir in includes_dirs:
+        all_sources.extend(get_sources_for_dir(sources_dir['path'], valid_exts, sources_dir['recursive'], exclusive))
     return all_sources
 
 def gen_outobj(source_fname, posix=True):
     s = source_fname.strip('./').strip('../').replace('/', '.')
     objname = os.path.splitext(s)[0]
     if posix:
-        outobj_format = '$(OUTDIR)/$(OUTOBJ)/{objname}.o'
+        outobj_format = '$(OUTDIR)/$(OUTOBJ)/{objname}.o '
     else:
-        outobj_format = '$(OUTDIR)\$(OUTOBJ)\{objname}.obj'
+        outobj_format = '$(OUTDIR)\$(OUTOBJ)\{objname}.obj '
     return outobj_format.format(objname=objname)
 
-def gen_makefile(platform='linux', target='a.out', outdir='build', sources=[]):
-    module = sys.modules['__main__']
+def gen_buildobj(conf, out, src):
+    return conf.format(build_obj=out.strip(), build_src=src)
+
+def gen_cflags(build_conf):
+    cflags = []
+    for option in build_conf['precompile_options']:
+        cflags.append('-D{opt} '.format(opt=option))
+    for option in build_conf['compile_options']:
+        cflags.append('-{opt} '.format(opt=option))
+    for option in build_conf['extra_options']:
+        cflags.append('-{opt} '.format(opt=option))
+    return ''.join(cflags).rstrip()
+
+def gen_includes(build_conf, posix=True):
+    includes = []
+    for inc in build_conf['compile_includes']:
+        if posix:
+            includes.append('-I{inc} '.format(inc=inc))
+        else:
+            includes.append('-I"{inc}" '.format(inc=inc.replace('/', '\\')))
+    return ''.join(includes).rstrip()
+
+def gen_ldflags(build_conf, posix=True):
+    ldincludes = []
+    ldlibraries = []
+    for inc in build_conf['link_includes']:
+        if posix:
+            ldincludes.append('-L{inc} '.format(inc=inc))
+        else:
+            ldincludes.append('-LIBPATH:"{inc}" '.format(inc=inc.replace('/', '\\')))
+
+    if posix:
+        for lib in build_conf['link_libraries']:
+            if not lib.endswith('.a') and not lib.endswith('.so'):
+                lib = lib.replace('lib', '-l')
+            ldlibraries.append(lib)
+    else:
+        ldlibraries = build_conf['link_libraries']
+    ldlibraries = ('{lib} '.format(lib=lib) for lib in ldlibraries)
+    return '{incs}{libs}'.format(incs=''.join(ldincludes), libs=''.join(ldlibraries).rstrip())
+
+def gen_makefile(surtr_path='./', platform='linux', outdir='build'):
+    is_posix = platform != 'windows'
+    build_mk, obj_mk, build_conf = get_conf(surtr_path, platform)
+    sources = get_all_sources(build_conf['sources_paths'], valid_exts=build_conf['extensions'])
 
     objs_list = []
-    makeobjs_list = []
+    buildobjs_list = []
     for s in sources:
-        objstr = gen_outobj(s, platform != 'windows')
+        objstr = gen_outobj(s, is_posix)
         objs_list.append(objstr)
-        makeobjs_list.append()
+        buildobjs_list.append(gen_buildobj(obj_mk, objstr, s))
+    build_dict = dict(
+        outdir = outdir,
+        target = build_conf['target'],
+        cflags = gen_cflags(build_conf),
+        includes = gen_includes(build_conf, is_posix),
+        ldflags = gen_ldflags(build_conf, is_posix),
+        objs = ''.join(objs_list).rstrip(),
+        build_objs = ''.join(buildobjs_list).rstrip(),
+    )
+
+    with do_open('Makefile', 'w', encoding='utf-8') as fp:
+        fp.write(build_mk.format(**build_dict))
+
+def gen_outdir(outdir=OUTDIR):
+    safe_mkdir(outdir)
+    safe_mkdir('{outdir}/bin'.format(outdir=outdir))
+    safe_mkdir('{outdir}/obj'.format(outdir=outdir))
 
 def main():
-    # Just for testing
-    bmk, omk, bconf = get_conf('../')
-    print (bmk)
-    print ('********************************************************************')
-    print (omk)
-    print ('********************************************************************')
-    print (bconf)
+    option, surtr_path = get_options()
+    if option == 'remove':
+        safe_rm(OUTDIR)
+        return
+
+    if surtr_path is None:
+        surtr_path = '../'
+
+    platform = get_platform()
+    gen_outdir()
+    gen_makefile(surtr_path=surtr_path, platform=platform, outdir=OUTDIR)
+
+    if option == 'config':
+        return
+    elif option is None:
+        option = 'build'
+
+    make = platform == 'windows' and 'nmake' or 'make'
+    make_flags = {
+        'build': '',
+        'rebuild': 'rebuild',
+        'clean': 'clean'
+    }
+    flag = make_flags.get(option)
+    if flag is None:
+        safe_rm(OUTDIR)
+    else:
+        subprocess.check_call('{make} {flag}'.format(make=make, flag=flag), shell=True)
 
 if __name__ == '__main__':
     main()
