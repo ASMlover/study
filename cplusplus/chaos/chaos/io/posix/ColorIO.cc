ef180cc84cecfe3fa29316bdfad170a08f0c22dc
ASMlover <asmlover@126.com>
2016-11-24 21:57:23 +0800

    chore(chaos.io): finished colcor io for posix

---
+++ cplusplus/chaos/chaos/io/posix/ColorIO.cc
@@ -0,0 +1,58 @@
+// Copyright (c) 2016 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#include <chaos/error/SystemError.h>
+#include <chaos/concurrent/Mutex.h>
+#include <chaos/io/ColorIO.h>
+
+namespace chaos {
+
+namespace ColorIO {
+  static chaos::Mutex g_color_mutex;
+
+  int vfprintf(FILE* stream, ColorType color, const char* format, va_list ap) {
+    chaos::ScopedLock<chaos::Mutex> guard(g_color_mutex);
+
+    switch (color) {
+    case ColorType::COLORTYPE_INVALID:
+      __chaos_throw_exception(std::logic_error("invalid color type"));
+      break;
+    case ColorType::COLORTYPE_RED:
+      fprintf(stream, "\033[31;1m");
+      break;
+    case ColorType::COLORTYPE_GREEN:
+      fprintf(stream, "\033[32;1m");
+      break;
+    }
+
+    int n = ::vfprintf(stream, format, ap);
+    fprintf(stream, "\033[0m");
+
+    return n;
+  }
+}
+
+}
