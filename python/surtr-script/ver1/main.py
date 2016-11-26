95610cdc97782584c839cb94ace0360831f946b0
ASMlover <asmlover@126.com>
2016-11-27 02:10:04 +0800

    chore(surtr.script): add function caller of surtr script

--- python/surtr-script/ver1/main.py
+++ python/surtr-script/ver1/main.py
@@ -38,6 +38,7 @@ TYPE_IDENTIFIER = 2
 TYPE_LPAREN = 3 # (
 TYPE_RPAREN = 4 # )
 TYPE_COMMENT = 5 # comment begin with `#`
+TYPE_COMMA = 6 # `,`
 
 TYPE_NAME_MAPS = {
     TYPE_INVALID: '<invalid>',
@@ -46,6 +47,7 @@ TYPE_NAME_MAPS = {
     TYPE_IDENTIFIER: '<identifier>',
     TYPE_LPAREN: '<left paren>',
     TYPE_RPAREN: '<right paren>',
+    TYPE_COMMA: '<comma>',
 }
 
 RESERVED_MAPS = {
@@ -140,14 +142,75 @@ class Lexer(object):
                 return Token('(', TYPE_LPAREN, self.lineno)
             elif c == ')':
                 return Token(')', TYPE_RPAREN, self.lineno)
+            elif c == ',':
+                return Token(',', TYPE_COMMA, self.lineno)
+
+class Functor(object):
+    def __init__(self, fn, name, nargs):
+        self.closure = fn
+        self.name = name
+        self.nargs = nargs
+
+    def __call__(self, args):
+        self.closure(args)
+
+class Executor(object):
+    def __init__(self):
+        self.init_env()
+
+    def init_env(self):
+        self._env = {
+            'show_msg': Functor(self._show_msg, '_show_msg', 1)
+        }
+
+    def env_func_wrapper(self, funcname, narg):
+        pass
+
+    def _show_msg(self, msg):
+        print(msg)
+
+    def do_funcall(self, lex, fn):
+        args = []
+        while True:
+            tok = lex.get_token()
+            if tok.type == TYPE_RPAREN:
+                break
+            elif tok.type == TYPE_COMMA:
+                if len(args) == 0:
+                    raise Exception('invalid function call argument with `%s` at %d' % (tok.text, tok.lineno))
+            elif tok.type == TYPE_STRING:
+                args.append(tok.text)
+            else:
+                raise Exception('error function arguments')
+
+        if fn.text not in self._env:
+            raise Exception('`%s` undefined, at line: %d' % (fn.text, fn.lineno))
+
+        functor = self._env[fn.text]
+        if functor.nargs != len(args):
+            raise Exception('`%s` arguments count unmatched, at line: %d' % (fn.text, fn.lineno))
+        functor(*args)
+
+    def do_execute(self, lex, token):
+        if token.type == TYPE_IDENTIFIER:
+            # parser function call
+            next_tok = lex.get_token()
+            if next_tok.type == TYPE_LPAREN:
+                self.do_funcall(lex, token)
+        else:
+            raise Exception('error syntx at line: %d' % token.lineno)
+
+    def run(self, fname):
+        lex = Lexer(fname)
+        while True:
+            tok = lex.get_token()
+            if tok is None:
+                break
+            self.do_execute(lex, tok)
 
 def main():
-    lex = Lexer('surtr.cl')
-    while True:
-        tok = lex.get_token()
-        if tok is None:
-            break
-        print (tok)
+    ex = Executor()
+    ex.run('surtr.su')
 
 if __name__ == '__main__':
     main()
