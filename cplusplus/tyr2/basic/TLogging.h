346c9fd6d72f8c567d338db78934326134e833b9
ASMlover <asmlover@126.com>
2016-12-17 18:33:58 +0800

    :bug: fix(logging): fixed bug of checking macro for logging module

--- cplusplus/tyr2/basic/TLogging.h
+++ cplusplus/tyr2/basic/TLogging.h
@@ -97,7 +97,7 @@ public:
 const char* strerror_tl(int saved_errno);
 
 #define TCHECK_NOTNULL(v)\
-  tyr::basic::check_not_null(__FILE__, __LINE__, "'" #v "' Must be not null", (val))
+  tyr::basic::check_not_null(__FILE__, __LINE__, "'" #v "' Must be not null", (v))
 
 template <typename T>
 T* check_not_null(Logger::SourceFile file, int lineno, const char* names, T* p) {
