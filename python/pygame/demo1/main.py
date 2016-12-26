31d4f46996807d1232356be8399f1a3d09a7f4e4
ASMlover <asmlover@126.com>
2016-12-26 14:14:46 +0800

    :sparkles: feat(pygame): add hello world demo for using pygame

---
+++ python/pygame/demo1/main.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# Copyright (c) 2016 ASMlover. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list ofconditions and the following disclaimer.
+#
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in
+#    the documentation and/or other materialsprovided with the
+#    distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+import pygame
+
+class Game(object):
+    def __init__(self, caption='Game.Default'):
+        self._screen = None
+        self._background = None
+        self._cursor = None
+        self._caption = caption
+
+    def setup(self):
+        pygame.init()
+
+        self._screen = pygame.display.set_mode((640, 480), 0, 32)
+        pygame.display.set_caption(self._caption)
+
+        self._background = pygame.image.load('background.jpg').convert()
+        self._cursor = pygame.image.load('cursor.png').convert_alpha()
+
+    def update(self):
+        self._screen.blit(self._background, (0, 0))
+        x, y = pygame.mouse.get_pos()
+        x -= self._cursor.get_width() / 2
+        y -= self._cursor.get_height() / 2
+        self._screen.blit(self._cursor, (x, y))
+        pygame.display.update()
+
+    def loop(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    break
+
+            running and self.update()
+
+_game = Game()
+
+def main():
+    _game.setup()
+    _game.loop()
+
+if __name__ == '__main__':
+    main()
