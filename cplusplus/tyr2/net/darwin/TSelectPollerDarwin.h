d32be46450b71018424cf550f1c9e968d3a5ac2c
ASMlover <asmlover@126.com>
2016-12-30 21:25:53 +0800

    :sparkles: chore(select): add select poller definitions for darwin

---
+++ cplusplus/tyr2/net/darwin/TSelectPollerDarwin.h
@@ -0,0 +1,60 @@
+// Copyright (c) 2016 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#ifndef __TYR_NET_DARWIN_SELECTPOLLERDARWIN_HEADER_H__
+#define __TYR_NET_DARWIN_SELECTPOLLERDARWIN_HEADER_H__
+
+#include <sys/select.h>
+#include "../TPoller.h"
+
+namespace tyr { namespace net {
+
+class Channel;
+
+class SelectPoller : public Poller {
+  int max_fd_{}; // record the highest-numbered filedes in any of three sets, plus 1
+  int fd_storage_{};
+  fd_set* rsets_in_{};
+  fd_set* wsets_in_{};
+  fd_set* rsets_out_{};
+  fd_set* wsets_out_{};
+
+  void fill_active_channels(int nevents, std::vector<Channel*>* active_channels) const;
+  void sets_init(void);
+  void sets_clear(void);
+  void sets_regrow(void);
+public:
+  explicit SelectPoller(EventLoop* loop);
+  virtual ~SelectPoller(void);
+
+  virtual basic::Timestamp poll(int timeout, std::vector<Channel*>* active_channels) override;
+  virtual void update_channel(Channel* channel) override;
+  virtual void remove_channel(Channel* channel) override;
+};
+
+}}
+
+#endif // __TYR_NET_DARWIN_SELECTPOLLERDARWIN_HEADER_H__
