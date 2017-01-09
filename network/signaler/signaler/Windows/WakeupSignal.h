9dfcfca06a96cfd9e1dc630999138bbd5444f2e3
ASMlover <asmlover@126.com>
2017-01-09 16:59:36 +0800

    :sparkles: feat(wake): add wakeup signal using socket in windows

--- network/signaler/signaler/Windows/WakeupSignal.h
+++ network/signaler/signaler/Windows/WakeupSignal.h
@@ -29,4 +29,76 @@
 
 #include <Winsock2.h>
 
+class WakeupSignal {
+  int rfd_{}; // reader
+  int wfd_{}; // writer
+
+  void open(void) {
+    struct sockaddr_in addr{};
+    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    addr.sin_family = AF_INET;
+
+    int listener = socket(AF_INET, SOCK_STREAM, 0);
+    if (listener < 0)
+      return;
+
+    int r = bind(listener, (const struct sockaddr*)&addr, sizeof(addr));
+    if (0 == r) {
+      int addrlen = sizeof(addr);
+      r = getsockname(listener, (struct sockaddr*)&addr, &addrlen);
+    }
+    if (0 == r)
+      r = listen(listener, 1);
+
+    wfd_ = socket(AF_INET, SOCK_STREAM, 0);
+    if (wfd_ < 0) {
+      close();
+      return;
+    }
+    if (0 == r)
+      r = connect(wfd_, (struct sockaddr*)&addr, sizeof(addr));
+    if (0 == r)
+      rfd_ = accept(listener, nullptr, nullptr);
+    if (rfd_ < 0)
+      close();
+
+    closesocket(listener);
+  }
+
+  void close(void) {
+    if (rfd_ > 0) {
+      closesocket(rfd_);
+      rfd_ = 0;
+    }
+    if (wfd_ > 0) {
+      closesocket(wfd_);
+      wfd_ = 0;
+    }
+  }
+public:
+  WakeupSignal(void) {
+    open();
+  }
+
+  ~WakeupSignal(void) {
+    close();
+  }
+
+  int get_reader_fd(void) const {
+    return rfd_;
+  }
+
+  bool is_opened(void) const {
+    return rfd_ > 0 && wfd_ > 0;
+  }
+
+  int set_signal(const char* buf, size_t len) {
+    return send(wfd_, buf, len, 0);
+  }
+
+  int get_signal(size_t len, char* buf) {
+    return recv(rfd_, buf, len, 0);
+  }
+};
+
 #endif // WINDOWS_WAKEUPSIGNAL_H_
