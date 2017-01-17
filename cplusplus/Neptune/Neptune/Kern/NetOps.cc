f5a7c9b0c5d4cb07b9fb9b628f2444ea55b14255
ASMlover <asmlover@126.com>
2017-01-17 12:08:10 +0800

    :construction: chore(netops): updated the including for windows

--- cplusplus/Neptune/Neptune/Kern/NetOps.cc
+++ cplusplus/Neptune/Neptune/Kern/NetOps.cc
@@ -24,10 +24,6 @@
 // LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
-#include <Chaos/Platform.h>
-#if defined(CHAOS_WINDOWS)
-# include <WinSock2.h>
-#endif
 #include <cerrno>
 #include <cstdio>
 #include <cstring>
