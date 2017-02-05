cce8c2636fff57fcb2dc65fc848f192f45649d7d
ASMloer <asmlover@126.com>
2017-02-05 19:21:00 +0800

    :construction: chore(chat.client): finished the implementation of chat client

--- network/asio/chat2/chat_client.cc
+++ network/asio/chat2/chat_client.cc
@@ -42,20 +42,56 @@ class ChatClient : private boost::noncopyable {
   ChatMessageQueue writmsg_queue_;
   std::string session_id_;
 
-  void do_connect(tcp::resolver::iterator endpoint_iter) {
-    // TODO:
-  }
-
   void do_read_header(void) {
-    // TODO:
+    boost::asio::async_read(socket_, boost::asio::buffer(readmsg_.data(), ChatProtocol::NHEADER),
+        [this](const boost::system::error_code& ec, std::size_t /*n*/) {
+          if (!ec && readmsg_.decode_header())
+            do_read_body();
+          else
+            socket_.close();
+        });
   }
 
   void do_read_body(void) {
-    // TODO:
+    boost::asio::async_read(socket_, boost::asio::buffer(readmsg_.body(), readmsg_.get_nbody()),
+        [this](const boost::system::error_code& ec, std::size_t /*n*/) {
+          if (!ec) {
+            if (readmsg_.get_proto() == ChatProtocol::CP_SESSION) {
+              session_id_ = readmsg_.body();
+            }
+            else {
+              std::cout.write(readmsg_.body(), readmsg_.get_nbody()); std::cout << std::endl;
+              do_read_header();
+            }
+          }
+          else {
+            socket_.close();
+          }
+        });
+  }
+
+  void write_message(const ChatMessage& msg) {
+    io_service_.post([this, msg](void) {
+          bool write_in_progress = !writmsg_queue_.empty();
+          writmsg_queue_.push_back(msg);
+          if (!write_in_progress)
+            do_write();
+        });
   }
 
   void do_write(void) {
-    // TODO:
+    boost::asio::async_write(socket_,
+        boost::asio::buffer(writmsg_queue_.front().data(), writmsg_queue_.front().size()),
+        [this](const boost::system::error_code& ec, std::size_t /*n*/) {
+          if (!ec) {
+            writmsg_queue_.pop_front();
+            if (!writmsg_queue_.empty())
+              do_write();
+          }
+          else {
+            socket_.close();
+          }
+        });
   }
 public:
   ChatClient(boost::asio::io_service& io_service)
@@ -64,19 +100,28 @@ public:
   }
 
   void start(tcp::resolver::iterator endpoint_iter) {
-    // TODO:
+    boost::asio::async_connect(socket_, endpoint_iter,
+        [this](const boost::system::error_code& ec, tcp::resolver::iterator) {
+          if (!ec)
+            do_read_header();
+          else
+            socket_.close();
+        });
   }
 
-  void write(const char* buf, std::size_t len) {
-    // TODO:
-  }
-
-  void write(const ChatMessage& msg) {
-    // TODO:
+  void write(const char* buf) {
+    char writbuf[1024]{};
+    std::snprintf(writbuf, sizeof(writbuf), "[%s] - %s", session_id_.c_str(), buf);
+    ChatMessage msg(gen_chat_message(ChatProtocol::CP_MESSAGE, writbuf, std::strlen(writbuf)));
+    write_message(msg);
   }
 
   void close(void) {
     io_service_.post([this](void) { socket_.close(); });
+  }
+
+  const std::string& get_session(void) const {
+    return session_id_;
   }
 };
 
