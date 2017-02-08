2e79a39f6e4287916e266b9cdfc2dccab4c062bf
ASMlover <ASMlover@126.com>
2017-02-08 19:53:59 +0800

    :construction: chore(rpc.server): finished the implementation of rpc server

--- network/rpc-examples/rpc/server.cc
+++ network/rpc-examples/rpc/server.cc
@@ -30,8 +30,106 @@
 #include <boost/asio.hpp>
 #include "echo.pb.h"
 
+using boost::asio::ip::tcp;
+namespace gpb = google::protobuf;
+
+class MyEchoService : public echo::EchoService {
+public:
+  void do_echo(gpb::RpcController* /*controller*/,
+      const echo::EchoRequest* request, echo::EchoResponse* response, gpb::Closure* done) {
+    std::cout << "MyEchoService::do_echo - request=" << request->request() << std::endl;
+    response->set_response("recevied request");
+
+    if (done)
+      done->Run();
+  }
+};
+
+class TcpSession : private boost::noncopyable, public std::enable_shared_from_this<TcpSession> {
+  tcp::socket socket_;
+  std::vector<char> buffer_;
+  std::unique_ptr<MyEchoService> service_;
+  echo::EchoRequest request_;
+  echo::EchoResponse response_;
+
+  enum { MAX_READBUFF = 1024 };
+
+  void do_read(void) {
+    auto self(shared_from_this());
+    socket_.async_read_some(boost::asio::buffer(buffer_),
+        [this, self](const boost::system::error_code& ec, std::size_t n) {
+          if (!ec) {
+            request_.ParseFromArray(buffer_.data(), n);
+            service_->do_echo(nullptr, &request_, &response_, nullptr);
+
+            char writbuf[1024]{};
+            response_.SerializeToArray(writbuf, sizeof(writbuf));
+            do_write(writbuf, std::strlen(writbuf));
+          }
+          else {
+            socket_.close();
+          }
+        });
+  }
+
+  void do_write(const char* buf, std::size_t len) {
+    auto self(shared_from_this());
+    boost::asio::async_write(socket_, boost::asio::buffer(buf, len),
+        [this, self](const boost::system::error_code& ec, std::size_t /*n*/) {
+          if (ec)
+            socket_.close();
+        });
+  }
+public:
+  TcpSession(tcp::socket&& socket)
+    : socket_(std::move(socket))
+    , buffer_(MAX_READBUFF)
+    , service_(new MyEchoService()) {
+  }
+
+  void start(void) {
+    do_read();
+  }
+};
+
+class TcpServer : private boost::noncopyable {
+  tcp::acceptor acceptor_;
+  tcp::socket socket_;
+
+  void do_accept(void) {
+    acceptor_.async_accept(socket_,
+        [this](const boost::system::error_code& ec) {
+          if (!ec)
+            std::make_shared<TcpSession>(std::move(socket_))->start();
+
+          do_accept();
+        });
+  }
+public:
+  TcpServer(boost::asio::io_service& io_service, std::uint16_t port = 5555)
+    : acceptor_(io_service, tcp::endpoint(tcp::v4(), port))
+    , socket_(io_service) {
+  }
+
+  void start(void) {
+    do_accept();
+  }
+};
+
 int main(int argc, char* argv[]) {
   (void)argc, (void)argv;
+
+  try {
+    boost::asio::io_service io_service;
+
+    TcpServer server(io_service);
+    server.start();
+
+    io_service.run();
+  }
+  catch (std::exception& ex) {
+    std::cerr << "exception: " << ex.what() << std::endl;
+  }
 
   return 0;
 }
