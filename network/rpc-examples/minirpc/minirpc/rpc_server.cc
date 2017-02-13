b67c22d4851cef545ac3de8d7340d1b287eb92e3
ASMlover <ASMlover@126.com>
2017-02-13 17:08:09 +0800

    :sparkles: feat(rpc.server): add rpc server definitions

---
+++ network/rpc-examples/minirpc/minirpc/rpc_server.cc
@@ -0,0 +1,51 @@
+// Copyright (c) 2017 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#include "rpc.pb.h"
+#include "rpc_channel.h"
+#include "rpc_server.h"
+
+namespace minirpc {
+
+
+RpcServer::RpcServer(boost::asio::io_service& io_service, std::uint16_t port)
+  : acceptor_(io_service, tcp::endpoint(tcp::v4(), port))
+  , socket_(io_service) {
+}
+
+RpcServer::~RpcServer(void) {
+  // TODO:
+}
+
+void RpcServer::register_service(gpb::Service* service) {
+  // TODO:
+}
+
+void RpcServer::start(void) {
+  // TODO:
+}
+
+}
---
+++ network/rpc-examples/minirpc/minirpc/rpc_server.h
@@ -0,0 +1,59 @@
+// Copyright (c) 2017 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+#include <map>
+#include <mutex>
+#include <set>
+#include <string>
+#include <vector>
+#include <google/protobuf/service.h>
+#include <boost/asio.hpp>
+
+namespace minirpc {
+
+using ::boost::asio::ip::tcp;
+namespace gpb = ::google::protobuf;
+
+class RpcChannel;
+
+class RpcServer : private boost::noncopyable {
+  tcp::acceptor acceptor_;
+  tcp::socket socket_;
+
+  std::map<std::string, gpb::Service*> services_;
+  std::mutex mutex_;
+  std::set<RpcChannel*> channels_;
+public:
+  RpcServer(boost::asio::io_service& io_service, std::uint16_t port = 5555);
+  ~RpcServer(void);
+
+  void register_service(gpb::Service* service);
+  void start(void);
+};
+
+}
