8f190d54e1080d0e10c22a3f974ca0929bbe4962
ASMlover <asmlover@126.com>
2017-02-23 20:55:18 +0800

    :sparkles: feat(tcp.server): add tcp server definitions for rpc of nyx core

---
+++ server/nyx.engine/core/rpc/tcp_server.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# Copyright (c) 2017 ASMlover. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list ofconditions and the following disclaimer.
+#
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in
+#    the documentation and/or other materialsprovided with the
+#    distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import asyncore
+import inspect
+import socket
+from log.nyx_log import LogManager
+from rpc.nyx_tcp_session import TcpSession
+
+class TcpServer(asyncore.dispatcher):
+    """负责监听TCP连接，创建连接"""
+    def __init__(self, addr, port, conn_handler=None, reuse_addr=False):
+        super(TcpServer, self).__init__()
+
+        self._logger = LogManager.get_logger('NyxCore.Rpc.TcpServer')
+        self._addr = addr
+        self._port = port
+        self._conn_handler = conn_handler
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        if reuse_addr:
+            self.set_reuse_addr()
+        self._started = False
+        self._try_bind()
+        self.listen(50)
+
+    def _try_bind(self):
+        while True:
+            try:
+                self.bind((self._addr, self._port))
+                break
+            except:
+                self._logger.warn('TcpServer._try_bind - failed to bind(%s:%s)', self._addr, self._port)
+                self._port += 1
+                if self._port > 65535:
+                    self._logger.error('TcpServer._try_bind - failed to find a usable port(%d)', self._port)
+                    raise StandardError('failed to find a usable port')
+        self._started = True
+
+    def get_localaddr(self):
+        return (self._addr, self._port)
+
+    def set_connection_handler(self, conn_handler):
+        self._conn_handler = conn_handler
+
+    def stop(self):
+        self.close()
+        self._started = False
+
+    def close(self):
+        super(TcpServer, self).close()
+
+    def handle_accept(self):
+        pass
+
+    def handle_error(self):
+        pass
+
+    def handle_close(self):
+        pass
