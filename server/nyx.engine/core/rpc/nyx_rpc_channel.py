9638fa99ef5619fd4d14cdd79819aef853e62464
ASMlover <asmlover@126.com>
2017-02-28 20:31:50 +0800

    :construction: chore(rpc.channel): add rpc channel creator and rpc channel holder and rpc channel manager for nyx core

--- server/nyx.engine/core/rpc/nyx_rpc_channel.py
+++ server/nyx.engine/core/rpc/nyx_rpc_channel.py
@@ -33,6 +33,10 @@ from google.protobuf import service
 from log.nyx_log import LogManager
 from rpc import nyx_request as _request
 
+# [doc]
+# ChannelListener
+#   - on_channel_disconnected(...) # TODO: 需要实现on_channel_disconnected回调
+
 class NyxRpcController(service.RpcController):
     def __init__(self, channel):
         super(NyxRpcController, self).__init__()
@@ -120,3 +124,68 @@ class NyxRpcChannel(service.RpcChannel):
 
     def input_data(self, data):
         pass
+
+class RpcChannelCreator(object):
+    def __init__(self, rpc_service, handler, max_datalen=0):
+        super(RpcChannelCreator, self).__init__()
+        self._logger = LogManager.get_logger('NyxCore.Rpc.RpcChannelCreator')
+        self._rpc_service = rpc_service
+        self._handler = handler
+        self._max_datalen = datalen
+
+    def set_max_datalen(self, datalen):
+        self._max_datalen = datalen
+
+    def on_new_connection(self, conn):
+        """TcpServer或TcpClient新连接建立的时候回调"""
+        # TODO:
+        pass
+
+    def on_connection_failed(self, conn):
+        """连接断开的时候回调"""
+        # TODO:
+        pass
+
+class RpcChannelHolder(object):
+    def __init__(self):
+        super(RpcChannelHolder, self).__init__()
+        self._logger = LogManager.get_logger('NyxCore.Rpc.RpcChannelHolder')
+        self._rpc_chanel = None
+
+    def get_rpc_channel(self):
+        return self._rpc_chanel
+
+    def on_new_channel(self, rpc_channel):
+        """TcpServer或TcpClient处理新连接时的回调"""
+        self._rpc_chanel = rpc_channel
+        rpc_channel.register_listener(self)
+
+    def on_channel_disconnected(self, rpc_channel):
+        """连接断开的时候channel回调"""
+        self._rpc_chanel = None
+
+class RpcChannelManager(object):
+    def __init__(self):
+        super(RpcChannelManager, self).__init__()
+        self._logger = LogManager.get_logger('NyxCore.Rpc.RpcChannelManager')
+        self._rpc_channels = {}
+
+    def get_rpc_channel(self, peeraddr):
+        return self._rpc_channels.get(peeraddr)
+
+    def get_rpc_channel_count(self):
+        return len(self._rpc_channels)
+
+    def on_new_channel(self, rpc_channel):
+        """TcpServer或TcpClient在处理新channel建立连接的时候回调"""
+        self._rpc_channels[rpc_channel.get_peeraddr()] = rpc_channel
+        rpc_channel.register_listener(self)
+
+    def on_channel_disconnected(self, rpc_channel):
+        """channel的connection连接断开的时候回调"""
+        peeraddr = rpc_channel.get_peeraddr()
+        if peeraddr in self._rpc_channels:
+            self._rpc_channels.pop(peeraddr, None)
+        else:
+            # TODO:
+            pass
