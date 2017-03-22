87b4242a8ee9025a609052f4bc02c6f1f7d08f90
ASMlover <asmlover@126.com>
2017-03-23 00:00:03 +0800

    :sparkles: feat(gateclient.proxy): add gate client proxy for gate service

---
+++ server/nyx.engine/core/distributed/gate/nyx_gate_service.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# Copyright (c) 2017 ASMlover. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list ofconditions and the following disclaimer.
+#
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in
+#    the documentation and/or other materialsprovided with the
+#    distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+from bson import objectid
+from common.nyx_id import IdCreator
+from distributed.gate import nyx_gate_global as _gglobal
+from log.nyx_log import LogManager
+from proto.pyproto import nyx_common_pb2 as _c_pb2
+from proto.pyproto import nyx_gate_game_pb2 as _gg_pb2
+from proto.pyproto import nyx_client_gate_pb2 as _cg_pb2
+from rpc.nyx_compressor import Compressor
+
+class GateClientProxy(object):
+    def __init__(self, stub, client_info):
+        super(GateClientProxy, self).__init__()
+        self.logger = LogManager.get_logger('NyxCore.Gate.GateClientProxy')
+        # TODO:
+        pass
+
+    def get_session_id(self):
+        # TODO:
+        pass
+
+    def get_client_id(self):
+        # TODO:
+        pass
+
+    def destroy(self):
+        # TODO:
+        pass
+
+    def disconnect(self):
+        # TODO:
+        pass
+
+    def connect_response(self, response):
+        # TODO:
+        pass
+
+    def create_entity(self, entity_info):
+        # TODO:
+        pass
+
+    def destroy_entity(self, entity_info):
+        # TODO:
+        pass
+
+    def entity_message(self, entity_msg):
+        # TODO:
+        pass
+
+    def chat_to_client(self, outband_info):
+        # TODO:
+        pass
+
+    def reg_md5index(self, md5_index):
+        # TODO:
+        pass
