40f408500b4b1a8d16f59ba5500796e20de0b034
ASMlover <asmlover@126.com>
2017-04-15 20:44:59 +0800

    :sparkles: feat(log): add easy logging module for njord

---
+++ gc/njord/njlog.c
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2017 ASMlover. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list ofconditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materialsprovided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "njlog.h"
+
+void
+njlog_repr(NjLogLevel level, const char* fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  switch (level) {
+  case INFO:
+    vfprintf(stdout, fmt, ap); break;
+  case FATAL:
+    vfprintf(stderr, fmt, ap);
+    abort();
+    break;
+  default:
+#if defined(Nj_DEBUG_OBJECT)
+    vfprintf(stdout, fmt, ap);
+#endif
+    break;
+  }
+
+  va_end(ap);
+}
---
+++ gc/njord/njlog.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2017 ASMlover. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list ofconditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materialsprovided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef Nj_NJLOG_H
+#define Nj_NJLOG_H
+
+#include "njconfig.h"
+
+typedef enum _level {
+  DEBUG,
+  INFO,
+  FATAL,
+} NjLogLevel;
+NjAPI_FUNC(void) njlog_repr(NjLogLevel level, const char* format, ...);
+
+#define njlog_debug(fmt, ...) njlog_repr(DEBUG, (fmt), ##__VA_ARGS__)
+#define njlog_log(fmt, ...) njlog_repr(INFO, (fmt), ##__VA_ARGS__)
+#define njlog_fatal(fmt, ...) njlog_repr(FATAL, (fmt), ##__VA_ARGS__)
+
+#endif /* Nj_NJLOG_H */
