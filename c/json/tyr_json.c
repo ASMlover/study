3f3ee29a2c7740b76f47d6ae319d20ba5cdd7291
ASMlover <asmlover@126.com>
2017-05-06 20:35:18 +0800

    :construction: chore(json): updated the json parser about parsing string

--- c/json/tyr_json.c
+++ c/json/tyr_json.c
@@ -28,17 +28,28 @@
  */
 #include <assert.h>
 #include <ctype.h>
+#include <errno.h>
+#include <math.h>
 #include <stdlib.h>
 #include <string.h>
 #include "tyr_json.h"
 
+#define TYR_PARSE_INIT_STACKSZ 256
+
+#define ISDIGIT1TO9(ch) ((ch) >= '1' && (ch) <= '9')
 #define EXPECT(c, ch) do {\
   assert(*c->json == (ch));\
   c->json++;\
 } while (0)
+#define PUTC(c, ch) do {\
+  *(char*)tyr_context_push(c, sizeof(char)) = (ch);\
+} while (0)
 
 typedef struct tyr_context {
   const char* json;
+  char* stack;
+  size_t size;
+  size_t top;
 } tyr_context;
 
 #define TYR_PARSE_IDENTITY(c, id, n)\
@@ -46,6 +57,39 @@ typedef struct tyr_context {
   if (0 != strncmp(c->json, id, (n)))\
     return TYR_PARSE_INVALID_VALUE;\
   c->json += (n) - 1
+
+#define tyr_context_init(c, json) do {\
+  c.json = json;\
+  c.stack = NULL;\
+  c.size = 0;\
+  c.top = 0;\
+} while (0)
+#define tyr_context_destroy(c) do {\
+  if (NULL != c.stack)\
+    free(c.stack);\
+} while (0)
+
+static void* tyr_context_push(tyr_context* c, size_t size) {
+  void* ret;
+
+  assert(size > 0);
+  if (c->top + size >= c->size) {
+    if (0 == c->size)
+      c->size = TYR_PARSE_INIT_STACKSZ;
+    while (c->top + size >= c->size)
+      c->size += c->size >> 1;
+    c->stack = (char*)realloc(c->stack, c->size);
+  }
+  ret = c->stack + c->top;
+  c->top += size;
+
+  return ret;
+}
+
+static void* tyr_context_pop(tyr_context* c, size_t size) {
+  assert(c->top >= size);
+  return c->stack + (c->top -= size);
+}
 
 static void tyr_parse_whitespace(tyr_context* c) {
   const char* p = c->json;
@@ -73,27 +117,97 @@ static int tyr_parse_false(tyr_context* c, tyr_value* value) {
 }
 
 static int tyr_parse_number(tyr_context* c, tyr_value* value) {
-  char* end;
-  value->u.number = strtod(c->json, &end);
-  if (c->json == end)
-    return TYR_PARSE_INVALID_VALUE;
-  c->json = end;
+  const char* p = c->json;
+
+  if ('-' == *p)
+    ++p;
+
+  if ('0' == *p) {
+    ++p;
+  }
+  else {
+    if (!isdigit(*p))
+      return TYR_PARSE_INVALID_VALUE;
+    for (p++; isdigit(*p); ++p) {}
+  }
+
+  if ('.' == *p) {
+    ++p;
+    if (!isdigit(*p))
+      return TYR_PARSE_INVALID_VALUE;
+    for (p++; isdigit(*p); ++p) {}
+  }
+
+  if ('e' == *p || 'E' == *p) {
+    ++p;
+    if ('+' == *p || '-' == *p)
+      ++p;
+    if (!isdigit(*p))
+      return TYR_PARSE_INVALID_VALUE;
+    for (p++; isdigit(*p); ++p) {}
+  }
+
+  errno = 0;
+  value->u.number = strtod(c->json, NULL);
+  if (ERANGE == errno
+      && (HUGE_VAL == value->u.number || -HUGE_VAL == value->u.number))
+    return TYR_PARSE_NUMBER_TO_BIG;
   value->type = TYR_NUMBER;
+  c->json = p;
   return TYR_PARSE_OK;
+}
+
+static int tyr_parse_string(tyr_context* c, tyr_value* value) {
+  size_t head = c->top;
+  size_t len;
+  const char* p;
+
+  EXPECT(c, '\"');
+  p = c->json;
+  for (;;) {
+    char ch = *p++;
+    switch (ch) {
+    case '\"':
+      len = c->top - head;
+      tyr_set_string(value, (const char*)tyr_context_pop(c, len), len);
+      c->json = p;
+      return TYR_PARSE_OK;
+    case '\\':
+      switch (*p++) {
+      case '\"': PUTC(c, '\"'); break;
+      case '\\': PUTC(c, '\\'); break;
+      case '/': PUTC(c, '/'); break;
+      case 'b': PUTC(c, '\b'); break;
+      case 'f': PUTC(c, '\f'); break;
+      case 'r': PUTC(c, '\r'); break;
+      case 'n': PUTC(c, '\n'); break;
+      case 't': PUTC(c, '\t'); break;
+      default:
+        c->top = head;
+        return TYR_PARSE_INVALID_STRING_ESCAPE;
+      }
+      break;
+    case '\0':
+      c->top = head;
+      return TYR_PARSE_MISS_QUOTATION_MARK;
+    default:
+      if ((unsigned char)ch < 0x20) {
+        c->top = head;
+        return TYR_PARSE_INVALID_STRING_CHAR;
+      }
+      PUTC(c, ch);
+    }
+  }
 }
 
 static int tyr_parse_value(tyr_context* c, tyr_value* value) {
   switch (*c->json) {
-  case 'n':
-    return tyr_parse_null(c, value);
-  case 't':
-    return tyr_parse_true(c, value);
-  case 'f':
-    return tyr_parse_false(c, value);
-  case '\0':
-    return TYR_PARSE_EXPECT_VALUE;
-  default:
-    return tyr_parse_number(c, value);
+  case 'n': return tyr_parse_null(c, value);
+  case 't': return tyr_parse_true(c, value);
+  case 'f': return tyr_parse_false(c, value);
+  case '"': return tyr_parse_string(c, value);
+  case '\0': return TYR_PARSE_EXPECT_VALUE;
+  default: return tyr_parse_number(c, value);
   }
 }
 
@@ -102,8 +216,8 @@ int tyr_parse(tyr_value* value, const char* json) {
   int r;
   assert(NULL != value);
 
-  c.json = json;
-  value->type = TYR_NULL;
+  tyr_context_init(c, json);
+  tyr_init(value);
   tyr_parse_whitespace(&c);
   if (TYR_PARSE_OK == (r = tyr_parse_value(&c, value))) {
     tyr_parse_whitespace(&c);
@@ -112,6 +226,9 @@ int tyr_parse(tyr_value* value, const char* json) {
       r = TYR_PARSE_ROOT_NOT_SINGULAR;
     }
   }
+
+  assert(0 == c.top);
+  tyr_context_destroy(c);
   return r;
 }
 
@@ -128,14 +245,15 @@ tyr_type tyr_get_type(const tyr_value* value) {
 }
 
 int tyr_get_boolean(const tyr_value* value) {
-  assert(NULL != value);
-  /* TODO: */
-  return 0;
+  assert(NULL != value
+      && (TYR_TRUE == value->type || TYR_FALSE == value->type));
+  return value->type == TYR_TRUE;
 }
 
 void tyr_set_boolean(tyr_value* value, int b) {
   assert(NULL != value);
-  /* TODO: */
+  tyr_free(value);
+  value->type = b ? TYR_TRUE : TYR_FALSE;
 }
 
 double tyr_get_number(const tyr_value* value) {
@@ -145,7 +263,9 @@ double tyr_get_number(const tyr_value* value) {
 
 void tyr_set_number(tyr_value* value, double n) {
   assert(NULL != value);
-  /* TODO: */
+  tyr_free(value);
+  value->u.number = n;
+  value->type = TYR_NUMBER;
 }
 
 const char* tyr_get_string(const tyr_value* value) {
