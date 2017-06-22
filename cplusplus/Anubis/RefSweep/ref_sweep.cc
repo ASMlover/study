d16dd1dd82e18c074e128e764f05729d69861951
ASMlover <ASMlover@126.com>
2017-06-22 19:43:42 +0800

    :sparkles: feat(counting-sweep): add abstruction reference-counting sweep gc

---
+++ cplusplus/Anubis/RefSweep/ref_sweep.cc
@@ -0,0 +1,139 @@
+// Copyright (c) 2017 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#include <iostream>
+#include "object.h"
+#include "ref_sweep.h"
+
+namespace gc {
+
+void RefSweep::inc_ref(BaseObject* obj) {
+  if (obj != nullptr)
+    inc_objects_.push(obj);
+}
+
+void RefSweep::dec_ref(BaseObject* obj) {
+  if (obj != nullptr)
+    dec_objects_.push(obj);
+}
+
+void RefSweep::apply_increments(void) {
+  while (!inc_objects_.empty()) {
+    auto* obj = inc_objects_.top();
+    inc_objects_.pop();
+    obj->inc_ref();
+  }
+}
+
+void RefSweep::scan_counting(void) {
+  while (!dec_objects_.empty()) {
+    auto* obj = dec_objects_.top();
+    dec_objects_.pop();
+    obj->dec_ref();
+    if (obj->ref() == 0 && obj->is_pair()) {
+      auto* first = as_pair(obj)->first();
+      if (first != nullptr)
+        dec_objects_.push(first);
+
+      auto* second = as_pair(obj)->second();
+      if (second != nullptr)
+        dec_objects_.push(second);
+    }
+  }
+}
+
+void RefSweep::sweep_counting(void) {
+  for (auto it = objects_.begin(); it != objects_.end();) {
+    if ((*it)->ref() == 0) {
+      delete *it;
+      objects_.erase(it++);
+    }
+    else {
+      ++it;
+    }
+  }
+}
+
+RefSweep& RefSweep::get_instance(void) {
+  static RefSweep ins;
+  return ins;
+}
+
+void RefSweep::collect_counting(void) {
+  auto old_count = objects_.size();
+
+  apply_increments();
+  scan_counting();
+  sweep_counting();
+
+  std::cout
+    << "[" << old_count - objects_.size() << "] objects colleced, "
+    << "[" << objects_.size() << "] objects remaining." << std::endl;
+}
+
+BaseObject* RefSweep::create_int(int value) {
+  if (objects_.size() >= kMaxObject)
+    collect_counting();
+
+  auto* obj = new Int();
+  obj->set_value(value);
+
+  objects_.push_back(obj);
+  roots_.push_back(obj);
+  inc_ref(obj);
+
+  return obj;
+}
+
+BaseObject* RefSweep::create_pair(BaseObject* first, BaseObject* second) {
+  if (objects_.size() >= kMaxObject)
+    collect_counting();
+
+  auto* obj = new Pair();
+  if (first != nullptr) {
+    inc_ref(first);
+    obj->set_first(first);
+  }
+  if (second != nullptr) {
+    inc_ref(second);
+    obj->set_second(second);
+  }
+
+  objects_.push_back(obj);
+  roots_.push_back(obj);
+  inc_ref(obj);
+
+  return obj;
+}
+
+BaseObject* RefSweep::release_object(void) {
+  auto* obj = roots_.back();
+  roots_.pop_back();
+  dec_ref(obj);
+  return obj;
+}
+
+}
---
+++ cplusplus/Anubis/RefSweep/ref_sweep.h
@@ -0,0 +1,63 @@
+// Copyright (c) 2017 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+#include <list>
+#include <stack>
+#include <vector>
+#include <Chaos/Types.h>
+
+namespace gc {
+
+class BaseObject;
+
+class RefSweep : private Chaos::UnCopyable {
+  std::vector<BaseObject*> roots_;
+  std::list<BaseObject*> objects_;
+  std::stack<BaseObject*> inc_objects_;
+  std::stack<BaseObject*> dec_objects_;
+  static constexpr std::size_t kMaxObject = 1024;
+
+  RefSweep(void) = default;
+  ~RefSweep(void) = default;
+
+  void inc_ref(BaseObject* obj);
+  void dec_ref(BaseObject* obj);
+  void apply_increments(void);
+  void scan_counting(void);
+  void sweep_counting(void);
+public:
+  static RefSweep& get_instance(void);
+
+  void collect_counting(void);
+  BaseObject* create_int(int value);
+  BaseObject* create_pair(
+      BaseObject* first = nullptr, BaseObject* second = nullptr);
+  BaseObject* release_object(void);
+};
+
+}
