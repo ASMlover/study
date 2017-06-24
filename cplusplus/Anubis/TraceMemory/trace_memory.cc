e9ba5e4507b318450f0c2344f1d4f195f3b2701d
ASMlover <asmlover@126.com>
2017-06-24 20:12:23 +0800

    :sparkles: feat(TraceMemory): add trace sweeping gc with memory pool

---
+++ cplusplus/Anubis/TraceMemory/trace_memory.cc
@@ -0,0 +1,134 @@
+// Copyright (c) 2017 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#include <iostream>
+#include <Chaos/Memory/MemoryPool.h>
+#include "object.h"
+#include "trace_memory.h"
+
+namespace gc {
+
+void* TraceMemory::alloc(std::size_t n) {
+  return Chaos::MemoryPool::get_instace().alloc(n);
+}
+
+void TraceMemory::dealloc(BaseObject* obj) {
+  if (obj != nullptr) {
+    if (obj->is_int())
+      Chaos::MemoryPool::get_instace().dealloc(obj, sizeof(Int));
+    else if (obj->is_pair())
+      Chaos::MemoryPool::get_instace().dealloc(obj, sizeof(Pair));
+  }
+}
+
+void TraceMemory::roots_tracing(std::stack<BaseObject*>& trace_objects) {
+  for (auto* obj : roots_)
+    trace_objects.push(obj);
+}
+
+void TraceMemory::scan_tracing(std::stack<BaseObject*>& trace_objects) {
+  while (!trace_objects.empty()) {
+    auto* obj = trace_objects.top();
+    trace_objects.pop();
+    if (obj->set_ref(1) == 1 && obj->is_pair()) {
+      auto append_fn = [&trace_objects](BaseObject* ob) {
+        if (ob != nullptr)
+          trace_objects.push(ob);
+      };
+
+      append_fn(as_pair(obj)->first());
+      append_fn(as_pair(obj)->second());
+    }
+  }
+}
+
+void TraceMemory::sweep_tracing(void) {
+  for (auto it = objects_.begin(); it != objects_.end();) {
+    if ((*it)->ref() == 0) {
+      dealloc((*it));
+      objects_.erase(it++);
+    }
+    else {
+      (*it)->set_ref(0);
+      ++it;
+    }
+  }
+}
+
+TraceMemory& TraceMemory::get_instance(void) {
+  static TraceMemory ins;
+  return ins;
+}
+
+void TraceMemory::collect_tracing(void) {
+  auto old_count = objects_.size();
+
+  std::stack<BaseObject*> trace_objects;
+  roots_tracing(trace_objects);
+  scan_tracing(trace_objects);
+  sweep_tracing();
+
+  std::cout
+    << "[" << old_count - objects_.size() << "] objects collected, "
+    << "[" << objects_.size() << "] remaining." << std::endl;
+}
+
+BaseObject* TraceMemory::create_int(int value) {
+  if (objects_.size() >= kMaxObjects)
+    collect_tracing();
+
+  auto* obj = new (alloc(sizeof(Int))) Int();
+  obj->set_value(value);
+
+  objects_.push_back(obj);
+  roots_.push_back(obj);
+
+  return obj;
+}
+
+BaseObject* TraceMemory::create_pair(BaseObject* first, BaseObject* second) {
+  if (objects_.size() >= kMaxObjects)
+    collect_tracing();
+
+  auto* obj = new (alloc(sizeof(Pair))) Pair();
+  if (first != nullptr)
+    obj->set_first(first);
+  if (second != nullptr)
+    obj->set_second(second);
+
+  objects_.push_back(obj);
+  roots_.push_back(obj);
+
+  return obj;
+}
+
+BaseObject* TraceMemory::release_object(void) {
+  auto* obj = roots_.back();
+  roots_.pop_back();
+  return obj;
+}
+
+}
