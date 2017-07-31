7b342b4f18fff6aa4d19958850991ad05ad4d949
ASMlover <ASMlover@126.com>
2017-07-31 19:33:40 +0800

    :sparkles: feat(ConcurrencyCopy): add concurrency copying gc

---
+++ cplusplus/LearnGC/ConcurrencyCopy/concurrency_copy.h
@@ -0,0 +1,67 @@
+// Copyright (c) 2017 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+#include <vector>
+#include <Chaos/Types.h>
+#include <Chaos/Concurrent/Mutex.h>
+#include <Chaos/Concurrent/Thread.h>
+
+namespace gc {
+
+class BaseObject;
+
+class ConcurrencyCopy {
+  byte_t* heaptr_{};
+  byte_t* fromspace_{};
+  byte_t* tospace_{};
+  byte_t* allocptr_{};
+  byte_t* scanptr_{};
+  bool running_{true};
+  mutable Chaos::Mutex mutex_;
+  Chaos::Thread thread_;
+  std::vector<BaseObject*> roots_;
+  std::vector<BaseObject*> worklist_;
+  static constexpr std::size_t kSemispaceSize = 512 << 9;
+
+  ConcurrencyCopy(void);
+  ~ConcurrencyCopy(void);
+
+  void* alloc(std::size_t n);
+  void concurrency_closure(void);
+  BaseObject* forward(BaseObject* from_ref);
+  BaseObject* copy(BaseObject* from_ref);
+  void collect(void);
+public:
+  static ConcurrencyCopy& get_instance(void);
+
+  BaseObject* put_in(int value);
+  BaseObject* put_in(void);
+  BaseObject* fetch_out(void);
+};
+
+}
