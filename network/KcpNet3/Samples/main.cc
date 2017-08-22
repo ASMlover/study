68d750b804115d9b4eac1c617026d338e4ba8ce3
ASMlover <ASMlover@126.com>
2017-08-22 17:13:59 +0800

    :construction: chore(samples): updated the sample about echo for kcpnet

--- network/KcpNet3/Samples/main.cc
+++ network/KcpNet3/Samples/main.cc
@@ -25,11 +25,49 @@
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 #include <iostream>
-
-void run_server(void) {
-}
+#include <asio.hpp>
+#include "../KcpNet/Session.h"
+#include "../KcpNet/Client.h"
+#include "../KcpNet/Server.h"
 
 void run_client(void) {
+  asio::io_context io_context;
+
+  KcpNet::Client c(io_context, 5656);
+  c.bind_error_functor([](void) {
+        std::cout << "connect to(127.0.0.1:5555) failed ..." << std::endl;
+      });
+  c.bind_connecttion_functor([](const KcpNet::SessionPtr& s) {
+        std::cout << "connect to(127.0.0.1:5555) success ..." << std::endl;
+        s->write("Hello, world!");
+      });
+  c.bind_message_functor(
+      [](const KcpNet::SessionPtr& /*s*/, const std::string& buf) {
+        std::cout << "from(127.0.0.1:5555) read: " << buf << std::endl;
+      });
+  c.connect("127.0.0.1", 5555);
+
+  io_context.run();
+}
+
+void run_server(void) {
+  asio::io_context io_context;
+
+  KcpNet::Server s(io_context, 5555);
+  s.bind_connecttion_functor([](const KcpNet::SessionPtr& sp) {
+        std::cout
+          << "accept session from {" << sp->get_endpoint()
+          << ", " << sp->get_conv() << "}" << std::endl;
+      });
+  s.bind_message_functor(
+      [](const KcpNet::SessionPtr& sp, const std::string& buf) {
+        std::cout
+          << "from{" << sp->get_endpoint() << "," << sp->get_conv() << "} "
+          << "read: " << buf << std::endl;
+        sp->write(buf);
+      });
+
+  io_context.run();
 }
 
 int main(int argc, char* argv[]) {
