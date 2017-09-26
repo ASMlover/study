298b8b40d4f871fff534e046e9bcc4eddd335520
ASMlover <ASMlover@126.com>
2017-09-26 16:54:37 +0800

    :sparkles:

---
+++ network/netproj/netpp/address.cc
@@ -0,0 +1,115 @@
+// Copyright (c) 2017 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#include <Chaos/Base/Platform.h>
+#if defined(CHAOS_WINDOWS)
+# include <WinSock2.h>
+#else
+# include <arpa/inet.h>
+#endif
+#include "compatible_endian.h"
+#include "primitive.h"
+#include "address.h"
+
+namespace netpp {
+
+Address::Address(const struct sockaddr_in& addr)
+  : addr_(reinterpret_cast<struct sockaddr*>(new sockaddr_in(addr))) {
+}
+
+Address::Address(const struct sockaddr_in6& addr6)
+  : addr_(reinterpret_cast<struct sockaddr*>(new sockaddr_in6(addr6))) {
+}
+
+Address::Address(std::uint16_t port, bool loopback, bool with_v6) {
+  if (with_v6) {
+    auto* addr6 = new sockaddr_in6;
+    addr6->sin6_family = AF_INET6;
+    addr6->sin6_addr = loopback ? in6addr_loopback : in6addr_any;
+    addr6->sin6_port = netpp::h2n16(port);
+    addr_ = reinterpret_cast<struct sockaddr*>(addr6);
+  }
+  else {
+    auto* addr4 = new sockaddr_in;
+    addr4->sin_family = AF_INET;
+    addr4->sin_addr.s_addr =
+      netpp::h2n32(loopback ? INADDR_LOOPBACK : INADDR_ANY);
+    addr4->sin_port = netpp::h2n16(port);
+    addr_ = reinterpret_cast<struct sockaddr*>(addr4);
+  }
+}
+
+Address::Address(const char* host, std::uint16_t port, bool with_v6) {
+  if (with_v6) {
+    auto* addr6 = new sockaddr_in6;
+    addr6->sin6_family = AF_INET6;
+    addr6->sin6_port = netpp::h2n16(port);
+    netpp::inet_pton(AF_INET6, host, &addr6->sin6_addr);
+    addr_ = reinterpret_cast<struct sockaddr*>(addr6);
+  }
+  else {
+    auto* addr4 = new sockaddr_in;
+    addr4->sin_family = AF_INET;
+    addr4->sin_port = netpp::h2n16(port);
+    netpp::inet_pton(AF_INET, host, &addr4->sin_addr);
+    addr_ = reinterpret_cast<struct sockaddr*>(addr4);
+  }
+}
+
+Address::~Address(void) {
+  delete addr_;
+}
+
+int Address::get_family(void) const {
+  return addr_->sa_family;
+}
+
+std::string Address::get_host(void) const {
+  char buf[64]{};
+  if (addr_->sa_family == AF_INET6) {
+    auto* addr6 = reinterpret_cast<struct sockaddr_in6*>(addr_);
+    netpp::inet_ntop(AF_INET6, &addr6->sin6_addr, sizeof(buf), buf);
+  }
+  else {
+    auto* addr4 = reinterpret_cast<struct sockaddr_in*>(addr_);
+    netpp::inet_ntop(AF_INET, &addr4->sin_addr, sizeof(buf), buf);
+  }
+
+  return std::string(buf);
+}
+
+std::uint16_t Address::get_port(void) const {
+  if (addr_->sa_family == AF_INET6) {
+    auto* addr6 = reinterpret_cast<struct sockaddr_in6*>(addr_);
+    return addr6->sin6_port;
+  }
+  else {
+    auto* addr4 = reinterpret_cast<struct sockaddr_in*>(addr_);
+    return addr4->sin_port;
+  }
+}
+
+}
---
+++ network/netproj/netpp/address.h
@@ -0,0 +1,58 @@
+// Copyright (c) 2017 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <Chaos/Base/UnCopyable.h>
+
+struct sockaddr;
+struct sockaddr_in;
+struct sockaddr_in6;
+
+namespace netpp {
+
+class Address : private Chaos::UnCopyable {
+  struct sockaddr* addr_{};
+public:
+  explicit Address(const struct sockaddr_in& addr);
+  explicit Address(const struct sockaddr_in6& addr6);
+  explicit Address(
+      std::uint16_t port, bool loopback = false, bool with_v6 = false);
+  Address(const char* host, std::uint16_t port, bool with_v6 = false);
+  ~Address(void);
+  int get_family(void) const;
+  std::string get_host(void) const;
+  std::uint16_t get_port(void) const;
+
+  const struct sockaddr* get_address(void) const {
+    return addr_;
+  }
+
+};
+
+}
