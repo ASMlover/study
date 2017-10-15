c5c4f234925236c4c1455f9a1683f17d19b79b4b
ASMlover <asmlover@126.com>
2017-10-15 19:30:31 +0800

    :construction: chore(pymem): updated the benchmark for using pymem

--- c/memory/pymem/main.c
+++ c/memory/pymem/main.c
@@ -34,34 +34,19 @@
 extern void* PyObject_Malloc(size_t nbytes);
 extern void PyObject_Free(void* p);
 
-int main(int argc, char* argv[]) {
-  (void)argc, (void)argv;
+static const int ALLOC_COUNT = 1000000;
+static const int TEST_COUNT = 100;
 
-#if defined(PYMEM_PYTHON)
-  fprintf(stdout, "memory pool testing: use python allocating ...\n");
-#else
-  fprintf(stdout, "memory pool testing: use self allocating ...\n");
-#endif
-
-  static const int ALLOC_COUNT = 1000000;
-  static const int TEST_COUNT = 100;
-  int* alloc_array = (int*)malloc(ALLOC_COUNT * sizeof(int));
-
-  srand((unsigned int)time(NULL));
-  for (int i = 0; i < ALLOC_COUNT; ++i) {
-    alloc_array[i] = rand() % 512;
-    if (alloc_array[i] == 0)
-      alloc_array[i] = 1;
-  }
-
+static void bench_test1(int alloc_size_array[]) {
   int count = 0;
 
+  fprintf(stdout, "#################### [bench_test1] ####################\n");
   while (count++ < TEST_COUNT) {
-    char* p;
+    void* p;
     clock_t beg = clock();
     clock_t end;
     for (int i = 0; i < ALLOC_COUNT; ++i) {
-      p = (char*)malloc(alloc_array[i]);
+      p = malloc(alloc_size_array[i]);
       free(p);
     }
     end = clock();
@@ -72,11 +57,11 @@ int main(int argc, char* argv[]) {
     beg = clock();
     for (int i = 0; i < ALLOC_COUNT; ++i) {
 #if defined(PYMEM_PYTHON)
-      p = (char*)pymem_alloc(alloc_array[i]);
-      pymem_dealloc(p);
-#else
-      p = (char*)PyObject_Malloc(alloc_array[i]);
+      p = PyObject_Malloc(alloc_size_array[i]);
       PyObject_Free(p);
+#else
+      p = pymem_alloc(alloc_size_array[i]);
+      pymem_dealloc(p);
 #endif
     }
     end = clock();
@@ -84,6 +69,70 @@ int main(int argc, char* argv[]) {
         "[pymem allocator][%d] %ld ~ %ld, used: %ld\n",
         count, beg, end, end - beg);
   }
+}
+
+static void bench_test2(int alloc_size_array[]) {
+  int count = 0;
+
+  fprintf(stdout, "#################### [bench_test2] ####################\n");
+  while (count++ < TEST_COUNT) {
+    void** alloc_memory_list = (void**)malloc(sizeof(void*) * ALLOC_COUNT);
+    clock_t beg = clock();
+    clock_t end;
+    for (int i = 0; i < ALLOC_COUNT; ++i) {
+      alloc_memory_list[i] = malloc(alloc_size_array[i]);
+    }
+    for (int i = 0; i < ALLOC_COUNT; ++i)
+      free(alloc_memory_list[i]);
+    end = clock();
+    fprintf(stdout,
+        "[system allocator][%d] %ld ~ %ld, used: %ld\n",
+        count, beg, end, end - beg);
+
+    beg = clock();
+    for (int i = 0; i < ALLOC_COUNT; ++i) {
+#if defined(PYMEM_PYTHON)
+      alloc_memory_list[i] = PyObject_Malloc(alloc_size_array[i]);
+#else
+      alloc_memory_list[i] = pymem_alloc(alloc_size_array[i]);
+#endif
+    }
+    for (int i = 0; i < ALLOC_COUNT; ++i) {
+#if defined(PYMEM_PYTHON)
+      PyObject_Free(alloc_memory_list[i]);
+#else
+      pymem_dealloc(alloc_memory_list[i]);
+#endif
+    }
+    end = clock();
+    fprintf(stdout,
+        "[pymem allocator][%d] %ld ~ %ld, used: %ld\n",
+        count, beg, end, end - beg);
+
+    free(alloc_memory_list);
+  }
+}
+
+int main(int argc, char* argv[]) {
+  (void)argc, (void)argv;
+
+#if defined(PYMEM_PYTHON)
+  fprintf(stdout, "memory pool testing: use python allocating ...\n");
+#else
+  fprintf(stdout, "memory pool testing: use self allocating ...\n");
+#endif
+
+  int* alloc_array = (int*)malloc(ALLOC_COUNT * sizeof(int));
+
+  srand((unsigned int)time(NULL));
+  for (int i = 0; i < ALLOC_COUNT; ++i) {
+    alloc_array[i] = rand() % 512;
+    if (alloc_array[i] == 0)
+      alloc_array[i] = 1;
+  }
+
+  bench_test1(alloc_array);
+  bench_test2(alloc_array);
 
   free(alloc_array);
 
