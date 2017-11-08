84663721d00617c072582a9882873dac1691c12c
ASMlover <asmlover@126.com>
2017-11-08 23:39:59 +0800

    :construction: chore(demo): updated the demo using netpp

--- network/netproj/netpp/main.cc
+++ network/netproj/netpp/main.cc
@@ -39,37 +39,105 @@
 #include "acceptor.h"
 #include "socket_service.h"
 
+class TcpConnection
+  : private Chaos::UnCopyable
+  , public std::enable_shared_from_this<TcpConnection> {
+  netpp::TcpSocket socket_;
+  std::vector<char> buffer_;
+
+  static constexpr std::size_t kMaxBuffer = 1024;
+
+  void do_read(void) {
+    auto self(shared_from_this());
+    socket_.async_read_some(netpp::buffer(buffer_),
+        [this, self](const std::error_code& ec, std::size_t n) {
+          if (!ec)
+            do_write(n);
+        });
+  }
+
+  void do_write(std::size_t n) {
+    auto self(shared_from_this());
+    socket_.async_write_some(netpp::buffer(buffer_, n),
+        [this, self](const std::error_code& ec, std::size_t) {
+          if (!ec)
+            do_read();
+        });
+  }
+public:
+  TcpConnection(netpp::TcpSocket&& s)
+    : socket_(std::move(s))
+    , buffer_(kMaxBuffer) {
+  }
+
+  void start(void) {
+    do_read();
+  }
+};
+
+class EchoTcpServer : private Chaos::UnCopyable {
+  netpp::Acceptor acceptor_;
+  netpp::TcpSocket socket_;
+
+  void do_accept(void) {
+    acceptor_.async_accept(socket_, [this](const std::error_code& ec) {
+          if (!ec)
+            std::make_shared<TcpConnection>(std::move(socket_));
+
+          do_accept();
+        });
+  }
+public:
+  EchoTcpServer(netpp::SocketService& service, std::uint16_t port)
+#if defined(CHAOS_POSIX)
+    : acceptor_(service, netpp::Address(netpp::IP::v6(), port))
+#else
+    : acceptor_(service, netpp::Address(netpp::IP::v4(), port))
+#endif
+    , socket_(service) {
+    acceptor_.set_non_blocking(true);
+  }
+
+  void start(void) {
+    do_accept();
+  }
+};
+
 void echo_tcp_server(void) {
   netpp::SocketService service;
-#if defined(CHAOS_POSIX)
-  netpp::Acceptor acceptor(service, netpp::Address(netpp::IP::v6(), 5555));
-#else
-  netpp::Acceptor acceptor(service, netpp::Address(netpp::IP::v4(), 5555));
-#endif
-  acceptor.set_non_blocking(true);
-  std::vector<std::unique_ptr<std::thread>> threads;
-  netpp::TcpSocket conn(service);
-  acceptor.async_accept(conn,
-      [&threads, &acceptor, &conn](const std::error_code& ec) {
-        if (!ec) {
-          threads.emplace_back(new std::thread([](netpp::TcpSocket&& conn) {
-                  std::error_code ec;
-                  std::vector<char> buf(1024);
-                  for (;;) {
-                    auto n = conn.read_some(netpp::buffer(buf));
-                    if (n > 0)
-                      conn.write_some(netpp::buffer(buf, n));
-                    else
-                      break;
-                  }
-                  conn.close();
-                }, std::move(conn)));
-        }
-      });
+  EchoTcpServer server(service, 5555);
+  server.start();
+
+// #if defined(CHAOS_POSIX)
+//   netpp::Acceptor acceptor(service, netpp::Address(netpp::IP::v6(), 5555));
+// #else
+//   netpp::Acceptor acceptor(service, netpp::Address(netpp::IP::v4(), 5555));
+// #endif
+//   acceptor.set_non_blocking(true);
+//   std::vector<std::unique_ptr<std::thread>> threads;
+//   netpp::TcpSocket conn(service);
+//   acceptor.async_accept(conn,
+//       [&threads, &acceptor, &conn](const std::error_code& ec) {
+//         if (!ec) {
+//           threads.emplace_back(new std::thread([](netpp::TcpSocket&& conn) {
+//                   std::error_code ec;
+//                   std::vector<char> buf(1024);
+//                   for (;;) {
+//                     auto n = conn.read_some(netpp::buffer(buf));
+//                     if (n > 0)
+//                       conn.write_some(netpp::buffer(buf, n));
+//                     else
+//                       break;
+//                   }
+//                   conn.close();
+//                 }, std::move(conn)));
+//         }
+//       });
 
   service.run();
-  for (auto& t : threads)
-    t->join();
+
+//   for (auto& t : threads)
+//     t->join();
 }
 
 void echo_tcp_client(void) {
