96446b8f787214100e1be0c1c5fe31684158e190
ASMlover <asmlover@126.com>
2017-12-29 00:11:35 +0800

    :construction: chore(libco): updated the libco implementation

--- cplusplus/libco/libco/main.cc
+++ cplusplus/libco/libco/main.cc
@@ -25,7 +25,7 @@
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 #include <iostream>
-#include "mutex.h"
+#include "co_mutex.h"
 
 int main(int argc, char* argv[]) {
   (void)argc, (void)argv;
