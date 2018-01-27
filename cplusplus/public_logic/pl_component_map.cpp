d64f48350665313af0f1fbb02b4911556da4bdcb
ASMlover <asmlover@126.com>
2018-01-27 19:11:37 +0800

    :construction: chore(map): updated the implementation of component map wrapper for python

--- cplusplus/public_logic/pl_component_map.cpp
+++ cplusplus/public_logic/pl_component_map.cpp
@@ -3,67 +3,80 @@
 namespace public_logic {
 
 ComponentPtr& ComponentMap::getitem(int key) {
-	if (find(key) == end()) {
-		PyErr_SetString(PyExc_KeyError, "key not in C++ map");
-		py::throw_error_already_set();
-	}
-	return operator[](key);
+  if (find(key) == end()) {
+    PyErr_SetString(PyExc_KeyError, "key not in C++ map");
+    py::throw_error_already_set();
+  }
+  return operator[](key);
 }
 
 void ComponentMap::delitem(int key) {
-	auto pos = find(key);
-	if (pos == end()) {
-		PyErr_SetString(PyExc_KeyError, "key not in C++ map");
-		py::throw_error_already_set();
-	}
-	erase(pos);
+  auto pos = find(key);
+  if (pos == end()) {
+    PyErr_SetString(PyExc_KeyError, "key not in C++ map");
+    py::throw_error_already_set();
+  }
+  erase(pos);
 }
 
-py::list ComponentMap::keys(void) {
-	py::list r;
-	for (auto& v : *this)
-		r.append(v.first);
-	return r;
+py::object ComponentMap::get(int key, const py::object& d) const {
+  auto pos = find(key);
+  if (pos == end())
+    return d;
+
+  py::return_by_value::apply<ComponentPtr>::type convertor;
+  return py::object(py::handle<>(convertor(pos->second)));
 }
 
-py::list ComponentMap::values(void) {
-	py::list r;
-	for (auto& v : *this)
-		r.append(v.second);
-	return r;
+py::object ComponentMap::pop(int key, const py::object& d) {
+  auto pos = find(key);
+  if (pos == end())
+    return d;
+
+  py::return_by_value::apply<ComponentPtr>::type convertor;
+  auto r = py::object(py::handle<>(convertor(pos->second)));
+
+  erase(pos);
+  return r;
 }
 
-py::list ComponentMap::items(void) {
-	py::list r;
-	for (auto& v : *this)
-		r.append(py::make_tuple(v.first, v.second));
-	return r;
+py::list ComponentMap::keys(void) const {
+  py::list r;
+  for (auto& v : *this)
+    r.append(v.first);
+  return r;
+}
+
+py::list ComponentMap::values(void) const {
+  py::list r;
+  for (auto& v : *this)
+    r.append(v.second);
+  return r;
+}
+
+py::list ComponentMap::items(void) const {
+  py::list r;
+  for (auto& v : *this)
+    r.append(py::make_tuple(v.first, v.second));
+  return r;
 }
 
 void ComponentMap::update(const ComponentMap& other) {
-	for (auto& v : other)
-		operator[](v.first) = v.second;
+  for (auto& v : other)
+    operator[](v.first) = v.second;
 }
 
 py::tuple ComponentMap::popitem(void) {
-	auto pos = begin();
-	if (pos == end()) {
-		PyErr_SetString(PyExc_KeyError, "popitem(): C++ map is empty");
-		py::throw_error_already_set();
-	}
+  auto pos = begin();
+  if (pos == end()) {
+    PyErr_SetString(PyExc_KeyError, "popitem(): C++ map is empty");
+    py::throw_error_already_set();
+  }
 
-	auto r = py::make_tuple(pos->first, pos->second);
-	erase(pos);
+  auto r = py::make_tuple(pos->first, pos->second);
+  erase(pos);
 
-	return r;
-}
-
-py::object ComponentMap::get(const py::object& m, const py::object& k, const py::object& d) {
-	const auto& self = py::extract<const ComponentMap&>(m)();
-	int key = py::extract<int>(k)();
-	if (self.find(key) == self.end())
-		return d;
-	return m[k];
+  return r;
 }
 
 }
--- cplusplus/public_logic/pl_component_map.h
+++ cplusplus/public_logic/pl_component_map.h
@@ -8,31 +8,51 @@ struct ComponentBase;
 using ComponentPtr = boost::shared_ptr<ComponentBase>;
 
 struct ComponentMap : public std::map<int, ComponentPtr> {
-	inline py::object iter(void) {
-		py::handle<> it(PyObject_GetIter(keys().ptr()));
-		return py::object(it);
-	}
+  inline py::object iter(void) const {
+    py::handle<> it(PyObject_GetIter(keys().ptr()));
+    return py::object(it);
+  }
 
-	inline py::tuple getinitargs(void) {
-		return py::make_tuple(py::dict(items()));
-	}
+  inline py::object iterkeys(void) const {
+    py::handle<> it(PyObject_GetIter(keys().ptr()));
+    return py::object(it);
+  }
 
-	inline bool contains(int key) const {
-		return find(key) != end();
-	}
+  inline py::object itervalues(void) const {
+    py::handle<> it(PyObject_GetIter(values().ptr()));
+    return py::object(it);
+  }
 
-	inline void setitem(int key, const ComponentPtr& val) {
-		operator[](key) = val;
-	}
+  inline py::object iteritems(void) const {
+    py::handle<> it(PyObject_GetIter(items().ptr()));
+    return py::object(it);
+  }
 
-	ComponentPtr& getitem(int key);
-	void delitem(int key);
-	py::list keys(void);
-	py::list values(void);
-	py::list items(void);
-	void update(const ComponentMap& other);
-	py::tuple popitem(void);
-	static py::object get(const py::object& m, const py::object& k, const py::object& d = py::object());
+  inline py::tuple getinitargs(void) const {
+    return py::make_tuple(py::dict(items()));
+  }
+
+  inline py::object as_dict(void) const {
+    return map_as_dict(*this);
+  }
+
+  inline bool contains(int key) const {
+    return find(key) != end();
+  }
+
+  inline void setitem(int key, const ComponentPtr& val) {
+    operator[](key) = val;
+  }
+
+  ComponentPtr& getitem(int key);
+  void delitem(int key);
+  py::object get(int key, const py::object& d = py::object()) const;
+  py::object pop(int key, const py::object& d = py::object());
+  py::list keys(void) const;
+  py::list values(void) const;
+  py::list items(void) const;
+  void update(const ComponentMap& other);
+  py::tuple popitem(void);
 };
 
 }
