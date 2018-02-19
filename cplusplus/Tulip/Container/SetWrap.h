134536322373b0c6749ccbb5c120ad7d46b97d3f
ASMloer <asmlover@126.com>
2018-02-19 10:41:21 +0800

    :fire: refactor(set): refactoring the implementation of set binding

--- cplusplus/Tulip/Container/SetWrap.h
+++ cplusplus/Tulip/Container/SetWrap.h
@@ -31,46 +31,86 @@
 
 namespace tulip {
 
-template <typename _Tp>
-struct SetWrap : public std::set<_Tp> {
-  using SetType = SetWrap<_Tp>;
+template <typename Container>
+struct SetWrap {
+  using ValueType = typename Container::value_type;
+  using SizeType = typename Container::size_type;
 
-  inline bool contains(const _Tp& x) const {
-    return this->find(x) != this->end();
+  static Container set_copy(const Container& self) {
+    Container r;
+    for (auto& x : self)
+      r.insert(x);
+    return r;
   }
 
-  inline void insert_item(const _Tp& x) {
-    this->insert(x);
+  static bool set_contains(const Container& self, const ValueType& x) {
+    return self.find(x) != self.end();
   }
 
-  inline void extend(const SetType& other) {
-    this->insert(other.begin(), other.end());
+  static void set_add(Container& self, const ValueType& x) {
+    self.insert(x);
   }
 
-  inline py::object as_list(void) const {
-    return set_as_list(*this);
+  static void set_discard(Container& self, const ValueType& x) {
+    auto pos = self.find(x);
+    if (pos != self.end())
+      self.erase(pos);
   }
 
-  inline py::tuple getinitargs(void) const {
-    return py::make_tuple(py::tuple(*this));
+  static ValueType set_pop(Container& self) {
+    auto pos = self.begin();
+    if (pos == self.end()) {
+      PyErr_SetString(PyExc_KeyError, "pop from an empty set");
+      py::throw_error_already_set();
+      return ValueType();
+    }
+
+    auto r = *pos;
+    self.erase(pos);
+    return r;
   }
 
-  static void wrap(const std::string& name) {
-    py::class_<SetType, boost::shared_ptr<SetType>>(name.c_str())
-      .def(py::init<const SetType&>())
-      .def("empty", &SetType::empty)
-      .def("size", &SetType::size)
-      .def("add", &SetType::insert_item)
-      .def("append", &SetType::insert_item)
-      .def("discard", (std::size_t (SetType::*)(const _Tp&))&SetType::erase)
-      .def("erase", (std::size_t (SetType::*)(const _Tp&))&SetType::erase)
-      .def("extend", &SetType::extend)
-      .def("clear", &SetType::clear)
-      .def("as_list", &SetType::as_list)
-      .def("__len__", &SetType::size)
-      .def("__contains__", &SetType::contains)
-      .def("__iter__", py::iterator<SetType>())
-      .def("__getinitargs__", &SetType::getinitargs)
+  static void set_remove(Container& self, const ValueType& x) {
+    auto pos = self.find(x);
+    if (pos == self.end()) {
+      PyErr_SetString(PyExc_KeyError, "key not in set");
+      py::throw_error_already_set();
+      return;
+    }
+    self.erase(pos);
+  }
+
+  static void set_update(Container& self, const py::object& other) {
+    // TODO:
+  }
+
+  static void set_foreach(Container& self, PyObject* callable) {
+    if (callable != nullptr) {
+      for (auto& x : self)
+        py::call<void>(callable, x);
+    }
+  }
+
+  static py::object as_list(const Container& self) {
+    return tulip::set_as_list(self);
+  }
+
+  static void wrap(const char* name) {
+    py::class_<Container, boost::shared_ptr<Container>>(name, py::init<>())
+      .def(py::init<const Container&>())
+      .def("copy", &SetWrap::set_copy)
+      .def("clear", &Container::clear)
+      .def("size", &Container::size)
+      .def("add", &SetWrap::set_add)
+      .def("discard", &SetWrap::set_discard)
+      .def("pop", &SetWrap::set_pop)
+      .def("remove", &SetWrap::set_remove)
+      .def("update", &SetWrap::set_update)
+      .def("foreach", &SetWrap::set_foreach)
+      .def("as_list", &SetWrap::as_list)
+      .def("__len__", &Container::size)
+      .def("__iter__", py::iterator<Container>())
+      .def("__contains__", &SetWrap::set_contains)
       ;
   }
 };
