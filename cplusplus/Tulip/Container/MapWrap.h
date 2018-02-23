974b43400c1853941237596f287ceebf24fbc20b
ASMloer <asmlover@126.com>
2018-02-23 15:06:23 +0800

    :construction: chore(map): updated the map python binding

--- cplusplus/Tulip/Container/MapWrap.h
+++ cplusplus/Tulip/Container/MapWrap.h
@@ -55,181 +55,158 @@ struct MapWrap {
     }
   };
 
+  static Container map_copy(const Container& self) {
+    Container r;
+    for (auto& x : self)
+      r[x.first] = x.second;
+    return r;
+  }
+
+  static bool map_contains(const Container& self, const KeyType& k) {
+    return self.find(k) != self.end();
+  }
+
+  static py::object map_get(
+      const Container& self, const KeyType& k, const py::object& d) {
+    auto pos = self.find(k);
+    if (pos == self.end())
+      return d;
+
+    return py::object(pos->second);
+  }
+
+  static py::object map_pop_1(Container& self, const KeyType& k) {
+    auto pos = self.find(k);
+    if (pos == self.end()) {
+      PyErr_SetString(PyExc_KeyError, "key not in c++ map");
+      py::throw_error_already_set();
+      return py::object();
+    }
+
+    auto r = py::object(pos->second);
+    self.erase(pos);
+    return r;
+  }
+
+  static py::object map_pop_2(
+      Container& self, const KeyType& k, const py::object& d) {
+    auto pos = self.find(k);
+    if (pos == self.end())
+      return d;
+
+    auto r = py::object(pos->second);
+    self.erase(pos);
+    return r;
+  }
+
+  static py::object map_popitem(Container& self) {
+    auto pos = self.begin();
+    if (pos == self.end()) {
+      PyErr_SetString(PyExc_KeyError, "pop from empty map");
+      py::throw_error_already_set();
+      return py::object();
+    }
+
+    auto r = py::make_tuple(pos->first, pos->second);
+    self.erase(pos);
+    return r;
+  }
+
+  static py::object map_keys(const Container& self) {
+    py::list r;
+    for (auto& x : self)
+      r.append(x.first);
+    return r;
+  }
+
+  static py::object map_values(const Container& self) {
+    py::list r;
+    for (auto& x : self)
+      r.append(x.second);
+    return r;
+  }
+
+  static py::object map_items(const Container& self) {
+    py::list r;
+    for (auto& x : self)
+      r.append(py::make_tuple(x.first, x.second));
+    return r;
+  }
+
+  static void map_setitem(
+      Container& self, const KeyType& k, const ValueType& v) {
+    self[k] = v;
+  }
+
+  static py::object map_getitem(const Container& self, const KeyType& k) {
+    auto pos = self.find(k);
+    if (pos == self.end()) {
+      PyErr_SetString(PyExc_KeyError, "key not found in c++ map");
+      py::throw_error_already_set();
+      return py::object();
+    }
+
+    return py::object(pos->second);
+  }
+
+  static void map_delitem(Container& self, const KeyType& k) {
+    auto pos = self.find(k);
+    if (pos == self.end()) {
+      PyErr_SetString(PyExc_KeyError, "key not found in c++ map");
+      py::throw_error_already_set();
+      return;
+    }
+    self.erase(pos);
+  }
+
+  static void map_update(py::object& self, const py::object& other) {
+    auto keys = other.attr("keys")();
+    auto n = keys.attr("__len__")();
+    for (auto i = 0; i < n; ++i) {
+      auto k = keys.attr("__getitem__")(i);
+      self.attr("__setitem__")(k, other.attr("__getitem__")(k));
+    }
+  }
+
+  static void map_foreach(Container& self, PyObject* callable) {
+    if (callable != nullptr) {
+      for (auto& x : self)
+        py::call<void>(callable, x.first, x.second);
+    }
+  }
+
+  static py::object as_dict(const Container& self) {
+    return map_as_dict(self);
+  }
+
   static void wrap(const char* name) {
     py::class_<Container, boost::shared_ptr<Container>>(name, py::init<>())
       .def(py::init<const Container&>())
+      .def("copy", &MapWrap::map_copy)
+      .def("has_key", &MapWrap::map_contains)
       .def("size", &Container::size)
+      .def("clear", &Container::clear)
+      .def("get", &MapWrap::map_get, (py::arg("d") = py::object()))
+      .def("pop", &MapWrap::map_pop_1)
+      .def("pop", &MapWrap::map_pop_2)
+      .def("popitem", &MapWrap::map_popitem)
+      .def("keys", &MapWrap::map_keys)
+      .def("values", &MapWrap::map_values)
+      .def("items", &MapWrap::map_items)
       .def("iterkeys", MakeTransform<Container, Iterkeys>::make())
       .def("itervalues", MakeTransform<Container, Itervalues>::make())
       .def("iteritems", MakeTransform<Container, Iteritems>::make())
+      .def("update", &MapWrap::map_update)
+      .def("foreach", &MapWrap::map_foreach)
+      .def("as_dict", &MapWrap::as_dict)
       .def("__len__", &Container::size)
       .def("__iter__", MakeTransform<Container, Iterkeys>::make())
+      .def("__contains__", &MapWrap::map_contains)
+      .def("__setitem__", &MapWrap::map_setitem)
+      .def("__getitem__", &MapWrap::map_getitem)
+      .def("__delitem__", &MapWrap::map_delitem)
       ;
   }
 };
-
-// template <typename _KeyTp, typename _ValTp>
-// struct MapWrap : public std::map<_KeyTp, _ValTp> {
-//   using MapType = MapWrap<_KeyTp, _ValTp>;
-//   using ItemType = typename MapType::value_type;
-//   using ConstIterator = typename MapType::const_iterator;
-//
-//   template <typename _TransformTp> struct MakeTransform {
-//     using Iterator = boost::transform_iterator<_TransformTp, ConstIterator>;
-//
-//     static Iterator begin(const MapType& m) {
-//       return boost::make_transform_iterator(m.begin(), _TransformTp());
-//     }
-//
-//     static Iterator end(const MapType& m) {
-//       return boost::make_transform_iterator(m.end(), _TransformTp());
-//     }
-//
-//     static py::object range(void) {
-//       return py::range(&begin, &end);
-//     }
-//   };
-//
-//   struct IterKeys {
-//     inline _KeyTp operator()(const ItemType& x) const {
-//       return x.first;
-//     }
-//   };
-//
-//   struct IterValues {
-//     inline _ValTp operator()(const ItemType& x) const {
-//       return x.second;
-//     }
-//   };
-//
-//   struct IterItems {
-//     inline py::object operator()(const ItemType& x) const {
-//       return py::make_tuple(x.first, x.second);
-//     }
-//   };
-//
-//   inline bool contains(const _KeyTp& k) const {
-//     return this->find(k) != this->end();
-//   }
-//
-//   inline void setitem(const _KeyTp& k, const _ValTp& v) {
-//     this->operator[](k) = v;
-//   }
-//
-//   inline _ValTp& getitem(const _KeyTp& k) {
-//     auto pos = this->find(k);
-//     if (pos == this->end()) {
-//       PyErr_SetString(PyExc_KeyError, "Key not in C++ map.");
-//       py::throw_error_already_set();
-//     }
-//     return pos->second;
-//   }
-//
-//   inline void delitem(const _KeyTp& k) {
-//     auto pos = this->find(k);
-//     if (pos == this->end()) {
-//       PyErr_SetString(PyExc_KeyError, "Key not in C++ map.");
-//       py::throw_error_already_set();
-//     }
-//     this->erase(pos);
-//   }
-//
-//   inline py::object as_dict(void) const {
-//     return map_as_dict(*this);
-//   }
-//
-//   inline py::tuple getinitargs(void) const {
-//     return py::make_tuple(py::dict(*this));
-//   }
-//
-//   py::object get(const _KeyTp& k, const py::object& d = py::object()) {
-//     auto pos = this->find(k);
-//     if (pos == this->end())
-//       return d;
-//
-//     typename py::return_by_value::apply<_ValTp>::type convertor;
-//     return _tulip_pyobject(convertor(pos->second));
-//   }
-//
-//   py::object pop(const _KeyTp& k, const py::object& d = py::object()) {
-//     auto pos = this->find(k);
-//     if (pos == this->end())
-//       return d;
-//
-//     typename py::return_by_value::apply<_ValTp>::type convertor;
-//     auto r = _tulip_pyobject(convertor(pos->second));
-//
-//     this->erase(pos);
-//     return r;
-//   }
-//
-//   py::list keys(void) const {
-//     py::list r;
-//     for (auto& x : *this)
-//       r.append(x.first);
-//     return r;
-//   }
-//
-//   py::list values(void) const {
-//     py::list r;
-//     for (auto& x : *this)
-//       r.append(x.second);
-//     return r;
-//   }
-//
-//   py::list items(void) const {
-//     py::list r;
-//     for (auto& x : *this)
-//       r.append(py::make_tuple(x.first, x.second));
-//     return r;
-//   }
-//
-//   void update(const MapType& other) {
-//     for (auto& x : other)
-//       this->operator[](x.first) = x.second;
-//   }
-//
-//   py::tuple popitem(void) {
-//     auto pos = this->begin();
-//     if (pos == this->end()) {
-//       PyErr_SetString(PyExc_KeyError, "popitem(): C++ map is empty.");
-//       py::throw_error_already_set();
-//     }
-//
-//     auto r = py::make_tuple(pos->first, pos->second);
-//     this->erase(pos);
-//
-//     return r;
-//   }
-//
-//   static void wrap(const std::string& name) {
-//     py::class_<MapType, boost::shared_ptr<MapType>>(name.c_str())
-//       .def(py::init<const MapType&>())
-//       .def("empty", &MapType::empty)
-//       .def("size", &MapType::size)
-//       .def("has_key", &MapType::contains)
-//       .def("clear", &MapType::clear)
-//       .def("get", &MapType::get, (py::arg("d") = py::object()))
-//       .def("pop", &MapType::pop, (py::arg("d") = py::object()))
-//       .def("erase", (std::size_t (MapType::*)(const _KeyTp&))&MapType::erase)
-//       .def("keys", &MapType::keys)
-//       .def("values", &MapType::values)
-//       .def("items", &MapType::items)
-//       .def("iterkeys", MakeTransform<IterKeys>::range())
-//       .def("itervalues", MakeTransform<IterValues>::range())
-//       .def("iteritems", MakeTransform<IterItems>::range())
-//       .def("update", &MapType::update)
-//       .def("popitem", &MapType::popitem)
-//       .def("as_dict", &MapType::as_dict)
-//       .def("__len__", &MapType::size)
-//       .def("__contains__", &MapType::contains)
-//       .def("__setitem__", &MapType::setitem)
-//       .def("__getitem__", &MapType::getitem, py::return_value_policy<py::copy_non_const_reference>())
-//       .def("__delitem__", &MapType::delitem)
-//       .def("__iter__", MakeTransform<IterKeys>::range())
-//       .def("__getinitargs__", &MapType::getinitargs)
-//       ;
-//   }
-// };
 
 }
