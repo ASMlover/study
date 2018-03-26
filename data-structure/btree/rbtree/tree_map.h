394a76d7679df873a13d32a44add2a20f1a8a6d3
ASMlover <asmlover@126.com>
2018-03-26 23:27:45 +0800

    :sparkles: feat(treemap): add tree map

---
+++ data-structure/btree/rbtree/tree_map.h
@@ -0,0 +1,54 @@
+// Copyright (c) 2018 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+namespace v1 {
+
+struct __tree_node {
+  enum __color { BLACK, RED };
+  __tree_node* left{};
+  __tree_node* right{};
+  __tree_node* parent{};
+  int key{};
+  int value{};
+  int color{BLACK};
+
+  __tree_node(int k, int v, __tree_node* p) : key(k), value(v), parent(p) {
+  }
+  int get_key(void) const { return key; }
+  int get_value(void) const { return value; }
+  void set_value(int v) { value = v; }
+};
+
+class tree_map {
+  __tree_node* root_{};
+
+  void rotate_left(__tree_node* node);
+public:
+};
+
+}
