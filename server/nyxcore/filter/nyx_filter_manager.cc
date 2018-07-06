860deb2c387f76155828fca6b744ab73a9305b1d
ASMlover <ASMlover@126.com>
2018-07-06 20:49:48 +0800

    :construction: chore(filter): updated the implementation of filter manager

--- server/nyxcore/filter/nyx_filter_manager.cc
+++ server/nyxcore/filter/nyx_filter_manager.cc
@@ -24,15 +24,82 @@
 // LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
+#include <cstdlib>
+#include <ctime>
+#include "nyx_int_filter.h"
+#include "nyx_float_filter.h"
+#include "nyx_str_filter.h"
 #include "nyx_filter_manager.h"
 
 namespace nyx { namespace filter {
 
+static const char* kUsageError = "Usage Error";
+
 void filter_manager::delete_filter_randomly(void) {
+  if (str_filters_.empty())
+    return;
+
+  std::srand(std::time(0));
+  int bucket;
+  int bucket_size;
+  do {
+    bucket = std::rand() % str_filters_.bucket_count();
+  } while ((bucket_size = str_filters_.bucket_size(bucket)) == 0);
+  auto it = str_filters_.begin(bucket);
+  std::advance(it, std::rand() % bucket_size);
+  del_filter(it->second);
 }
 
 filter_ptr filter_manager::from_object(PyObject* v) {
   filter_ptr filter;
+  if (!PyTuple_Check(v) || (Py_SIZE(v) != 3)) {
+    PyErr_SetString(PyExc_TypeError, kUsageError);
+    return filter;
+  }
+  auto* type = PyTuple_GET_ITEM(v, 0);
+  auto* key = PyTuple_GET_ITEM(v, 1);
+  auto* val = PyTuple_GET_ITEM(v, 2);
+  if (!PyInt_Check(type) || !PyString_Check(key)) {
+    PyErr_SetString(PyExc_TypeError, kUsageError);
+    return filter;
+  }
+
+  auto raw_type = PyInt_AsSsize_t(type);
+  if (raw_type == -1)
+    return filter;
+  const char* raw_key = PyString_AsString(key);
+  if (raw_key == nullptr)
+    return filter;
+
+  if (PyInt_Check(val)) {
+    auto raw_val = PyInt_AsSsize_t(val);
+    if (raw_val == -1 && PyErr_Occurred())
+      return filter;
+    filter = std::make_shared<int_filter>(
+        static_cast<filter::filter_type>(raw_type), raw_key, raw_val);
+    return filter;
+  }
+  else if (PyFloat_Check(val)) {
+    auto raw_val = PyFloat_AsDouble(val);
+    if (raw_val == -1.f && PyErr_Occurred())
+      return filter;
+    filter = std::make_shared<float_filter>(
+        static_cast<filter::filter_type>(raw_type), raw_key, raw_val);
+    return filter;
+  }
+  else if (PyString_Check(val)) {
+    auto raw_val = PyString_AsString(val);
+    if (raw_val == nullptr)
+      return filter;
+    filter = std::make_shared<str_filter>(
+        static_cast<filter::filter_type>(raw_type), raw_key, raw_val);
+    return filter;
+  }
+  else {
+    PyErr_SetString(PyExc_TypeError, kUsageError);
+    return filter;
+  }
+
   return filter;
 }
 
