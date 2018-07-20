56447c33912b357b706f1963a72070cabba5095e
ASMlover <ASMlover@126.com>
2018-07-20 20:44:07 +0800

    :construction: chore(helper): finished the implementation of service helpers

--- server/nyxcore/rpc/nyx_rpc_service_helper.cc
+++ server/nyxcore/rpc/nyx_rpc_service_helper.cc
@@ -24,4 +24,48 @@
 // LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
+#include "../nyx_service.h"
 #include "nyx_rpc_service_helper.h"
+
+namespace nyx { namespace rpc {
+
+rpc_service_call_helper::rpc_service_call_helper(
+    base_service* service,
+    const nyx::callback_guard_ptr& guard,
+    const pb::MethodDescriptor* method,
+    const pb::Message* request)
+  : service_(service)
+  , guard_(guard)
+  , method_(method)
+  , request_(request) {
+}
+
+rpc_service_call_helper::~rpc_service_call_helper(void) {
+  if (request_ != nullptr)
+    delete request_;
+}
+
+void rpc_service_call_helper::call_method(void) {
+  if (guard_->is_calling())
+    service_->do_call_method(method_, request_);
+}
+
+callback_service_call_helper::callback_service_call_helper(
+    const channel_callback& callback)
+  : callback_(callback) {
+}
+
+callback_service_call_helper::callback_service_call_helper(
+    channel_callback&& callback)
+  : callback_(std::move(callback)) {
+}
+
+callback_service_call_helper::~callback_service_call_helper(void) {
+}
+
+void callback_service_call_helper::call_method(void) {
+  if (callback_)
+    callback_();
+}
+
+}}
