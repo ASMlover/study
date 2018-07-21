20ce44d49ca211b62738aa3c07416196e251ecec
ASMlover <ASMlover@126.com>
2018-07-21 23:16:38 +0800

    :sparkles: feat(service.manager): add service manager implementation

---
+++ server/nyxcore/nyx_service_manager.cc
@@ -0,0 +1,53 @@
+// Copyright (c) 2018 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#include "nyx_service_manager.h"
+
+namespace nyx {
+
+service_ptr service_manager::get_service(
+    service_type type, net::connection* conn) {
+  service_created_ = true;
+  return factory_->create_service(type, conn);
+}
+
+void service_manager::push_request(rpc::service_call_helper* call_helper) {
+  requests_.push(call_helper);
+  // task_notify();
+}
+
+std::size_t service_manager::call_request(void) {
+  rpc::service_call_helper* call_helper;
+  std::size_t counter{};
+  while (requests_.pop(call_helper)) {
+    call_helper->call_method();
+    delete call_helper;
+    // TODO:
+  }
+  return counter;
+}
+
+}
