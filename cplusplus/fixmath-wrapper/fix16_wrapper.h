6e973b19daf041f9a25aa50d5b45d557ea11f973
ASMlover <asmlover@126.com>
2018-09-24 20:19:39 +0800

    :construction: chore(fix16): updated the operators of fix16 class

--- cplusplus/fixmath-wrapper/fix16_wrapper.h
+++ cplusplus/fixmath-wrapper/fix16_wrapper.h
@@ -40,24 +40,206 @@ public:
   fix16(void) noexcept {
   }
 
-  explicit fix16(fix16_t v) noexcept
+  fix16(fix16_t v) noexcept
     : value_(v) {
   }
 
-  explicit fix16(std::int16_t v) noexcept
+  fix16(std::int16_t v) noexcept
     : value_(fix16_from_int(v)) {
   }
 
-  explicit fix16(float v) noexcept
+  fix16(float v) noexcept
     : value_(fix16_from_float(v)) {
   }
 
-  explicit fix16(double v) noexcept
+  fix16(double v) noexcept
     : value_(fix16_from_dbl(v)) {
   }
 
-  explicit fix16(const fix16& o) noexcept
+  fix16(const fix16& o) noexcept
     : value_(o.value_) {
+  }
+
+  fix16(fix16&& o) noexcept {
+    std::swap(value_, o.value_);
+  }
+
+  explicit operator fix16_t(void) const noexcept {
+    return value_;
+  }
+
+  explicit operator std::int16_t(void) const noexcept {
+    return fix16_to_int(value_);
+  }
+
+  explicit operator float(void) const noexcept {
+    return fix16_to_float(value_);
+  }
+
+  explicit operator double(void) const noexcept {
+    return fix16_to_dbl(value_);
+  }
+
+  fix16& operator=(const fix16& r) noexcept {
+    if (this != &r)
+      value_ = r.value_;
+    return *this;
+  }
+
+  fix16& operator=(fix16&& r) noexcept {
+    if (this != &r)
+      std::swap(value_, r.value_);
+    return *this;
+  }
+
+  fix16& operator=(fix16_t r) noexcept {
+    value_ = r;
+    return *this;
+  }
+
+  fix16& operator=(std::int16_t r) noexcept {
+    value_ = fix16_from_int(r);
+    return *this;
+  }
+
+  fix16& operator=(float r) noexcept {
+    value_ = fix16_from_float(r);
+    return *this;
+  }
+
+  fix16& operator=(double r) noexcept {
+    value_ = fix16_from_float(r);
+    return *this;
+  }
+
+  fix16& operator+=(const fix16& r) noexcept {
+    value_ += r.value_;
+    return *this;
+  }
+
+  fix16& operator+=(fix16_t r) noexcept {
+    value_ += r;
+    return *this;
+  }
+
+  fix16& operator+=(std::int16_t r) noexcept {
+    value_ += fix16_from_int(r);
+    return *this;
+  }
+
+  fix16& operator+=(float r) noexcept {
+    value_ += fix16_from_float(r);
+    return *this;
+  }
+
+  fix16& operator+=(double r) noexcept {
+    value_ += fix16_from_dbl(r);
+    return *this;
+  }
+
+  fix16& operator-=(const fix16& r) noexcept {
+    value_ -= r.value_;
+    return *this;
+  }
+
+  fix16& operator-=(fix16_t r) noexcept {
+    value_ -= r;
+    return *this;
+  }
+
+  fix16& operator-=(std::int16_t r) noexcept {
+    value_ -= fix16_from_int(r);
+    return *this;
+  }
+
+  fix16& operator-=(float r) noexcept {
+    value_ -= fix16_from_float(r);
+    return *this;
+  }
+
+  fix16& operator-=(double r) noexcept {
+    value_ -= fix16_from_dbl(r);
+    return *this;
+  }
+
+  fix16& operator*=(const fix16& r) noexcept {
+    value_ = fix16_mul(value_, r.value_);
+    return *this;
+  }
+
+  fix16& operator*=(fix16_t r) noexcept {
+    value_ = fix16_mul(value_, r);
+    return *this;
+  }
+
+  fix16& operator*=(std::int16_t r) noexcept {
+    value_ *= r;
+    return *this;
+  }
+
+  fix16& operator*=(float r) noexcept {
+    value_ = fix16_mul(value_, fix16_from_float(r));
+    return *this;
+  }
+
+  fix16& operator*=(double r) noexcept {
+    value_ = fix16_mul(value_, fix16_from_dbl(r));
+    return *this;
+  }
+
+  fix16& operator/=(const fix16& r) noexcept {
+    value_ = fix16_div(value_, r.value_);
+    return *this;
+  }
+
+  fix16& operator/=(fix16_t r) noexcept {
+    value_ = fix16_div(value_, r);
+    return *this;
+  }
+
+  fix16& operator/=(std::int16_t r) noexcept {
+    value_ /= r;
+    return *this;
+  }
+
+  fix16& operator/=(float r) noexcept {
+    value_ = fix16_div(value_, fix16_from_float(r));
+    return *this;
+  }
+
+  fix16& operator/=(double r) noexcept {
+    value_ = fix16_div(value_, fix16_from_dbl(r));
+    return *this;
+  }
+
+  fix16 operator+(const fix16& x) const noexcept {
+    auto r = *this;
+    r += x;
+    return r;
+  }
+
+  fix16 operator+(fix16_t x) const noexcept {
+    auto r = *this;
+    r += x;
+    return r;
+  }
+
+  fix16 operator+(std::int16_t x) const noexcept {
+    auto r = *this;
+    r += x;
+    return r;
+  }
+
+  fix16 operator+(float x) const noexcept {
+    auto r = *this;
+    r += x;
+    return r;
+  }
+
+  fix16 operator+(double x) const noexcept {
+    auto r = *this;
+    r += x;
+    return r;
   }
 };
 
