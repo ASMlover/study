a7f57fa1882179c517c682ef8f5407a046f0da36
ASMlover <ASMlover@126.com>
2018-09-25 22:02:25 +0800

    :construction: chore(session): finished the implementation of tcp sesstion

--- network/rpc-examples/nyx-net/core/TcpSession.cc
+++ network/rpc-examples/nyx-net/core/TcpSession.cc
@@ -24,7 +24,46 @@
 // LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
+#include <iostream>
 #include "TcpSession.h"
 
 namespace nyx {
+
+TcpSession::TcpSession(tcp::socket&& socket)
+  : socket_(std::move(socket))
+  , buffer_(1024) {
+}
+
+TcpSession::~TcpSession(void) {
+}
+
+void TcpSession::do_read(void) {
+  auto self(shared_from_this());
+  socket_.async_read_some(asio::buffer(buffer_),
+      [this, self](const std::error_code& ec, std::size_t /*n*/) {
+        if (!ec) {
+          handle_data(buffer_);
+          do_read();
+        }
+        else {
+          std::cerr << "TcpSession::do_read - read data error" << std::endl;
+          socket_.close();
+        }
+      });
+}
+
+void TcpSession::do_write(const std::string& buf) {
+  do_write(buf.data(), buf.size());
+}
+
+void TcpSession::do_write(const char* buf, std::size_t len) {
+  auto self(shared_from_this());
+  asio::async_write(socket_, asio::buffer(buf, len),
+      [this, self](const std::error_code& /*ec*/, std::size_t /*n*/) {
+      });
+}
+
+void TcpSession::handle_data(std::vector<char>& buf) {
+}
+
 }
