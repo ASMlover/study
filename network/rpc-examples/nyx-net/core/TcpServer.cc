be6e4949849f26c1d568e6bd8e80f4548ba9757a
ASMlover <ASMlover@126.com>
2018-09-28 20:04:03 +0800

    :sparkles: feat(server): add tcp server module

---
+++ network/rpc-examples/nyx-net/core/TcpServer.cc
@@ -0,0 +1,53 @@
+// Copyright (c) 2018 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#include "TcpServer.h"
+
+namespace nyx {
+
+TcpServer::TcpServer(asio::io_context& context, std::uint16_t port)
+  : context_(context)
+  , acceptor_(context_, tcp::endpoint(tcp::v4(), port))
+  , socket_(context_) {
+}
+
+TcpServer::~TcpServer(void) {
+}
+
+void TcpServer::do_accept(void) {
+  acceptor_.async_accept(socket_,
+      [this](const std::error_code& ec) {
+        if (!ec) {
+          auto conn = std::make_shared<TcpSession>(std::move(socket_));
+          connections_.push_back(conn);
+
+          conn->do_read();
+        }
+        do_accept();
+      });
+}
+
+}
