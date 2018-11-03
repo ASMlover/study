b0bbd6058cf7d3f030947f2c9a60384a41d8bae6
ASMlover <asmlover@126.com>
2018-11-03 20:55:12 +0800

    :sparkles: feat(server): add server base class definitions

---
+++ network/rpc-examples/nyx-net/core_v2/BaseServer.h
@@ -0,0 +1,61 @@
+// Copyright (c) 2018 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+#include <memory>
+#include "Nyx.h"
+
+namespace nyx {
+
+class BaseServer
+  : private UnCopyable, public std::enable_shared_from_this<BaseServer> {
+public:
+  enum class Status : int {
+    INIT,
+    STARTED,
+    STOPED,
+  };
+protected:
+  asio::io_context& context_;
+  Status status_{Status::INIT};
+public:
+  BaseServer(asio::io_context& context);
+  virtual ~BaseServer(void);
+
+  virtual void start_impl(void);
+  virtual void stop_impl(void);
+
+  Status get_status(void) const {
+    return status_;
+  }
+
+  bool is_stoped(void) const {
+    return status_ == Status::STOPED;
+  }
+};
+
+}
