97d5c4b986931d2ea3087947f89274e9add58560
ASMlover <asmlover@126.com>
2018-11-16 22:28:50 +0800

    :white_check_mark: feat(test): add test module for core version2

---
+++ network/rpc-examples/nyx-net/test_v2/server.cc
@@ -0,0 +1,44 @@
+// Copyright (c) 2018 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#include <memory>
+#include "../core_v2/BaseSession.h"
+#include "../core_v2/TcpServer.h"
+#include "../core_v2/ServerManager.h"
+
+void run_server(void) {
+  auto server = std::make_shared<nyx::TcpServer>();
+  server->enable_reuse_addr(true);
+  server->set_new_connection_callback([](const nyx::SessionPtr& conn) {
+      }).set_message_callback(
+        [](const nyx::SessionPtr& conn, const std::string& buf) {
+      });
+  server->bind("127.0.0.1", 5555);
+  server->listen(5);
+
+  nyx::ServerManager::get_instance().set_worker();
+  nyx::ServerManager::get_instance().start();
+}
