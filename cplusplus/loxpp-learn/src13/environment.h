2e8fe6f87596aa74ef0cc9a63fb2df9212013ee6
ASMlover <asmlover@126.com>
2019-01-17 23:29:48 +0800

    :sparkles: feat(methods): add new copy for methods on classes

---
+++ cplusplus/loxpp-learn/src13/environment.h
@@ -0,0 +1,64 @@
+// Copyright (c) 2019 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+#include <memory>
+#include <unordered_map>
+#include "common.h"
+
+namespace lox {
+
+class Token;
+class Value;
+class Environment;
+
+using EnvironmentPtr = std::shared_ptr<Environment>;
+
+class Environment
+  : private UnCopyable, public std::enable_shared_from_this<Environment> {
+  EnvironmentPtr enclosing_{};
+  std::unordered_map<std::string, Value> values_;
+
+  EnvironmentPtr ancestor(int distance);
+public:
+  Environment(void) {
+  }
+
+  Environment(const EnvironmentPtr& enclosing)
+    : enclosing_(enclosing) {
+  }
+
+  void define(const std::string& name, const Value& value);
+  void define(const Token& name, const Value& value);
+  const Value& get(const std::string& name) const;
+  const Value& get(const Token& name) const;
+  const Value& get_at(int distance, const Token& name);
+  void assign(const Token& name, const Value& value);
+  void assign_at(int distance, const Token& name, const Value& value);
+};
+
+}
