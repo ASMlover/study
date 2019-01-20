6e0c95916f6532c53d22ea40334eac777de33f32
ASMlover <asmlover@126.com>
2019-01-21 01:02:23 +0800

    :sparkles: feat(token): add token definitions

---
+++ cplusplus/lvmpp/kind_defs.h
@@ -0,0 +1,94 @@
+// Copyright (c) 2019 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef TOKDEF
+# define TOKDEF(k, s)
+#endif
+
+#ifndef TOKEN
+# define TOKEN(k, s) TOKDEF(TK_##k, s)
+#endif
+
+#ifndef PUNCTUATOR
+# define PUNCTUATOR(k, s) TOKEN(k, s)
+#endif
+
+#ifndef KEYWORD
+# define KEYWORD(k, s) TOKDEF(KW_##k, s)
+#endif
+
+// language tokens
+TOKEN(ERROR, "ERROR") // error token
+TOKEN(EOF, "EOF") // end of file
+TOKEN(IDENTIFILER, "identifiler")
+TOKEN(NUMERICCONST, "numeric_const")
+TOKEN(STRINGLITERAL, "string_literal")
+
+// punctuators
+PUNCTUATOR(LPAREN, "(")
+PUNCTUATOR(RPAREN, ")")
+PUNCTUATOR(LBRACE, "{")
+PUNCTUATOR(RBRACE, "}")
+PUNCTUATOR(COMMA, ",")
+PUNCTUATOR(DOT, ".")
+PUNCTUATOR(SEMI, ";")
+PUNCTUATOR(PLUS, "+")
+PUNCTUATOR(MINUS, "-")
+PUNCTUATOR(STAR, "*")
+PUNCTUATOR(SLASH, "/")
+PUNCTUATOR(PERCENT, "%")
+PUNCTUATOR(BANG, "!")
+PUNCTUATOR(BANGEQUAL, "!=")
+PUNCTUATOR(EQUAL, "=")
+PUNCTUATOR(EQUALEQUAL, "==")
+PUNCTUATOR(GREATER, ">")
+PUNCTUATOR(GREATEREQUAL, ">=")
+PUNCTUATOR(LESS, "<")
+PUNCTUATOR(LESSEQUAL, "<=")
+
+// keywords
+KEYWORD(AND, "and")
+KEYWORD(CLASS, "class")
+KEYWORD(ELSE, "else")
+KEYWORD(FALSE, "false")
+KEYWORD(FOR, "for")
+KEYWORD(FUN, "fun")
+KEYWORD(IF, "if")
+KEYWORD(NIL, "nil")
+KEYWORD(OR, "or")
+KEYWORD(PRINT, "print")
+KEYWORD(RETURN, "return")
+KEYWORD(SUPER, "super")
+KEYWORD(THIS, "this")
+KEYWORD(TRUE, "true")
+KEYWORD(VAR, "var")
+KEYWORD(WHILE, "while")
+
+#undef KEYWORD
+#undef PUNCTUATOR
+#undef TOKEN
+#undef TOKDEF
---
+++ cplusplus/lvmpp/token.cc
@@ -0,0 +1,68 @@
+// Copyright (c) 2019 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#include <cstdlib>
+#include <iomanip>
+#include <sstream>
+#include "token.h"
+
+namespace lox {
+
+static constexpr const char* const kTokenNames[] = {
+#define TOKDEF(k, s) s,
+#include "kind_defs.h"
+#undef TOKDEF
+  nullptr
+};
+
+const char* get_token_name(TokenKind kind) {
+  if (kind < TokenKind::NUM_TOKENS)
+    return kTokenNames[kind];
+  return nullptr;
+}
+
+std::string Token::stringify(void) const {
+  std::stringstream ss;
+
+  ss << std::left << std::setw(20) << get_token_name(kind_) << ":"
+    << std::right << std::setw(24) << literal_ << "|"
+    << std::right << std::setw(4) << lineno_;
+  return ss.str();
+}
+
+double Token::as_numeric(void) const {
+  return std::atof(literal_.c_str());
+}
+
+std::string Token::as_string(void) const {
+  return literal_;
+}
+
+std::ostream& operator<<(std::ostream& out, const Token& tok) {
+  return out << tok.stringify();
+}
+
+}
