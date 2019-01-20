03cb73ce76c0389c7fb1cef050b0859a72a8f555
ASMlover <asmlover@126.com>
2019-01-21 01:02:46 +0800

    :sparkles: feat(scanner): add scanner framework

---
+++ cplusplus/lvmpp/scanner.cc
@@ -0,0 +1,56 @@
+// Copyright (c) 2019 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#include "scanner.h"
+
+namespace lox {
+
+std::string Scanner::gen_literal(std::size_t begpos, std::size_t endpos) const {
+  return source_bytes_.substr(begpos, endpos - begpos);
+}
+
+bool Scanner::is_end(void) const {
+  return curpos_ >= source_bytes_.size();
+}
+
+Token Scanner::make_token(TokenKind kind) {
+  auto literal = gen_literal(begpos_, curpos_);
+  return Token(kind, literal, lineno_);
+}
+
+Token Scanner::error_token(const std::string& message) {
+  return Token(TokenKind::TK_ERROR, message, lineno_);
+}
+
+Token Scanner::scan_token(void) {
+  begpos_ = curpos_;
+
+  if (is_end())
+    return make_token(TokenKind::TK_EOF);
+  return error_token("unexpected character ...");
+}
+
+}
