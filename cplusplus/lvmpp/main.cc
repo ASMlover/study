8502c88727ae242938dc6bff922c251b45d8255e
ASMlover <asmlover@126.com>
2019-01-20 15:55:52 +0800

    :white_check_mark: test(vm): add vm and chunk test

--- cplusplus/lvmpp/main.cc
+++ cplusplus/lvmpp/main.cc
@@ -28,19 +28,92 @@
 #include "chunk.h"
 #include "vm.h"
 
+static void chunk_sample(void) {
+  {
+    lox::Chunk chunk;
+    lox::VM vm(chunk);
+
+    int constant = chunk.add_constant(1.2);
+    chunk.write(lox::OpCode::OP_CONSTANT, 123);
+    chunk.write(constant, 123);
+    chunk.write(lox::OpCode::OP_NEGATIVE, 123);
+    chunk.write(lox::OpCode::OP_RETURN, 123);
+
+    chunk.disassemble("test chunk");
+    std::cout << std::endl;
+
+    vm.interpret();
+  }
+
+  {
+    lox::Chunk chunk;
+    lox::VM vm(chunk);
+
+    // test (2.45 + 4.67) / 5.6 * (67.8 - 56.3)
+    // constant 2.45
+    int constant = chunk.add_constant(2.45);
+    chunk.write(lox::OpCode::OP_CONSTANT, 124);
+    chunk.write(constant, 124);
+    // constant 4.67
+    constant = chunk.add_constant(4.67);
+    chunk.write(lox::OpCode::OP_CONSTANT, 124);
+    chunk.write(constant, 124);
+    // binary +
+    chunk.write(lox::OpCode::OP_ADD, 124);
+    // constant 5.6
+    constant = chunk.add_constant(5.6);
+    chunk.write(lox::OpCode::OP_CONSTANT, 124);
+    chunk.write(constant, 124);
+    // binary /
+    chunk.write(lox::OpCode::OP_DIVIDE, 124);
+    // constant 67.8
+    constant = chunk.add_constant(67.8);
+    chunk.write(lox::OpCode::OP_CONSTANT, 124);
+    chunk.write(constant, 124);
+    // constant 56.3
+    constant = chunk.add_constant(56.3);
+    chunk.write(lox::OpCode::OP_CONSTANT, 124);
+    chunk.write(constant, 124);
+    // binary -
+    chunk.write(lox::OpCode::OP_SUBTRACT, 124);
+    // binary *
+    chunk.write(lox::OpCode::OP_MULTIPLY, 124);
+    chunk.write(lox::OpCode::OP_RETURN, 124);
+
+    chunk.disassemble("test chunk");
+    std::cout << std::endl;
+
+    vm.interpret();
+  }
+
+  {
+    lox::Chunk chunk;
+    lox::VM vm(chunk);
+
+    // 33 % 7
+    // constant 33
+    int constant = chunk.add_constant(33);
+    chunk.write(lox::OpCode::OP_CONSTANT, 125);
+    chunk.write(constant, 125);
+    // constant 7
+    constant = chunk.add_constant(7);
+    chunk.write(lox::OpCode::OP_CONSTANT, 125);
+    chunk.write(constant, 125);
+    // binary %
+    chunk.write(lox::OpCode::OP_MODULO, 125);
+    chunk.write(lox::OpCode::OP_RETURN, 125);
+
+    chunk.disassemble("test chunk");
+    std::cout << std::endl;
+
+    vm.interpret();
+  }
+}
+
 int main(int argc, char* argv[]) {
   (void)argc, (void)argv;
 
-  lox::Chunk chunk;
-  int constant = chunk.add_constant(1.2);
-  chunk.write(lox::OP_CONSTANT, 123);
-  chunk.write(constant, 123);
-  chunk.write(lox::OP_RETURN, 123);
-  chunk.disassemble("test chunk");
-  std::cout << std::endl;
-
-  lox::VM vm(chunk);
-  vm.interpret();
+  chunk_sample();
 
   return 0;
 }
