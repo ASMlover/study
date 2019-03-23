b560046a6b87b72f2bd8440b2b5e670b3bc3a33b
ASMlover <asmlover@126.com>
2019-03-23 16:08:58 +0800

    :sparkles: feat(variables): add new copy for reading variables

---
+++ cplusplus/lvmpp-learn/lvmpp1b/chunk.hh
@@ -0,0 +1,81 @@
+// Copyright (c) 2019 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+#include <string>
+#include <vector>
+#include "common.hh"
+#include "value.hh"
+
+namespace lvm {
+
+enum class OpCode : int {
+  OP_CONSTANT,
+  OP_NIL, // nil
+  OP_TRUE, // true
+  OP_FALSE, // false
+  OP_POP, // pop
+  OP_DEFINE_GLOBAL, // define global variable
+  OP_EQ, // logical ==
+  OP_NE, // logical !=
+  OP_GT, // logical >
+  OP_GE, // logical >=
+  OP_LT, // logical <
+  OP_LE, // logical <=
+  OP_ADD, // +
+  OP_SUB, // -
+  OP_MUL, // *
+  OP_DIV, // /
+  OP_NOT, // !
+  OP_NEGATE, // -
+  OP_PRINT, // print
+  OP_RETURN
+};
+
+std::ostream& operator<<(std::ostream& out, OpCode code);
+
+class Chunk : public Copyable {
+  std::vector<OpCode> codes_;
+  std::vector<Value> constants_;
+  std::vector<int> lines_;
+public:
+  OpCode add_constant(const Value& value);
+
+  void write(OpCode code, int line);
+  void write_constant(const Value& value, int line);
+
+  void disassemble(const std::string& name);
+  int disassemble_instruction(int offset);
+
+  bool is_valid(void) const { return !codes_.empty(); }
+  const OpCode* get_codes(void) const { return &codes_[0]; }
+  OpCode get_code(int offset) const { return codes_[offset]; }
+  Value get_constant(int constant) const { return constants_[constant]; }
+  int get_line(int instruction) const { return lines_[instruction]; }
+};
+
+}
