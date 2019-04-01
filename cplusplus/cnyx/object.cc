38524fb2797e6cdf505fd6c276672df48868d369
ASMlover <ASMlover@126.com>
2019-04-01 16:48:32 +0800

    :bug: fix(memcpy): fixed compiling error for linux platform

--- cplusplus/cnyx/object.cc
+++ cplusplus/cnyx/object.cc
@@ -25,6 +25,7 @@
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 #include <cassert>
+#include <cstring>
 #include <iostream>
 #include <sstream>
 #include "object.hh"
