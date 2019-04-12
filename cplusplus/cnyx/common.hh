f01e02f1f8bbe2779aea58f8d851eabb58160705
ASMlover <asmlover@126.com>
2019-04-12 20:45:49 +0800

    :construction: chore(common): updated the common type defintions

--- cplusplus/cnyx/common.hh
+++ cplusplus/cnyx/common.hh
@@ -31,8 +31,7 @@
 
 namespace nyx {
 
-using byte_t = unsigned char;
-using sz_t = std::size_t;
+using byte_t = std::uint8_t;
 using i8_t = std::int8_t;
 using u8_t = std::uint8_t;
 using i16_t = std::int16_t;
@@ -41,6 +40,7 @@ using i32_t = std::int32_t;
 using u32_t = std::uint32_t;
 using i64_t = std::int64_t;
 using u64_t = std::uint64_t;
+using sz_t = std::size_t;
 using str_t = std::string;
 
 class Copyable {
@@ -58,11 +58,11 @@ protected:
 };
 
 namespace Xenum {
-  template <typename Int, typename Enum>
-  inline Int as_int(Enum x) { return static_cast<Int>(x); }
+  template <typename I, typename E>
+  inline I as_int(E x) { return static_cast<I>(x); }
 
-  template <typename Enum, typename Int>
-  inline Enum as_enum(Int x) { return static_cast<Enum>(x); }
+  template <typename E, typename I>
+  inline E as_enum(I x) { return static_cast<E>(x); }
 }
 
 namespace Xptr {
