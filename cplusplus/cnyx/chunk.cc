1164b47c2379dfc8aaafca82ac61a3c13edd0f93
ASMlover <asmlover@126.com>
2019-04-24 01:33:55 +0800

    :construction: chore(chunk): updated the chunk disassemble

--- cplusplus/cnyx/chunk.cc
+++ cplusplus/cnyx/chunk.cc
@@ -50,25 +50,27 @@ void Chunk::disassemble(const str_t& name) {
 }
 
 int Chunk::disassemble_instruction(int i) {
-  static auto simple_instruction = [](const char* name) {
+  static auto simple_instruction = [](int i, const char* name) -> int {
     std::cout << name << std::endl;
+    return i;
   };
 
-  static auto simple_instructionN = [](const char* name, int n) {
+  static auto simple_instructionN = [](int i, const char* name, int n) -> int {
     std::cout << name << n << std::endl;
+    return i;
   };
 
   static auto code_instruction = [](Chunk& c, int i, const char* name) -> int {
-    u8_t slot = c.get_code(i++);
+    u8_t slot = c.get_code(i);
     fprintf(stdout, "%-16s %4d\n", name, slot);
-    return i;
+    return i + 1;
   };
 
   static auto const_instruction = [](Chunk& c, int i, const char* name) -> int {
     u8_t constant = c.get_code(i);
     fprintf(stdout, "%-16s %4d ", name, constant);
     std::cout << "`" << c.get_constant(constant) << "`" << std::endl;
-    return i;
+    return i + 1;
   };
 
   static auto const_instructionN = [](
@@ -77,15 +79,15 @@ int Chunk::disassemble_instruction(int i) {
     fprintf(stdout, "%s%-*d %4d ",
         name, 15 - static_cast<int>(strlen(name)), n, constant);
     std::cout << "`" << c.get_constant(constant) << "`" << std::endl;
-    return i;
+    return i + 1;
   };
 
   static auto jump_instruction = [](
       Chunk& c, int i, const char* name, int sign = 1) -> int {
-    u16_t offset = static_cast<u16_t>(c.get_code(i++) << 8);
-    offset |= c.get_code(i++);
+    u16_t offset = static_cast<u16_t>(c.get_code(i) << 8);
+    offset |= c.get_code(i + 1);
     fprintf(stdout, "%-16s %4d -> %d\n", name, offset, i + offset * sign);
-    return i;
+    return i + 2;
   };
 
   fprintf(stdout, "%04d ", i);
@@ -96,50 +98,36 @@ int Chunk::disassemble_instruction(int i) {
 
   const u8_t* codes = codes_.data();
   switch (auto instruction = codes[i++]; instruction) {
-  case OpCode::OP_CONSTANT:
-    i = const_instruction(*this, i, "OP_CONSTANT"); break;
-  case OpCode::OP_NIL: simple_instruction("OP_NIL"); break;
-  case OpCode::OP_TRUE: simple_instruction("OP_TRUE"); break;
-  case OpCode::OP_FALSE: simple_instruction("OP_FALSE"); break;
-  case OpCode::OP_POP: simple_instruction("OP_POP"); break;
-  case OpCode::OP_GET_LOCAL:
-    i = code_instruction(*this, i, "OP_GET_LOCAL"); break;
-  case OpCode::OP_SET_LOCAL:
-    i = code_instruction(*this, i, "OP_SET_LOCAL"); break;
-  case OpCode::OP_DEF_GLOBAL:
-    i = const_instruction(*this, i, "OP_DEF_GLOBAL"); break;
-  case OpCode::OP_GET_GLOBAL:
-    i = const_instruction(*this, i, "OP_GET_GLOBAL"); break;
-  case OpCode::OP_SET_GLOBAL:
-    i = const_instruction(*this, i, "OP_SET_GLOBAL"); break;
-  case OpCode::OP_GET_UPVALUE:
-    i = code_instruction(*this, i, "OP_GET_UPVALUE"); break;
-  case OpCode::OP_SET_UPVALUE:
-    i = code_instruction(*this, i, "OP_SET_UPVALUE"); break;
-  case OpCode::OP_GET_FIELD:
-    i = const_instruction(*this, i, "OP_GET_FIELD"); break;
-  case OpCode::OP_SET_FIELD:
-    i = const_instruction(*this, i, "OP_SET_FIELD"); break;
-  case OpCode::OP_GET_SUPER:
-    i = const_instruction(*this, i, "OP_GET_SUPER"); break;
-  case OpCode::OP_EQ: simple_instruction("OP_EQ"); break;
-  case OpCode::OP_NE: simple_instruction("OP_NE"); break;
-  case OpCode::OP_GT: simple_instruction("OP_GT"); break;
-  case OpCode::OP_GE: simple_instruction("OP_GE"); break;
-  case OpCode::OP_LT: simple_instruction("OP_LT"); break;
-  case OpCode::OP_LE: simple_instruction("OP_LE"); break;
-  case OpCode::OP_ADD: simple_instruction("OP_ADD"); break;
-  case OpCode::OP_SUB: simple_instruction("OP_SUB"); break;
-  case OpCode::OP_MUL: simple_instruction("OP_MUL"); break;
-  case OpCode::OP_DIV: simple_instruction("OP_DIV"); break;
-  case OpCode::OP_NOT: simple_instruction("OP_NOT"); break;
-  case OpCode::OP_NEG: simple_instruction("OP_NEG"); break;
-  case OpCode::OP_JUMP:
-    i = jump_instruction(*this, i, "OP_JUMP"); break;
-  case OpCode::OP_JUMP_IF_FALSE:
-    i = jump_instruction(*this, i, "OP_JUMP_IF_FALSE"); break;
-  case OpCode::OP_LOOP:
-    i = jump_instruction(*this, i, "OP_LOOP", -1); break;
+  case OpCode::OP_CONSTANT: return const_instruction(*this, i, "OP_CONSTANT");
+  case OpCode::OP_NIL: return simple_instruction(i, "OP_NIL");
+  case OpCode::OP_TRUE: return simple_instruction(i, "OP_TRUE");
+  case OpCode::OP_FALSE: return simple_instruction(i, "OP_FALSE");
+  case OpCode::OP_POP: return simple_instruction(i, "OP_POP");
+  case OpCode::OP_GET_LOCAL: return code_instruction(*this, i, "OP_GET_LOCAL");
+  case OpCode::OP_SET_LOCAL: return code_instruction(*this, i, "OP_SET_LOCAL");
+  case OpCode::OP_DEF_GLOBAL: return const_instruction(*this, i, "OP_DEF_GLOBAL");
+  case OpCode::OP_GET_GLOBAL: return const_instruction(*this, i, "OP_GET_GLOBAL");
+  case OpCode::OP_SET_GLOBAL: return const_instruction(*this, i, "OP_SET_GLOBAL");
+  case OpCode::OP_GET_UPVALUE: return code_instruction(*this, i, "OP_GET_UPVALUE");
+  case OpCode::OP_SET_UPVALUE: return code_instruction(*this, i, "OP_SET_UPVALUE");
+  case OpCode::OP_GET_FIELD: return const_instruction(*this, i, "OP_GET_FIELD");
+  case OpCode::OP_SET_FIELD: return const_instruction(*this, i, "OP_SET_FIELD");
+  case OpCode::OP_GET_SUPER: return const_instruction(*this, i, "OP_GET_SUPER");
+  case OpCode::OP_EQ: return simple_instruction(i, "OP_EQ");
+  case OpCode::OP_NE: return simple_instruction(i, "OP_NE");
+  case OpCode::OP_GT: return simple_instruction(i, "OP_GT");
+  case OpCode::OP_GE: return simple_instruction(i, "OP_GE");
+  case OpCode::OP_LT: return simple_instruction(i, "OP_LT");
+  case OpCode::OP_LE: return simple_instruction(i, "OP_LE");
+  case OpCode::OP_ADD: return simple_instruction(i, "OP_ADD");
+  case OpCode::OP_SUB: return simple_instruction(i, "OP_SUB");
+  case OpCode::OP_MUL: return simple_instruction(i, "OP_MUL");
+  case OpCode::OP_DIV: return simple_instruction(i, "OP_DIV");
+  case OpCode::OP_NOT: return simple_instruction(i, "OP_NOT");
+  case OpCode::OP_NEG: return simple_instruction(i, "OP_NEG");
+  case OpCode::OP_JUMP: return jump_instruction(*this, i, "OP_JUMP");
+  case OpCode::OP_JUMP_IF_FALSE: return jump_instruction(*this, i, "OP_JUMP_IF_FALSE");
+  case OpCode::OP_LOOP: return jump_instruction(*this, i, "OP_LOOP", -1);
   case OpCode::OP_CALL_0:
   case OpCode::OP_CALL_1:
   case OpCode::OP_CALL_2:
@@ -149,7 +137,7 @@ int Chunk::disassemble_instruction(int i) {
   case OpCode::OP_CALL_6:
   case OpCode::OP_CALL_7:
   case OpCode::OP_CALL_8:
-    simple_instructionN("OP_CALL_", instruction - OpCode::OP_CALL_0); break;
+    return simple_instructionN(i, "OP_CALL_", instruction - OpCode::OP_CALL_0);
   case OpCode::OP_INVOKE_0:
   case OpCode::OP_INVOKE_1:
   case OpCode::OP_INVOKE_2:
@@ -159,8 +147,7 @@ int Chunk::disassemble_instruction(int i) {
   case OpCode::OP_INVOKE_6:
   case OpCode::OP_INVOKE_7:
   case OpCode::OP_INVOKE_8:
-    i = const_instructionN(*this, i, "OP_INVOKE_", instruction - OpCode::OP_INVOKE_0);
-    break;
+    return const_instructionN(*this, i, "OP_INVOKE_", instruction - OpCode::OP_INVOKE_0);
   case OpCode::OP_SUPER_0:
   case OpCode::OP_SUPER_1:
   case OpCode::OP_SUPER_2:
@@ -170,8 +157,7 @@ int Chunk::disassemble_instruction(int i) {
   case OpCode::OP_SUPER_6:
   case OpCode::OP_SUPER_7:
   case OpCode::OP_SUPER_8:
-    i = const_instructionN(*this, i, "OP_SUPER_", instruction - OpCode::OP_SUPER_0);
-    break;
+    return const_instructionN(*this, i, "OP_SUPER_", instruction - OpCode::OP_SUPER_0);
   case OpCode::OP_CLOSURE:
     {
       u8_t constant = codes[i++];
@@ -187,16 +173,13 @@ int Chunk::disassemble_instruction(int i) {
             i - 2, is_local ? "local" : "upvalue", index);
       }
     } break;
-  case OpCode::OP_CLOSE_UPVALUE: simple_instruction("OP_CLOSE_UPVALUE"); break;
-  case OpCode::OP_RETURN: simple_instruction("OP_RETURN"); break;
-  case OpCode::OP_CLASS:
-    i = const_instruction(*this, i, "OP_CLASS"); break;
-  case OpCode::OP_SUBCLASS:
-    i = const_instruction(*this, i, "OP_SUBCLASS"); break;
-  case OpCode::OP_METHOD:
-    i = const_instruction(*this, i, "OP_METHOD"); break;
+  case OpCode::OP_CLOSE_UPVALUE: return simple_instruction(i, "OP_CLOSE_UPVALUE");
+  case OpCode::OP_RETURN: return simple_instruction(i, "OP_RETURN");
+  case OpCode::OP_CLASS: return const_instruction(*this, i, "OP_CLASS");
+  case OpCode::OP_SUBCLASS: return const_instruction(*this, i, "OP_SUBCLASS");
+  case OpCode::OP_METHOD: return const_instruction(*this, i, "OP_METHOD");
   }
-  return i;
+  return 0;
 }
 
 }
