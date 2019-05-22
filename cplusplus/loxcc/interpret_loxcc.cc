f1c2f834023a0da7576a76ae6fe69e1de37250f1
ASMlover <asmlover@126.com>
2019-05-22 23:15:01 +0800

    :sparkles: feat(loxcc): add loxcc startup class for interpreter

---
+++ cplusplus/loxcc/interpret_loxcc.cc
@@ -0,0 +1,96 @@
+// Copyright (c) 2019 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include "lexer.hh"
+#include "interpret_parser.hh"
+#include "interpret_resolver.hh"
+#include "interpret_interpreter.hh"
+#include "interpret_loxcc.hh"
+
+namespace loxcc::interpret {
+
+Loxcc::Loxcc(void) noexcept
+  : err_report_()
+  , interp_(new Interpreter(err_report_)) {
+}
+
+void Loxcc::eval(int argc, char** argv) {
+  if (argc == 2) {
+    eval_with_repl();
+  }
+  else if (argc == 3) {
+    eval_with_file(argv[2]);
+  }
+  else {
+    std::cerr << "USAGE: " << argv[0] << " [i|c] [FILE_NAME]" << std::endl;
+    std::exit(-1);
+  }
+}
+
+void Loxcc::eval_with_repl(void) {
+  str_t line;
+  for (;;) {
+    std::cout << ">>> ";
+
+    if (!std::getline(std::cin, line) || line == "exit")
+      break;
+
+    eval(line);
+  }
+}
+
+void Loxcc::eval_with_file(const str_t& fname) {
+  std::fstream fp(fname);
+  if (fp.is_open()) {
+    std::stringstream ss;
+    ss << fp.rdbuf();
+
+    eval(ss.str());
+  }
+}
+
+void Loxcc::eval(const str_t& source_bytes) {
+  Lexer lex(source_bytes);
+  Parser p(err_report_, lex);
+
+  auto stmts = p.parse();
+  if (err_report_.had_error())
+    std::exit(-1);
+
+  auto resolver = std::make_shared<Resolver>(err_report_, interp_);
+  resolver->invoke_resolve(stmts);
+  if (err_report_.had_error())
+    std::exit(-2);
+
+  interp_->interpret(stmts);
+  if (err_report_.had_error())
+    std::exit(-3);
+}
+
+}
---
+++ cplusplus/loxcc/interpret_loxcc.hh
@@ -0,0 +1,50 @@
+// Copyright (c) 2019 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+#include <memory>
+#include "common.hh"
+#include "interpret_errors.hh"
+
+namespace loxcc::interpret {
+
+class Interpreter;
+
+class Loxcc final : private UnCopyable {
+  ErrorReport err_report_;
+  std::shared_ptr<Interpreter> interp_;
+
+  void eval_with_repl(void);
+  void eval_with_file(const str_t& fname);
+  void eval(const str_t& source_bytes);
+public:
+  Loxcc(void) noexcept;
+
+  void eval(int argc, char** argv);
+};
+
+}
