3590a614840bc5d5897e5206298e80283a75ebc2
ASMlover <asmlover@126.com>
2019-11-30 22:38:28 +0800

    :construction: chore(array): add array list for wrencc

--- cplusplus/wrencc2/array_list.hh
+++ cplusplus/wrencc2/array_list.hh
@@ -39,27 +39,49 @@ class ArrayList final : private UnCopyable {
   sz_t size_{};
   sz_t capacity_{kDefCapacity};
   T* data_{};
+
+  void regrow(sz_t new_capacity = 0) {
+    if (new_capacity == 0)
+      new_capacity = capacity_ * 3 / 2;
+
+    T* new_data = SimpleAlloc<T>::allocate(new_capacity);
+    try {
+      uninitialized_copy(data_, data_ + size_, new_data);
+    }
+    catch (...) {
+      destroy(new_data, new_data + size_);
+      SimpleAlloc<T>::deallocate(new_data);
+      throw;
+    }
+
+    destroy(data_, data_ + size_);
+    SimpleAlloc<T>::deallocate(data_);
+
+    capacity_ = new_capacity;
+    data_ = new_data;
+  }
 public:
   ArrayList(sz_t capacity = kDefCapacity) noexcept
     : capacity_(capacity < kDefCapacity ? kDefCapacity : capacity) {
-    data_ = Alloc<T>::allocate(capacity_);
+    data_ = SimpleAlloc<T>::allocate(capacity_);
   }
 
   ~ArrayList() noexcept {
     clear();
-    Alloc<T>::deallocate(data_);
+    SimpleAlloc<T>::deallocate(data_);
   }
 
-  ArrayList(const T& value, sz_t count) noexcept
+  ArrayList(sz_t count, const T& value) noexcept
     : size_(count)
     , capacity_(count < kDefCapacity ? kDefCapacity : count) {
-    data_ = Alloc<T>::allocate(capacity_);
-    fill_n(data_, count, value);
+    data_ = SimpleAlloc<T>::allocate(capacity_);
+    uninitialized_fill(data_, size_, value);
   }
 
   inline bool empty() const noexcept { return size_ == 0; }
   inline sz_t size() const noexcept { return size_; }
   inline sz_t capacity() const noexcept { return capacity_; }
+  inline T* data() noexcept { return data_; }
   inline const T* data() const noexcept { return data_; }
   inline T& at(sz_t i) noexcept { return data_[i]; }
   inline const T& at(sz_t i) const noexcept { return data_[i]; }
@@ -70,18 +92,50 @@ public:
   inline T& last() noexcept { return data_[size_ - 1]; }
   inline const T& last() const noexcept { return data_[size_ - 1]; }
 
-  void clear() noexcept {
+  inline void clear() noexcept {
     destroy(data_, data_ + size_);
+    size_ = 0;
   }
 
-  void resize(sz_t size);
-  void resize_capacity(sz_t capacity);
-  void append(const T& x);
-  void append(T&& x);
-  T erase(sz_t i);
+  template <typename Function> void for_each(Function&& fn) noexcept {
+    for (sz_t i = 0; i < size_; ++i)
+      fn(i, data_[i]);
+  }
 
-  void for_each(std::function<void (T&)>&& fn);
-  void for_each(std::function<void (const T&)>&& fn);
+  void resize(sz_t size) {
+    if (size > size_) {
+      regrow(size * 3 / 2);
+    }
+    else if (size < size_) {
+      destroy(data_ + size, data_ + size_);
+      size_ = size;
+    }
+  }
+
+  void append(const T& x) {
+    if (size_ >= capacity_)
+      regrow();
+    construct(&data_[size_++], x);
+  }
+
+  void append(T&& x) {
+    if (size_ >= capacity_)
+      regrow();
+    construct(&data_[size_++], std::move(x));
+  }
+
+  template <typename... Args> void append(Args&&... args) {
+    if (size_ >= capacity_)
+      regrow();
+    construct(&data_[size_++], std::forward<Args>(args)...);
+  }
+
+  T pop() {
+    T r = data_[size_ - 1];
+    destroy(&data_[--size_]);
+    return r;
+  }
+
 };
 
 }
