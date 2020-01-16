2c3283e0ce9fc3a6794cac97e698a69aed9d5410
ASMlover <asmlover@126.com>
2020-01-16 22:48:47 +0800

    :white_check_mark: test(container): updated the test for containers

--- cplusplus/wrencc2/main.cc
+++ cplusplus/wrencc2/main.cc
@@ -25,9 +25,94 @@
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 #include <iostream>
+#include <vector>
+#include "utility.hh"
+#include "lexer.hh"
+#include "./container/array_list.hh"
+#include "./container/linked_list.hh"
+#include "./container/double_list.hh"
+#include "./container/tree.hh"
+
+struct Dummy {
+  int _xval{};
+  int _yval{};
+
+  Dummy(int x, int y) noexcept : _xval(x), _yval(y) {}
+
+  void show() noexcept {
+    std::cout << "Dummy {" << _xval << ", " << _yval << "}" << std::endl;
+  }
+};
 
 int main(int argc, char* argv[]) {
   (void)argc, (void)argv;
+
+  wrencc::SymbolTable st;
+  st.append("Hello");
+  st.ensure("Hello");
+  std::cout << st.count() << " " << st.find("Hello") << std::endl;
+  st.for_each([](const wrencc::String& s) {
+      std::cout << "[item value] -> " << s << std::endl;
+      });
+
+  wrencc::Lexer lex("class A {}");
+  do {
+    auto t = lex.next_token();
+    std::cout << t << std::endl;
+    if (t.kind() == wrencc::TokenKind::TK_EOF)
+      break;
+  } while (true);
+
+  wrencc::ArrayList<int> v(5, 5);
+  v.append(45);
+  v.for_each([](int v) {
+        std::cout << "[item value] -> " << v << std::endl;
+      });
+  std::cout << v.size() << " -> " << v.capacity() << std::endl;
+  std::cout << v.get_head() << " -> " << v.get_tail() << std::endl;
+
+  for (auto x : v)
+    std::cout << x << std::endl;
+
+  v.resize(20);
+  std::cout << v.size() << " -> " << v.capacity() << std::endl;
+  std::cout << v.pop() << std::endl << std::endl;
+
+  wrencc::ArrayList<int> vv(std::move(v));
+  std::cout << v.size() << " -> " << v.capacity() << std::endl;
+
+  wrencc::LinkedList<int> l;
+  l.append(1);
+  l.append(2);
+  l.for_each([](int x) {
+      std::cout << "[list item value] -> " << x << std::endl;
+      });
+  for (auto x : l)
+    std::cout << "iterator linked-list value: " << x << std::endl;
+  while (!l.empty()) {
+    int x = l.pop_head();
+    std::cout << "[list pop item value] -> " << x << std::endl;
+  }
+  std::cout << "list info: " << l.size() << std::endl;
+
+  wrencc::LinkedList<Dummy> dummy_list;
+  dummy_list.append(11, 22);
+  dummy_list.append(111, 222);
+  dummy_list.append(1111, 2222);
+  for (auto& d : dummy_list)
+    d.show();
+
+  wrencc::DoubleList<int> dl;
+  dl.append(11);
+  dl.append(22);
+  dl.for_each([](int x) {
+      std::cout << "[double linked list item value] -> " << x << std::endl;
+      });
+  dl.pop_head();
+  dl.for_each([](int x) {
+      std::cout << "[double linked list item value] -> " << x << std::endl;
+      });
+  std::cout << "double linked list: " << dl.size() << std::endl;
 
   return 0;
 }
