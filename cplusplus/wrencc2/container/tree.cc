5862993c28927736ead42825818a7f0b44ec40ce
ASMlover <asmlover@126.com>
2020-01-19 00:29:13 +0800

    :construction: chore(rb-tree): implementation for tb-tree insert and erase methods

--- cplusplus/wrencc2/container/tree.cc
+++ cplusplus/wrencc2/container/tree.cc
@@ -227,11 +227,191 @@ namespace impl::avl {
 }
 
 namespace impl::rb {
-  void insert(
-      bool insert_left, BasePtr x, BasePtr p, NodeBase& header) noexcept {
+  inline void insert_fixup(BasePtr x, BasePtr& root) noexcept {
+    while (x != root && x->parent->as_rb()->is_red()) {
+      if (x->parent == x->parent->parent->left) {
+        RBNodeBase* y = x->parent->parent->right->as_rb();
+        if (y != nullptr && y->is_red()) {
+          x->parent->as_rb()->as_blk();
+          y->as_blk();
+          x->parent->parent->as_rb()->as_red();
+          x = x->parent->parent;
+        }
+        else {
+          if (x == x->parent->right) {
+            x = x->parent;
+            left_rotate(x, root);
+          }
+          x->parent->as_rb()->as_blk();
+          x->parent->parent->as_rb()->as_red();
+          right_rotate(x->parent->parent, root);
+        }
+      }
+      else {
+        RBNodeBase* y = x->parent->parent->left->as_rb();
+        if (y != nullptr && y->is_red()) {
+          x->parent->as_rb()->as_blk();
+          y->as_blk();
+          x->parent->parent->as_rb()->as_red();
+          x = x->parent->parent;
+        }
+        else {
+          if (x == x->parent->left) {
+            x = x->parent;
+            right_rotate(x, root);
+          }
+          x->parent->as_rb()->as_blk();
+          x->parent->parent->as_rb()->as_red();
+          left_rotate(x->parent->parent, root);
+        }
+      }
+    }
+    root->as_rb()->as_blk();
   }
 
-  void erase(BasePtr x, NodeBase& header) noexcept {
+  inline void erase_fixup(BasePtr x, BasePtr x_parent, BasePtr& root) noexcept {
+    while (x != root && (x == nullptr || x->as_rb()->is_blk())) {
+      if (x == x_parent->left) {
+        RBNodeBase* w = x_parent->right->as_rb();
+        if (w->is_red()) {
+          w->as_blk();
+          x_parent->as_rb()->as_red();
+          left_rotate(x_parent, root);
+          w = x_parent->right->as_rb();
+        }
+
+        if ((w->left == nullptr || w->left->as_rb()->is_blk())
+            && (w->right == nullptr || w->right->as_rb()->is_blk())) {
+          w->as_red();
+          x = x_parent;
+          x_parent = x_parent->parent;
+        }
+        else {
+          if (w->right == nullptr || w->right->as_rb()->is_blk()) {
+            if (w->left != nullptr)
+              w->left->as_rb()->as_blk();
+            w->as_red();
+            right_rotate(w, root);
+            w = x_parent->right->as_rb();
+          }
+          w->set_color(x_parent->as_rb()->color);
+          x_parent->as_rb()->as_blk();
+          if (w->right != nullptr)
+            w->right->as_rb()->as_blk();
+          left_rotate(x_parent, root);
+          break;
+        }
+      }
+      else {
+        RBNodeBase* w = x_parent->left->as_rb();
+        if (w->is_red()) {
+          w->as_blk();
+          x_parent->as_rb()->as_red();
+          right_rotate(x_parent, root);
+          w = x_parent->left->as_rb();
+        }
+
+        if ((w->left == nullptr || w->left->as_rb()->is_blk())
+            && (w->right == nullptr || w->right->as_rb()->is_blk())) {
+          w->as_red();
+          x = x_parent;
+          x_parent = x_parent->parent;
+        }
+        else {
+          if (w->left == nullptr || w->left->as_rb()->is_blk()) {
+            if (w->right != nullptr)
+              w->right->as_rb()->as_blk();
+            w->as_red();
+            left_rotate(w, root);
+            w = x_parent->left->as_rb();
+          }
+          w->set_color(x_parent->as_rb()->color);
+          x_parent->as_rb()->as_blk();
+          if (w->left != nullptr)
+            w->left->as_rb()->as_blk();
+          right_rotate(x_parent, root);
+          break;
+        }
+      }
+    }
+    if (x != nullptr)
+      x->as_rb()->as_blk();
+  }
+
+  void insert(
+      bool insert_left, BasePtr x, BasePtr p, NodeBase& header) noexcept {
+    BasePtr& root = header.parent;
+
+    x->parent = p;
+    x->left = x->right = nullptr;
+    x->as_rb()->as_red();
+
+    if (insert_left) {
+      p->left = x;
+      if (p == &header)
+        header.parent = header.right = x;
+      else if (p == header.left)
+        header.left = x;
+    }
+    else {
+      p->right = x;
+      if (p == header.right)
+        header.right = x;
+    }
+
+    insert_fixup(x, root);
+  }
+
+  void erase(BasePtr z, NodeBase& header) noexcept {
+    BasePtr& root = header.parent;
+    BasePtr& lmost = header.left;
+    BasePtr& rmost = header.right;
+
+    BasePtr x = nullptr;
+    BasePtr x_parent = nullptr;
+    BasePtr y = z;
+    if (y->left != nullptr && y->right != nullptr) {
+      y = minimum(y->right);
+      x = y->right;
+    }
+    else {
+      x = y->left == nullptr ? y->right : y->left;
+    }
+
+    if (y != z) {
+      y->left = z->left;
+      y->left->parent = y;
+      if (y != z->right) {
+        x_parent = y->parent;
+        if (x != nullptr)
+          x->parent = y->parent;
+        y->parent->left = x;
+        y->right = z->right;
+        y->right->parent = y;
+      }
+      else {
+        x_parent = y;
+      }
+
+      transplant(z, y, root);
+      y->parent = z->parent;
+      std::swap(y->as_rb()->color, z->as_rb()->color);
+      y = z;
+    }
+    else {
+      x_parent = y->parent;
+      if (x != nullptr)
+        x->parent = y->parent;
+      transplant(z, x, root);
+
+      if (lmost == z)
+        lmost = z->right == nullptr ? z->parent : minimum(x);
+      if (rmost == z)
+        rmost = z->left == nullptr ? z->parent : maximum(x);
+    }
+
+    if (y->as_rb()->is_blk())
+      erase_fixup(x, x_parent, root);
   }
 }
 
