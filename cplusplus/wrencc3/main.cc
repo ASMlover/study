208badee4bbea79b1bf1742cb7983aa2b86dbbe6
ASMlover <asmlover@126.com>
2020-02-25 23:26:58 +0800

    :construction: chore(wrencc): updated the test harness

--- cplusplus/wrencc3/main.cc
+++ cplusplus/wrencc3/main.cc
@@ -24,10 +24,10 @@
 // LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
-#include <iostream>
+#include "harness.hh"
 
 int main(int argc, char* argv[]) {
   (void)argc, (void)argv;
 
-  return 0;
+  return wrencc::run_all_harness();
 }
