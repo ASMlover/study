241240c970a3c36605f9c22f11060c23fff488fd
ASMlover <asmlover@126.com>
2020-03-03 23:01:01 +0800

    :recycle: refactor(reverse-linked-list): refactoring the implementation of reversing single linked list

--- cplusplus/leetcode/leetcode_reverse_linked_list.cc
+++ cplusplus/leetcode/leetcode_reverse_linked_list.cc
@@ -25,6 +25,7 @@
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 #include <iostream>
+#include "common.hh"
 #include "harness.hh"
 
 // Question: https://leetcode.com/problems/reverse-linked-list/description/
@@ -37,78 +38,82 @@
 // A linked list can be reversed either iterativly or recursively, could you
 // implement both ?
 
-struct ListNode {
-  ListNode* next{};
-  int value{};
+class ReverseLinkedList final : public Singleton<ReverseLinkedList> {
+  struct ListNode {
+    ListNode* next{};
+    int value{};
 
-  ListNode(int val = 0) noexcept : value(val) {}
+    ListNode(int v = 0) noexcept : value(v) {}
+  };
+
+  void show(ListNode* head) noexcept {
+    std::cout << "show list: " << head << std::endl;
+    for (auto* n = head; n != nullptr; n = n->next)
+      std::cout << "\tlist node value is: " << n->value << std::endl;
+  }
+
+  void destroy(ListNode* head) {
+    while (head != nullptr) {
+      ListNode* node = head;
+      head = head->next;
+      delete node;
+    }
+  }
+
+  ListNode* append(ListNode* head, int value) {
+    ListNode* node = new ListNode(value);
+    node->next = head;
+    head = node;
+
+    return head;
+  }
+
+  ListNode* reverse_iteratively(ListNode* head) {
+    // prev记录节点的前驱，next记录节点的后继，不停更新current->next = prev
+
+    ListNode* curr{head};
+    ListNode* prev{};
+    ListNode* next{};
+    while (curr != nullptr) {
+      next = curr->next;
+      curr->next = prev;
+      prev = curr;
+      curr = next;
+    }
+    return prev;
+  }
+
+  ListNode* reverse_recursively(ListNode* head) {
+    // current节点或next节点为null直接返回，否则prev为next的递归调用返回的节点
+    // 同时将next的next设置为当前节点，当前节点的next设置为null
+
+    if (head == nullptr || head->next == nullptr)
+      return head;
+
+    ListNode* next = head->next;
+    ListNode* prev = reverse_recursively(next);
+    next->next = head;
+    head->next = nullptr;
+
+    return prev;
+  }
+public:
+  void run() {
+    ListNode* head{};
+    for (int i = 0; i < 5; ++i)
+      head = append(head, i + 1);
+    show(head);
+
+    head = reverse_iteratively(head);
+    show(head);
+
+    head = reverse_recursively(head);
+    show(head);
+
+    destroy(head);
+  }
 };
 
-static void list_show(ListNode* head) {
-  std::cout << "list_show for: " << head << std::endl;
-  for (auto* n = head; n != nullptr; n = n->next)
-    std::cout << "\tlist node value is: " << n->value << std::endl;
-}
-
-static ListNode* list_append(ListNode* head, int value) {
-  ListNode* node = new ListNode(value);
-  node->next = head;
-  head = node;
-
-  return head;
-}
-
-static void list_destroy(ListNode* head) {
-  while (head != nullptr) {
-    ListNode* node = head;
-    head = head->next;
-    delete node;
-  }
-}
-
-static ListNode* list_reverse_iteratively(ListNode* head) {
-  // [方法]
-  // prev记录节点的前驱，next记录节点的后继，不停更新current->next = prev
-
-  ListNode* curr{head};
-  ListNode* prev{};
-  ListNode* next{};
-  while (curr != nullptr) {
-    next = curr->next;
-    curr->next = prev;
-    prev = curr;
-    curr = next;
-  }
-  return prev;
-}
-
-static ListNode* list_reverse_recursively(ListNode* head) {
-  // [方法]
-  // current节点或next节点为null直接返回，否则prev为next的递归调用返回的节点
-  // 同时将next的next设置为当前节点，当前节点的next设置为null
-
-  if (head == nullptr || head->next == nullptr)
-    return head;
-
-  ListNode* next = head->next;
-  ListNode* prev = list_reverse_recursively(next);
-  next->next = head;
-  head->next = nullptr;
-
-  return prev;
-}
-
 HARNESS_TEST(ReverseLinkedList, harness::FakeTester) {
-  ListNode* head = nullptr;
-  for (int i = 0; i < 5; ++i)
-    head = list_append(head, i + 1);
-  list_show(head);
-
-  head = list_reverse_iteratively(head);
-  list_show(head);
-
-  head = list_reverse_recursively(head);
-  list_show(head);
-
-  list_destroy(head);
+  ReverseLinkedList::get_instance().run();
 }
