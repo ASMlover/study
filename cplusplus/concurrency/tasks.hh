9e9c3973e2cb0f4ea6601de4f908a07c3e5407d3
ASMlover <ASMlover@126.com>
2020-03-15 20:06:27 +0800

    :construction: chore(task): updated the task declaration

--- cplusplus/concurrency/tasks.hh
+++ cplusplus/concurrency/tasks.hh
@@ -2,11 +2,13 @@
 
 #include <string_view>
 
+class FakeTask {};
+
 bool register_task(std::string_view name, void (*closure)());
 int run_all_tasks();
 
 #define TASK_DECL(Name)\
-class _ConcurrencyTask_##Name final {\
+class _ConcurrencyTask_##Name final : public FakeTask {\
 public:\
   void _run();\
   static void _run_task() {\
@@ -14,5 +16,5 @@ public:\
     t._run();\
   }\
 };\
-bool _ConcurrencyTask_##Name_ignored = register_task(#Name, &_ConcurrencyTask_##Name::_run_task);\
+bool _Ignored_ConcurrencyTask_##Name = register_task(#Name, &_ConcurrencyTask_##Name::_run_task);\
 void _ConcurrencyTask_##Name::_run()
