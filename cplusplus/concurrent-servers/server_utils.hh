03dd796f9c0a312125327232532107b22b0ba2ee
ASMlover <ASMlover@126.com>
2020-03-24 11:53:51 +0800

    :construction: chore(svrutils): add server operations for concurrency utils

--- cplusplus/concurrent-servers/server_utils.hh
+++ cplusplus/concurrent-servers/server_utils.hh
@@ -1,9 +1,11 @@
 #pragma once
 
+#include <optional>
 #include "common_utils.hh"
 
 namespace svrutils {
 
+std::optional<SOCKET> create_server(std::uint16_t port = 5555);
 void serve_connection(common::UniqueSocket& sockfd);
 
 }
