bdf69a4e4ec68cabef718bf546189e6999156d1e
ASMlover <ASMlover@126.com>
2020-03-26 11:46:41 +0800

    :recycle: refactor(poll): refactoring select-polling server

--- cplusplus/concurrent-servers/select_polling_server.cc
+++ cplusplus/concurrent-servers/select_polling_server.cc
@@ -1,4 +1,5 @@
 #include <unordered_map>
+#include <list>
 #include "common_utils.hh"
 #include "server_utils.hh"
 #include "examples.hh"
@@ -9,58 +10,59 @@ static constexpr int EVENT_NO = 0; // no event
 static constexpr int EVENT_RD = 1; // read event
 static constexpr int EVENT_WR = 2; // write event
 
-class Connector : private common::UnCopyable {
+class Conn : private common::UnCopyable {
   enum class State {
     INIT_ACK,
     WAIT_MSG,
     READ_MSG,
   };
 
-  SOCKET sockfd_{};
+  svrutils::Socket s_;
   State state_{ State::INIT_ACK };
   int event_{ EVENT_NO };
   std::vector<char> send_buff_;
 public:
-  Connector(SOCKET fd) noexcept
-    : sockfd_(fd) {
+  Conn(SOCKET fd) noexcept
+    : s_(fd) {
+    s_.set_nonblocking();
   }
 
-  inline void add_event(int ev) noexcept { event_ = event_ | ev; }
-  inline void del_event(int ev) noexcept { event_ = event_ & ~(ev); }
-  inline SOCKET get_fd() const { return sockfd_; }
-
-  void close() {
-    shutdown(sockfd_, SD_BOTH);
-    closesocket(sockfd_);
+  Conn(svrutils::Socket s) noexcept
+    : s_(s) {
+    s_.set_nonblocking();
   }
 
-  int handle_connected() {
+  inline svrutils::Socket& get_socket() noexcept { return s_; }
+
+  inline bool is_nevent() const noexcept { return event_ == EVENT_NO; }
+  inline bool is_revent() const noexcept { return event_ | EVENT_RD; }
+  inline bool is_wevent() const noexcept { return event_ | EVENT_WR; }
+  inline void set_event(int ev) noexcept { event_ |= ev; }
+  inline void cls_event(int ev) noexcept { event_ &= ~ev; }
+  inline int get_event() const noexcept { return event_; }
+
+  void on_handle_connected() {
     state_ = State::INIT_ACK;
+    send_buff_.clear();
     send_buff_.push_back('*');
-    event_ = EVENT_WR;
-
-    return EVENT_WR;
+    event_ = EVENT_RD;
   }
 
-  int handle_revent() {
+  void on_handle_read() {
     if (state_ == State::INIT_ACK || !send_buff_.empty())
-      return EVENT_WR;
+      event_ |= EVENT_WR;
 
     char buf[1024];
-    int nbytes = recv(sockfd_, buf, sizeof(buf), 0);
-    if (nbytes == 0) {
-      return EVENT_NO;
+    auto [r, bytes] = s_.read_async(buf, sizeof(buf));
+    if (!r) {
+      event_ = EVENT_NO;
+      return;
     }
-    else if (nbytes < 0) {
-      auto err = WSAGetLastError();
-      if (err == WSAEWOULDBLOCK)
-        return EVENT_RD;
-      else
-        return EVENT_NO;
+    else {
+      event_ |= EVENT_RD;
     }
 
-    bool ready_to_send = false;
-    for (int i = 0; i < nbytes; ++i) {
+    for (int i = 0; i < bytes; ++i) {
       switch (state_) {
       case State::INIT_ACK: break;
       case State::WAIT_MSG:
@@ -73,159 +75,154 @@ public:
         }
         else {
           send_buff_.push_back(buf[i] + 1);
-          ready_to_send = true;
+          event_ |= EVENT_WR;
         }
         break;
       }
     }
-
-    return (!ready_to_send ? EVENT_RD : EVENT_NO) | (ready_to_send ? EVENT_WR : EVENT_NO);
   }
 
-  int handle_wevent() {
-    if (send_buff_.empty())
-      return EVENT_RD | EVENT_WR;
-
-    int nbytes = send(sockfd_, send_buff_.data(), static_cast<int>(send_buff_.size()), 0);
-    if (nbytes == -1) {
-      auto err = WSAGetLastError();
-      if (err == WSAEWOULDBLOCK)
-        return EVENT_WR;
-      else
-        return EVENT_NO;
+  void on_handle_write() {
+    if (send_buff_.empty()) {
+      event_ |= EVENT_RD;
+      event_ |= EVENT_WR;
+      return;
     }
 
-    if (nbytes < static_cast<int>(send_buff_.size())) {
-      send_buff_.erase(send_buff_.begin(), send_buff_.begin() + nbytes);
-      send_buff_.shrink_to_fit();
-      return EVENT_WR;
+    auto [r, bytes] = s_.write_async(send_buff_.data(), send_buff_.size());
+    if (!r) {
+      event_ = EVENT_NO;
+      return;
+    }
+    else {
+      event_ |= EVENT_WR;
+    }
+    if (bytes <= 0)
+      return;
+
+    if (bytes < send_buff_.size()) {
+      send_buff_.erase(send_buff_.begin(), send_buff_.begin() + bytes);
+      event_ |= EVENT_WR;
     }
     else {
       send_buff_.clear();
       if (state_ == State::INIT_ACK)
         state_ = State::WAIT_MSG;
+      event_ |= EVENT_RD;
+    }
+  }
+};
+using ConnPtr = std::shared_ptr<Conn>;
 
-      return EVENT_RD;
+class PollEvent : private common::UnCopyable {
+  std::list<ConnPtr> conns_;
+
+  fd_set rfds_master_;
+  fd_set wfds_master_;
+public:
+  PollEvent() noexcept {
+    FD_ZERO(&rfds_master_);
+    FD_ZERO(&wfds_master_);
+  }
+
+  ~PollEvent() {
+    for (auto& c : conns_)
+      c->get_socket().close();
+  }
+
+  void set_conn_event(ConnPtr& conn, int ev) {
+    auto fd = conn->get_socket().get();
+    if (ev | EVENT_RD) {
+      FD_SET(fd, &rfds_master_);
+      conn->set_event(EVENT_RD);
+    }
+    else {
+      FD_CLR(fd, &rfds_master_);
+      conn->cls_event(EVENT_RD);
+    }
+
+    if (ev | EVENT_WR) {
+      FD_SET(fd, &wfds_master_);
+      conn->set_event(EVENT_WR);
+    }
+    else {
+      FD_CLR(fd, &wfds_master_);
+      conn->cls_event(EVENT_WR);
+    }
+  }
+
+  void append_conn(SOCKET fd, int ev, bool is_new = true) {
+    auto conn = std::make_shared<Conn>(fd);
+    conn->on_handle_connected();
+    set_conn_event(conn, ev);
+    conns_.push_back(conn);
+  }
+
+  void poll(SOCKET listen_fd) {
+    fd_set rfds = rfds_master_;
+    fd_set wfds = wfds_master_;
+    int nready = ::select(-1, &rfds, &wfds, nullptr, nullptr);
+    if (nready < 0)
+      return;
+
+    for (auto it = conns_.begin(); it != conns_.end();) {
+      if (nready <= 0)
+        break;
+
+      auto& conn = *it;
+      auto fd = conn->get_socket().get();
+
+      bool need_erase = false;
+      bool has_sloved = false;
+      if (FD_ISSET(fd, &rfds)) {
+        if (fd == listen_fd) {
+          auto [r, s] = conn->get_socket().accept_async();
+          if (!r)
+            return;
+
+          append_conn(s.get(), EVENT_RD | EVENT_WR);
+        }
+        else {
+          conn->on_handle_read();
+          set_conn_event(conn, conn->get_event());
+          if (conn->is_nevent())
+            need_erase = true;
+        }
+        has_sloved = true;
+      }
+
+      if (FD_ISSET(fd, &wfds)) {
+        conn->on_handle_write();
+        set_conn_event(conn, conn->get_event());
+        if (conn->is_nevent())
+          need_erase = true;
+
+        has_sloved = true;
+      }
+
+      if (has_sloved)
+        --nready;
+
+      if (need_erase)
+        conns_.erase(it++);
+      else
+        ++it;
     }
   }
 };
 
 void launch_server() {
-  std::unordered_map<SOCKET, std::shared_ptr<Connector>> conns;
-
-  auto append_conn = [&conns](SOCKET fd, fd_set* set, int ev) {
-    FD_SET(fd, set);
-
-    auto conn = std::make_shared<Connector>(fd);
-    conn->add_event(ev);
-
-    conns.insert(std::make_pair(fd, conn));
-  };
-
-  auto append_newconn = [&conns](SOCKET fd, fd_set* rset, fd_set* wset) {
-    u_long flags = 1;
-    ioctlsocket(fd, FIONBIO, &flags);
-    auto conn = std::make_shared<Connector>(fd);
-    int ev = conn->handle_connected();
-    if (ev | EVENT_RD)
-      FD_SET(fd, rset);
-    else
-      FD_CLR(fd, rset);
-    if (ev | EVENT_WR)
-      FD_SET(fd, wset);
-    else
-      FD_CLR(fd, wset);
-
-    conns.insert(std::make_pair(fd, conn));
-  };
-
   common::UniqueSocket listen_sockfd;
   if (auto fd = svrutils::create_server(); !fd)
     return;
   else
     listen_sockfd.reset(*fd);
 
-  svrutils::set_nonblocking(listen_sockfd);
-
-  fd_set rfds_master;
-  FD_ZERO(&rfds_master);
-  fd_set wfds_master;
-  FD_ZERO(&wfds_master);
-  append_conn(listen_sockfd.get(), &rfds_master, EVENT_RD);
+  PollEvent pev;
+  pev.append_conn(listen_sockfd, EVENT_RD, false);
 
   for (;;) {
-    fd_set rfds = rfds_master;
-    fd_set wfds = wfds_master;
-
-    int nready = select(-1, &rfds, &wfds, nullptr, nullptr);
-    if (nready < 0)
-      return;
-
-    std::vector<SOCKET> fds;
-    for (auto& c : conns)
-      fds.push_back(c.first);
-
-    for (auto& fd : fds) {
-      if (nready <= 0)
-        break;
-      auto it = conns.find(fd);
-      if (it == conns.end())
-        break;
-
-      bool need_erase = false;
-      if (FD_ISSET(fd, &rfds_master)) {
-        --nready;
-
-        if (fd == listen_sockfd.get()) {
-          sockaddr_in peer_addr;
-          int peer_addr_len = static_cast<int>(sizeof(peer_addr));
-
-          SOCKET new_sockfd = accept(listen_sockfd, (sockaddr*)&peer_addr, &peer_addr_len);
-          if (new_sockfd == INVALID_SOCKET) {
-            auto err = WSAGetLastError();
-            if (err != WSAEWOULDBLOCK)
-              return;
-          }
-          else {
-            append_newconn(new_sockfd, &rfds_master, &wfds_master);
-          }
-        }
-        else {
-          int ev = it->second->handle_revent();
-          if (ev | EVENT_RD)
-            FD_SET(fd, &rfds_master);
-          else
-            FD_CLR(fd, &rfds_master);
-          if (ev | EVENT_WR)
-            FD_SET(fd, &wfds_master);
-          else
-            FD_CLR(fd, &wfds_master);
-
-          if (ev == EVENT_NO)
-            need_erase = true;
-        }
-      }
-
-      if (FD_ISSET(fd, &wfds_master)) {
-        --nready;
-
-        int ev = it->second->handle_wevent();
-        if (ev | EVENT_RD)
-          FD_SET(fd, &rfds_master);
-        else
-          FD_CLR(fd, &rfds_master);
-        if (ev | EVENT_WR)
-          FD_SET(fd, &wfds_master);
-        else
-          FD_CLR(fd, &wfds_master);
-
-        if (ev == EVENT_NO)
-          need_erase = true;
-      }
-
-      if (need_erase)
-        conns.erase(it);
-    }
+    pev.poll(listen_sockfd);
   }
 }
 
