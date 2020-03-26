018aba033bd7d54445081167e3310c7afba6c7a8
ASMlover <ASMlover@126.com>
2020-03-26 11:45:48 +0800

    :construction: chore(socket): add socket utils class

--- cplusplus/concurrent-servers/server_utils.cc
+++ cplusplus/concurrent-servers/server_utils.cc
@@ -7,6 +7,111 @@ enum class NetStatus {
   MSG_RECV,
 };
 
+bool Socket::open() {
+  fd_ = ::socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  return fd_ != INVALID_SOCKET;
+}
+
+void Socket::close() {
+  if (fd_ != INVALID_SOCKET) {
+    ::shutdown(fd_, SD_BOTH);
+    ::closesocket(fd_);
+    fd_ = INVALID_SOCKET;
+  }
+}
+
+void Socket::set_nonblocking(bool mode) {
+  u_long flag = mode ? 1 : 0;
+  ::ioctlsocket(fd_, FIONBIO, &flag);
+}
+
+bool Socket::bind(std::string_view addr, std::uint16_t port) {
+  sockaddr_in bind_addr;
+  bind_addr.sin_addr.s_addr = ::inet_addr(addr.data());
+  bind_addr.sin_family = AF_INET;
+  bind_addr.sin_port = ::htons(port);
+
+  return ::bind(fd_, (const sockaddr*)&bind_addr, static_cast<int>(sizeof(bind_addr))) != SOCKET_ERROR;
+}
+
+bool Socket::listen(int backlog) {
+  return ::listen(fd_, backlog) != SOCKET_ERROR;
+}
+
+bool Socket::connect(std::string_view addr, std::uint16_t port) {
+  sockaddr_in remote_addr;
+  remote_addr.sin_addr.s_addr = inet_addr(addr.data());
+  remote_addr.sin_family = AF_INET;
+  remote_addr.sin_port = htons(port);
+
+  return ::connect(fd_, (const sockaddr*)&remote_addr, static_cast<int>(sizeof(remote_addr))) != SOCKET_ERROR;
+}
+
+std::tuple<bool, bool> Socket::connect_async(std::string_view addr, std::uint16_t port) {
+  // return (success:bool, connect_again:bool)
+
+  if (!connect(addr, port)) {
+    if (WSAGetLastError() == WSAEWOULDBLOCK)
+      return std::make_tuple(true, true);
+    return std::make_tuple(false, false);
+  }
+  return std::make_tuple(true, false);
+}
+
+std::tuple<bool, Socket> Socket::accept() {
+  sockaddr_in peer_addr;
+  int peer_addr_len = static_cast<int>(sizeof(peer_addr));
+
+  SOCKET fd = ::accept(fd_, (sockaddr*)&peer_addr, &peer_addr_len);
+  return std::make_tuple(fd != INVALID_SOCKET, fd);
+}
+
+std::tuple<bool, Socket> Socket::accept_async() {
+  sockaddr_in peer_addr;
+  int peer_addr_len = static_cast<int>(sizeof(peer_addr));
+
+  SOCKET fd = ::accept(fd_, (sockaddr*)&peer_addr, &peer_addr_len);
+  if (fd == INVALID_SOCKET && WSAGetLastError() != WSAEWOULDBLOCK)
+    return std::make_tuple(false, fd);
+  return std::make_tuple(true, fd);
+}
+
+std::tuple<bool, std::size_t> Socket::read(char* buf, std::size_t len) {
+  int rlen = ::recv(fd_, buf, static_cast<int>(len), 0);
+  return std::make_tuple(rlen > 0, rlen);
+}
+
+std::tuple<bool, std::size_t> Socket::read_async(char* buf, std::size_t len) {
+  int rlen = ::recv(fd_, buf, static_cast<int>(len), 0);
+  if (rlen < 0) {
+    if (WSAGetLastError() == WSAEWOULDBLOCK)
+      return std::make_tuple(true, 0);
+    return std::make_tuple(false, 0);
+  }
+  else if (rlen == 0) {
+    return std::make_tuple(false, 0);
+  }
+  return std::make_tuple(true, rlen);
+}
+
+std::tuple<bool, std::size_t> Socket::write(const char* buf, std::size_t len) {
+  int wlen = ::send(fd_, buf, static_cast<int>(len), 0);
+  return std::make_tuple(wlen > 0, wlen);
+}
+
+std::tuple<bool, std::size_t> Socket::write_async(const char* buf, std::size_t len) {
+  int wlen = ::send(fd_, buf, static_cast<int>(len), 0);
+  if (wlen < 0) {
+    if (WSAGetLastError() == WSAEWOULDBLOCK)
+      return std::make_tuple(true, 0);
+    return std::make_tuple(false, 0);
+  }
+  else if (wlen == 0) {
+    return std::make_tuple(false, 0);
+  }
+  return std::make_tuple(true, wlen);
+}
+
 std::optional<SOCKET> create_server(std::uint16_t port) {
   SOCKET sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
   if (sockfd == INVALID_SOCKET)
