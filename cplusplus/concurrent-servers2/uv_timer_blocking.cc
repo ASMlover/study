5c651f348778d4cc5f3bdf0f4f5e3de160d4a586
ASMlover <ASMlover@126.com>
2020-04-01 17:55:55 +0800

    :sparkles: feat(uv-timer): add a blocking-timer and nonblocking-timer use libuv

---
+++ cplusplus/concurrent-servers2/uv_timer_blocking.cc
@@ -0,0 +1,27 @@
+#include <uv.h>
+#include "examples.hh"
+
+namespace ut1 {
+
+void on_timer(uv_timer_t* timer) {
+  auto timestamp = uv_hrtime();
+  std::cout << "on_timer [" << (timestamp / 1000000) % 1000000 << " ms]" << std::endl;
+
+  if (rand() % 13 == 0) {
+    std::cout << "sleeping ..." << std::endl;
+    ::Sleep(3000);
+  }
+}
+
+void launch_timer() {
+  uv_timer_t timer;
+  uv_timer_init(uv_default_loop(), &timer);
+  uv_timer_start(&timer, on_timer, 0, 1000);
+  uv_run(uv_default_loop(), UV_RUN_DEFAULT);
+}
+
+}
+
+COEXT_EXAMPLE(UVTimerBlock, ut1, "A blocking timer using libuv") {
+  ut1::launch_timer();
+}
