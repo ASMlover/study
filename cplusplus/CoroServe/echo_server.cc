e1e1288deb6426abf0642e273e08091cfc7d79a4
ASMlover <ASMlover@126.com>
2020-04-14 19:51:05 +0800

    :sparkles: feat(echo-server): add echo server with blocking

---
+++ cplusplus/CoroServe/echo_server.cc
@@ -0,0 +1,31 @@
+#include "net/net.hh"
+#include "msg_helper.hh"
+#include "examples.hh"
+
+namespace echo_server {
+
+using coro::net::Socket;
+
+void launch() {
+  coro::net::Initializer<2, 2> init;
+
+  std::unique_ptr<Socket, std::function<void (Socket*)>> server{
+    new Socket{}, [](Socket* s) { s->close(); }
+  };
+
+  if (!server->open() || !server->listen())
+    return;
+
+  for (;;) {
+    if (auto c = server->accept(); c) {
+      coro::msg::on_blocking_serve(*c);
+      (*c).close();
+    }
+  }
+}
+
+}
+
+CORO_EXAMPLE(EchoServer, es, "an easy blocking server") {
+  echo_server::launch();
+}
