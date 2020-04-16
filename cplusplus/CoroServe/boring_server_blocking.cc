6af5d848af52b37e7377ffd06ce44f9eed70dffa
ASMlover <ASMlover@126.com>
2020-04-16 15:25:16 +0800

    :sparkles: feat(boring-server): add boring server with blocking mode

---
+++ cplusplus/CoroServe/boring_server_blocking.cc
@@ -0,0 +1,41 @@
+#include "net/net.hh"
+#include "examples.hh"
+
+namespace boring_server_block {
+
+using coro::net::Socket;
+
+void on_boring(Socket c, coro::strv_t msg) {
+  for (;;) {
+    if (auto n = c.write(msg.data(), msg.size()); n <= 0)
+      break;
+    std::this_thread::sleep_for(std::chrono::seconds(rand() % 5));
+  }
+}
+
+void launch() {
+  coro::net::Initializer<> init;
+
+  std::unique_ptr<Socket, std::function<void (Socket*)>> server{
+    new Socket{}, [](Socket* s) { s->close(); }
+  };
+
+  if (!server->start_listen())
+    return;
+
+  for (;;) {
+    if (auto s = server->accept(); s) {
+      on_boring(*s, "boring!");
+      (*s).close();
+    }
+    else {
+      break;
+    }
+  }
+}
+
+}
+
+CORO_EXAMPLE(BoringServerBlocking, bsb, "a blocking boring server") {
+  boring_server_block::launch();
+}
