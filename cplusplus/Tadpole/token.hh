2a0b9312ff725dd28466202b22ebfe830685f925
ASMlover <ASMlover@126.com>
2020-05-19 19:56:58 +0800

    :construction: chore(token): add token declaration

--- cplusplus/Tadpole/token.hh
+++ cplusplus/Tadpole/token.hh
@@ -16,4 +16,54 @@ enum class TokenKind {
 const char* get_token_name(TokenKind kind);
 TokenKind get_keyword_kind(const str_t& key);
 
+class Token final : public Copyable {
+  TokenKind kind_{TokenKind::TK_ERR};
+  str_t literal_;
+  int lineno_{};
+public:
+  Token() noexcept {}
+
+  Token(TokenKind kind, const str_t& literal, int lineno) noexcept
+    : kind_(kind), literal_(literal), lineno_(lineno) {
+  }
+
+  Token(const Token& r) noexcept
+    : kind_(r.kind_), literal_(r.literal_), lineno_(r.lineno_) {
+  }
+
+  Token(Token&& r) noexcept
+    : kind_(std::move(r.kind_))
+    , literal_(std::move(r.literal_))
+    , lineno_(std::move(r.lineno_)) {
+  }
+
+  Token& operator=(const Token& r) noexcept {
+    if (this != &r) {
+      kind_ = r.kind_;
+      literal_ = r.literal_;
+      lineno_ = r.lineno_;
+    }
+    return *this;
+  }
+
+  Token& operator=(Token&& r) noexcept {
+    if (this != &r) {
+      kind_ = std::move(r.kind_);
+      literal_ = std::move(r.literal_);
+      lineno_ = std::move(r.lineno_);
+    }
+    return *this;
+  }
+
+  inline TokenKind kind() const noexcept { return kind_; }
+  inline const str_t& literal() const noexcept { return literal_; }
+  inline int lineno() const noexcept { return lineno_; }
+  inline double as_numeric() const noexcept { return std::atof(literal_.c_str()); }
+  inline const str_t& as_string() const noexcept { return literal_; }
+
+  static Token make(const str_t& literal) {
+    return Token(TokenKind::TK_STRING, literal, 0);
+  }
+};
+
 }
