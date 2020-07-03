c0ef7bdc890913d2a2f82ad8b2fcdc35577f0ed3
ASMlover <asmlover@126.com>
2020-07-03 21:14:30 +0800

    :sparkles: feat(two-sum): add implementation of two-sum

---
+++ cplusplus/leetcode/two_sum/two_sum.cc
@@ -0,0 +1,75 @@
+// Copyright (c) 2020 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#include <vector>
+#include <unordered_map>
+#include "../common.hh"
+#include "../harness.hh"
+
+// [ADDR] https://leetcode-cn.com/problems/two-sum/?utm_source=LCUS&utm_medium=ip_redirect_q_uns&utm_campaign=transfer2china
+//
+// 说明：
+//
+// 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，
+// 并返回他们的数组下标。
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+//
+// 示例:
+//
+// 给定 nums = [2, 7, 11, 15], target = 9
+// 因为 nums[0] + nums[1] = 2 + 7 = 9
+// 所以返回 [0, 1]
+
+class TwoSum final : public Singleton<TwoSum> {
+  std::vector<int> get_result(const std::vector<int>& arr, int sum) {
+    std::vector<int> result;
+    std::unordered_map<int, int> m;
+
+    for (auto i = 0; i < arr.size(); ++i) {
+      auto x = arr[i];
+      if (auto it = m.find(x); it != m.end()) {
+        result.push_back(it->second);
+        result.push_back(i);
+        break;
+      }
+      else {
+        m[sum - x] = i;
+      }
+    }
+
+    return result;
+  }
+public:
+  void run() {
+    auto r = get_result({2, 11, 7, 56}, 9);
+    HARNESS_EQ(r[0], 0);
+    HARNESS_EQ(r[1], 2);
+  }
+};
+
+HARNESS_TEST(TwoSum, harness::FakeTester) {
+  TwoSum::get_instance().run();
+}
