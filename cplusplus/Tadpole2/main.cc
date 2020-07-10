1b6a141f25a9f3032bb08456f05ab9670ece9beb
ASMlover <asmlover@126.com>
2020-07-10 16:11:15 +0800

    :construction: chore(tadpole): use TADPOLE_UNUSED

--- cplusplus/Tadpole2/main.cc
+++ cplusplus/Tadpole2/main.cc
@@ -24,9 +24,10 @@
 // LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
+#include "common.hh"
 
 int main(int argc, char* argv[]) {
-  (void)argc, (void)argv;
+  TADPOLE_UNUSED(argc), TADPOLE_UNUSED(argv);
 
   return 0;
 }
