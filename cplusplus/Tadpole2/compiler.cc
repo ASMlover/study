91922d54a0abe550f80e081aff226c0a5989946c
ASMlover <asmlover@126.com>
2020-08-16 18:41:30 +0800

    :construction: chore(compiler): add basic compiler for function

--- cplusplus/Tadpole2/compiler.cc
+++ cplusplus/Tadpole2/compiler.cc
@@ -89,6 +89,54 @@ struct Upvalue {
   }
 };
 
+enum class FunType {
+  FUNCTION,
+  TOPLEVEL,
+};
+
+class Compiler final : private UnCopyable {
+  using ErrorFn = std::function<void (const str_t&)>;
+
+  Compiler* enclosing_{};
+  FunctionObject* fn_{};
+  FunType fn_type_{};
+
+  int scope_depth_{};
+
+  std::vector<LocalVar> locals_;
+  std::vector<Upvalue> upvalues_;
+public:
+  Compiler() noexcept {}
+
+  Compiler(Compiler* enclosing,
+      FunctionObject* fn, FunType fn_type, int scope_depth = 0) noexcept
+    : enclosing_(enclosing), fn_(fn), fn_type_(fn_type), scope_depth_(scope_depth) {
+  }
+
+  inline void set_compiler(Compiler* enclosing,
+      FunctionObject* fn, FunType fn_type, int scope_depth = 0) noexcept {
+    enclosing_ = enclosing;
+    fn_ = fn;
+    fn_type_ = fn_type;
+    scope_depth_ = scope_depth;
+  }
+
+  inline Compiler* enclosing() const noexcept { return enclosing_; }
+  inline FunctionObject* fn() const noexcept { return fn_; }
+  inline FunType fn_type() const noexcept { return fn_type_; }
+  inline int scope_depth() const noexcept { return scope_depth_; }
+  inline void set_scope_depth(int depth) noexcept { scope_depth_ = depth; }
+  inline int locals_count() const noexcept { return as_type<int>(locals_.size()); }
+  inline LocalVar& get_local(sz_t i) noexcept { return locals_[i]; }
+  inline const LocalVar& get_local(sz_t i) const noexcept { return locals_[i]; }
+  inline LocalVar& peek_local() noexcept { return locals_.back(); }
+  inline const LocalVar& peek_local() const noexcept { return locals_.back(); }
+  inline void append_local(const LocalVar& var) noexcept { return locals_.push_back(var); }
+  inline Upvalue& get_upvalue(sz_t i) noexcept { return upvalues_[i]; }
+  inline const Upvalue& get_upvalue(sz_t i) const noexcept { return upvalues_[i]; }
+  inline void append_upvalue(const Upvalue& u) noexcept { upvalues_.push_back(u); }
+};
+
 FunctionObject* GlobalCompiler::compile(VM& vm, const str_t& source_bytes) {
   return nullptr;
 }
