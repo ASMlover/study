9cb3153850049553042015de1fcb387a4c1d34df
ASMlover <asmlover@126.com>
2020-09-20 18:23:06 +0800

    :recycle: refactor(value): refactoring of value definiiotns

--- cplusplus/Tadpole3/value.cc
+++ cplusplus/Tadpole3/value.cc
@@ -28,11 +28,7 @@
 
 namespace tadpole {
 
-StringObject* BaseObject::as_string() { return nullptr; }
-const char* BaseObject::as_cstring() { return nullptr; }
-NativeObject* BaseObject::as_native() { return nullptr; }
-FunctionObject* BaseObject::as_function() { return nullptr; }
-UpvalueObject* BaseObject::as_upvalue() { return nullptr; }
-ClosureObject* BaseObject::as_closure() { return nullptr; }
+bool Value::is_truthy() const { return false; }
+str_t Value::stringify() const { return ""; }
 
 }
--- cplusplus/Tadpole3/value.hh
+++ cplusplus/Tadpole3/value.hh
@@ -25,48 +25,9 @@
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 #include "common.hh"
+#include "object.hh"
 
 namespace tadpole {
-
-enum class ObjType : u8_t {
-  STRING,
-  NATIVE,
-  FUNCTION,
-  UPVALUE,
-  CLOSURE,
-};
-
-class Chunk;
-class VM;
-
-class StringObject;
-class NativeObject;
-class FunctionObject;
-class UpvalueObject;
-class ClosureObject;
-
-class BaseObject : private UnCopyable {
-  ObjType type_;
-  bool marked_{};
-public:
-  BaseObject(ObjType type) noexcept : type_(type) {}
-  virtual ~BaseObject() {}
-
-  inline ObjType type() const noexcept { return type_; }
-  inline bool is_marked() const noexcept { return marked_; }
-  inline void set_marked(bool marked = true) noexcept { marked_ = marked; }
-
-  virtual bool is_truthy() const { return true; }
-  virtual str_t stringify() const { return "<object>"; }
-  virtual void gc_blacken(VM& vm) {}
-
-  StringObject* as_string();
-  const char* as_cstring();
-  NativeObject* as_native();
-  FunctionObject* as_function();
-  UpvalueObject* as_upvalue();
-  ClosureObject* as_closure();
-};
 
 enum class ValueType : u8_t {
   NIL,
@@ -75,6 +36,20 @@ enum class ValueType : u8_t {
   OBJECT,
 };
 
-using Value = double;
+class Value final : public Copyable {
+  ValueType type_{ValueType::NIL};
+  union {
+    bool boolean;
+    double numeric;
+    BaseObject* object{};
+  } as_;
+public:
+  Value() noexcept {}
+
+  inline ObjType objtype() const noexcept { return as_.object->type(); }
+
+  bool is_truthy() const;
+  str_t stringify() const;
+};
 
 }
