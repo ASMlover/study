bf769ad04d1a40f688978aece06449b2eb34aa39
ASMlover <asmlover@126.com>
2020-12-05 20:14:58 +0800

    :sparkles: feat(kinds): add definitions of kinds

---
+++ cplusplus/Tadpole4/tadpole/tadpole/kinds_def.hh
@@ -0,0 +1,45 @@
+#if !defined(TOKDEF)
+# define TOKDEF(k, s)
+#endif
+
+#if !defined(TOKEN)
+# define TOKEN(k, s) TOKDEF(TK_##k, s)
+#endif
+
+#if !defined(PUNCTUATOR)
+# define PUNCTUATOR(k, s) TOKEN(k, s)
+#endif
+
+#if !defined(KEYWORD)
+# define KEYWORD(k, s) TOKDEF(KW_##k, s)
+#endif
+
+PUNCTUATOR(LPARAN, "(")
+PUNCTUATOR(RPARAN, ")")
+PUNCTUATOR(LBRACE, "{")
+PUNCTUATOR(RBRACE, "}")
+PUNCTUATOR(COMMA, ",")
+PUNCTUATOR(MINUS, "-")
+PUNCTUATOR(PLUS, "+")
+PUNCTUATOR(SEMI, ";")
+PUNCTUATOR(SLASH, "/")
+PUNCTUATOR(STAR, "*")
+PUNCTUATOR(EQ, "=")
+
+TOKEN(IDENTIFIER, "Identifier")
+TOKEN(NUMERIC, "Numeric")
+TOKEN(STRING, "String")
+
+KEYWORD(FALSE, "false")
+KEYWORD(FN, "fn")
+KEYWORD(NIL, "nil")
+KEYWORD(TRUE, "true")
+KEYWORD(VAR, "var")
+
+TOKEN(EOF, "Eof")
+TOKEN(ERR, "Error")
+
+#undef KEYWORD
+#undef PUNCTUATOR
+#undef TOKEN
+#undef TOKDEF
