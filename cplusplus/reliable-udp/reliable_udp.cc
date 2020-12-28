0425185a0e9d2d307d60b8b3291d823c299b6fb4
ASMlover <asmlover@126.com>
2020-12-28 23:16:19 +0800

    :construction: chore(rudp): updated the implementation of rudp

--- cplusplus/reliable-udp/reliable_udp.cc
+++ cplusplus/reliable-udp/reliable_udp.cc
@@ -24,6 +24,7 @@
 // LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
+#include <cassert>
 #include <cstdlib>
 #include <cstring>
 #include <cstdint>
@@ -47,11 +48,58 @@ struct message_queue {
   struct message* tail;
 };
 
+static void queue_push(struct message_queue* mq, struct message* m) {
+  if (mq->head == nullptr) {
+    mq->head = mq->tail = m;
+  }
+  else {
+    mq->tail->next = m;
+    mq->tail = m;
+  }
+}
+
+static struct message* queue_pop(struct message_queue* mq, int id) {
+  if (mq->head == nullptr)
+    return nullptr;
+
+  struct message* m = mq->head;
+  if (m->id != id)
+    return nullptr;
+
+  mq->head = m->next;
+  m->next = nullptr;
+  if (mq->head == nullptr)
+    mq->tail = nullptr;
+  return m;
+}
+
 struct array {
   int cap;
   int n;
   int* a;
 };
+
+static void array_insert(struct array* a, int id) {
+  int i{};
+  for (; i < a->n; ++i) {
+    if (a->a[i] == id)
+      return;
+    if (a->a[i] > id)
+      break;
+  }
+  // insert id before index [i]
+  if (a->n >= a->cap) {
+    if (a->cap == 0)
+      a->cap = 16;
+    else
+      a->cap *= 2;
+    a->a = (int*)realloc(a->a, sizeof(int) * a->cap);
+  }
+  for (int j = a->n; j > i; --j)
+    a->a[j] = a->a[j - 1];
+  a->a[i] = id;
+  ++a->n;
+}
 
 struct rudp {
   struct message_queue send_queue; // user packages will send
@@ -92,11 +140,67 @@ static void free_message_list(struct message* m) {
   }
 }
 
+static struct message* new_message(struct rudp* u, const uint8_t* buffer, int sz) {
+  struct message* m = u->free_list;
+  if (m) {
+    u->free_list = m->next;
+    if (m->cap < sz) {
+      free(m);
+      m = nullptr;
+    }
+  }
+  if (m == nullptr) {
+    int cap = sz;
+    if (cap < GENERAL_PACKAGE)
+      cap = GENERAL_PACKAGE;
+    m = (struct message*)malloc(sizeof(*m) + cap);
+    if (m)
+      m->cap = cap;
+  }
+  if (m == nullptr)
+    return m;
+
+  m->sz = sz;
+  m->buffer = (uint8_t*)(m + 1);
+  if (sz > 0 && buffer)
+    memcpy(m->buffer, buffer, sz);
+  m->tick = 0;
+  m->id = 0;
+  m->next = nullptr;
+  return m;
+}
+
+static void delete_message(struct rudp* u, struct message* m) {
+  m->next = u->free_list;
+  u->free_list = m;
+}
+
+static void clear_send_expired(struct rudp* u, int tick) {
+  struct message* m = u->send_history.head;
+  struct message* last = nullptr;
+  while (m) {
+    if (m->tick >= tick)
+      break;
+    last = m;
+    m = m->next;
+  }
+  if (last) {
+    last->next = u->free_list;
+    u->free_list = u->send_history.head;
+  }
+  u->send_history.head = m;
+  if (m == nullptr)
+    u->send_history.tail = nullptr;
+}
+
+// rudp export methods
 struct rudp* rudp_new(int send_delay, int expired_time) {
-  struct rudp* u = malloc(sizeof(*u));
-  memset(u, 0, sizeof(*u));
-  u->send_delay = send_delay;
-  u->expired = expired_time;
+  struct rudp* u = (struct rudp*)malloc(sizeof(*u));
+  if (u) {
+    memset(u, 0, sizeof(*u));
+    u->send_delay = send_delay;
+    u->expired = expired_time;
+  }
 
   return u;
 }
@@ -109,6 +213,14 @@ void rudp_delete(struct rudp* u) {
   clear_outpackage(u);
   free(u->send_argain.a);
   free(u);
+}
+
+void rudp_send(struct rudp* u, const char* buffer, int sz) {
+  assert(sz <= MAX_PACKAGE);
+  struct message* m = new_message(u, (const uint8_t*)buffer, sz);
+  m->id = u->send_id++;
+  m->tick = u->current_tick;
+  queue_push(&u->send_queue, m);
 }
 
 }
