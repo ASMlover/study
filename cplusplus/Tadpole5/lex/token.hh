62d3407c4a84f42ed0c8652be7e75dd23a9620ab
ASMlover <asmlover@126.com>
2021-01-13 23:39:08 +0800

    :construction: chore(token): finished the token class

--- cplusplus/Tadpole5/lex/token.hh
+++ cplusplus/Tadpole5/lex/token.hh
@@ -55,4 +55,61 @@ inline std::ostream& operator<<(std::ostream& out, TokenKind kind) noexcept {
   return out << get_kind_name(kind);
 }
 
+class Token final : public Copyable {
+  TokenKind kind_{TokenKind::TK_ERR};
+  str_t literal_;
+  int lineno_{};
+public:
+  Token() noexcept {}
+
+  Token(TokenKind kind, const str_t& literal, int lineno = 0) noexcept
+    : kind_(kind), literal_(literal), lineno_(lineno) {
+  }
+
+  Token(const Token& r) noexcept
+    : kind_(r.kind_), literal_(r.literal_), lineno_(r.lineno_) {
+  }
+
+  Token(Token&& r) noexcept
+    : kind_(std::move(r.kind_)), literal_(std::move(r.literal_)), lineno_(std::move(r.lineno_)) {
+  }
+
+  inline Token& operator=(const Token& r) noexcept {
+    if (this != &r) {
+      kind_ = r.kind_;
+      literal_ = r.literal_;
+      lineno_ = r.lineno_;
+    }
+    return *this;
+  }
+
+  inline Token& operator=(Token&& r) noexcept {
+    if (this != &r) {
+      kind_ = std::move(r.kind_);
+      literal_ = std::move(r.literal_);
+      lineno_ = std::move(r.lineno_);
+    }
+    return *this;
+  }
+
+  inline bool operator==(const Token& r) const noexcept {
+    return this == &r ? true : literal_ == r.literal_;
+  }
+
+  inline bool operator!=(const Token& r) const noexcept {
+    return !(*this == r);
+  }
+
+  inline TokenKind kind() const noexcept { return kind_; }
+  inline const str_t& literal() const noexcept { return literal_; }
+  inline int lineno() const noexcept { return lineno_; }
+  inline double as_numeric() const noexcept { return std::atof(literal_.c_str()); }
+  inline const str_t& as_string() const noexcept { return literal_; }
+  inline const char* as_cstring() const noexcept { return literal_.c_str(); }
+
+  static Token make(const str_t& literal) noexcept {
+    return Token(TokenKind::TK_STRING, literal);
+  }
+};
+
 }
