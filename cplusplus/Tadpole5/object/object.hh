ead8b956f27d01b1c785a153b594df04bdeba755
ASMlover <asmlover@126.com>
2021-01-27 22:29:53 +0800

    :sparkles: feat(object): add basic object declaration

---
+++ cplusplus/Tadpole5/object/object.hh
@@ -0,0 +1,79 @@
+// Copyright (c) 2021 ASMlover. All rights reserved.
+//
+//  _____         _             _
+// |_   _|_ _  __| |_ __   ___ | | ___
+//   | |/ _` |/ _` | '_ \ / _ \| |/ _ \
+//   | | (_| | (_| | |_) | (_) | |  __/
+//   |_|\__,_|\__,_| .__/ \___/|_|\___|
+//                 |_|
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+#include "../common/common.hh"
+
+namespace tadpole {
+
+enum class ObjType : u8_t {
+  STRING,
+  NATIVE,
+  FUNCTION,
+  UPVALUE,
+  CLOSURE,
+};
+
+class VM;
+
+class StringObject;
+class NativeObject;
+class FunctionObject;
+class UpvalueObject;
+class ClosureObject;
+
+class BaseObject : private UnCopyable {
+  ObjType type_;
+  bool marked_{};
+public:
+  BaseObject(ObjType type) noexcept : type_(type) {}
+  virtual ~BaseObject() {}
+
+  inline ObjType type() const noexcept { return type_; }
+  inline bool is_marked() const noexcept { return marked_; }
+  inline void set_marked(bool marked = true) noexcept { marked_ = marked; }
+
+  virtual bool is_truthy() const { return true; }
+  virtual str_t stringify() const { return "<object>"; }
+  virtual void gc_blacken(VM& vm) {}
+
+  StringObject* as_string();
+  const char* as_cstring();
+  NativeObject* as_native();
+  FunctionObject* as_function();
+  UpvalueObject* as_upvalue();
+  ClosureObject* as_closure();
+};
+
+}
