1c9a7a11a3b557d26a8dedf5381abff7ba3f06cf
ASMlover <asmlover@126.com>
2021-03-04 22:30:50 +0800

    :sparkles: feat(callframe): add callframe module

---
+++ cplusplus/Tadpole5/core/callframe.hh
@@ -0,0 +1,52 @@
+// Copyright (c) 2021 ASMlover. All rights reserved.
+//
+//  _____         _             _
+// |_   _|_ _  __| |_ __   ___ | | ___
+//   | |/ _` |/ _` | '_ \ / _ \| |/ _ \
+//   | | (_| | (_| | |_) | (_) | |  __/
+//   |_|\__,_|\__,_| .__/ \___/|_|\___|
+//                 |_|
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+#include "../common/common.hh"
+#include "../object/object.hh"
+
+namespace tadpole {
+
+class CallFrame final : public Copyable {
+  ClosureObject* closure_{};
+  const u8_t* ip_{};
+  sz_t stack_begpos_{};
+public:
+  CallFrame() noexcept {}
+  CallFrame(ClosureObject* c, const u8_t* i, sz_t begpos = 0) noexcept
+    : closure_(c), ip_(i), stack_begpos_(begpos) {
+  }
+};
+
+}
