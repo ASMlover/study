f821a625f254d0b18839b6b9ba63135efa9baedd
ASMlover <asmlover@126.com>
2021-03-09 23:48:59 +0800

    :construction: chore(main): updated the import header for main entry

--- cplusplus/Tadpole5/main.cc
+++ cplusplus/Tadpole5/main.cc
@@ -31,7 +31,6 @@
 // LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
-#include "common/common.hh"
 #include "common/harness.hh"
 
 int main(int argc, char* argv[]) {
