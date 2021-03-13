c2de0d0c1547b374e061bf380430a13d2f2b23ce
ASMlover <asmlover@126.com>
2021-03-13 23:12:20 +0800

    :construction: chore(string-object): updated the implementation of string object

--- cplusplus/Tadpole5/object/string_object.cc
+++ cplusplus/Tadpole5/object/string_object.cc
@@ -31,7 +31,7 @@
 // LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
-#include "object_helper.hh"
+#include "../gc/gc.hh"
 #include "string_object.hh"
 
 namespace tadpole {
