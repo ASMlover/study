0c69c6cbd3013c927a330f9d6165c10bcc56c012
ASMlover <asmlover@126.com>
2021-03-16 22:16:46 +0800

    :sparkles: feat(gc): add basic gc

---
+++ cplusplus/Tadpole5/gc/base_gc.hh
@@ -0,0 +1,77 @@
+// Copyright (c) 2021 ASMlover. All rights reserved.
+//
+//  _____         _             _
+// |_   _|_ _  __| |_ __   ___ | | ___
+//   | |/ _` |/ _` | '_ \ / _ \| |/ _ \
+//   | | (_| | (_| | |_) | (_) | |  __/
+//   |_|\__,_|\__,_| .__/ \___/|_|\___|
+//                 |_|
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+#include <functional>
+#include <unordered_map>
+#include "../object/value.hh"
+
+namespace tadpole {
+
+using MarkCallback = std::function<void ()>;
+
+class BaseGC : private UnCopyable {
+  std::unordered_map<str_t, MarkCallback> custom_marks_;
+  std::unordered_map<u32_t, StringObject*> interned_strings_;
+public:
+  virtual ~BaseGC() {
+    interned_strings_.clear();
+  }
+
+  virtual void collect() {}
+  virtual void append_object(BaseObject* o) {}
+  virtual void mark_object(BaseObject* o) {}
+  virtual void mark_object(Value& v) {}
+
+  inline void reg_custom_mark(const str_t& name, MarkCallback&& mark) noexcept {
+    custom_marks_[name] = std::move(mark);
+  }
+
+  inline void unreg_custom_mark(const str_t& name) noexcept {
+    if (auto it = custom_marks_.find(name); it != custom_marks_.end())
+      custom_marks_.erase(it);
+  }
+
+  inline void set_interned(u32_t h, StringObject* s) noexcept {
+    interned_strings_[h] = s;
+  }
+
+  inline StringObject* get_interned(u32_t h) const noexcept {
+    if (auto it = interned_strings_.find(h); it != interned_strings_.end())
+      return it->second;
+    return nullptr;
+  }
+};
+
+}
