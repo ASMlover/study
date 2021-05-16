b71eccb8c78822bd8d969a19790f1243cd73e3fe
ASMlover <asmlover@126.com>
2021-05-16 20:24:57 +0800

    :white_check_mark: test(token): updated the unitcase for token

--- cplusplus/Tadpole6/test_token.cc
+++ cplusplus/Tadpole6/test_token.cc
@@ -35,3 +35,57 @@
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 #include "harness.hh"
+#include "token.hh"
+
+TADPOLE_TEST(TadpoleToken) {
+  using TK = tadpole::TokenKind;
+#define NEWTK2(k, s)    tadpole::Token(k, s, 0)
+#define NEWTK3(k, s, l) tadpole::Token(k, s, l)
+#define TESTEQ(a, b)    TADPOLE_CHECK_EQ(a, b)
+#define TESTTK(k, s)    do {\
+  auto t = NEWTK2(k, s);\
+  TESTEQ(t.kind(), k);\
+  TESTEQ(t.literal(), s);\
+  TESTEQ(t.lineno(), 0);\
+} while (false)
+#define TESTNUM(n)      do {\
+  auto t = NEWTK2(TK::TK_NUMERIC, #n);\
+  TESTEQ(t.kind(), TK::TK_NUMERIC);\
+  TESTEQ(t.as_numeric(), n);\
+  TESTEQ(t.lineno(), 0);\
+} while (false)
+#define TESTSTR(s, l)   do {\
+  auto t = NEWTK3(TK::TK_STRING, s, l);\
+  TESTEQ(t.kind(), TK::TK_STRING);\
+  TESTEQ(t.literal(), s);\
+  TESTEQ(t.lineno(), l);\
+} while (false)
+
+  TESTTK(TK::TK_LPAREN, "(");
+  TESTTK(TK::TK_RPAREN, ")");
+  TESTTK(TK::TK_LBRACE, "{");
+  TESTTK(TK::TK_RBRACE, "}");
+  TESTTK(TK::TK_COMMA, ",");
+  TESTTK(TK::TK_MINUS, "-");
+  TESTTK(TK::TK_PLUS, "+");
+  TESTTK(TK::TK_SEMI, ";");
+  TESTTK(TK::TK_SLASH, "/");
+  TESTTK(TK::TK_STAR, "*");
+  TESTTK(TK::TK_EQ, "=");
+
+  TESTTK(TK::KW_FALSE, "false");
+  TESTTK(TK::KW_FN, "fn");
+  TESTTK(TK::KW_NIL, "nil");
+  TESTTK(TK::KW_TRUE, "true");
+  TESTTK(TK::KW_VAR, "var");
+
+  TESTTK(TK::TK_EOF, "EOF");
+  TESTTK(TK::TK_ERR, "ERR");
+
+#undef TESTSTR
+#undef TESTNUM
+#undef TESTTK
+#undef TESTEQ
+#undef NEWTK3
+#undef NEWTK2
+}
