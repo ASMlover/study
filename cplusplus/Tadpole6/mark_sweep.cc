0687d43d7d6d42d3d9240f7fbd35eb4e90119a19
ASMlover <asmlover@126.com>
2021-06-13 23:29:30 +0800

    :sparkles: feat(mark-sweep): add mark sweep gc implementation

---
+++ cplusplus/Tadpole6/mark_sweep.cc
@@ -0,0 +1,109 @@
+// Copyright (c) 2021 ASMlover. All rights reserved.
+//
+//  ______             __                  ___
+// /\__  _\           /\ \                /\_ \
+// \/_/\ \/    __     \_\ \  _____     ___\//\ \      __
+//    \ \ \  /'__`\   /'_` \/\ '__`\  / __`\\ \ \   /'__`\
+//     \ \ \/\ \L\.\_/\ \L\ \ \ \L\ \/\ \L\ \\_\ \_/\  __/
+//      \ \_\ \__/.\_\ \___,_\ \ ,__/\ \____//\____\ \____\
+//       \/_/\/__/\/_/\/__,_ /\ \ \/  \/___/ \/____/\/____/
+//                             \ \_\
+//                              \/_/
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#include <iostream>
+#include "colorful.hh"
+#include "mark_sweep.hh"
+
+namespace tadpole {
+
+MarkSweep::MarkSweep() noexcept {
+}
+
+MarkSweep::~MarkSweep() {
+  while (!objects_.empty()) {
+    auto* o = objects_.back();
+    objects_.pop_back();
+
+    reclaim_object(o);
+  }
+}
+
+void MarkSweep::collect() {
+  mark_from_roots();
+  sweep();
+
+  gc_threshold_ = std::max(std::min(gc_threshold_, kGCThresholdMin), objects_.size() * kGCFactor);
+  gc_threshold_ = as_align(gc_threshold_, kGCAlign);
+}
+
+void MarkSweep::append_object(BaseObject* o) {
+  if (objects_.size() >= gc_threshold_)
+    collect();
+  objects_.push_back(o);
+}
+
+void MarkSweep::mark_object(BaseObject* o) {
+  if (o == nullptr || o->is_marked())
+    return;
+
+#if defined(_TADPOLE_DEBUG_GC)
+  std::cout
+    << colorful::fg::lightcyan << "[" << o << "] mark object: `" << o->stringify() << "`"
+    << colorful::reset << std::endl;
+#endif
+
+  o->set_marked(true);
+  worklist_.push_back(o);
+}
+
+sz_t MarkSweep::get_count() const {
+  return objects_.size();
+}
+
+sz_t MarkSweep::get_threshold() const {
+  return gc_threshold_;
+}
+
+void MarkSweep::set_threshold(sz_t threshold) {
+  gc_threshold_ = as_align(threshold, kGCAlign);
+}
+
+void MarkSweep::mark() {}
+void MarkSweep::mark_from_roots() {}
+void MarkSweep::sweep() {}
+
+void MarkSweep::reclaim_object(BaseObject* o) {
+#if defined(_TADPOLE_DEBUG_GC)
+  std::cout
+    << colorful::fg::gray << "[" << o << "] reclaim object type: `" << o->type_asstr() << "`"
+    << colorful::reset << std::endl;
+#endif
+
+  delete o;
+}
+
+}
---
+++ cplusplus/Tadpole6/mark_sweep.hh
@@ -0,0 +1,71 @@
+// Copyright (c) 2021 ASMlover. All rights reserved.
+//
+//  ______             __                  ___
+// /\__  _\           /\ \                /\_ \
+// \/_/\ \/    __     \_\ \  _____     ___\//\ \      __
+//    \ \ \  /'__`\   /'_` \/\ '__`\  / __`\\ \ \   /'__`\
+//     \ \ \/\ \L\.\_/\ \L\ \ \ \L\ \/\ \L\ \\_\ \_/\  __/
+//      \ \_\ \__/.\_\ \___,_\ \ ,__/\ \____//\____\ \____\
+//       \/_/\/__/\/_/\/__,_ /\ \ \/  \/___/ \/____/\/____/
+//                             \ \_\
+//                              \/_/
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+#include <list>
+#include "object.hh"
+#include "base_gc.hh"
+
+namespace tadpole {
+
+class MarkSweep final : public BaseGC {
+  static constexpr sz_t kGCAlign        = 8;
+  static constexpr sz_t kGCThresholdMin = 1 << 3;
+  static constexpr sz_t kGCThresholdDef = 1 << 8;
+  static constexpr sz_t kGCFactor       = 2;
+
+  sz_t gc_threshold_{kGCThresholdDef};
+  std::list<BaseObject*> objects_;
+  std::list<BaseObject*> worklist_;
+
+  void mark();
+  void mark_from_roots();
+  void sweep();
+  void reclaim_object(BaseObject* o);
+public:
+  MarkSweep() noexcept;
+  virtual ~MarkSweep();
+
+  virtual void collect() override;
+  virtual void append_object(BaseObject* o) override;
+  virtual void mark_object(BaseObject* o) override;
+  virtual sz_t get_count() const override;
+  virtual sz_t get_threshold() const override;
+  virtual void set_threshold(sz_t threshold) override;
+};
+
+}
