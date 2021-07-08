1a1110efd94ea3705154f71b37f3452be7978afa
ASMlover <asmlover@126.com>
2021-07-08 23:10:53 +0800

    :construction: chore(mark-sweep): updated the implementation of mark-sweep gc

--- cplusplus/Tadpole6/mark_sweep.cc
+++ cplusplus/Tadpole6/mark_sweep.cc
@@ -116,6 +116,13 @@ void MarkSweep::mark_from_roots() {
 }
 
 void MarkSweep::sweep() {
+  for (auto it = interned_strings_.begin(); it != interned_strings_.end();) {
+    if (!it->second->is_marked())
+      interned_strings_.erase(it++);
+    else
+      ++it;
+  }
+
   // TODO:
 }
 
