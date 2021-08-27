0ace794d61e6cb96c214ea23f33e694a92f8fd20
ASMlover <asmlover@126.com>
2021-08-27 23:56:54 +0800

    :constrution: chore(Token): add token implementation

---
+++ cplusplus/Tadpole7/Lex/Token.cc
@@ -0,0 +1,66 @@
+// Copyright (c) 2021 ASMlover. All rights reserved.
+//
+//  ______             __                  ___
+// /\__  _\           /\ \                /\_ \
+// \/_/\ \/    __     \_\ \  _____     ___\//\ \      __
+//    \ \ \  /'__`\   /'_` \/\ '__`\  / __`\\ \ \   /'__`\
+//     \ \ \/\ \L\.\_/\ \L\ \ \ \L\ \/\ \L\ \\_\ \_/\  __/
+//      \ \_\ \__/.\_\ \___,_\ \ ,__/\ \____//\____\ \____\
+//       \/_/\/__/\/_/\/__,_ /\ \ \/  \/___/ \/____/\/____/
+//                             \ \_\
+//                              \/_/
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#include <unordered_map>
+#include <Lex/Token.hh>
+
+namespace Tadpole::Lex {
+
+static constexpr const char* kNames[] = {
+#undef TOKDEF
+#define TOKDEF(k, s) s,
+#include <Lex/KindsDef.hh>
+#undef TOKDEF
+
+  nullptr
+};
+
+static const std::unordered_map<str_t, TokenKind> kKWs = {
+#undef KEYWORD
+#define KEYWORD(k, s) {s, TokenKind::KW_##k},
+#include <Lex/KindsDef.hh>
+#undef KEYWORD
+};
+
+const char* get_kind_name(TokenKind kind) noexcept {
+  return nullptr;
+}
+
+TokenKind get_keyword_kind(const str_t& key) noexcept {
+  return TokenKind::TK_IDENTIFIER;
+}
+
+}
