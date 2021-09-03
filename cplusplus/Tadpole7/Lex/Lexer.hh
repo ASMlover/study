70e6aebf75e79dda2eecb1f2da9614cf4201f5d5
ASMlover <asmlover@126.com>
2021-09-03 23:21:54 +0800

    :construction: chore(lexer): updated the declaration of lexer

--- cplusplus/Tadpole7/Lex/Lexer.hh
+++ cplusplus/Tadpole7/Lex/Lexer.hh
@@ -52,6 +52,34 @@ class Lexer final : private Common::UnCopyable {
   inline bool is_digit(char c) const noexcept { return std::isdigit(c); }
   inline bool is_tail() const noexcept { return curpos_ >= source_bytes_.size(); }
   inline bool advance() noexcept { return source_bytes_[curpos_++]; }
+
+  inline str_t gen_literal(sz_t begpos, sz_t endpos) const noexcept {
+    return source_bytes_.substr(begpos, endpos - begpos);
+  }
+
+  inline char peek(sz_t distance = 0) const noexcept {
+    return curpos_ + distance >= source_bytes_.size() ? 0 : source_bytes_[curpos_ + distance];
+  }
+
+  inline bool match(char expected) noexcept {
+    if (source_bytes_[curpos_] == expected) {
+      advance();
+      return true;
+    }
+    return false;
+  }
+
+  void skip_whitespace();
+  Token make_token(TokenKind kind);
+  Token make_token(TokenKind kind, const str_t& literal);
+  Token make_error(const str_t& message);
+  Token make_identifier();
+  Token make_numeric();
+  Token make_string();
+public:
+  Lexer(const str_t& source_bytes) noexcept : source_bytes_(source_bytes) {}
+
+  Token next_token();
 };
 
 }
