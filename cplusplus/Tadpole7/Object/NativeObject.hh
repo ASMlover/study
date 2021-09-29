01a2993ca37a8b7c719f5e878ba7312621a825ae
ASMlover <asmlover@126.com>
2021-09-30 03:57:14 +0800

    :sparkles: feat(native): add native object definitions

---
+++ cplusplus/Tadpole7/Object/NativeObject.hh
@@ -0,0 +1,58 @@
+// Copyright (c) 2021 ASMlover. All rights reserved.
+//
+//  ______             __                  ___
+// /\__  _\           /\ \                /\_ \
+// \/_/\ \/    __     \_\ \  _____     ___\//\ \      __
+//    \ \ \  /'__`\   /'_` \/\ '__`\  / __`\\ \ \   /'__`\
+//     \ \ \/\ \L\.\_/\ \L\ \ \ \L\ \/\ \L\ \\_\ \_/\  __/
+//      \ \_\ \__/.\_\ \___,_\ \ ,__/\ \____//\____\ \____\
+//       \/_/\/__/\/_/\/__,_ /\ \ \/  \/___/ \/____/\/____/
+//                             \ \_\
+//                              \/_/
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+#include <Object/Object.hh>
+#include <Object/Value.hh>
+
+namespace Tadpole::Object {
+
+class NativeObject final : public BaseObject {
+  Value::TadpoleCFun fn_{};
+public:
+  NativeObject(Value::TadpoleCFun&& fn) noexcept : BaseObject(ObjType::NATIVE), fn_(std::move(fn)) {}
+
+  inline const Value::TadpoleCFun& fn() const noexcept { return fn_; }
+  inline Value::Value call(sz_t nargs, Value::Value* args) noexcept { return fn_(nargs, args); }
+  inline Value::Value operator()(sz_t nargs, Value::Value* args) noexcept { return fn_(nargs, args); }
+
+  virtual str_t stringify() const override;
+
+  static NativeObject* create(Value::TadpoleCFun&& fn);
+};
+
+}
