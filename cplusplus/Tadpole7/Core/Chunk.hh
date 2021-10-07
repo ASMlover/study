469ef3f4d191517143432f0052d7f4be0d78d37a
ASMlover <asmlover@126.com>
2021-10-07 20:20:27 +0800

    :sparkles: feat(core): add core module and code definitions

---
+++ cplusplus/Tadpole7/Core/Chunk.hh
@@ -0,0 +1,91 @@
+// Copyright (c) 2021 ASMlover. All rights reserved.
+//
+//  ______             __                  ___
+// /\__  _\           /\ \                /\_ \
+// \/_/\ \/    __     \_\ \  _____     ___\//\ \      __
+//    \ \ \  /'__`\   /'_` \/\ '__`\  / __`\\ \ \   /'__`\
+//     \ \ \/\ \L\.\_/\ \L\ \ \ \L\ \/\ \L\ \\_\ \_/\  __/
+//      \ \_\ \__/.\_\ \___,_\ \ ,__/\ \____//\____\ \____\
+//       \/_/\/__/\/_/\/__,_ /\ \ \/  \/___/ \/____/\/____/
+//                             \ \_\
+//                              \/_/
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+#include <vector>
+#include <Common/Common.hh>
+#include <Object/Value.hh>
+
+namespace Tadpole::Core {
+
+enum class Code : u8_t {
+  CONSTANT,
+
+  NIL,
+  FALSE,
+  TRUE,
+  POP,
+
+  DEF_GLOBAL,
+  GET_GLOBAL,
+  SET_GLOBAL,
+  GET_LOCAL,
+  SET_LOCAL,
+  GET_UPVALUE,
+  SET_UPVALUE,
+
+  ADD,
+  SUB,
+  MUL,
+  DIV,
+
+  CALL_0,
+  CALL_1,
+  CALL_2,
+  CALL_3,
+  CALL_4,
+  CALL_5,
+  CALL_6,
+  CALL_7,
+  CALL_8,
+
+  CLOSURE,
+
+  CLOSE_UPVALUE,
+
+  RETURN,
+};
+
+inline int operator-(Code a, Code b) noexcept {
+  return Common::as_type<int>(a) - Common::as_type<int>(b);
+}
+
+template <typename N> inline Code operator+(Code a, N b) noexcept {
+  return Common::as_type<Code>(Common::as_type<int>(a) + Common::as_type<int>(b));
+}
+
+}
