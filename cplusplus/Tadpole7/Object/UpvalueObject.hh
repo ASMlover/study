c64cd0d2de026f7cd4d278b0d19d20da24632558
ASMlover <asmlover@126.com>
2021-10-20 20:28:58 +0800

    :sparkles: feat(upvalue): add upvalue object definitions

---
+++ cplusplus/Tadpole7/Object/UpvalueObject.hh
@@ -0,0 +1,72 @@
+// Copyright (c) 2021 ASMlover. All rights reserved.
+//
+//  ______             __                  ___
+// /\__  _\           /\ \                /\_ \
+// \/_/\ \/    __     \_\ \  _____     ___\//\ \      __
+//    \ \ \  /'__`\   /'_` \/\ '__`\  / __`\\ \ \   /'__`\
+//     \ \ \/\ \L\.\_/\ \L\ \ \ \L\ \/\ \L\ \\_\ \_/\  __/
+//      \ \_\ \__/.\_\ \___,_\ \ ,__/\ \____//\____\ \____\
+//       \/_/\/__/\/_/\/__,_ /\ \ \/  \/___/ \/____/\/____/
+//                             \ \_\
+//                              \/_/
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+#include <Object/Object.hh>
+#include <Object/Value.hh>
+
+namespace Tadpole::Object {
+
+class UpvalueObject final : public BaseObject {
+  Value::Value* value_{};
+  Value::Value closed_{};
+  UpvalueObject* next_{};
+public:
+  UpvalueObject(Value::Value* value, UpvalueObject* next = nullptr) noexcept
+    : BaseObject(ObjType::UPVALUE), value_(value), next_(next) {
+  }
+
+  inline Value::Value* value() const noexcept { return value_; }
+  inline Value::Value* value_asptr() const noexcept { return value_; }
+  inline const Value::Value& value_asref() const noexcept { return *value_; }
+  inline void set_value(Value::Value* value) noexcept { value_ = value; }
+  inline void set_value(const Value::Value& value) noexcept { *value_ = value; }
+  inline const Value::Value& closed() const noexcept { return closed_; }
+  inline Value::Value* closed_asptr() const noexcept { return const_cast<Value::Value*>(&closed_); }
+  inline const Value::Value& closed_ref() const noexcept { return closed_; }
+  inline void set_closed(const Value::Value& closed) noexcept { closed_ = closed; }
+  inline void set_closed(Value::Value* closed) noexcept { closed_ = *closed; }
+  inline UpvalueObject* next() const noexcept { return next_; }
+  inline void set_next(UpvalueObject* next = nullptr) noexcept { next_ = next; }
+
+  virtual str_t stringify() const override;
+  virtual void iter_children(ObjectVisitor&& visitor) override;
+
+  static UpvalueObject* create(Value::Value* value, UpvalueObject* next = nullptr);
+};
+
+}
