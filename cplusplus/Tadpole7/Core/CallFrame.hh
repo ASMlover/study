ee06afbd30fe00f920b8504f5be643b0f4332c65
ASMlover <asmlover@126.com>
2021-10-26 01:50:21 +0800

    :sparkles: feat(call-frame): add call frame for tadpole

---
+++ cplusplus/Tadpole7/Core/CallFrame.hh
@@ -0,0 +1,67 @@
+// Copyright (c) 2021 ASMlover. All rights reserved.
+//
+//  ______             __                  ___
+// /\__  _\           /\ \                /\_ \
+// \/_/\ \/    __     \_\ \  _____     ___\//\ \      __
+//    \ \ \  /'__`\   /'_` \/\ '__`\  / __`\\ \ \   /'__`\
+//     \ \ \/\ \L\.\_/\ \L\ \ \ \L\ \/\ \L\ \\_\ \_/\  __/
+//      \ \_\ \__/.\_\ \___,_\ \ ,__/\ \____//\____\ \____\
+//       \/_/\/__/\/_/\/__,_ /\ \ \/  \/___/ \/____/\/____/
+//                             \ \_\
+//                              \/_/
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+#include <Common/Common.hh>
+#include <Object/FunctionObject.hh>
+#include <Object/ClosureObject.hh>
+
+namespace Tadpole::Core {
+
+class Chunk;
+
+class CallFrame final : public Common::Copyable {
+  Object::ClosureObject* closure_{};
+  const u8_t* ip_{};
+  sz_t stack_begpos_{};
+public:
+  CallFrame() noexcept {}
+  CallFrame(Object::ClosureObject* c, const u8_t* i, sz_t begpos = 0) noexcept
+    : closure_(c), ip_(i), stack_begpos_(begpos) {
+  }
+
+  inline Object::ClosureObject* closure() const noexcept { return closure_; }
+  inline Object::FunctionObject* frame_fn() const noexcept { return closure_->fn(); }
+  inline Chunk* frame_chunk() const noexcept { return frame_fn()->chunk(); }
+  inline const u8_t* ip() const noexcept { return ip_; }
+  inline u8_t get_ip(sz_t i) const noexcept { return ip_[i]; }
+  inline u8_t inc_ip() noexcept { return *ip_++; }
+  inline u8_t dec_ip() noexcept { return *ip_--; }
+  inline sz_t stack_begpos() const noexcept { return stack_begpos_; }
+};
+
+}
