64a971eaede02c047687d3b19ba6bef47f8629c3
ASMlover <asmlover@126.com>
2021-11-17 00:39:24 +0800

    :construction: chore(built-in): updated the built-in implementation

--- cplusplus/Tadpole7/Builtin/Builtins.cc
+++ cplusplus/Tadpole7/Builtin/Builtins.cc
@@ -44,7 +44,26 @@
 namespace Tadpole::Builtin {
 
 void register_builtins(Core::TadpoleVM& vm) noexcept {
-  // TODO:
+  // fn help() -> Nil
+  vm.define_native("help", [](sz_t, Value::Value*) -> Value::Value {
+        std::cout
+          << Common::Colorful::fg::green << "Welcome To Tadpole! This is the help utility."
+          << Common::Colorful::reset << std::endl << std::endl;
+
+          std::cout << "print(...)      Prints the values to stdout stream" << std::endl;
+          std::cout << "exit()          Exit the Tadpole terminal" << std::endl;
+
+          return nullptr;
+      });
+
+  // fn print(arg1: Value, arg2: Value, arg3: Value, ...) -> Nil
+  vm.define_native("print", [](sz_t nargs, Value::Value* args) -> Value::Value {
+        for (sz_t i = 0; i < nargs; ++i)
+          std::cout << args[i] << " ";
+        std::cout << std::endl;
+
+        return nullptr;
+      });
 }
 
 }
