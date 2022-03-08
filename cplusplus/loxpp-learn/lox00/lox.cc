9aedcd7751af933b58e8607c26c630a849766d62
ASMlover <asmlover@126.com>
2022-03-09 01:42:00 +0800

    :sparkles: feat(lox): add lox entry module

---
+++ cplusplus/loxpp-learn/lox00/lox.cc
@@ -0,0 +1,54 @@
+// Copyright (c) 2022 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#include <iostream>
+#include "lox.hh"
+
+namespace loxpp {
+
+int Lox::run(int argc, char* argv[]) {
+  LOXPP_UNUSED(argc), LOXPP_UNUSED(argv);
+
+  if (argc > 1) {
+    std::cout << "Usage: loxpp [script]" << std::endl;
+  }
+  else if (argc == 1) {
+    run_from_file(argv[0]);
+  }
+  else {
+    run_from_prompt();
+  }
+
+  return 0;
+}
+
+void Lox::run_from_file(const str_t& filepath) {
+}
+
+void Lox::run_from_prompt() {
+}
+
+}
