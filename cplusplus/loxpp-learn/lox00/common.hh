d8d1de7c624ee8c9c3f9624c1e9dbd8fb6877976
ASMlover <asmlover@126.com>
2022-03-09 00:50:43 +0800

    :sparkles: feat(lox): add basic lox initial-project

---
+++ cplusplus/loxpp-learn/lox00/common.hh
@@ -0,0 +1,97 @@
+// Copyright (c) 2022 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+#include <string>
+#include <string_view>
+#include <sstream>
+
+#if !defined(LOXPP_UNUSED)
+# define LOXPP_UNUSED(x)  ((void)x)
+#endif
+
+#if defined(__GNUC__) || defined(__clang__)
+# define LOXPP_GNUC
+#else
+# define LOXPP_MSVC
+#endif
+
+#if !defined(interface)
+# define interface struct
+#endif
+
+namespace lox {
+
+using nil_t   = std::nullptr_t;
+using byte_t  = std::uint8_t;
+using i8_t    = std::int8_t;
+using u8_t    = std::uint8_t;
+using i16_t   = std::int16_t;
+using u16_t   = std::uint16_t;
+using i32_t   = std::int32_t;
+using u32_t   = std::uint32_t;
+using i64_t   = std::int64_t;
+using u64_t   = std::uint64_t;
+using sz_t    = std::size_t;
+#if defined(LOXPP_GNUC)
+  using ssz_t = std::ssize_t;
+#else
+  using ssz_t = std::int64_t;
+#endif
+using str_t   = std::string;
+using strv_t  = std::string_view;
+using ss_t    = std::stringstream;
+
+class Copyable {
+protected:
+  Copyable() noexcept = default;
+  ~Copyable() noexcept = default;
+  Copyable(const Copyable&) noexcept = default;
+  Copyable(Copyable&&) noexcept = default;
+  Copyable& operator=(const Copyable&) noexcept = default;
+  Copyable& operator=(Copyable&&) noexcept = default;
+};
+
+class UnCopyable {
+  UnCopyable(const UnCopyable&) noexcept = delete;
+  UnCopyable(UnCopyable&&) noexcept = delete;
+  UnCopyable& operator=(const UnCopyable&) noexcept = delete;
+  UnCopyable& operator=(UnCopyable&&) noexcept = delete;
+protected:
+  UnCopyable() noexcept = default;
+  ~UnCopyable() noexcept = default;
+};
+
+template <typename Object> class Singleton : private UnCopyable {
+public:
+  static Object& get_instance() noexcept {
+    static Object ins;
+    return ins;
+  }
+};
+
+}
