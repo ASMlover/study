bd79f696ed2e4f8bcf01c29de1476c6e5a9c1c5e
ASMlover <asmlover@126.com>
2022-07-26 01:12:03 +0800

    :memo: doc(parser): add comment for parser

--- cplusplus/loxpp-learn/lox00/parser.hh
+++ cplusplus/loxpp-learn/lox00/parser.hh
@@ -35,17 +35,19 @@
 
 namespace loxpp::parser {
 
-// program    -> statement* EOF ;
-// statement  -> exprStmt | printStmt ;
-// exprStmt   -> expression ";" ;
-// printStmt  -> "print" expression ";" ;
-// expression -> equality ;
-// equality   -> comparison ( ( "!=" | "==" ) comparison )* ;
-// comparison -> term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
-// term       -> factor ( ( "-" | "+" ) factor )* ;
-// factor     -> unary ( ( "/" | "*" ) unary )* ;
-// unary      -> ( "!" | "-" ) unary | primary ;
-// primary    -> NUMBER | STRING | "true" | "false" | "nil" | "(" expression ")" ;
+// program      -> declaration* EOF ;
+// declaration  -> varDecl | statement ;
+// varDecl      -> "var" IDENTIFIER ( "=" expression )? ";" ;
+// statement    -> exprStmt | printStmt ;
+// exprStmt     -> expression ";" ;
+// printStmt    -> "print" expression ";" ;
+// expression   -> equality ;
+// equality     -> comparison ( ( "!=" | "==" ) comparison )* ;
+// comparison   -> term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
+// term         -> factor ( ( "-" | "+" ) factor )* ;
+// factor       -> unary ( ( "/" | "*" ) unary )* ;
+// unary        -> ( "!" | "-" ) unary | primary ;
+// primary      -> NUMBER | STRING | "true" | "false" | "nil" | "(" expression ")" | IDENTIFIER ;
 
 class ParserError final : public Copyable, public std::exception {};
 
