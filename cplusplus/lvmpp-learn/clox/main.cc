38b987d78d8e573d9408c8956a544119b29a4acf
ASMlover <asmlover@126.com>
2023-03-13 00:55:05 +0800

    :construction: chore(run-entry): add running entry for clox

--- cplusplus/lvmpp-learn/clox/main.cc
+++ cplusplus/lvmpp-learn/clox/main.cc
@@ -24,11 +24,47 @@
 // LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
+#include <iostream>
+#include <fstream>
+#include <string>
 #include "common.hh"
 #include "harness.hh"
 #include "chunk.hh"
 #include "vm.hh"
 
+static clox::VM& get_vm() noexcept {
+  static clox::VM vm;
+  return vm;
+}
+
+static void repl() noexcept {
+  clox::str_t line;
+  for (;;) {
+    std::cout << "> ";
+
+    if (!std::getline(std::cin, line))
+      break;
+
+    get_vm().interpret(line);
+  }
+}
+
+static void run_file(const char* filepath) noexcept {
+  std::fstream fp(filepath);
+  if (!fp.is_open()) {
+    std::cerr << "ERROR LOAD: `" << filepath << "` FAILED !!!" << std::endl;
+    std::exit(74);
+  }
+
+  clox::ss_t ss;
+  ss << fp.rdbuf();
+
+  clox::InterpretResult result = get_vm().interpret(ss.str());
+  if (result == clox::InterpretResult::INTERPRET_COMPILE_ERROR)
+    std::exit(65);
+  if (result == clox::InterpretResult::INTERPRET_RUNTIME_ERROR)
+    std::exit(70);
+}
 
 int main(int argc, char* argv[]) {
   CLOX_UNUSED(argc), CLOX_UNUSED(argv);
@@ -37,7 +73,18 @@ int main(int argc, char* argv[]) {
   clox::harness::run_all_harness();
 #endif
 
-  clox::VM vm;
+  clox::VM& vm = get_vm();
+
+  if (argc == 1) {
+    repl();
+  }
+  else if (argc == 2) {
+    run_file(argv[1]);
+  }
+  else {
+    std::cerr << "Usage: clox [filepath]" << std::endl;
+    std::exit(64);
+  }
 
   clox::Chunk chunk;
 
