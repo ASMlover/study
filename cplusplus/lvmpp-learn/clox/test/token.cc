856bf95622c2890eb0e81bd19d2f1008b142a072
ASMlover <asmlover@126.com>
2023-03-31 01:33:10 +0800

    :white_check_mark: test(token): updated the token test case

--- cplusplus/lvmpp-learn/clox/test/token.cc
+++ cplusplus/lvmpp-learn/clox/test/token.cc
@@ -39,45 +39,139 @@ CLOX_TEST(Token) {
   TESTEQ(t.literal(), s);\
   TESTEQ(t.lineno(), 0);\
 } while (false)
+#define TESTID(id)      TESTTK(TTy::TOKEN_IDENTIFIER, id)
 
-  TESTTK(TTy::TOKEN_LEFT_PAREN, "(");
-  TESTTK(TTy::TOKEN_RIGHT_PAREN, ")");
-  TESTTK(TTy::TOKEN_LEFT_BRACE, "{");
-  TESTTK(TTy::TOKEN_RIGHT_BRACE, "}");
-  TESTTK(TTy::TOKEN_COMMA, ",");
-  TESTTK(TTy::TOKEN_DOT, ".");
-  TESTTK(TTy::TOKEN_MINUS, "-");
-  TESTTK(TTy::TOKEN_PLUS, "+");
-  TESTTK(TTy::TOKEN_SEMICOLON, ";");
-  TESTTK(TTy::TOKEN_SLASH, "/");
-  TESTTK(TTy::TOKEN_STAR, "*");
+  {
+    TESTTK(TTy::TOKEN_LEFT_PAREN, "(");
+    TESTTK(TTy::TOKEN_RIGHT_PAREN, ")");
+    TESTTK(TTy::TOKEN_LEFT_BRACE, "{");
+    TESTTK(TTy::TOKEN_RIGHT_BRACE, "}");
+    TESTTK(TTy::TOKEN_COMMA, ",");
+    TESTTK(TTy::TOKEN_DOT, ".");
+    TESTTK(TTy::TOKEN_MINUS, "-");
+    TESTTK(TTy::TOKEN_PLUS, "+");
+    TESTTK(TTy::TOKEN_SEMICOLON, ";");
+    TESTTK(TTy::TOKEN_SLASH, "/");
+    TESTTK(TTy::TOKEN_STAR, "*");
 
-  TESTTK(TTy::TOKEN_BANG, "!");
-  TESTTK(TTy::TOKEN_BANG_EQUAL, "!=");
-  TESTTK(TTy::TOKEN_EQUAL, "=");
-  TESTTK(TTy::TOKEN_EQUAL_EQUAL, "==");
-  TESTTK(TTy::TOKEN_GREATER, ">");
-  TESTTK(TTy::TOKEN_GREATER_EQUAL, ">=");
-  TESTTK(TTy::TOKEN_LESS, "<");
-  TESTTK(TTy::TOKEN_LESS_EQUAL, "<=");
+    TESTTK(TTy::TOKEN_BANG, "!");
+    TESTTK(TTy::TOKEN_BANG_EQUAL, "!=");
+    TESTTK(TTy::TOKEN_EQUAL, "=");
+    TESTTK(TTy::TOKEN_EQUAL_EQUAL, "==");
+    TESTTK(TTy::TOKEN_GREATER, ">");
+    TESTTK(TTy::TOKEN_GREATER_EQUAL, ">=");
+    TESTTK(TTy::TOKEN_LESS, "<");
+    TESTTK(TTy::TOKEN_LESS_EQUAL, "<=");
 
-  TESTTK(TTy::KEYWORD_AND, "and");
-  TESTTK(TTy::KEYWORD_CLASS, "class");
-  TESTTK(TTy::KEYWORD_ELSE, "else");
-  TESTTK(TTy::KEYWORD_FALSE, "false");
-  TESTTK(TTy::KEYWORD_FOR, "for");
-  TESTTK(TTy::KEYWORD_FUN, "fun");
-  TESTTK(TTy::KEYWORD_IF, "if");
-  TESTTK(TTy::KEYWORD_NIL, "nil");
-  TESTTK(TTy::KEYWORD_OR, "or");
-  TESTTK(TTy::KEYWORD_PRINT, "print");
-  TESTTK(TTy::KEYWORD_RETURN, "return");
-  TESTTK(TTy::KEYWORD_SUPER, "super");
-  TESTTK(TTy::KEYWORD_THIS, "this");
-  TESTTK(TTy::KEYWORD_TRUE, "true");
-  TESTTK(TTy::KEYWORD_VAR, "var");
-  TESTTK(TTy::KEYWORD_WHILE, "while");
+    TESTTK(TTy::KEYWORD_AND, "and");
+    TESTTK(TTy::KEYWORD_CLASS, "class");
+    TESTTK(TTy::KEYWORD_ELSE, "else");
+    TESTTK(TTy::KEYWORD_FALSE, "false");
+    TESTTK(TTy::KEYWORD_FOR, "for");
+    TESTTK(TTy::KEYWORD_FUN, "fun");
+    TESTTK(TTy::KEYWORD_IF, "if");
+    TESTTK(TTy::KEYWORD_NIL, "nil");
+    TESTTK(TTy::KEYWORD_OR, "or");
+    TESTTK(TTy::KEYWORD_PRINT, "print");
+    TESTTK(TTy::KEYWORD_RETURN, "return");
+    TESTTK(TTy::KEYWORD_SUPER, "super");
+    TESTTK(TTy::KEYWORD_THIS, "this");
+    TESTTK(TTy::KEYWORD_TRUE, "true");
+    TESTTK(TTy::KEYWORD_VAR, "var");
+    TESTTK(TTy::KEYWORD_WHILE, "while");
+  }
 
+  {
+    TESTID("foo");
+    TESTID("Foo");
+    TESTID("FOO");
+    TESTID("Foo_");
+    TESTID("FOO_");
+    TESTID("Foo__");
+    TESTID("FOO__");
+    TESTID("Foo1");
+    TESTID("FOO1");
+    TESTID("Foo1_");
+    TESTID("FOO1_");
+    TESTID("Foo1__");
+    TESTID("FOO1__");
+    TESTID("Foo_1");
+    TESTID("FOO_1");
+    TESTID("Foo__1");
+    TESTID("FOO__1");
+    TESTID("Foo_1_");
+    TESTID("FOO_1_");
+    TESTID("Foo_1__");
+    TESTID("FOO_1__");
+    TESTID("Foo__1_");
+    TESTID("FOO__1_");
+    TESTID("Foo__1__");
+    TESTID("FOO__1__");
+    TESTID("_Foo");
+    TESTID("_FOO");
+    TESTID("_Foo_");
+    TESTID("_FOO_");
+    TESTID("_Foo__");
+    TESTID("_FOO__");
+    TESTID("_Foo1");
+    TESTID("_FOO1");
+    TESTID("_Foo1_");
+    TESTID("_FOO1_");
+    TESTID("_Foo1__");
+    TESTID("_FOO1__");
+    TESTID("_1Foo_");
+    TESTID("_1FOO_");
+    TESTID("__1_Foo_");
+    TESTID("__1_FOO_");
+    TESTID("_Foo_1");
+    TESTID("_FOO_1");
+    TESTID("_Foo__1");
+    TESTID("_FOO__1");
+    TESTID("_Foo_1_");
+    TESTID("_FOO_1_");
+    TESTID("_Foo_1__");
+    TESTID("_FOO_1__");
+    TESTID("_Foo__1_");
+    TESTID("_FOO__1_");
+    TESTID("_Foo__1__");
+    TESTID("_FOO__1__");
+    TESTID("__Foo");
+    TESTID("__FOO");
+    TESTID("__Foo_");
+    TESTID("__FOO_");
+    TESTID("__Foo__");
+    TESTID("__FOO__");
+    TESTID("__Foo1");
+    TESTID("__FOO1");
+    TESTID("__Foo1_");
+    TESTID("__FOO1_");
+    TESTID("__Foo1__");
+    TESTID("__FOO1__");
+    TESTID("__Foo_1");
+    TESTID("__FOO_1");
+    TESTID("__Foo_1_");
+    TESTID("__FOO_1_");
+    TESTID("__Foo_1__");
+    TESTID("__FOO_1__");
+    TESTID("__Foo__1");
+    TESTID("__FOO__1");
+    TESTID("__Foo__1_");
+    TESTID("__FOO__1_");
+    TESTID("__Foo__1__");
+    TESTID("__FOO__1__");
+    TESTID("_");
+    TESTID("__");
+    TESTID("___");
+    TESTID("____");
+    TESTID("_1");
+    TESTID("_1_");
+    TESTID("_1__");
+    TESTID("__1");
+    TESTID("__1_");
+    TESTID("__1__");
+  }
+
+#undef TESTID
 #undef TESTTK
 #undef TESTEQ
 #undef NEWTK3
