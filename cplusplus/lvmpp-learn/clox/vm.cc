123d777f7f7922c363b4265627d05e63e819f01f
ASMlover <asmlover@126.com>
2023-06-16 02:52:30 +0800

    :construction: chore(vm): updated the vm implementation

--- cplusplus/lvmpp-learn/clox/vm.cc
+++ cplusplus/lvmpp-learn/clox/vm.cc
@@ -103,6 +103,7 @@ InterpretResult VM::run() noexcept {
     case OpCode::OP_SUBTRACT: BINARY_OP(-); break;
     case OpCode::OP_MULTIPLY: BINARY_OP(*); break;
     case OpCode::OP_DIVIDE: BINARY_OP(/); break;
+    case OpCode::OP_NOT: push(pop().is_falsey()); break;
     case OpCode::OP_NEGATE:
       {
         if (!peek().is_number()) {
