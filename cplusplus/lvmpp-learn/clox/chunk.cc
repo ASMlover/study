552b6d951f9d51bb5c3d398843b74c4f37f3e7b7
ASMlover <asmlover@126.com>
2023-10-25 00:31:54 +0800

    :construction: chore(chunk-disassemble): add disassemble op-code about CALL

--- cplusplus/lvmpp-learn/clox/chunk.cc
+++ cplusplus/lvmpp-learn/clox/chunk.cc
@@ -98,6 +98,7 @@ sz_t Chunk::dis_code(sz_t offset) noexcept {
   case OpCode::OP_JUMP: JUMP(OP_JUMP, 1);
   case OpCode::OP_JUMP_IF_FALSE: JUMP(OP_JUMP_IF_FALSE, 1);
   case OpCode::OP_LOOP: JUMP(OP_LOOP, -1);
+  case OpCode::OP_CALL: SIMPLE(OP_CALL);
   case OpCode::OP_RETURN: SIMPLE(OP_RETURN);
   default: std::cerr << "<Invalid `OpCode`>" << std::endl;
   }
