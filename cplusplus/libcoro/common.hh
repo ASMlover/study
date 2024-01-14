99266b77f11205efa31013907ebde6c7ed3c7387
ASMlover <ASMlover@126.com>
2024-01-14 17:07:55 +0800

    :sparkles: feat(libcoro): add coroutine library for c++

---
+++ cplusplus/libcoro/common.hh
@@ -0,0 +1,52 @@
+// Copyright (c) 2024 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+namespace coro {
+
+class Copyable {
+protected:
+	Copyable() noexcept = default;
+	~Copyable() noexcept = default;
+
+	Copyable(const Copyable&) noexcept = default;
+	Copyable(Copyable&&) noexcept = default;
+	Copyable& operator=(const Copyable&) noexcept = default;
+	Copyable& operator=(Copyable&&) noexcept = default;
+};
+
+class UnCopyable {
+	UnCopyable(const UnCopyable&) noexcept = delete;
+	UnCopyable(UnCopyable&&) noexcept = delete;
+	UnCopyable& operator=(const UnCopyable&) noexcept = delete;
+	UnCopyable& operator=(UnCopyable&&) noexcept = delete;
+protected:
+	UnCopyable() noexcept = default;
+	~UnCopyable() noexcept = default;
+};
+
+}
