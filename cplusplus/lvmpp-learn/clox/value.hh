5c4358887cd3ad3a823cd8920f07acec5a61c53c
ASMlover <asmlover@126.com>
2024-02-21 02:40:38 +0800

    :construction: chore(value-method): add interfaces of value for clox

--- cplusplus/lvmpp-learn/clox/value.hh
+++ cplusplus/lvmpp-learn/clox/value.hh
@@ -108,6 +108,7 @@ public:
   inline bool is_function() const noexcept { return is_obj_type(ObjType::OBJ_FUNCTION); }
   inline bool is_native() const noexcept { return is_obj_type(ObjType::OBJ_NATIVE); }
   inline bool is_closure() const noexcept { return is_obj_type(ObjType::OBJ_CLOSURE); }
+  inline bool is_upvalue() const noexcept { return is_obj_type(ObjType::OBJ_UPVALUE); }
 
   inline bool as_boolean() const noexcept { return as_.boolean; }
   inline double as_number() const noexcept { return as_.number; }
@@ -118,6 +119,7 @@ public:
   ObjFunction* as_function() const noexcept;
   ObjNative* as_native() const noexcept;
   ObjClosure* as_closure() const noexcept;
+  ObjUpvalue* as_upvalue() const noexcept;
 
   inline bool is_falsey() const noexcept {
     return is_nil() || (is_boolean() && !as_boolean());
