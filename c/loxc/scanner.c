04ea9e8fd840e39b92162c1bfa9f0a9834bcdcdb
ASMlover <asmlover@126.com>
2024-03-16 05:27:56 +0800

    :construction: chore(scanner): updated the scanner implementation

--- c/loxc/scanner.c
+++ c/loxc/scanner.c
@@ -158,8 +158,57 @@ static TokenType identifierType() {
     }
     break;
   case 'i': return checkKeyword(1, 1, "f", KEYWORD_IF);
+  case 'n': return checkKeyword(1, 2, "il", KEYWORD_NIL);
+  case 'o': return checkKeyword(1, 1, "r", KEYWORD_OR);
+  case 'p': return checkKeyword(1, 4, "rint", KEYWORD_PRINT);
+  case 'r': return checkKeyword(1, 5, "eturn", KEYWORD_RETURN);
+  case 's': return checkKeyword(1, 4, "uper", KEYWORD_SUPER);
+  case 't':
+    if (scanner.current - scanner.start > 1) {
+      switch (scanner.start[1]) {
+      case 'h': return checkKeyword(2, 2, "is", KEYWORD_THIS);
+      case 'r': return checkKeyword(2, 2, "ue", KEYWORD_TRUE);
+      }
+    }
+    break;
+  case 'v': return checkKeyword(1, 2, "ar", KEYWORD_VAR);
+  case 'w': return checkKeyword(1, 4, "hile", KEYWORD_WHILE);
   }
   return TOKEN_IDENTIFIER;
+}
+
+static Token identifier() {
+  while (isAlpha(peek()) || isDigit(peek()))
+    advance();
+  return makeToken(identifierType());
+}
+
+static Token number() {
+  while (isDigit(peek()))
+    advance();
+
+  if (peek() == '.' && isDigit(peekNext())) {
+    advance();
+
+    while (isDigit(peek()))
+      advance();
+  }
+  return makeToken(TOKEN_NUMBER);
+}
+
+static Token string() {
+  while (peek() != '"' && !isAtEnd()) {
+    if (peek() == '\n')
+      ++scanner.lineno;
+
+    advance();
+  }
+
+  if (isAtEnd())
+    return errorToken("Unterminated string.");
+
+  advance();
+  return makeToken(TOKEN_STRING);
 }
 
 void initScanner(const char* sourceCode) {
@@ -169,5 +218,27 @@ void initScanner(const char* sourceCode) {
 }
 
 Token scanToken() {
+  skipWhitespace();
+  scanner.start = scanner.current;
+
+  if (isAtEnd())
+    return makeToken(TOKEN_EOF);
+
+  char c = advance();
+  if (isAlpha(c))
+    return identifier();
+  if (isDigit(c))
+    return number();
+
+  switch (c) {
+  case '(': return makeToken(TOKEN_LEFT_PAREN);
+  case ')': return makeToken(TOKEN_RIGHT_PAREN);
+  case '{': return makeToken(TOKEN_LEFT_BRACE);
+  case '}': return makeToken(TOKEN_RIGHT_BRACE);
+  case ';': return makeToken(TOKEN_SEMICOLON);
+  case ',': return makeToken(TOKEN_COMMA);
+  default: break;
+  }
+
   return errorToken("Unexpected character.");
 }
