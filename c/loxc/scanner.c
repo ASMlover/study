e2091e3bb8d3d4748da4758c78cbb34346505366
ASMlover <asmlover@126.com>
2024-03-17 01:09:41 +0800

    :construction: chore(scanner): finished the scanner implementation for lox of c

--- c/loxc/scanner.c
+++ c/loxc/scanner.c
@@ -237,6 +237,16 @@ Token scanToken() {
   case '}': return makeToken(TOKEN_RIGHT_BRACE);
   case ';': return makeToken(TOKEN_SEMICOLON);
   case ',': return makeToken(TOKEN_COMMA);
+  case '.': return makeToken(TOKEN_DOT);
+  case '-': return makeToken(TOKEN_MINUS);
+  case '+': return makeToken(TOKEN_PLUS);
+  case '/': return makeToken(TOKEN_SLASH);
+  case '*': return makeToken(TOKEN_STAR);
+  case '!': return makeToken(match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+  case '=': return makeToken(match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+  case '<': return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+  case '>': return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+  case '"': return string();
   default: break;
   }
 
