0ab20c7eb5b3897614d18828d4358697fb605c28
ASMlover <asmlover@126.com>
2024-03-22 03:09:51 +0800

    :construction: chore(table): updated the table implementation

--- c/loxc/table.c
+++ c/loxc/table.c
@@ -31,8 +31,53 @@
 #include "table.h"
 #include "value.h"
 
+#define LOXC_TABLE_MAX_LOAD                 (0.75)
+
 static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
-  return NULL;
+  u32_t index = key->hash & (capacity - 1);
+  Entry* tombstone = NULL;
+
+  for (;;) {
+    Entry* entry = &entries[index];
+    if (entry->key == NULL) {
+      if (IS_NIL(entry->value)) {
+        return tombstone != NULL ? tombstone : entry;
+      }
+      else {
+        if (tombstone == NULL)
+          tombstone = entry;
+      }
+    }
+    else if (entry->key == key) {
+      return entry;
+    }
+
+    index = (index + 1) & (capacity - 1);
+  }
+}
+
+static void adjustCapacity(Table* table, int capacity) {
+  Entry* entries = ALLOCATE(Entry, capacity);
+  for (int i = 0; i < capacity; ++i) {
+    entries[i].key = NULL;
+    entries[i].value = NIL_VAL;
+  }
+
+  table->count = 0;
+  for (int i = 0; i < table->capacity; ++i) {
+    Entry* entry = &table->entries[i];
+    if (entry->key == NULL)
+      continue;
+
+    Entry* dest = findEntry(entries, capacity, entry->key);
+    dest->key = entry->key;
+    dest->value = entry->value;
+    ++table->count;
+  }
+
+  FREE_ARRAY(Entry, table->entries, table->capacity);
+  table->entries = entries;
+  table->capacity = capacity;
 }
 
 void initTable(Table* table) {
@@ -46,8 +91,35 @@ void freeTable(Table* table) {
   initTable(table);
 }
 
-bool tableGet(Table* table, ObjString* key, Value* value) { return false; }
-bool tableSet(Table* table, ObjString* key, Value value) { return false; }
+bool tableGet(Table* table, ObjString* key, Value* value) {
+  if (table->count <= 0)
+    return false;
+
+  Entry* entry = findEntry(table->entries, table->capacity, key);
+  if (entry->key == NULL)
+    return false;
+
+  *value = entry->value;
+  return true;
+}
+
+bool tableSet(Table* table, ObjString* key, Value value) {
+  if (table->count + 1 > table->capacity * LOXC_TABLE_MAX_LOAD) {
+    int newCapacity = GROW_CAPACITY(table->capacity);
+    adjustCapacity(table, newCapacity);
+  }
+
+  Entry* entry = findEntry(table->entries, table->capacity, key);
+  bool isNewKey = entry->key == NULL;
+
+  if (isNewKey && IS_NIL(entry->value))
+    ++table->count;
+
+  entry->key = key;
+  entry->value = value;
+  return isNewKey;
+}
+
 bool tableDelete(Table* table, ObjString* key) { return false; }
 void tableAddAll(Table* from, Table* to) {}
 ObjString* tableFindString(Table* table, const char* chars, int length, u32_t hash) { return NULL; }
