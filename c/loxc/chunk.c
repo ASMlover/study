cb6844cf432807be22a7d211edd6c59bdc537be5
ASMlover <asmlover@126.com>
2024-04-07 02:38:16 +0800

    :construction: chore(chunk): updated the disassembing of chunk opcode

--- c/loxc/chunk.c
+++ c/loxc/chunk.c
@@ -31,7 +31,7 @@
 #include "chunk.h"
 #include "vm.h"
 
-static int constantInstruction(Chunk* chunk, const char* name, int offset) {
+static inline int constantInstruction(Chunk* chunk, const char* name, int offset) {
   u8_t constant = chunk->code[offset + 1];
   fprintf(stdout, "%-16s %4d `", name, constant);
   printValue(chunk->constants.values[constant]);
@@ -40,7 +40,7 @@ static int constantInstruction(Chunk* chunk, const char* name, int offset) {
   return offset + 2;
 }
 
-static int invokeInstruction(Chunk* chunk, const char* name, int offset) {
+static inline int invokeInstruction(Chunk* chunk, const char* name, int offset) {
   u8_t constant = chunk->code[offset + 1];
   u8_t argCount = chunk->code[offset + 2];
   fprintf(stdout, "%-16s (%d args) %4d `", name, argCount, constant);
@@ -50,12 +50,18 @@ static int invokeInstruction(Chunk* chunk, const char* name, int offset) {
   return offset + 3;
 }
 
-static inline simpleInstruction(const char* name, int offset) {
+static inline int simpleInstruction(const char* name, int offset) {
   fprintf(stdout, "%s\n", name);
   return offset + 1;
 }
 
-static int jumpInstruction(Chunk* chunk, const char* name, int sign, int offset) {
+static inline int byteInstruction(Chunk* chunk, const char* name, int offset) {
+  u8_t slot = chunk->code[offset + 1];
+  fprintf(stdout, "%-16s %4d\n", name, slot);
+  return offset + 2;
+}
+
+static inline int jumpInstruction(Chunk* chunk, const char* name, int sign, int offset) {
   u16_t jump = (u16_t)(chunk->code[offset + 1] << 8);
   jump |= chunk->code[offset + 2];
   fprintf(stdout, "%-16s %4d -> %d\n", name, offset, offset + 3 + sign * jump);
@@ -114,8 +120,46 @@ int disassebleInstruction(Chunk* chunk, int offset) {
 
   u8_t instruction = chunk->code[offset];
   switch (instruction) {
-  case OP_CONSTANT: return constantInstruction(chunk, "OP_CONSTANT", offset);
-  default: fprintf(stdout, "Unknown opcode %d\n", instruction); break;
+  case OP_CONSTANT:       return constantInstruction(chunk, "OP_CONSTANT", offset);
+  case OP_NIL:            return simpleInstruction("OP_NIL", offset);
+  case OP_TRUE:           return simpleInstruction("OP_TRUE", offset);
+  case OP_FALSE:          return simpleInstruction("OP_FALSE", offset);
+  case OP_POP:            return simpleInstruction("OP_POP", offset);
+  case OP_GET_LOCAL:      return byteInstruction(chunk, "OP_GET_LOCAL", offset);
+  case OP_SET_LOCAL:      return byteInstruction(chunk, "OP_SET_LOCAL", offset);
+  case OP_GET_GLOBAL:     return constantInstruction(chunk, "OP_GET_GLOBAL", offset);
+  case OP_DEFINE_GLOBAL:  return constantInstruction(chunk, "OP_DEFINE_GLOBAL", offset);
+  case OP_SET_GLOBAL:     return constantInstruction(chunk, "OP_SET_GLOBAL", offset);
+  case OP_GET_UPVALUE:    return byteInstruction(chunk, "OP_GET_UPVALUE", offset);
+  case OP_SET_UPVALUE:    return byteInstruction(chunk, "OP_SET_UPVALUE", offset);
+  case OP_GET_PROPERTY:   return constantInstruction(chunk, "OP_GET_PROPERTY", offset);
+  case OP_SET_PROPERTY:   return constantInstruction(chunk, "OP_SET_PROPERTY", offset);
+  case OP_GET_SUPER:      return constantInstruction(chunk, "OP_GET_SUPER", offset);
+  case OP_EQUAL:          return simpleInstruction("OP_EQUAL", offset);
+  case OP_GREATER:        return simpleInstruction("OP_GREATER", offset);
+  case OP_LESS:           return simpleInstruction("OP_LESS", offset);
+  case OP_ADD:            return simpleInstruction("OP_ADD", offset);
+  case OP_SUBTRACT:       return simpleInstruction("OP_SUBTRACT", offset);
+  case OP_MULTIPLY:       return simpleInstruction("OP_MULTIPLY", offset);
+  case OP_DIVIDE:         return simpleInstruction("OP_DIVIDE", offset);
+  case OP_NOT:            return simpleInstruction("OP_NOT", offset);
+  case OP_NEGATE:         return simpleInstruction("OP_NEGATE", offset);
+  case OP_PRINT:          return simpleInstruction("OP_PRINT", offset);
+  case OP_JUMP:           return jumpInstruction(chunk, "OP_JUMP", 1, offset);
+  case OP_JUMP_IF_FALSE:  return jumpInstruction(chunk, "OP_JUMP_IF_FALSE", 1, offset);
+  case OP_LOOP:           return jumpInstruction(chunk, "OP_LOOP", -1, offset);
+  case OP_CALL:           return byteInstruction(chunk, "OP_CALL", offset);
+  case OP_INVOKE:         return invokeInstruction(chunk, "OP_INVOKE", offset);
+  case OP_SUPER_INVOKE:   return invokeInstruction(chunk, "OP_SUPER_INVOKE", offset);
+  case OP_CLOSURE:
+    {
+    } break;
+  case OP_CLOSE_UPVALUE:  return simpleInstruction("OP_CLOSE_UPVALUE", offset);
+  case OP_RETURN:         return simpleInstruction("OP_RETURN", offset);
+  case OP_CLASS:          return constantInstruction(chunk, "OP_CLASS", offset);
+  case OP_INHERIT:        return simpleInstruction("OP_INHERIT", offset);
+  case OP_METHOD:         return constantInstruction(chunk, "OP_METHOD", offset);
+  default:                fprintf(stdout, "Unknown opcode %d\n", instruction); break;
   }
 
   return offset + 1;
