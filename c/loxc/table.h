3ac6205138e1f5a256d3c8d04f17a1253aa5c399
ASMlover <asmlover@126.com>
2024-04-08 01:07:22 +0800

    :construction: chore(table): updated the definition of table

--- c/loxc/table.h
+++ c/loxc/table.h
@@ -33,14 +33,14 @@
 #include "value.h"
 
 typedef struct {
-  ObjString* key;
-  Value value;
+  ObjString*            key;
+  Value                 value;
 } Entry;
 
 typedef struct {
-  int count;
-  int capacity;
-  Entry* entries;
+  int                   count;
+  int                   capacity;
+  Entry*                entries;
 } Table;
 
 void initTable(Table* table);
