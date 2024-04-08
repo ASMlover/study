5089db258511835b590091c83cb07c7e0d43f655
ASMlover <asmlover@126.com>
2024-04-09 01:30:42 +0800

    :construction: chore(chunk): updated the chunk disassemble opcode

--- c/loxc/chunk.c
+++ c/loxc/chunk.c
@@ -153,6 +153,19 @@ int disassebleInstruction(Chunk* chunk, int offset) {
   case OP_SUPER_INVOKE:   return invokeInstruction(chunk, "OP_SUPER_INVOKE", offset);
   case OP_CLOSURE:
     {
+      ++offset;
+      u8_t constant = chunk->code[offset++];
+      fprintf(stdout, "%-16s %4d ", "OP_CLOSURE", constant);
+      printValue(chunk->constants.values[constant]);
+      fprintf(stdout, "\n");
+
+      ObjFunction* function = AS_FUNCTION(chunk->constants.values[constant]);
+      for (int j = 0; j < function->upvalueCount; ++j) {
+        int isLocal = chunk->code[offset++];
+        int index = chunk->code[offset++];
+        fprintf(stdout, "%04d      |                     %s %d\n", offset - 2, isLocal ? "local" : "upvalue", index);
+      }
+      return offset;
     } break;
   case OP_CLOSE_UPVALUE:  return simpleInstruction("OP_CLOSE_UPVALUE", offset);
   case OP_RETURN:         return simpleInstruction("OP_RETURN", offset);
