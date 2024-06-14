1529395b0dd9626e18603766568e0ade04b02702
ASMlover <asmlover@126.com>
2024-06-15 01:21:49 +0800

    :construction: chore(vm): updated the return op-code implementation

--- c/loxc/vm.c
+++ c/loxc/vm.c
@@ -460,6 +460,24 @@ static InterpretResult run() {
       closeUpvalues(vm.stackTop - 1);
       pop();
       break;
+    case OP_RETURN:
+      {
+        Value result = pop();
+        closeUpvalues(frame->slots);
+        --vm.frameCount;
+
+        if (0 == vm.frameCount) {
+          pop();
+          return INTERPRET_OK;
+        }
+
+        vm.stackTop = frame->slots;
+        push(result);
+        frame = &vm.frames[vm.frameCount - 1];
+      } break;
+    case OP_CLASS:
+      push(OBJ_VAL(newClass(READ_STRING())));
+      break;
     }
   }
 
