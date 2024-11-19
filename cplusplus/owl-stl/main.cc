b8ddaddd993aa03306f2d2d3295d89878bcab1d2
ASMlover <ASMlover@126.com>
2024-11-19 14:29:35 +0800

    :construction: chore(main): add entry including

--- cplusplus/owl-stl/main.cc
+++ cplusplus/owl-stl/main.cc
@@ -25,7 +25,8 @@
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 #include <iostream>
-#include "owl_type_traits.hh"
+#include "owl_iterator.hh"
+#include "owl_allocator.hh"
 
 int main(int argc, char* argv[]) {
   (void)argc, (void)argv;
