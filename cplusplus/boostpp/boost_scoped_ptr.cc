c337188ed9f5a8d0374eae760a028044b5000836
ASMlover <asmlover@126.com>
2024-11-26 01:30:54 +0800

    :construction: chore(log-scoped-ptr): add log for scoped_ptr test

--- cplusplus/boostpp/boost_scoped_ptr.cc
+++ cplusplus/boostpp/boost_scoped_ptr.cc
@@ -24,10 +24,12 @@
 // LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
-#include <boost/scoped_ptr.hpp>
 #include <iostream>
+#include <boost/scoped_ptr.hpp>
 
 void boost_scoped_ptr() noexcept {
+  std::cout << "========= [scoped_ptr] =========" << std::endl;
+
   boost::scoped_ptr<int> p{new int{1}};
   std::cout << *p << std::endl;
 
