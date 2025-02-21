8a7511010b5511704e1ffe60255bc4694907acde
ASMlover <asmlover@126.com>
2025-02-22 01:09:42 +0800

    :white_check_mark: test(xpressive): add xpressive test for boost

---
+++ cplusplus/boostpp/boost_xpressive.cc
@@ -0,0 +1,43 @@
+// Copyright (c) 2025 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#include <boost/xpressive/xpressive.hpp>
+#include <string>
+#include <iostream>
+
+static void boost_xpressive_compare_strings() noexcept {
+  std::cout << "--------- [xpressive.compare_strings] ---------" << std::endl;
+
+  std::string s = "Boost Libraries";
+  boost::xpressive::sregex expr = boost::xpressive::sregex::compile("\\w+\\s\\w+");
+  std::cout << "[demo.xpressive] " << std::boolalpha << boost::xpressive::regex_match(s, expr) << std::endl;
+}
+
+void boost_xpressive() noexcept {
+  std::cout << "========= [xpressive] =========" << std::endl;
+
+  boost_xpressive_compare_strings();
+}
