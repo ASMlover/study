efbcbaf788692f063a2121be4a03c0491f79f63d
ASMlover <asmlover@126.com>
2025-03-13 23:14:27 +0800

    :white_check_mark: test(parsing-with-lexeme): updated the parsing with lexeme using boost.spirit

--- cplusplus/boostpp/boost_spirit.cc
+++ cplusplus/boostpp/boost_spirit.cc
@@ -98,6 +98,20 @@ static void boost_spirit_parser_consecutive_digits() noexcept {
     std::cout << "[demo.spirit] " << std::string{it, s.end()} << std::endl;
 }
 
+static void boost_spirit_parsing_with_lexeme() noexcept {
+  std::cout << "--------- [spirit.parsing_with_lexeme] ---------" << std::endl;
+  using namespace boost::spirit;
+
+  std::string s;
+  std::cout << "[demo.spirit] PLEASE INPUT: ";
+  std::getline(std::cin, s);
+  auto it = s.begin();
+  bool match = qi::phrase_parse(it, s.end(), qi::lexeme[ascii::digit >> ascii::digit], ascii::space);
+  std::cout << "[demo.spirit] " << std::boolalpha << match << std::endl;
+  if (it != s.end())
+    std::cout << "[demo.spirit] " << std::string{it, s.end()} << std::endl;
+}
+
 void boost_spirit() noexcept {
   std::cout << "========= [spirit] =========" << std::endl;
 
@@ -106,4 +120,5 @@ void boost_spirit() noexcept {
   boost_spirit_with_dont_postskip();
   boost_spirit_with_wide_strings();
   boost_spirit_parser_consecutive_digits();
+  boost_spirit_parsing_with_lexeme();
 }
