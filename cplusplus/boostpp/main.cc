06d598b3040c2571b2ed8387dbac1cb25743f977
ASMlover <asmlover@126.com>
2025-04-25 02:49:06 +0800

    :white_check_mark: test(entry): add entry framework  for test of boost

--- cplusplus/boostpp/main.cc
+++ cplusplus/boostpp/main.cc
@@ -24,6 +24,7 @@
 // LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
+#include <unordered_map>
 #include <iostream>
 
 extern void boost_scoped_ptr() noexcept;
@@ -50,36 +51,56 @@ extern void boost_unordered() noexcept;
 extern void boost_circular_buffer() noexcept;
 extern void boost_heap() noexcept;
 extern void boost_intrusive() noexcept;
+extern void boost_multi_array() noexcept;
+
+typedef void (*FuncPtr)();
+static std::unordered_map<std::string, FuncPtr> kBoostFuncs = {
+  {"scoped_ptr",          boost_scoped_ptr},
+  {"scoped_array",        boost_scoped_array},
+  {"shared_ptr",          boost_shared_ptr},
+  {"make_shared",         boost_make_shared},
+  {"shared_array",        boost_shared_array},
+  {"weak_ptr",            boost_weak_ptr},
+  {"ptr_vector",          boost_ptr_vector},
+  {"ptr_set",             boost_ptr_set},
+  {"scope_exit",          boost_scope_exit},
+  {"pool",                boost_pool},
+  {"string_algorithms",   boost_string_algorithms},
+  {"lexical_cast",        boost_lexical_cast},
+  {"format",              boost_format},
+  {"regex",               boost_regex},
+  {"xpressive",           boost_xpressive},
+  {"tokenizer",           boost_tokenizer},
+  {"spirit",              boost_spirit},
+  {"multiindex",          boost_multiindex},
+  {"bimap",               boost_bimap},
+  {"array",               boost_array},
+  {"unordered",           boost_unordered},
+  {"circular_buffer",     boost_circular_buffer},
+  {"heap",                boost_heap},
+  {"intrusive",           boost_intrusive},
+  {"multi_array",         boost_multi_array},
+};
 
 int main(int argc, char* argv[]) {
   (void)argc, (void)argv;
 
   std::cout << "Hello, Boost !!!" << std::endl;
 
-  boost_scoped_ptr();
-  boost_scoped_array();
-  boost_shared_ptr();
-  boost_make_shared();
-  boost_shared_array();
-  boost_weak_ptr();
-  boost_ptr_vector();
-  boost_ptr_set();
-  boost_scope_exit();
-  boost_pool();
-  boost_string_algorithms();
-  boost_lexical_cast();
-  boost_format();
-  boost_regex();
-  boost_xpressive();
-  boost_tokenizer();
-  // boost_spirit();
-  boost_multiindex();
-  boost_bimap();
-  boost_array();
-  boost_unordered();
-  boost_circular_buffer();
-  boost_heap();
-  boost_intrusive();
+
+  if (kBoostFuncs.empty())
+    return 0;
+  const char* func_key = nullptr;
+  if (argc < 2) {
+    auto it = kBoostFuncs.end();
+    func_key = (--it)->first.c_str();
+  }
+  else {
+    func_key = argv[1];
+  }
+
+  if (auto it = kBoostFuncs.find(func_key); it != kBoostFuncs.end())
+    it->second();
 
   return 0;
 }
