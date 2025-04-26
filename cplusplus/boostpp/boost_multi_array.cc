853c4f0d2c84c19746e56fd4cbbed4c70d2d8b90
ASMlover <asmlover@126.com>
2025-04-27 01:24:44 +0800

    :white_check_mark: test(multi-array): updated the views and subarrays of testing multi_array

--- cplusplus/boostpp/boost_multi_array.cc
+++ cplusplus/boostpp/boost_multi_array.cc
@@ -25,7 +25,9 @@
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 #include <boost/multi_array.hpp>
+#include <algorithm>
 #include <iostream>
+#include <cstring>
 
 static void boost_multi_array_one_dimensional_array() noexcept {
   std::cout << "--------- [multi_array.one_dimensional_array] ---------" << std::endl;
@@ -40,8 +42,29 @@ static void boost_multi_array_one_dimensional_array() noexcept {
   std::cout << "[demo.multi_array] " << a.origin() << std::endl;
 }
 
+static void boost_multi_array_views_and_subarrays() noexcept {
+  std::cout << "--------- [multi_array.views_and_subarrays] ---------" << std::endl;
+
+  using array_view = boost::multi_array<char, 2>::array_view<1>::type;
+  using range      = boost::multi_array_types::index_range;
+
+  boost::multi_array<char, 2> a{boost::extents[2][6]};
+  array_view view = a[boost::indices[0][range{0, 5}]];
+
+  std::memcpy(view.origin(), "tsooB", 6);
+  std::reverse(view.begin(), view.end());
+
+  std::cout << "[demo.multi_array] " << view.origin() << std::endl;
+
+  boost::multi_array<char, 2>::reference subarray = a[1];
+  std::memcpy(subarray.origin(), "C++", 4);
+
+  std::cout << "[demo.multi_array] " << subarray.origin() << std::endl;
+}
+
 void boost_multi_array() noexcept {
   std::cout << "========= [multi_array] =========" << std::endl;
 
   boost_multi_array_one_dimensional_array();
+  boost_multi_array_views_and_subarrays();
 }
