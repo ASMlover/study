1832d552e3c1e6016094b7fef50d15e723a33cf6
asmlover <asmlover@126.com>
2025-08-01 00:59:35 +0800

    :white_check_mark: test(graph): add test about boost.graph

---
+++ cplusplus/boostpp/boost_graph.cc
@@ -0,0 +1,46 @@
+// Copyright (c) 2025 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#include <boost/graph/adjacency_list.hpp>
+#include <iostream>
+
+static void boost_graph_with_four_vertices() noexcept {
+  std::cout << "--------- [graph.with_four_vertices] ---------" << std::endl;
+
+  boost::adjacency_list<> g;
+  boost::adjacency_list<>::vertex_descriptor v1 = boost::add_vertex(g);
+  boost::adjacency_list<>::vertex_descriptor v2 = boost::add_vertex(g);
+  boost::adjacency_list<>::vertex_descriptor v3 = boost::add_vertex(g);
+  boost::adjacency_list<>::vertex_descriptor v4 = boost::add_vertex(g);
+
+  std::cout << "[demo.graph] " << __func__ << " => " << v1 << "," << v2 << "," << v3 << "," << v4 << std::endl;
+}
+
+void boost_graph() noexcept {
+  std::cout << "========= [graph] =========" << std::endl;
+
+  boost_graph_with_four_vertices();
+}
