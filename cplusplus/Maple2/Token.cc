e4a9e58f416787add8b17f8f822333082de4b1d6
asmlover <asmlover@126.com>
2025-08-30 03:19:24 +0800

    :construction: chore(token): add token module

---
+++ cplusplus/Maple2/Token.cc
@@ -0,0 +1,69 @@
+// Copyright (c) 2025 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#include <unordered_map>
+#include "Token.hh"
+
+namespace ms {
+
+static constexpr cstr_t kNames[] = {
+#undef TOKDEF
+#define TOKDEF(k, s) s,
+#include "TokenTypes.hh"
+#undef TOKDEF
+
+  nullptr,
+};
+
+static const std::unordered_map<str_t, TokenType> kKeywords = {
+#undef TOKDEF
+#define KEYWORD(k, s) {s, TokenType::KW_##k},
+#include "TokenTypes.hh"
+#undef TOKDEF
+};
+
+cstr_t get_type_name(TokenType type) noexcept {
+  if (type > TokenType::TOKENTYPE_BEG && type < TokenType::TOKENTYPE_END)
+    return kNames[as_type<int>(type)];
+  return nullptr;
+}
+
+TokenType get_keyword_type(const str_t& keyword) noexcept {
+  if (auto it = kKeywords.find(keyword); it != kKeywords.end())
+    return it->second;
+  return TokenType::TK_IDENTIFIER;
+}
+
+str_t Token::stringify() const {
+  ss_t ss;
+
+  ss << std::left << std::setw(20) << get_type_name(type_) << ":"
+    << std::right << std::setw(24) << literal_ << "|"
+    << std::right << std::setw(04) << lineno_;
+  return ss.str();
+}
+
+}
---
+++ cplusplus/Maple2/Token.hh
@@ -0,0 +1,117 @@
+// Copyright (c) 2025 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+#include "Common.hh"
+
+namespace ms {
+
+enum class TokenType : i8_t {
+  TOKENTYPE_BEG = -1,
+
+#undef TOKDEF
+#define TOKDEF(k, s) k,
+#include "TokenTypes.hh"
+#undef TOKDEF
+
+  TOKENTYPE_END,
+};
+
+cstr_t get_type_name(TokenType type) noexcept;
+TokenType get_keyword_type(const str_t& keyword) noexcept;
+
+class Token final : public Copyable {
+  TokenType type_{TokenType::TK_ERR};
+  str_t literal_;
+  int lineno_{};
+public:
+  Token() noexcept {}
+
+  Token(TokenType type, const str_t& literal, int lineno = 0) noexcept
+    : type_{type}, literal_{literal}, lineno_{lineno} {
+  }
+
+  Token(const Token& r) noexcept
+    : type_{r.type_}, literal_{r.literal_}, lineno_{r.lineno_} {
+  }
+
+  Token(Token&& r) noexcept
+    : type_{std::move(r.type_)}, literal_{std::move(r.literal_)}, lineno_{std::move(r.lineno_)} {
+  }
+
+  inline Token& operator=(const Token& r) noexcept {
+    if (this != &r) {
+      type_ = r.type_;
+      literal_ = r.literal_;
+      lineno_ = r.lineno_;
+    }
+    return *this;
+  }
+
+  inline Token& operator=(Token&& r) noexcept {
+    if (this != &r) {
+      type_ = std::move(r.type_);
+      literal_ = std::move(r.literal_);
+      lineno_ = std::move(r.lineno_);
+    }
+    return *this;
+  }
+
+  inline bool operator==(const Token& r) const noexcept {
+    return this == &r ? true : literal_ == r.literal_;
+  }
+
+  inline bool operator!=(const Token& r) const noexcept {
+    return !(*this == r);
+  }
+
+  inline bool is_equal_to(const Token& r) const noexcept { return *this == r; }
+  inline bool is_equal_to(const str_t& s) const noexcept { return literal_ == s; }
+
+  inline TokenType type() const noexcept { return type_; }
+  inline const str_t& literal() const noexcept { return literal_; }
+  inline int lineno() const noexcept { return lineno_; }
+  inline double as_number() const noexcept { return std::atof(literal_.c_str()); }
+  inline const str_t& as_string() const noexcept { return literal_; }
+  inline cstr_t as_cstring() const noexcept { return literal_.c_str(); }
+
+  str_t stringify() const;
+
+  static Token make_from_literal(const str_t& literal) noexcept {
+    return Token{TokenType::TK_STRING, literal};
+  }
+
+  static Token make_from_details(TokenType type, const str_t& literal, int lineno) noexcept {
+    return Token{type, literal, lineno};
+  }
+};
+
+inline std::ostream& operator<<(std::ostream& out, const Token& t) noexcept {
+  return out << t.stringify();
+}
+
+}
