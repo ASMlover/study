6afa5b6fb74123f2889b94ddebfed7b6e15509b5
asmlover <asmlover@126.com>
2026-01-19 00:02:04 +0800

    :sparkles: feat(common): add common module

---
+++ cplusplus/Maple3/Common.hh
@@ -0,0 +1,130 @@
+// Copyright (c) 2026 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <memory>
+#include "Types.hh"
+#include "Consts.hh"
+
+namespace ms {
+
+class Copyable {
+protected:
+  Copyable() noexcept = default;
+  ~Copyable() noexcept = default;
+  Copyable(const Copyable&) noexcept = default;
+  Copyable(Copyable&&) noexcept = default;
+  Copyable& operator=(const Copyable&) noexcept = default;
+  Copyable& operator=(Copyable&&) noexcept = default;
+};
+
+class UnCopyable {
+  UnCopyable(const UnCopyable&) noexcept = delete;
+  UnCopyable(UnCopyable&&) noexcept = delete;
+  UnCopyable& operator=(const UnCopyable&) noexcept = delete;
+  UnCopyable& operator=(UnCopyable&&) noexcept = delete;
+protected:
+  UnCopyable() noexcept = default;
+  ~UnCopyable() noexcept = default;
+};
+
+template <typename Object> class Singleton : private UnCopyable {
+public:
+  static Object& get_instance() noexcept {
+    static Object ins;
+    return ins;
+  }
+};
+
+template <typename... Mixins> class MixinClass : public Mixins... {
+public:
+  constexpr MixinClass() : Mixins()... {}
+};
+
+template <typename T, typename S> inline T as_type(S x) noexcept {
+  return static_cast<T>(x);
+}
+
+template <typename T, typename S> inline T* as_down(S* x) noexcept {
+  return dynamic_cast<T*>(x);
+}
+
+template <typename T> inline T* as_ptr(void* p) noexcept {
+  return reinterpret_cast<T*>(p);
+}
+
+template <typename T, typename PTR> inline T* get_rawptr(const PTR& p) noexcept {
+  return p.get();
+}
+
+inline sz_t as_align(sz_t bytes, sz_t align = kALIGNMENT) noexcept {
+  return (bytes + align - 1) & ~(align - 1);
+}
+
+inline str_t* make_string(const str_t& s) noexcept {
+  return new str_t{s};
+}
+
+inline str_t* make_string(strv_t s) noexcept {
+  return new str_t{s};
+}
+
+inline str_t* make_string(cstr_t s) noexcept {
+  return new str_t{s};
+}
+
+inline void free_string(str_t* s) noexcept {
+  if (nullptr != s)
+    delete s;
+}
+
+inline str_t as_string(double d) noexcept {
+  ss_t ss;
+  ss << std::setprecision(std::numeric_limits<double>::max_digits10) << d;
+  return ss.str();
+}
+
+template <typename T, typename... Args> inline str_t as_string(T&& x, Args&&... args) noexcept {
+  ss_t ss;
+
+  ss << std::forward<T>(x);
+  ((ss << std::forward<Args>(args)), ...);
+
+  return ss.str();
+}
+
+template <typename... Args> inline str_t from_fmt(strv_t fmt, const Args&... args) noexcept {
+  int sz = std::snprintf(nullptr, 0, fmt.data(), args...);
+  std::unique_ptr<char []> buf{new char[sz + 1]};
+  std::snprintf(buf.get(), sz, fmt.data(), args...);
+  return str_t{buf.get(), buf.get() + sz};
+}
+
+}
