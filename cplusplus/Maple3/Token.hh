b9f591e66beac080c1a9d162ae5dc1ed040474a6
asmlover <asmlover@126.com>
2026-01-22 22:33:34 +0800

    :construction: chore(token): add token module

---
+++ cplusplus/Maple3/Token.hh
@@ -0,0 +1,50 @@
+// Copyright (c) 2026 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+#include "Common.hh"
+
+namespace ms {
+
+enum class TokenType : i8_t {
+  TOKENTYPE_BEG = -1,
+
+#undef TOKDEF
+#define TOKDEF(k, s) k,
+#include "TokenTypes.hh"
+#undef TOKDEF
+
+  TOKENTYPE_END,
+};
+
+cstr_t get_type_name(TokenType type) noexcept;
+TokenType get_keyword_type(const str_t& keyword) noexcept;
+
+class Token final : public Copyable {
+};
+
+}
